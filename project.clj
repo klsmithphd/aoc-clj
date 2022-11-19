bd7dbafd34d9fa8316b9e2331143caa2834f9d03
Ken Smith <ken.smith@2scientists.net>
2022-11-19 12:54:32 -0700

    Skeleton project setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject aoc-clj "0.1.0-SNAPSHOT"
+  :description "Solutions to Advent of Code challenges implemented in Clojure"
+  :url "https://github.com/Ken-2scientists/aoc-clj"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns aoc-clj.core})
