6a0ba2eb15e699a49a745c52b7aa004803b9973d
Ken Smith <ken.smith@2scientists.net>
2022-11-20 11:05:11 -0700

    Solutions for day03

---
+++ src/aoc_clj/2019/day03.clj
@@ -0,0 +1,102 @@
+(ns aoc-clj.2019.day03
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.math :as math]))
+
+(defn parse-segment
+  [segment]
+  [(first segment) (read-string (str/join (rest segment)))])
+
+(defn parse-segments
+  [segments]
+  (map parse-segment segments))
+
+(def day03-input
+  (->> (u/puzzle-input "2019/day03-input.txt")
+       (map #(str/split % #","))
+       (map parse-segments)))
+
+(defn segment-points
+  "From a given starting point [`x0` `y0`], list all the points
+   along a segment indicated by a direction `dir` and `length`"
+  [[x0 y0] [dir length]]
+  (vec (case dir
+         \U (for [y (range (inc y0) (inc (+ y0 length)))]    [x0 y])
+         \D (for [y (range (dec y0) (dec (- y0 length)) -1)] [x0 y])
+         \R (for [x (range (inc x0) (inc (+ x0 length)))]    [x y0])
+         \L (for [x (range (dec x0) (dec (- x0 length)) -1)] [x y0]))))
+
+(defn append-segment
+  "Given a list of `points` and a `segment` directive, return a new list
+   of all points with the new segment appended"
+  [points segment]
+  (vec (concat points (segment-points (peek points) segment))))
+
+(defn wire-path
+  "List all the points along a wire path starting at [0 0] following
+   the segments (direction/distance pairs) given in `segments`"
+  [segments]
+  (reduce append-segment [[0 0]] segments))
+
+(defn intersections
+  "Find all the intersecting points along two wires described by their segments"
+  [[wire1 wire2]]
+  (let [path1 (set (wire-path wire1))
+        path2 (set (wire-path wire2))]
+    (set/intersection path1 path2)))
+
+(defn closest-intersection-dist
+  "Find the distance to the nearest intersection point for two wires
+   each described by their segments"
+  [wires]
+  (->> (intersections wires)
+       (map (partial math/manhattan [0 0]))
+       sort
+       ;; we take second because [0 0] is in the set
+       second))
+
+(defn day03-part1-soln
+  []
+  (closest-intersection-dist day03-input))
+
+;; Slightly convoluted. I want a mapping from a point
+;; to the shortest distance to that point. Because 
+;; later values clobber earlier ones, I traverse the
+;; path backwards so the shortest distance to a single
+;; point remains in the map at the end
+(defn shortest-distance-to-point
+  "Returns a map from a point to the shortest distance from the origin
+   to reach that point for a given path (assumes the path can cross 
+   itself many times)"
+  [path]
+  (let [len (count path)]
+    (zipmap (reverse path) (reverse (range len)))))
+
+(defn distances-to-intersections
+  "For each set of intersecting points along two lists of points, 
+   find the combined distance (sum of distance along both wires)
+   to all of the intersection points"
+  [[wire1 wire2]]
+  (let [path1 (wire-path wire1)
+        path2 (wire-path wire2)
+        path1-dists (shortest-distance-to-point path1)
+        path2-dists (shortest-distance-to-point path2)
+        ints (set/intersection (set path1) (set path2))]
+    (map (fn [pos] [pos (+ (get path1-dists pos)
+                           (get path2-dists pos))]) ints)))
+
+(defn shortest-steps-to-intersection
+  "Find the minimum number of steps to an intersection point for two wires
+   each described by their segments"
+  [[wire1 wire2]]
+  (->> (distances-to-intersections [wire1 wire2])
+       (sort-by second)
+       ;; we take second because [0 0] is always the first intersection
+       second
+       ;; the second value of the pair is the number of steps
+       second))
+
+(defn day03-part2-soln
+  []
+  (shortest-steps-to-intersection day03-input))
---
+++ test/aoc_clj/2019/day03_test.clj
@@ -0,0 +1,58 @@
+(ns aoc-clj.2019.day03-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2019.day03 :as t]))
+
+(def path1-segs (t/parse-segments ["R8","U5","L5","D3"]))
+(def path1-path
+  [[0 0] [1 0] [2 0] [3 0] [4 0] [5 0] [6 0] [7 0] [8 0]
+   [8 1] [8 2] [8 3] [8 4] [8 5]
+   [7 5] [6 5] [5 5] [4 5] [3 5]
+   [3 4] [3 3] [3 2]])
+
+(def path2-segs (t/parse-segments ["U7","R6","D4","L4"]))
+(def path2-path
+  [[0 0] [0 1] [0 2] [0 3] [0 4] [0 5] [0 6] [0 7]
+   [1 7] [2 7] [3 7] [4 7] [5 7] [6 7]
+   [6 6] [6 5] [6 4] [6 3]
+   [5 3] [4 3] [3 3] [2 3]])
+
+(deftest wire-path-test
+  (testing "Convert instructions into the wire path"
+    (is (= path1-path (t/wire-path path1-segs)))
+    (is (= path2-path (t/wire-path path2-segs)))))
+
+(def input1 [path1-segs path2-segs])
+(def dist1 6)
+(def steps1 30)
+
+(def input2 (mapv t/parse-segments
+                  [["R75","D30","R83","U83","L12","D49","R71","U7","L72"]
+                   ["U62","R66","U55","R34","D71","R55","D58","R83"]]))
+(def dist2 159)
+(def steps2 610)
+
+(def input3 (mapv t/parse-segments
+                  [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"]
+                   ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]]))
+(def dist3 135)
+(def steps3 410)
+
+(deftest part1-test
+  (testing "Find closest intersection of the wire paths"
+    (is (= dist1 (t/closest-intersection-dist input1)))
+    (is (= dist2 (t/closest-intersection-dist input2)))
+    (is (= dist3 (t/closest-intersection-dist input3)))))
+
+(deftest part2-test
+  (testing "Find fewest number of steps to first intersection of the wire paths"
+    (is (= steps1 (t/shortest-steps-to-intersection input1)))
+    (is (= steps2 (t/shortest-steps-to-intersection input2)))
+    (is (= steps3 (t/shortest-steps-to-intersection input3)))))
+
+(deftest day03-part1-soln-test
+  (testing "Can reproduce the answer for part1"
+    (is (= 375 (t/day03-part1-soln)))))
+
+(deftest day03-part2-soln-test
+  (testing "Can reproduce the answer for part2"
+    (is (= 14746 (t/day03-part2-soln)))))
