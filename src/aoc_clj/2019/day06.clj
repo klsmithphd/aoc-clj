28d9a6049b56d2d7e0fb14f5ddf0837326c5a728
Ken Smith <ken.smith@2scientists.net>
2022-11-20 16:21:00 -0700

    Copy over day06 solution

---
+++ src/aoc_clj/2019/day06.clj
@@ -0,0 +1,57 @@
+(ns aoc-clj.2019.day06
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
+
+(def day06-input
+  (->> (u/puzzle-input "2019/day06-input.txt")
+       (map #(str/split % #"\)"))
+       (mapcat reverse)
+       (apply hash-map)))
+
+(defn orbit-chain
+  [orbits obj]
+  (loop [chain [obj] nextobj obj]
+    (let [parent (get orbits nextobj)]
+      (if (nil? parent)
+        chain
+        (recur (conj chain parent) parent)))))
+
+(defn orbit-length
+  [chain]
+  (dec (count chain)))
+
+(defn orbit-count
+  [orbits]
+  (->> orbits
+       keys
+       (map (partial orbit-chain orbits))
+       (map orbit-length)
+       (reduce +)))
+
+(defn first-common-ancestor
+  [orbits objA objB]
+  (let [chainA (orbit-chain orbits objA)
+        chainB (orbit-chain orbits objB)
+        common (map vector (reverse chainA) (reverse chainB))
+        first-delta (first (filter (fn [[a b]] (not= a b)) common))]
+    (get orbits (first first-delta))))
+
+(defn distance-to-ancestor
+  [orbits des anc]
+  (let [chain (orbit-chain orbits des)]
+    (count (take-while (partial not= anc) chain))))
+
+(defn orbit-transfers
+  [orbits objA objB]
+  (let [ancestor (first-common-ancestor orbits objA objB)]
+    (- (+ (distance-to-ancestor orbits objA ancestor)
+          (distance-to-ancestor orbits objB ancestor))
+       2)))
+
+(defn day06-part1-soln
+  []
+  (orbit-count day06-input))
+
+(defn day06-part2-soln
+  []
+  (orbit-transfers day06-input "YOU" "SAN"))
---
+++ test/aoc_clj/2019/day06_test.clj
@@ -0,0 +1,41 @@
+(ns aoc-clj.2019.day06-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2019.day06 :as t]))
+
+(deftest orbit-count-test
+  (testing "Can correctly count the total number of orbits"
+    (is (= 42 (t/orbit-count {"B" "COM"
+                              "C" "B"
+                              "D" "C"
+                              "E" "D"
+                              "F" "E"
+                              "G" "B"
+                              "H" "G"
+                              "I" "D"
+                              "J" "E"
+                              "K" "J"
+                              "L" "K"})))))
+
+(deftest orbit-transfers-test
+  (testing "Can correctly determine the number of orbit transfers between YOU and SAN"
+    (is (= 4 (t/orbit-transfers {"B" "COM"
+                                 "C" "B"
+                                 "D" "C"
+                                 "E" "D"
+                                 "F" "E"
+                                 "G" "B"
+                                 "H" "G"
+                                 "I" "D"
+                                 "J" "E"
+                                 "K" "J"
+                                 "L" "K"
+                                 "YOU" "K"
+                                 "SAN" "I"} "YOU" "SAN")))))
+
+(deftest day06-part1-soln-test
+  (testing "Can reproduce the answer for part1"
+    (is (= 261306 (t/day06-part1-soln)))))
+
+(deftest day06-part2-soln-test
+  (testing "Can reproduce the answer for part2"
+    (is (= 382 (t/day06-part2-soln)))))
