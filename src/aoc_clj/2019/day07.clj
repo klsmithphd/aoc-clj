e13914648c684d644b03fcffb1774e6920ab9d1f
Ken Smith <ken.smith@2scientists.net>
2022-11-24 16:47:50 -0700

    2019 Day 07 solution

---
+++ src/aoc_clj/2019/day07.clj
@@ -0,0 +1,70 @@
+(ns aoc-clj.2019.day07
+  (:require [clojure.math.combinatorics :as combo]
+            [manifold.stream :as s]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.intcode :as intcode]))
+
+(def day05-input (u/firstv (u/puzzle-input "2019/day07-input.txt")))
+
+(defn amplifier
+  "Creates an 'amplifier' `intcode` program to run on another thread
+   using input stream `in` and output stream `out`"
+  [intcode [in out]]
+  (future (intcode/intcode-exec intcode in out)))
+
+(defn inout-streams
+  "Construct the correct number of input/output streams for the amplifer config"
+  [config]
+  (case config
+    :chain (repeatedly 6 s/stream)
+    :loop  (repeatedly 5 s/stream)))
+
+(defn inout-pairs
+  "Construct the correct input/output stream pairs for the amplifer config"
+  [config streams]
+  (case config
+    ;; '((0 1) (1 2) (2 3) (3 4) (4 5))
+    :chain (partition 2 1 streams)
+    ;; '((0 1) (1 2) (2 3) (3 4) (4 0))
+    :loop  (partition 2 1 streams streams)))
+
+(defn amplifier-config
+  "For a given `intcode` program and initial phase values given by `phases`
+   for each of the five amplifiers in the config (either `:chain` or `:loop`),
+   return the final amplifier output value"
+  [intcode phases config]
+  (let [streams (inout-streams config)
+        amps    (map #(amplifier intcode %) (inout-pairs config streams))
+        _       (doall (map #(s/put! %1 %2) streams phases))
+        _       (s/put! (first streams) 0)]
+    (intcode/last-out @(last amps))))
+
+(defn amplifier-chain
+  [intcode phases]
+  (amplifier-config intcode phases :chain))
+
+(defn amplifier-loop
+  [intcode phases]
+  (amplifier-config intcode phases :loop))
+
+(defn max-output
+  "Return the highest possible output value from running the amplifier config
+   on all possible valid phase permutations"
+  [intcode amp-config phase-options]
+  (apply max (map #(amp-config intcode %) (combo/permutations phase-options))))
+
+(defn max-amplifier-chain-output
+  [intcode]
+  (max-output intcode amplifier-chain [0 1 2 3 4]))
+
+(defn max-amplifier-loop-output
+  [intcode]
+  (max-output intcode amplifier-loop [5 6 7 8 9]))
+
+(defn day07-part1-soln
+  []
+  (max-amplifier-chain-output day05-input))
+
+(defn day07-part2-soln
+  []
+  (max-amplifier-loop-output day05-input))
---
+++ test/aoc_clj/2019/day07_test.clj
@@ -0,0 +1,58 @@
+(ns aoc-clj.2019.day07-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2019.day07 :as t]))
+
+(def p1 [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+(def s1 [4 3 2 1 0])
+(def m1 43210)
+
+(def p2 [3,23,3,24,1002,24,10,24,1002,23,-1,23,
+         101,5,23,23,1,24,23,23,4,23,99,0,0])
+(def s2 [0 1 2 3 4])
+(def m2 54321)
+
+(def p3 [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+         1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])
+(def s3 [1 0 4 3 2])
+(def m3 65210)
+
+(def p4 [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+         27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
+(def s4 [9 8 7 6 5])
+(def m4 139629729)
+
+(def p5 [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+         -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+         53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10])
+(def s5 [9 7 8 5 6])
+(def m5 18216)
+
+(deftest amplifier-chain-test
+  (testing "Tests that a chain of five amplifiers produces the right thrust given sample phases"
+    (is (= m1 (t/amplifier-chain p1 s1)))
+    (is (= m2 (t/amplifier-chain p2 s2)))
+    (is (= m3 (t/amplifier-chain p3 s3)))))
+
+(deftest highest-amplifier-chain-output-test
+  (testing "Finds the best output value for a given intcode program"
+    (is (= m1 (t/max-amplifier-chain-output p1)))
+    (is (= m2 (t/max-amplifier-chain-output p2)))
+    (is (= m3 (t/max-amplifier-chain-output p3)))))
+
+(deftest amplifier-loop-test
+  (testing "Tests that a loop of five amplifiers produces the right thrust given sample phases"
+    (is (= m4 (t/amplifier-loop p4 s4)))
+    (is (= m5 (t/amplifier-loop p5 s5)))))
+
+(deftest highest-amplifier-loop-output-test
+  (testing "Finds the best output value for a given intcode program"
+    (is (= m4 (t/max-amplifier-loop-output p4)))
+    (is (= m5 (t/max-amplifier-loop-output p5)))))
+
+(deftest day07-part1-soln-test
+  (testing "Can reproduce the answer for part1"
+    (is (= 567045 (t/day07-part1-soln)))))
+
+(deftest day07-part2-soln-test
+  (testing "Can reproduce the answer for part2"
+    (is (= 39016654 (t/day07-part2-soln)))))
