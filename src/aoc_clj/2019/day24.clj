c1fa1ad254e35b98af77fc5b6dc95f563514208c
Ken Smith <ken.smith@2scientists.net>
2022-11-27 14:41:01 -0700

    2019 Day 24

---
+++ src/aoc_clj/2019/day24.clj
@@ -0,0 +1,156 @@
+(ns aoc-clj.2019.day24
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid :as grid]
+            [aoc-clj.utils.grid.mapgrid :as mapgrid]))
+
+(def bug-map
+  {\. :space
+   \# :bug
+   \? :down})
+
+(defn parse
+  [input]
+  (:grid (mapgrid/ascii->MapGrid2D bug-map input :down true)))
+
+(def day24-input (parse (u/puzzle-input "2019/day24-input.txt")))
+
+(defn conway-rule
+  [space pos neighbor-fn]
+  (let [neighbors (neighbor-fn space pos)
+        state (space pos)
+        adjacent-bug-count (u/count-if neighbors #(= :bug (val %)))]
+    (case state
+      :space (if (or (= 1 adjacent-bug-count) (= 2 adjacent-bug-count))
+               :bug
+               :space)
+      :bug   (if (= 1 adjacent-bug-count)
+               :bug
+               :space)
+      state)))
+
+(defn conway-step
+  [space rule]
+  (zipmap (keys space)
+          (map (partial rule space) (keys space))))
+
+(defn conway-rule-2d
+  [space pos]
+  (conway-rule space pos grid/neighbors-2d))
+
+(defn conway-step-2d
+  [space]
+  (conway-step space conway-rule-2d))
+
+(defn biodiversity
+  [space]
+  (read-string (apply str "2r" (map {:bug 1 :space 0} (reverse (for [y (range 5) x (range 5)] (space [x y])))))))
+
+(defn find-recurrence
+  [space]
+  (loop [grid space last (biodiversity space) seen #{}]
+    (if (some? (seen last))
+      last
+      (let [nextgrid (conway-step-2d grid)
+            nextbio (biodiversity nextgrid)]
+        (recur nextgrid nextbio (conj seen last))))))
+
+(defn day24-part1-soln
+  []
+  (find-recurrence day24-input))
+
+(defn base-neighbor-coords
+  [[x y z]]
+  [[x (dec y) z] [(dec x) y z] [x (inc y) z] [(inc x) y z]])
+
+(defn outer-top
+  [coords [_ y z]]
+  (if (= 0 y)
+    (conj coords [2 1 (inc z)])
+    coords))
+(defn outer-bottom
+  [coords [_ y z]]
+  (if (= 4 y)
+    (conj coords [2 3 (inc z)])
+    coords))
+(defn outer-left
+  [coords [x _ z]]
+  (if (= 0 x)
+    (conj coords [1 2 (inc z)])
+    coords))
+(defn outer-right
+  [coords [x _ z]]
+  (if (= 4 x)
+    (conj coords [3 2 (inc z)])
+    coords))
+(defn inner-top
+  [coords [x y z]]
+  (if (= [2 1] [x y])
+    (into coords (for [x (range 5)] [x 0 (dec z)]))
+    coords))
+(defn inner-bottom
+  [coords [x y z]]
+  (if (= [2 3] [x y])
+    (into coords (for [x (range 5)] [x 4 (dec z)]))
+    coords))
+(defn inner-left
+  [coords [x y z]]
+  (if (= [1 2] [x y])
+    (into coords (for [y (range 5)] [0 y (dec z)]))
+    coords))
+(defn inner-right
+  [coords [x y z]]
+  (if (= [3 2] [x y])
+    (into coords (for [y (range 5)] [4 y (dec z)]))
+    coords))
+
+(def appenders
+  [outer-top outer-bottom outer-left outer-right inner-top inner-bottom inner-left inner-right])
+
+(defn neighbors3d-coords
+  [pos]
+  (let [base-coords (base-neighbor-coords pos)]
+    (reduce #(%2 %1 pos) base-coords appenders)))
+
+(defn neighbors3d
+  [space pos]
+  (let [coords (neighbors3d-coords pos)
+        vals (map space coords)]
+    (zipmap coords vals)))
+
+(defn space3d-init
+  [level0 depth]
+  (merge
+   (zipmap (for [z (range (- depth) (inc depth))
+                 y (range 5)
+                 x (range 5)]
+             [x y z])
+           (cycle (flatten [(repeat 12 :space) :down (repeat 12 :space)])))
+   (zipmap (map (fn [[x y]] [x y 0]) (keys level0))
+           (vals (assoc level0 [2 2] :down)))))
+
+(defn space3d-level
+  [space z]
+  (let [coords (for [x (range 5) y (range 5)] [x y])]
+    (zipmap coords (map (fn [[x y]] (space [x y z])) coords))))
+
+(defn conway-rule-3d
+  [space pos]
+  (conway-rule space pos neighbors3d))
+
+(defn conway-step3d
+  [space]
+  (conway-step space conway-rule-3d))
+
+(defn simulate
+  [level0 steps]
+  (let [depth (quot steps 2)
+        space3d (space3d-init level0 depth)]
+    (first (drop steps (iterate conway-step3d space3d)))))
+
+(defn bug-count
+  [space3d]
+  (u/count-if space3d #(= :bug (val %))))
+
+(defn day24-part2-soln
+  []
+  (bug-count (simulate day24-input 200)))
---
+++ test/aoc_clj/2019/day24_test.clj
@@ -0,0 +1,75 @@
+(ns aoc-clj.2019.day24-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2019.day24 :as t]))
+
+
+(def d24-s1       (t/parse ["....#" "#..#." "#..##" "..#.." "#...."]))
+(def d24-s1-step1 (t/parse ["#..#." "####." "###.#" "##.##" ".##.."]))
+(def d24-s1-step2 (t/parse ["#####" "....#" "....#" "...#." "#.###"]))
+(def d24-s1-step3 (t/parse ["#...." "####." "...##" "#.##." ".##.#"]))
+(def d24-s1-step4 (t/parse ["####." "....#" "##..#" "....." "##..."]))
+
+(def d24-s2 (t/parse ["....." "....." "....." "#...." ".#..."]))
+
+(deftest conway-step-test
+  (testing "Can properly execute the rules for Conway's game of life"
+    (is (= d24-s1-step1 (t/conway-step-2d d24-s1)))
+    (is (= d24-s1-step2 (t/conway-step-2d d24-s1-step1)))
+    (is (= d24-s1-step3 (t/conway-step-2d d24-s1-step2)))
+    (is (= d24-s1-step4 (t/conway-step-2d d24-s1-step3)))))
+
+(deftest biodiversity-test
+  (testing "Can properly compute biodiversity"
+    (is (= 2129920 (t/biodiversity d24-s2)))))
+
+(deftest day24-part1-soln-test
+  (testing "Can reproduce the answer for part1"
+    (is (= 17863711 (t/day24-part1-soln)))))
+
+(deftest neighbors3d-coords-test
+  (testing "Can correctly determine the right neighbors in 3d recursive space"
+    (is (= [[3 2 1] [2 3 1] [3 4 1] [4 3 1]]
+           (t/neighbors3d-coords [3 3 1])))
+    (is (= [[1 0 0] [0 1 0] [1 2 0] [2 1 0]]
+           (t/neighbors3d-coords [1 1 0])))
+    (is (= [[3 -1 0] [2 0 0] [3 1 0] [4 0 0] [2 1 1]]
+           (t/neighbors3d-coords [3 0 0])))
+    (is (= [[4 -1 0] [3 0 0] [4 1 0] [5 0 0] [2 1 1] [3 2 1]]
+           (t/neighbors3d-coords [4 0 0])))
+    (is (= [[3 1 1] [2 2 1] [3 3 1] [4 2 1] [4 0 0] [4 1 0] [4 2 0] [4 3 0] [4 4 0]]
+           (t/neighbors3d-coords [3 2 1])))))
+
+(def d24-s3         (t/parse ["....#" "#..#." "#.?##" "..#.." "#...."]))
+(def d24-s3-level-5 (t/parse ["..#.." ".#.#." "..?.#" ".#.#." "..#.."]))
+(def d24-s3-level-4 (t/parse ["...#." "...##" "..?.." "...##" "...#."]))
+(def d24-s3-level-3 (t/parse ["#.#.." ".#..." "..?.." ".#..." "#.#.."]))
+(def d24-s3-level-2 (t/parse [".#.##" "....#" "..?.#" "...##" ".###."]))
+(def d24-s3-level-1 (t/parse ["#..##" "...##" "..?.." "...#." ".####"]))
+(def d24-s3-level0  (t/parse [".#..." ".#.##" ".#?.." "....." "....."]))
+(def d24-s3-level+1 (t/parse [".##.." "#..##" "..?.#" "##.##" "#####"]))
+(def d24-s3-level+2 (t/parse ["###.." "##.#." "#.?.." ".#.##" "#.#.."]))
+(def d24-s3-level+3 (t/parse ["..###" "....." "#.?.." "#...." "#...#"]))
+(def d24-s3-level+4 (t/parse [".###." "#..#." "#.?.." "##.#." "....."]))
+(def d24-s3-level+5 (t/parse ["####." "#..#." "#.?#." "####." "....."]))
+
+(deftest simulate-test
+  (testing "Simulation works correctly in 3d"
+    (let [sim (t/simulate d24-s3 10)]
+      (is (= d24-s3-level-5 (t/space3d-level sim 5)))
+      (is (= d24-s3-level-4 (t/space3d-level sim 4)))
+      (is (= d24-s3-level-3 (t/space3d-level sim 3)))
+      (is (= d24-s3-level-2 (t/space3d-level sim 2)))
+      (is (= d24-s3-level-1 (t/space3d-level sim 1)))
+      (is (= d24-s3-level0  (t/space3d-level sim 0)))
+      (is (= d24-s3-level+1 (t/space3d-level sim -1)))
+      (is (= d24-s3-level+2 (t/space3d-level sim -2)))
+      (is (= d24-s3-level+3 (t/space3d-level sim -3)))
+      (is (= d24-s3-level+4 (t/space3d-level sim -4)))
+      (is (= d24-s3-level+5 (t/space3d-level sim -5)))
+      (is (= 99 (t/bug-count sim))))))
+
+;; FIXME: 2019 Day 24 part 2 solutiion is a bit slow to test
+;; https://github.com/Ken-2scientists/aoc-clj/issues/24
+(deftest ^:slow day24-part2-soln-test
+  (testing "Can reproduce the answer for part2"
+    (is (= 1937 (t/day24-part2-soln)))))
