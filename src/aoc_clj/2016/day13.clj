89d516dcc20507ebb2d55900d7e017144047d964
Ken Smith <ken.smith@2scientists.net>
2022-12-01 05:13:25 -0700

    2016 day13 WIP

--- src/aoc_clj/2016/day13.clj
+++ src/aoc_clj/2016/day13.clj
@@ -1,11 +1,18 @@
 (ns aoc-clj.2016.day13
-  (:require [aoc-clj.utils.binary :as b]))
+  (:require [aoc-clj.utils.binary :as b]
+            [aoc-clj.utils.graph :as g]
+            [aoc-clj.utils.grid.mapgrid :as mapgrid :refer [->MapGrid2D]]
+            [aoc-clj.utils.maze :as maze :refer [->Maze]]))
+
+(def charmap
+  {\. :open
+   \# :wall})
 
 (defn wall-calc
   [fav [x y]]
   (+ fav (* x x) (* 3 x) (* 2 x y) y (* y y)))
 
-(defn space
+(defn cell
   [fav pos]
   (let [ones (-> (wall-calc fav pos)
                  b/int->bitstr
@@ -14,3 +21,25 @@
     (if (even? ones)
       :open
       :wall)))
+
+(defn construct-grid
+  [fav width height]
+  (->MapGrid2D
+   width
+   height
+   (into {}
+         (for [y (range height)
+               x (range width)]
+           [[x y] (cell fav [x y])]))))
+
+(defn path-distance
+  [fav finish]
+  (let [start [1 1]
+        grid  (construct-grid fav 10 7)
+        maze  (->Maze (:grid grid) #(= :open %))
+        graph (-> maze maze/Maze->Graph (g/pruned #{start finish}))
+        path  (g/dijkstra graph start finish)]
+    (println graph)
+    (g/path-distance graph path)))
+
+(path-distance 10 [7 4])
--- test/aoc_clj/2016/day13_test.clj
+++ test/aoc_clj/2016/day13_test.clj
@@ -1,6 +1,23 @@
-;; (ns aoc-clj.2016.day13-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2016.day13 :as t]))
+(ns aoc-clj.2016.day13-test
+  (:require [aoc-clj.2016.day13 :as t]
+            [aoc-clj.utils.grid :as grid]
+            [clojure.test :refer [deftest is testing]]))
+
+(def d13-s1
+  ".#.####.##
+..#..#...#
+#....##...
+###.#.###.
+.##..#..#.
+..##....#.
+#...##.###")
+
+(deftest layout-test
+  (testing "Wall logic creates the same grid as the sample"
+    (is (= d13-s1
+           (grid/Grid2D->ascii
+            t/charmap
+            (t/construct-grid 10 10 7) :down true)))))
 
 ;; (deftest day13-part1-soln
 ;;   (testing "Reproduces the answer for day13, part1"
