7702af6a252b68135bf61462d48e7f894a89c5f8
Ken Smith <ken.smith@2scientists.net>
2022-12-01 05:12:34 -0700

    2022 Day01

--- src/aoc_clj/2022/day01.clj
+++ src/aoc_clj/2022/day01.clj
@@ -1,4 +1,29 @@
-;; (ns aoc-clj.2022.day01
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day01
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day01-input (u/puzzle-input "2022/day01-input.txt"))
+(defn parse
+  [input]
+  (map read-string input))
+
+(def day01-input (->> (u/puzzle-input "2022/day01-input.txt")
+                      (u/split-at-blankline)
+                      (map parse)))
+(defn elf-capacity
+  [calorie-counts]
+  (map-indexed (fn [idx cals] [idx (reduce + cals)]) calorie-counts))
+
+(defn max-capacity
+  [input]
+  (apply max (map second (elf-capacity input))))
+
+(defn day01-part1-soln
+  []
+  (max-capacity day01-input))
+
+(defn top-three-max-capacity
+  [input]
+  (reduce + (take 3 (sort > (map second (elf-capacity input))))))
+
+(defn day01-part2-soln
+  []
+  (top-three-max-capacity day01-input))
--- test/aoc_clj/2022/day01_test.clj
+++ test/aoc_clj/2022/day01_test.clj
@@ -1,11 +1,11 @@
-;; (ns aoc-clj.2022.day01-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day01 :as t]))
+(ns aoc-clj.2022.day01-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day01 :as t]))
 
-;; (deftest day01-part1-soln
-;;   (testing "Reproduces the answer for day01, part1"
-;;     (is (= 0 (t/day01-part1-soln)))))
+(deftest day01-part1-soln
+  (testing "Reproduces the answer for day01, part1"
+    (is (= 70116 (t/day01-part1-soln)))))
 
-;; (deftest day01-part2-soln
-;;   (testing "Reproduces the answer for day01, part2"
-;;     (is (= 0 (t/day01-part2-soln)))))
+(deftest day01-part2-soln
+  (testing "Reproduces the answer for day01, part2"
+    (is (= 206582 (t/day01-part2-soln)))))
