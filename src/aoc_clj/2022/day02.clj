39e20b139e7a463fc4c884f79d6590982da42eaa
Ken Smith <ken.smith@2scientists.net>
2022-12-02 05:23:10 -0700

    2022 day02 part 1

--- src/aoc_clj/2022/day02.clj
+++ src/aoc_clj/2022/day02.clj
@@ -1,4 +1,51 @@
-;; (ns aoc-clj.2022.day02
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day02
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day02-input (u/puzzle-input "2022/day02-input.txt"))
+(def player1
+  {"A" :rock
+   "B" :paper
+   "C" :scissors})
+
+(def player2
+  {"X" :rock
+   "Y" :paper
+   "Z" :scissors})
+
+(def shape-score
+  {:rock 1
+   :paper 2
+   :scissors 3})
+
+(def outcome-score
+  {:player2-lose 0
+   :draw         3
+   :player2-win  6})
+
+(def round->outcome
+  {[:rock :rock] :draw
+   [:rock :paper] :player2-win
+   [:rock :scissors] :player2-lose
+   [:paper :rock] :player2-lose
+   [:paper :paper] :draw
+   [:paper :scissors] :player2-win
+   [:scissors :rock] :player2-win
+   [:scissors :paper] :player2-lose
+   [:scissors :scissors] :draw})
+
+(defn parse-line
+  [line]
+  (let [[p1 p2] (str/split line #" ")]
+    [(player1 p1) (player2 p2)]))
+
+(def day02-input (map parse-line (u/puzzle-input "2022/day02-input.txt")))
+
+(defn round-score
+  [round]
+  (+ (shape-score (second round))
+     (outcome-score (round->outcome round))))
+
+(defn day02-part1-soln
+  []
+  (reduce + (map round-score day02-input)))
+
--- test/aoc_clj/2022/day02_test.clj
+++ test/aoc_clj/2022/day02_test.clj
@@ -1,6 +1,22 @@
-;; (ns aoc-clj.2022.day02-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day02 :as t]))
+(ns aoc-clj.2022.day02-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day02 :as t]))
+
+(def d02-s01
+  (mapv
+   t/parse-line
+   ["A Y"
+    "B X"
+    "C Z"]))
+
+(deftest parse-test
+  (testing "Parses letter codes to plays"
+    (is (= [[:rock :paper] [:paper :rock] [:scissors :scissors]]
+           d02-s01))))
+
+(deftest round-score
+  (testing "Computes the score for each round"
+    (is (= [8 1 6] (mapv t/round-score d02-s01)))))
 
 ;; (deftest day02-part1-soln
 ;;   (testing "Reproduces the answer for day02, part1"
