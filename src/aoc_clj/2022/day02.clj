384f8e3ad7da1b0ae24f9944e104a9741e91bee2
Ken Smith <ken.smith@2scientists.net>
2022-12-03 10:15:43 -0700

    Refactor/comment 2022 day02

--- src/aoc_clj/2022/day02.clj
+++ src/aoc_clj/2022/day02.clj
@@ -2,80 +2,85 @@
   (:require [clojure.string :as str]
             [aoc-clj.utils.core :as u]))
 
-(def player1
-  {"A" :rock
-   "B" :paper
-   "C" :scissors})
+(def guide->p1move
+  {"A" ::rock
+   "B" ::paper
+   "C" ::scissors})
 
-(def player2
-  {"X" :rock
-   "Y" :paper
-   "Z" :scissors})
+(def guide->p2move
+  {"X" ::rock
+   "Y" ::paper
+   "Z" ::scissors})
 
-(def player2-outcome
-  {"X" :player2-lose
-   "Y" :draw
-   "Z" :player2-win})
+(def guide->p2outcome
+  {"X" ::lose
+   "Y" ::draw
+   "Z" ::win})
 
 (def shape-score
-  {:rock 1
-   :paper 2
-   :scissors 3})
+  {::rock     1
+   ::paper    2
+   ::scissors 3})
 
 (def outcome-score
-  {:player2-lose 0
-   :draw         3
-   :player2-win  6})
+  {::lose 0
+   ::draw 3
+   ::win  6})
 
-(def round->outcome
-  {[:rock :rock] :draw
-   [:rock :paper] :player2-win
-   [:rock :scissors] :player2-lose
-   [:paper :rock] :player2-lose
-   [:paper :paper] :draw
-   [:paper :scissors] :player2-win
-   [:scissors :rock] :player2-win
-   [:scissors :paper] :player2-lose
-   [:scissors :scissors] :draw})
+(def permutations
+  "All possible rock-paper-scissor plays listed as [p1, p2, outcome]"
+  [[::rock     ::rock     ::draw]
+   [::rock     ::paper    ::win]
+   [::rock     ::scissors ::lose]
+   [::paper    ::rock     ::lose]
+   [::paper    ::paper    ::draw]
+   [::paper    ::scissors ::win]
+   [::scissors ::rock     ::win]
+   [::scissors ::paper    ::lose]
+   [::scissors ::scissors ::draw]])
 
-(def outcome-player2-play
-  {:player2-win {:rock :paper
-                 :paper :scissors
-                 :scissors :rock}
-   :draw        {:rock :rock
-                 :paper :paper
-                 :scissors :scissors}
-   :player2-lose {:rock :scissors
-                  :paper :rock
-                  :scissors :paper}})
+(def outcomes
+  "Map from [p1 p2] to game outcome (for player 2)"
+  (into {} (map (fn [[p1 p2 o]] [[p1 p2] o]) permutations)))
+
+(def p2-plays
+  "Map from [p1 outcome] to player 2 play"
+  (into {} (map (fn [[p1 p2 o]] [[p1 o] p2]) permutations)))
 
 (defn parse-line
-  [col2 line]
+  "Parse a `line` from the strategy guide input using `f` to parse
+   the second column's value"
+  [f line]
   (let [[p1 p2] (str/split line #" ")]
-    [(player1 p1) (col2 p2)]))
+    [(guide->p1move p1) (f p2)]))
 
-(def parse-line-1 (partial parse-line player2))
-(def parse-line-2 (partial parse-line player2-outcome))
+(def parse-line-part1 (partial parse-line guide->p2move))
+(def parse-line-part2 (partial parse-line guide->p2outcome))
 
 (def day02-input (u/puzzle-input "2022/day02-input.txt"))
-(def day02-input-1 (map parse-line-1 day02-input))
-(def day02-input-2 (map parse-line-2 day02-input))
+(def day02-input-part1 (map parse-line-part1 day02-input))
+(def day02-input-part2 (map parse-line-part2 day02-input))
 
-(defn round-score-1
-  [round]
-  (+ (shape-score (second round))
-     (outcome-score (round->outcome round))))
+(defn score-part1
+  "Compute the round score using the part1 interpretation of the strategy guide"
+  [[_ p2 :as round]]
+  (+ (shape-score p2)
+     (outcome-score (outcomes round))))
 
-(defn round-score-2
-  [[p1 outcome]]
-  (let [p2 (get-in outcome-player2-play [outcome p1])]
-    (+ (shape-score p2)
-       (outcome-score outcome))))
+(defn score-part2
+  "Compute the round score using the part2 interpretation of the strategy guide"
+  [[_ outcome :as round]]
+  (+ (shape-score (p2-plays round))
+     (outcome-score outcome)))
 
 (defn day02-part1-soln
+  "What would your total score be if everything goes exactly according to 
+   your strategy guide?"
   []
-  (reduce + (map round-score-1 day02-input-1)))
+  (reduce + (map score-part1 day02-input-part1)))
 
 (defn day02-part2-soln
+  "Following the Elf's instructions for the second column, what would your 
+   total score be if everything goes exactly according to your strategy guide?"
   []
-  (reduce + (map round-score-2 day02-input-2)))
+  (reduce + (map score-part2 day02-input-part2)))
--- test/aoc_clj/2022/day02_test.clj
+++ test/aoc_clj/2022/day02_test.clj
@@ -6,20 +6,22 @@
   ["A Y"
    "B X"
    "C Z"])
-(def d02-s01-1 (mapv t/parse-line-1 d02-s01))
-(def d02-s01-2 (mapv t/parse-line-2 d02-s01))
+(def d02-s01-1 (mapv t/parse-line-part1 d02-s01))
+(def d02-s01-2 (mapv t/parse-line-part2 d02-s01))
 
 (deftest parse-test
   (testing "Parses letter codes to plays"
-    (is (= [[:rock :paper] [:paper :rock] [:scissors :scissors]]
+    (is (= [[::t/rock ::t/paper]
+            [::t/paper ::t/rock]
+            [::t/scissors ::t/scissors]]
            d02-s01-1))
-    (is (= [[:rock :draw] [:paper :player2-lose] [:scissors :player2-win]]
+    (is (= [[::t/rock ::t/draw] [::t/paper ::t/lose] [::t/scissors ::t/win]]
            d02-s01-2))))
 
 (deftest round-score
   (testing "Computes the score for each round"
-    (is (= [8 1 6] (mapv t/round-score-1 d02-s01-1)))
-    (is (= [4 1 7] (mapv t/round-score-2 d02-s01-2)))))
+    (is (= [8 1 6] (mapv t/score-part1 d02-s01-1)))
+    (is (= [4 1 7] (mapv t/score-part2 d02-s01-2)))))
 
 (deftest day02-part1-soln
   (testing "Reproduces the answer for day02, part1"
