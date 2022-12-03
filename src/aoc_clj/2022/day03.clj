c4317b2f619097fee2831a0c2f0460efaf6c8f80
Ken Smith <ken.smith@2scientists.net>
2022-12-03 08:52:19 -0700

    2022 Day 03

--- src/aoc_clj/2022/day03.clj
+++ src/aoc_clj/2022/day03.clj
@@ -1,4 +1,51 @@
-;; (ns aoc-clj.2022.day03
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day03
+  (:require [clojure.set :as set]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day03-input (u/puzzle-input "2022/day03-input.txt"))
+(def day03-input (u/puzzle-input "2022/day03-input.txt"))
+
+(defn split-halfway
+  [line]
+  (split-at (/ (count line) 2) line))
+
+(defn overlap
+  [coll]
+  (apply set/intersection (map set coll)))
+
+(defn split
+  [type coll]
+  (if (= :thirds type)
+    (partition 3 coll)
+    (map split-halfway coll)))
+
+(defn overlaps
+  ([input]
+   (overlaps input :halfway))
+  ([input type]
+   (->> input
+        (split type)
+        (map overlap)
+        (map seq)
+        flatten)))
+
+(defn priority
+  [c]
+  (if (Character/isUpperCase c)
+    (+ 27 (- (int c) (int \A)))
+    (+  1 (- (int c) (int \a)))))
+
+(defn overlaps-priority
+  ([input]
+   (overlaps-priority input :halfway))
+  ([input type]
+   (->> (overlaps input type)
+        (map priority)
+        (reduce +))))
+
+(defn day03-part1-soln
+  []
+  (overlaps-priority day03-input))
+
+(defn day03-part2-soln
+  []
+  (overlaps-priority day03-input :thirds))
--- test/aoc_clj/2022/day03_test.clj
+++ test/aoc_clj/2022/day03_test.clj
@@ -1,11 +1,40 @@
-;; (ns aoc-clj.2022.day03-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day03 :as t]))
+(ns aoc-clj.2022.day03-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day03 :as t]))
 
-;; (deftest day03-part1-soln
-;;   (testing "Reproduces the answer for day03, part1"
-;;     (is (= 0 (t/day03-part1-soln)))))
+(def d03-s01
+  ["vJrwpWtwJgWrhcsFMMfFFhFp"
+   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+   "PmmdzqPrVvPwwTWBwg"
+   "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+   "ttgJtRGJQctTZtZT"
+   "CrZsJsPPZsGzwwsLwLmpwMDw"])
 
-;; (deftest day03-part2-soln
-;;   (testing "Reproduces the answer for day03, part2"
-;;     (is (= 0 (t/day03-part2-soln)))))
+(deftest overlaps-test
+  (testing "finds the overlapping items in the first/second halves"
+    (is (= [\p \L \P \v \t \s]
+           (t/overlaps d03-s01)))
+    (is (= [\r \Z]
+           (t/overlaps d03-s01 :thirds)))))
+
+(deftest priority-test
+  (testing "Computes the priority for a given character"
+    (is (= 16 (t/priority \p)))
+    (is (= 38 (t/priority \L)))
+    (is (= 42 (t/priority \P)))
+    (is (= 22 (t/priority \v)))
+    (is (= 20 (t/priority \t)))
+    (is (= 19 (t/priority \s)))))
+
+(deftest overlaps-priority-test
+  (testing "Finds the sum of the priorities of the overlapping items"
+    (is (= 157 (t/overlaps-priority d03-s01)))
+    (is (= 70 (t/overlaps-priority d03-s01 :thirds)))))
+
+(deftest day03-part1-soln
+  (testing "Reproduces the answer for day03, part1"
+    (is (= 7597 (t/day03-part1-soln)))))
+
+(deftest day03-part2-soln
+  (testing "Reproduces the answer for day03, part2"
+    (is (= 2607 (t/day03-part2-soln)))))
