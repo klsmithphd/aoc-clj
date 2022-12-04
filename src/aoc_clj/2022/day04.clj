47b248cf35a0c8fd6e1a2113dc669f605040e410
Ken Smith <ken.smith@2scientists.net>
2022-12-04 08:39:01 -0700

    Solution to 2022 day 04

--- src/aoc_clj/2022/day04.clj
+++ src/aoc_clj/2022/day04.clj
@@ -1,4 +1,61 @@
-;; (ns aoc-clj.2022.day04
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day04
+  "Solution to https://adventofcode.com/2022/day/4"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day04-input (u/puzzle-input "2022/day04-input.txt"))
+(defn parse-range
+  "Split range at the `-` character"
+  [range]
+  (mapv read-string (str/split range #"\-")))
+
+(defn parse-line
+  "Split pair at the `,` character"
+  [line]
+  (mapv parse-range (str/split line #",")))
+
+(defn parse
+  "Parse the input format across all lines"
+  [input]
+  (mapv parse-line input))
+
+(def day04-input (parse (u/puzzle-input "2022/day04-input.txt")))
+
+(defn fully-contained?
+  "Return true if one of the ranges (determined by inclusive left/right bounds)
+   is fully contained within the other"
+  [[[l1 r1] [l2 r2]]]
+  (or
+   ;;     l1-r1
+   ;; l2----------r2
+   (<= l2 l1 r1 r2)
+   ;; l1----------r1
+   ;;     l2--r2   
+   (<= l1 l2 r2 r1)))
+
+(defn overlap?
+  "Return true if one of the ranges (determined by inclusive left/right bounds)
+   overlaps with the other"
+  [[[l1 r1] [l2 r2]]]
+  (or
+   ;; l1-----r1
+   ;;     l2------r2
+   (<= l1 l2 r1 r2)
+   ;;       l1-----r1
+   ;; l2------r2
+   (<= l2 l1 r2 r1)
+   ;;     l1-r1
+   ;; l2----------r2
+   (<= l2 l1 r1 r2)
+   ;; l1----------r1
+   ;;     l2--r2   
+   (<= l1 l2 r2 r1)))
+
+(defn day04-part1-soln
+  "In how many assignment pairs does one range fully contain the other?"
+  []
+  (count (filter fully-contained? day04-input)))
+
+(defn day04-part2-soln
+  "In how many assignment pairs do the ranges overlap?"
+  []
+  (count (filter overlap? day04-input)))
