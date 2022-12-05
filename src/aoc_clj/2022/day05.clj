4c2c2d5977a0d3cf71489e44b853c72eab0fab8d
Ken Smith <ken.smith@2scientists.net>
2022-12-05 06:21:26 -0700

    Add docstrings to 2022 Day05

--- src/aoc_clj/2022/day05.clj
+++ src/aoc_clj/2022/day05.clj
@@ -4,11 +4,16 @@
             [aoc-clj.utils.core :as u]))
 
 (defn parse-stack
+  "Convert a text string representation given by `text` with the first
+   character being the stack id and the remaining characters
+   representing the crates in the stack in order from bottom to top"
   [stack]
   [(read-string (str (first stack)))
    (mapv str (rest stack))])
 
 (defn parse-stacks
+  "Convert ASCII art text representation of `stacks` into a map
+   representing the ordered stack contents, keyed by id"
   [stacks]
   (->> stacks
        ;; transposes columns to rows
@@ -29,14 +34,18 @@
        (into {})))
 
 (defn parse-move
+  "Extract the number of crates to move, the source stack and destination
+   stack ids"
   [line]
   (mapv read-string (re-seq #"\d+" line)))
 
 (defn parse-moves
+  "Convert the text representations into move data"
   [moves]
   (map parse-move moves))
 
 (defn parse
+  "Parse the text-based representation of the inputs"
   [input]
   (let [[stacks moves] (u/split-at-blankline input)]
     {:stacks (parse-stacks stacks)
@@ -44,13 +53,23 @@
 
 (def day05-input (parse (u/puzzle-input "2022/day05-input.txt")))
 
-(defn take-crates-2 [num stacks src]
+(defn take-crates-2
+  "The crates taken off of a stack, maintaining order"
+  [num stacks src]
   (take-last num (stacks src)))
 
-(defn take-crates-1 [num stacks src]
+(defn take-crates-1
+  "The crates taken off of a stack as if sequentially popped"
+  [num stacks src]
   (reverse (take-crates-2 num stacks src)))
 
 (defn apply-move
+  "Execute the move order against the stack. Returns a new state of the stacks.
+   `take-fn` is a function which indicates how items are removed from the
+   source stack.
+   `stacks` is the initial state of the stacks
+   `moves` is a vector of 3 integers: number of crates to move, the 
+   source stack id, and the destination stack id"
   [take-fn stacks [num src dst]]
   (-> stacks
       (update src #(vec (drop-last num %)))
@@ -60,14 +79,18 @@
 (def apply-move-2 (partial apply-move take-crates-2))
 
 (defn final-arrangement-1
+  "The final arrangement of the stacks using the move rules of part 1"
   [{:keys [stacks moves]}]
   (reduce apply-move-1 stacks moves))
 
 (defn final-arrangement-2
+  "The final arrangement of the stacks using the move rules of part 2"
   [{:keys [stacks moves]}]
   (reduce apply-move-2 stacks moves))
 
 (defn stack-tops
+  "Returns a concatenated string of the letters representing the crates
+   at the top of all the stacks in order by their id"
   [stacks]
   (let [n (count stacks)]
     (->> (map stacks (range 1 (inc n)))
