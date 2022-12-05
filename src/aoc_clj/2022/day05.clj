aa74d620711d4037ea3f0126f119bea93deab8f7
Ken Smith <ken.smith@2scientists.net>
2022-12-05 02:55:29 -0700

    2022 Day 05

--- src/aoc_clj/2022/day05.clj
+++ src/aoc_clj/2022/day05.clj
@@ -1,5 +1,91 @@
-;; (ns aoc-clj.2022.day05
-;;  "Solution to https://adventofcode.com/2022/day/5"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day05
+  "Solution to https://adventofcode.com/2022/day/5"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day05-input (u/puzzle-input "2022/day05-input.txt"))
+(defn parse-stack
+  [stack]
+  [(read-string (str (first stack)))
+   (mapv str (rest stack))])
+
+(defn parse-stacks
+  [stacks]
+  (->> stacks
+       ;; transposes columns to rows
+       (apply map vector)
+       ;; reassembles as strings
+       (map str/join)
+       ;; Skip the first row (containing "[")
+       (drop 1)
+       ;; Actual stacks are now every 4th row
+       (take-nth 4)
+       ;; Reverse each stack NZ1 -> 1ZN
+       (map str/reverse)
+       ;; Remove whitespace at ends
+       (map str/trim)
+       ;; Parse into number and letters
+       (map parse-stack)
+       ;; Pack into a map
+       (into {})))
+
+(defn parse-move
+  [line]
+  (mapv read-string (re-seq #"\d+" line)))
+
+(defn parse-moves
+  [moves]
+  (map parse-move moves))
+
+(defn parse
+  [input]
+  (let [[stacks moves] (u/split-at-blankline input)]
+    {:stacks (parse-stacks stacks)
+     :moves  (parse-moves moves)}))
+
+(def day05-input (parse (u/puzzle-input "2022/day05-input.txt")))
+
+(defn take-crates-2 [num stacks src]
+  (take-last num (stacks src)))
+
+(defn take-crates-1 [num stacks src]
+  (reverse (take-crates-2 num stacks src)))
+
+(defn apply-move
+  [take-fn stacks [num src dst]]
+  (-> stacks
+      (update src #(vec (drop-last num %)))
+      (update dst #(apply conj % (take-fn num stacks src)))))
+
+(def apply-move-1 (partial apply-move take-crates-1))
+(def apply-move-2 (partial apply-move take-crates-2))
+
+(defn final-arrangement-1
+  [{:keys [stacks moves]}]
+  (reduce apply-move-1 stacks moves))
+
+(defn final-arrangement-2
+  [{:keys [stacks moves]}]
+  (reduce apply-move-2 stacks moves))
+
+(defn stack-tops
+  [stacks]
+  (let [n (count stacks)]
+    (->> (map stacks (range 1 (inc n)))
+         (map peek)
+         (str/join))))
+
+(defn day05-part1-soln
+  "After the rearrangement procedure completes, 
+   what crate ends up on top of each stack?"
+  []
+  (-> day05-input final-arrangement-1 stack-tops))
+
+(defn day05-part2-soln
+  "The CrateMover 9001 is notable for many new and exciting features: 
+   air conditioning, leather seats, an extra cup holder, and 
+   the ability to pick up and move multiple crates at once.
+   
+   After the rearrangement procedure completes, 
+   what crate ends up on top of each stack?"
+  []
+  (-> day05-input final-arrangement-2 stack-tops))
--- test/aoc_clj/2022/day05_test.clj
+++ test/aoc_clj/2022/day05_test.clj
@@ -1,11 +1,33 @@
-;; (ns aoc-clj.2022.day05-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day05 :as t]))
+(ns aoc-clj.2022.day05-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day05 :as t]))
 
-;; (deftest day05-part1-soln
-;;   (testing "Reproduces the answer for day05, part1"
-;;     (is (= 0 (t/day05-part1-soln)))))
+(def d05-s01
+  (t/parse
+   ["    [D]    "
+    "[N] [C]    "
+    "[Z] [M] [P]"
+    " 1   2   3 "
+    ""
+    "move 1 from 2 to 1"
+    "move 3 from 1 to 3"
+    "move 2 from 2 to 1"
+    "move 1 from 1 to 2"]))
 
-;; (deftest day05-part2-soln
-;;   (testing "Reproduces the answer for day05, part2"
-;;     (is (= 0 (t/day05-part2-soln)))))
+(deftest final-arrangement-1-test
+  (testing "Stacks end up in the correct final state in part1"
+    (is (= {1 ["C"], 2 ["M"], 3 ["P" "D" "N" "Z"]}
+           (t/final-arrangement-1 d05-s01)))))
+
+(deftest final-arrangement-2-test
+  (testing "Stacks end up in the correct final state in part1"
+    (is (= {1 ["M"], 2 ["C"], 3 ["P" "Z" "N" "D"]}
+           (t/final-arrangement-2 d05-s01)))))
+
+(deftest day05-part1-soln
+  (testing "Reproduces the answer for day05, part1"
+    (is (= "CNSZFDVLJ" (t/day05-part1-soln)))))
+
+(deftest day05-part2-soln
+  (testing "Reproduces the answer for day05, part2"
+    (is (= "QNDWLMGNS" (t/day05-part2-soln)))))
