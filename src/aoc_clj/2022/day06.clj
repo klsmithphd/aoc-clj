562b861ae40c5a2823cbba4c22a696ae8f221680
Ken Smith <ken.smith@2scientists.net>
2022-12-06 05:25:34 -0700

    Solution to 2022 Day 06

--- src/aoc_clj/2022/day06.clj
+++ src/aoc_clj/2022/day06.clj
@@ -1,5 +1,28 @@
-;; (ns aoc-clj.2022.day06
-;;  "Solution to https://adventofcode.com/2022/day/6"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day06
+  "Solution to https://adventofcode.com/2022/day/6"
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day06-input (u/puzzle-input "2022/day06-input.txt"))
+(def day06-input (first (u/puzzle-input "2022/day06-input.txt")))
+
+(defn chars-to-distinct-run
+  "Returns the number of characters that need to be examined to
+   find a run of distinct characters of length `len` in a string `s`
+   Returns nil if no such run is found"
+  [len s]
+  (->> (partition len 1 s)
+       (map #(apply distinct? %))
+       (u/index-of true)
+       (+ len)))
+
+(defn day06-part1-soln
+  "How many characters need to be processed before the 
+   first start-of-packet marker is detected?"
+  []
+  (chars-to-distinct-run 4 day06-input))
+
+(defn day06-part2-soln
+  "How many characters need to be processed before the 
+   first start-of-message marker is detected?"
+  []
+  (chars-to-distinct-run 14 day06-input))
+
--- test/aoc_clj/2022/day06_test.clj
+++ test/aoc_clj/2022/day06_test.clj
@@ -1,11 +1,39 @@
-;; (ns aoc-clj.2022.day06-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day06 :as t]))
+(ns aoc-clj.2022.day06-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day06 :as t]))
 
-;; (deftest day06-part1-soln
-;;   (testing "Reproduces the answer for day06, part1"
-;;     (is (= 0 (t/day06-part1-soln)))))
+(def d06-s01 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+(def d06-s02 "bvwbjplbgvbhsrlpgdmjqwftvncz")
+(def d06-s03 "nppdvjthqldpwncqszvftbrmjlhg")
+(def d06-s04 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+(def d06-s05 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
 
-;; (deftest day06-part2-soln
-;;   (testing "Reproduces the answer for day06, part2"
-;;     (is (= 0 (t/day06-part2-soln)))))
+(def d06-s06 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+(def d06-s07 "bvwbjplbgvbhsrlpgdmjqwftvncz")
+(def d06-s08 "nppdvjthqldpwncqszvftbrmjlhg")
+(def d06-s09 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+(def d06-s10 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+
+(deftest chars-to-distinct-run-test
+  (testing "Finds number of chars to examine to find first
+            start-of-packet or start-of-message sequence"
+    ;; start of packet: sequence of 4 unique chars
+    (is (= 7  (t/chars-to-distinct-run 4 d06-s01)))
+    (is (= 5  (t/chars-to-distinct-run 4 d06-s02)))
+    (is (= 6  (t/chars-to-distinct-run 4 d06-s03)))
+    (is (= 10 (t/chars-to-distinct-run 4 d06-s04)))
+    (is (= 11 (t/chars-to-distinct-run 4 d06-s05)))
+    ;; start of message: sequence of 14 unique chars
+    (is (= 19 (t/chars-to-distinct-run 14 d06-s06)))
+    (is (= 23 (t/chars-to-distinct-run 14 d06-s07)))
+    (is (= 23 (t/chars-to-distinct-run 14 d06-s08)))
+    (is (= 29 (t/chars-to-distinct-run 14 d06-s09)))
+    (is (= 26 (t/chars-to-distinct-run 14 d06-s10)))))
+
+(deftest day06-part1-soln
+  (testing "Reproduces the answer for day06, part1"
+    (is (= 1855 (t/day06-part1-soln)))))
+
+(deftest day06-part2-soln
+  (testing "Reproduces the answer for day06, part2"
+    (is (= 3256 (t/day06-part2-soln)))))
