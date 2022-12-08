42bbaba7613350a9070de71b0d8cab3f2baaf122
Ken Smith <ken.smith@2scientists.net>
2022-12-08 05:07:16 -0700

    WIP on day07

--- src/aoc_clj/2022/day07.clj
+++ src/aoc_clj/2022/day07.clj
@@ -1,5 +1,59 @@
-;; (ns aoc-clj.2022.day07
-;;  "Solution to https://adventofcode.com/2022/day/7"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day07
+  "Solution to https://adventofcode.com/2022/day/7"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day07-input (u/puzzle-input "2022/day07-input.txt"))
+(def d07-s01
+  ["$ cd /"
+   "$ ls"
+   "dir a"
+   "14848514 b.txt"
+   "8504156 c.dat"
+   "dir d"
+   "$ cd a"
+   "$ ls"
+   "dir e"
+   "29116 f"
+   "2557 g"
+   "62596 h.lst"
+   "$ cd e"
+   "$ ls"
+   "584 i"
+   "$ cd .."
+   "$ cd .."
+   "$ cd d"
+   "$ ls"
+   "4060174 j"
+   "8033020 d.log"
+   "5626152 d.ext"
+   "7214296 k"])
+
+[["/"] ["a" {} "b.txt" 14848514 "c.dat" 8504156 "d" {}]
+ ["/" "a"] ["e" {} "f"]]
+
+(update-in
+ (update-in {} [:a :b :c] #(assoc % :d 2557))
+ [:a :b] #(assoc % :e {}))
+
+(def day07-input (u/puzzle-input "2022/day07-input.txt"))
+
+(defn change-dir
+  [tree node cmds]
+  (let [line (first cmds)
+        newdir (subs line 5)])
+  [tree])
+
+(defn process
+  [tree node cmds]
+  (let [line (first cmds)]
+    (if (str/starts-with? line "$ cd")
+      (change-dir tree node cmds)
+      (list-dir tree node cmds))))
+
+(defn crawl-tree
+  [terminal]
+  (loop [tree {} node nil cmds terminal]
+    (if (empty? cmds)
+      tree
+      (let [[t n c] (process tree node cmds)]
+        (recur t n c)))))
