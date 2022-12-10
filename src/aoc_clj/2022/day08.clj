62f6eeb5d2c65870fd0214329fd281da7b928da8
Ken Smith <ken.smith@2scientists.net>
2022-12-10 10:45:17 -0700

    WIP 2022 Day08

--- src/aoc_clj/2022/day08.clj
+++ src/aoc_clj/2022/day08.clj
@@ -1,5 +1,21 @@
-;; (ns aoc-clj.2022.day08
-;;  "Solution to https://adventofcode.com/2022/day/8"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day08
+  "Solution to https://adventofcode.com/2022/day/8"
+  (:require [aoc-clj.utils.grid.vecgrid :as vecgrid
+             :refer [->VecGrid2D width height value slice]]
+            [aoc-clj.utils.core :as u]))
+
+(def d08-s01
+  ["30373"
+   "25512"
+   "65332"
+   "33549"
+   "35390"])
+
+(->VecGrid2D d08-s01)
+
+(defn something
+  [grid]
+  (let [indices (for [j (range 1 (dec (height grid)))
+                      i (range 1 (dec (width grid)))])]))
 
 ;; (def day08-input (u/puzzle-input "2022/day08-input.txt"))
