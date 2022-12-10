30ee2fc0d5aa1558b96e957fe04529d02945ff75
Ken Smith <ken.smith@2scientists.net>
2022-12-10 15:02:50 -0700

    2022 day08 part 1

--- src/aoc_clj/2022/day08.clj
+++ src/aoc_clj/2022/day08.clj
@@ -1,21 +1,61 @@
 (ns aoc-clj.2022.day08
   "Solution to https://adventofcode.com/2022/day/8"
-  (:require [aoc-clj.utils.grid.vecgrid :as vecgrid
-             :refer [->VecGrid2D width height value slice]]
-            [aoc-clj.utils.core :as u]))
+  (:require [aoc-clj.utils.core :as u]))
+
+(defn parse-row
+  [row]
+  (mapv (comp read-string str) row))
+
+(defn parse
+  [input]
+  (mapv parse-row input))
+
+(def day08-input (parse (u/puzzle-input "2022/day08-input.txt")))
 
 (def d08-s01
-  ["30373"
-   "25512"
-   "65332"
-   "33549"
-   "35390"])
+  (parse
+   ["30373"
+    "25512"
+    "65332"
+    "33549"
+    "35390"]))
 
-(->VecGrid2D d08-s01)
+(defn transpose
+  [m]
+  (apply map vector m))
 
-(defn something
+(defn visible?
+  [s]
+  (loop [nxt (rest s) mx (first s) acc [1]]
+    (if (empty? nxt)
+      acc
+      (let [tree    (first nxt)
+            taller? (> tree mx)]
+        (recur (rest nxt)
+               (if taller? tree mx)
+               (conj acc (if taller? 1 0)))))))
+
+(defn row-visibility
+  [row]
+  (mapv bit-or (visible? row) (reverse (visible? (reverse row)))))
+
+
+(defn grid-visibility
   [grid]
-  (let [indices (for [j (range 1 (dec (height grid)))
-                      i (range 1 (dec (width grid)))])]))
+  (let [width (count (first grid))]
+    (vec
+     (concat [(vec (repeat width 1))]
+             (mapv row-visibility (butlast (drop 1 grid)))
+             [(vec (repeat width 1))]))))
 
-;; (def day08-input (u/puzzle-input "2022/day08-input.txt"))
+(defn visible-trees
+  [input]
+  (reduce + (map bit-or
+                 (flatten (grid-visibility input))
+                 (flatten (transpose (grid-visibility (transpose input)))))))
+
+
+
+(defn day08-part1-soln
+  []
+  (visible-trees day08-input))
--- test/aoc_clj/2022/day08_test.clj
+++ test/aoc_clj/2022/day08_test.clj
@@ -1,10 +1,22 @@
-;; (ns aoc-clj.2022.day08-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day08 :as t]))
+(ns aoc-clj.2022.day08-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day08 :as t]))
 
-;; (deftest day08-part1-soln
-;;   (testing "Reproduces the answer for day08, part1"
-;;     (is (= 0 (t/day08-part1-soln)))))
+(def d08-s01
+  (t/parse
+   ["30373"
+    "25512"
+    "65332"
+    "33549"
+    "35390"]))
+
+(deftest visible-trees-test
+  (testing "Counts the number of visible trees in the sample data"
+    (is (= 21 (t/visible-trees d08-s01)))))
+
+(deftest day08-part1-soln
+  (testing "Reproduces the answer for day08, part1"
+    (is (= 1538 (t/day08-part1-soln)))))
 
 ;; (deftest day08-part2-soln
 ;;   (testing "Reproduces the answer for day08, part2"
