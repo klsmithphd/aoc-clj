cc99588ac2aceab88003aeb5bb882e41742df1e8
Ken Smith <ken.smith@2scientists.net>
2022-12-10 16:29:52 -0700

    2022 Day 09 part 1

--- src/aoc_clj/2022/day09.clj
+++ src/aoc_clj/2022/day09.clj
@@ -1,5 +1,71 @@
-;; (ns aoc-clj.2022.day09
-;;  "Solution to https://adventofcode.com/2022/day/9"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day09
+  "Solution to https://adventofcode.com/2022/day/9"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day09-input (u/puzzle-input "2022/day09-input.txt"))
+(defn parse-line
+  [line]
+  (let [[l r] (str/split line #" ")]
+    [l (read-string r)]))
+
+(defn parse
+  [input]
+  (map parse-line input))
+
+(def d09-s01
+  (parse
+   ["R 4"
+    "U 4"
+    "L 3"
+    "D 1"
+    "R 4"
+    "D 1"
+    "L 5"
+    "R 2"]))
+
+(defn expand
+  [input]
+  (flatten (map #(repeat (second %) (first %)) input)))
+
+(def day09-input (parse (u/puzzle-input "2022/day09-input.txt")))
+
+(defn move-tail
+  [tail head]
+  (let [delta (mapv - head tail)]
+    (mapv + tail (case delta
+                   [-1  2] [-1  1]
+                   [0   2] [0   1]
+                   [1   2] [1   1]
+                   [-1 -2] [-1 -1]
+                   [0  -2] [0  -1]
+                   [1  -2] [1  -1]
+                   [2   1] [1   1]
+                   [2   0] [1   0]
+                   [2  -1] [1  -1]
+                   [-2  1] [-1  1]
+                   [-2  0] [-1  0]
+                   [-2 -1] [-1 -1]
+                   [0 0]))))
+
+(defn step
+  [{:keys [tail head]} cmd]
+  (let [newhead (mapv + head (case cmd
+                               "R" [1 0]
+                               "L" [-1 0]
+                               "U" [0 1]
+                               "D" [0 -1]))]
+    {:head newhead :tail (move-tail tail newhead)}))
+
+(defn distinct-tail-positions
+  [input]
+  (->> (expand input)
+       (reductions step {:head [0 0] :tail [0 0]})
+       (map :tail)
+       distinct
+       count))
+
+(defn day09-part1-soln
+  "Simulate your complete hypothetical series of motions. How many positions 
+   does the tail of the rope visit at least once?"
+  []
+  (distinct-tail-positions day09-input))
--- test/aoc_clj/2022/day09_test.clj
+++ test/aoc_clj/2022/day09_test.clj
@@ -1,10 +1,25 @@
-;; (ns aoc-clj.2022.day09-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day09 :as t]))
+(ns aoc-clj.2022.day09-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day09 :as t]))
 
-;; (deftest day09-part1-soln
-;;   (testing "Reproduces the answer for day09, part1"
-;;     (is (= 0 (t/day09-part1-soln)))))
+(def d09-s01
+  (t/parse
+   ["R 4"
+    "U 4"
+    "L 3"
+    "D 1"
+    "R 4"
+    "D 1"
+    "L 5"
+    "R 2"]))
+
+(deftest distinct-tail-positions-test
+  (testing "Counts all the unique positions the tail of the rope occupied"
+    (is (= 13 (t/distinct-tail-positions d09-s01)))))
+
+(deftest day09-part1-soln
+  (testing "Reproduces the answer for day09, part1"
+    (is (= 5874 (t/day09-part1-soln)))))
 
 ;; (deftest day09-part2-soln
 ;;   (testing "Reproduces the answer for day09, part2"
