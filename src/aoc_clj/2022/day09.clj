287e200e73b853f653bcb7c5e9ea0f019d12f945
Ken Smith <ken.smith@2scientists.net>
2022-12-11 10:33:06 -0700

    2022 Day 09 part 2 soln

--- src/aoc_clj/2022/day09.clj
+++ src/aoc_clj/2022/day09.clj
@@ -8,59 +8,65 @@
   (let [[l r] (str/split line #" ")]
     [l (read-string r)]))
 
-(defn parse
-  [input]
-  (map parse-line input))
-
-(def d09-s01
-  (parse
-   ["R 4"
-    "U 4"
-    "L 3"
-    "D 1"
-    "R 4"
-    "D 1"
-    "L 5"
-    "R 2"]))
-
 (defn expand
   [input]
   (flatten (map #(repeat (second %) (first %)) input)))
 
+(defn parse
+  [input]
+  (expand (map parse-line input)))
+
 (def day09-input (parse (u/puzzle-input "2022/day09-input.txt")))
 
-(defn move-tail
+(def move->delta
+  {"R" [1 0]
+   "L" [-1 0]
+   "U" [0 1]
+   "D" [0 -1]})
+
+(defn cap
+  "Limit the size of the move to no more than one move in each direction."
+  [num]
+  (if (even? num)
+    (/ num 2)
+    num))
+
+(defn delta
+  "Compute the amount by which the tail position should change based
+   on the new position of the knot in front of it"
   [tail head]
-  (let [delta (mapv - head tail)]
-    (mapv + tail (case delta
-                   [-1  2] [-1  1]
-                   [0   2] [0   1]
-                   [1   2] [1   1]
-                   [-1 -2] [-1 -1]
-                   [0  -2] [0  -1]
-                   [1  -2] [1  -1]
-                   [2   1] [1   1]
-                   [2   0] [1   0]
-                   [2  -1] [1  -1]
-                   [-2  1] [-1  1]
-                   [-2  0] [-1  0]
-                   [-2 -1] [-1 -1]
-                   [0 0]))))
+  (let [d (mapv - head tail)]
+    (if (every? #(<= % 1) (map abs d))
+      [0 0]
+      (mapv cap d))))
+
+(defn move-tail
+  "Update the tail position based on the position of the knot in front of it"
+  [tail head]
+  (mapv + tail (delta tail head)))
 
 (defn step
-  [{:keys [tail head]} cmd]
-  (let [newhead (mapv + head (case cmd
-                               "R" [1 0]
-                               "L" [-1 0]
-                               "U" [0 1]
-                               "D" [0 -1]))]
-    {:head newhead :tail (move-tail tail newhead)}))
+  "Recursively update the location of the tail based on the new position
+   of the knot in front of it. When reaching the head, update the position
+   based on the move `cmd`"
+  [chain cmd]
+  (if (= 1 (count chain))
+    (vector (mapv + (first chain) (move->delta cmd)))
+    (let [newchain (step (rest chain) cmd)]
+      (cons (move-tail (first chain) (first newchain)) newchain))))
+
+(defn all-moves
+  "Returns a seq of all of the states of the chain after each move
+   in `moves` has been applied"
+  [chain-length moves]
+  (reductions step (repeat chain-length [0 0]) moves))
 
 (defn distinct-tail-positions
-  [input]
-  (->> (expand input)
-       (reductions step {:head [0 0] :tail [0 0]})
-       (map :tail)
+  "Computes how many unique positions the tail of the chain of length
+   `chain-length` and the head moves in `moves`"
+  [chain-length moves]
+  (->> (all-moves chain-length moves)
+       (map first)
        distinct
        count))
 
@@ -68,4 +74,11 @@
   "Simulate your complete hypothetical series of motions. How many positions 
    does the tail of the rope visit at least once?"
   []
-  (distinct-tail-positions day09-input))
+  (distinct-tail-positions 2 day09-input))
+
+(defn day09-part2-soln
+  "Simulate your complete series of motions on a larger rope with ten knots. 
+   How many positions does the tail of the rope visit at least once?"
+  []
+  (distinct-tail-positions 10 day09-input))
+
--- test/aoc_clj/2022/day09_test.clj
+++ test/aoc_clj/2022/day09_test.clj
@@ -13,14 +13,37 @@
     "L 5"
     "R 2"]))
 
+(def d09-s02
+  (t/parse
+   ["R 5"
+    "U 8"
+    "L 8"
+    "D 3"
+    "R 17"
+    "D 10"
+    "L 25"
+    "U 20"]))
+
+(deftest all-moves-test
+  (testing "The chain ends in the expected final state"
+    (is (= [[1 2] [2 2]]
+           (last (t/all-moves 2 d09-s01))))
+    (is (= [[0 0] [0 0] [0 0] [0 0] [1 1] [2 2] [3 2] [2 2] [1 2] [2 2]]
+           (last (t/all-moves 10 d09-s01))))
+    (is (= [[-11 6] [-11 7] [-11 8] [-11 9] [-11 10]
+            [-11 11] [-11 12] [-11 13] [-11 14] [-11 15]]
+           (last (t/all-moves 10 d09-s02))))))
+
 (deftest distinct-tail-positions-test
   (testing "Counts all the unique positions the tail of the rope occupied"
-    (is (= 13 (t/distinct-tail-positions d09-s01)))))
+    (is (= 13 (t/distinct-tail-positions 2 d09-s01)))
+    (is (= 1  (t/distinct-tail-positions 10 d09-s01)))
+    (is (= 36 (t/distinct-tail-positions 10 d09-s02)))))
 
 (deftest day09-part1-soln
   (testing "Reproduces the answer for day09, part1"
     (is (= 5874 (t/day09-part1-soln)))))
 
-;; (deftest day09-part2-soln
-;;   (testing "Reproduces the answer for day09, part2"
-;;     (is (= 0 (t/day09-part2-soln)))))
+(deftest day09-part2-soln
+  (testing "Reproduces the answer for day09, part2"
+    (is (= 2467 (t/day09-part2-soln)))))
