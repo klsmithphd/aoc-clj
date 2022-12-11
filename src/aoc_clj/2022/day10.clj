eecff9d215a497e36cd3df9778ce40f2bde359f9
Ken Smith <ken.smith@2scientists.net>
2022-12-11 11:56:25 -0700

    2022 Day 10 solutions

--- src/aoc_clj/2022/day10.clj
+++ src/aoc_clj/2022/day10.clj
@@ -1,5 +1,89 @@
-;; (ns aoc-clj.2022.day10
-;;  "Solution to https://adventofcode.com/2022/day/10"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day10
+  "Solution to https://adventofcode.com/2022/day/10"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day10-input (u/puzzle-input "2022/day10-input.txt"))
+(defn parse-line
+  [line]
+  (if (= "noop" line)
+    :noop
+    (read-string (subs line 5))))
+
+(defn parse
+  [input]
+  (map parse-line input))
+
+(def day10-input (parse (u/puzzle-input "2022/day10-input.txt")))
+
+(defn op->change
+  "addx V takes two cycles to complete. After two cycles, the X register
+   is increased by the value V. (V can be negative.)
+   noop takes one cycle to complete. It has no other effect."
+  [op]
+  (if (= :noop op)
+    [0]
+    [0 op]))
+
+(defn collapse
+  "Convert the input instructions into the timed changes they will make."
+  [input]
+  (mapcat op->change input))
+
+(defn register-values
+  "Starting with the X register with the value 1, return a seq of the 
+   X register values at each tick of the CPU"
+  [input]
+  (reductions + 1 (collapse input)))
+
+(defn sampled-signal-strength-sums
+  "consider the signal strength (the cycle number multiplied by the 
+   value of the X register) during the 20th cycle and every 40 cycles 
+   after that (that is, during the 20th, 60th, 100th, 140th, 180th, 
+   and 220th cycles)"
+  [input]
+  (->> (register-values input)
+       (drop 19)
+       (take-nth 40)
+       (map * [20 60 100 140 180 220])
+       (reduce +)))
+
+(defn light
+  "Determine whether to light up the screen at the pixel location
+   given the value of the X register"
+  [pixel register]
+  (if (<= (abs (- register pixel)) 1)
+    "#"
+    "."))
+
+(defn screen
+  "Return a string representation of the CRT screen based on the 
+   programmed instructions"
+  [input]
+  (->>
+   (map light
+        (flatten (repeat 6 (range 40)))
+        (register-values input))
+   (partition 40)
+   (map str/join)
+   (str/join "\n")))
+
+(defn day10-part1-soln
+  "Find the signal strength during the 20th, 60th, 100th, 140th, 180th, 
+   and 220th cycles. What is the sum of these six signal strengths?"
+  []
+  (sampled-signal-strength-sums day10-input))
+
+(defn day10-part2-soln
+  "Render the image given by your program. 
+   What eight capital letters appear on your CRT?"
+  []
+  "ECZUZALR")
+
+(comment
+  (print (screen day10-input))
+  "####  ##  #### #  # ####  ##  #    ###  
+   #    #  #    # #  #    # #  # #    #  # 
+   ###  #      #  #  #   #  #  # #    #  # 
+   #    #     #   #  #  #   #### #    ###  
+   #    #  # #    #  # #    #  # #    # #  
+   ####  ##  ####  ##  #### #  # #### #  #")
--- test/aoc_clj/2022/day10_test.clj
+++ test/aoc_clj/2022/day10_test.clj
@@ -1,11 +1,186 @@
-;; (ns aoc-clj.2022.day10-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day10 :as t]))
+(ns aoc-clj.2022.day10-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day10 :as t]))
 
-;; (deftest day10-part1-soln
-;;   (testing "Reproduces the answer for day10, part1"
-;;     (is (= 0 (t/day10-part1-soln)))))
+(def d10-01
+  (t/parse
+   ["noop"
+    "addx 3"
+    "addx -5"]))
 
-;; (deftest day10-part2-soln
-;;   (testing "Reproduces the answer for day10, part2"
-;;     (is (= 0 (t/day10-part2-soln)))))
+(def d10-02
+  (t/parse
+   ["addx 15"
+    "addx -11"
+    "addx 6"
+    "addx -3"
+    "addx 5"
+    "addx -1"
+    "addx -8"
+    "addx 13"
+    "addx 4"
+    "noop"
+    "addx -1"
+    "addx 5"
+    "addx -1"
+    "addx 5"
+    "addx -1"
+    "addx 5"
+    "addx -1"
+    "addx 5"
+    "addx -1"
+    "addx -35"
+    "addx 1"
+    "addx 24"
+    "addx -19"
+    "addx 1"
+    "addx 16"
+    "addx -11"
+    "noop"
+    "noop"
+    "addx 21"
+    "addx -15"
+    "noop"
+    "noop"
+    "addx -3"
+    "addx 9"
+    "addx 1"
+    "addx -3"
+    "addx 8"
+    "addx 1"
+    "addx 5"
+    "noop"
+    "noop"
+    "noop"
+    "noop"
+    "noop"
+    "addx -36"
+    "noop"
+    "addx 1"
+    "addx 7"
+    "noop"
+    "noop"
+    "noop"
+    "addx 2"
+    "addx 6"
+    "noop"
+    "noop"
+    "noop"
+    "noop"
+    "noop"
+    "addx 1"
+    "noop"
+    "noop"
+    "addx 7"
+    "addx 1"
+    "noop"
+    "addx -13"
+    "addx 13"
+    "addx 7"
+    "noop"
+    "addx 1"
+    "addx -33"
+    "noop"
+    "noop"
+    "noop"
+    "addx 2"
+    "noop"
+    "noop"
+    "noop"
+    "addx 8"
+    "noop"
+    "addx -1"
+    "addx 2"
+    "addx 1"
+    "noop"
+    "addx 17"
+    "addx -9"
+    "addx 1"
+    "addx 1"
+    "addx -3"
+    "addx 11"
+    "noop"
+    "noop"
+    "addx 1"
+    "noop"
+    "addx 1"
+    "noop"
+    "noop"
+    "addx -13"
+    "addx -19"
+    "addx 1"
+    "addx 3"
+    "addx 26"
+    "addx -30"
+    "addx 12"
+    "addx -1"
+    "addx 3"
+    "addx 1"
+    "noop"
+    "noop"
+    "noop"
+    "addx -9"
+    "addx 18"
+    "addx 1"
+    "addx 2"
+    "noop"
+    "noop"
+    "addx 9"
+    "noop"
+    "noop"
+    "noop"
+    "addx -1"
+    "addx 2"
+    "addx -37"
+    "addx 1"
+    "addx 3"
+    "noop"
+    "addx 15"
+    "addx -21"
+    "addx 22"
+    "addx -6"
+    "addx 1"
+    "noop"
+    "addx 2"
+    "addx 1"
+    "noop"
+    "addx -10"
+    "noop"
+    "noop"
+    "addx 20"
+    "addx 1"
+    "addx 2"
+    "addx 2"
+    "addx -6"
+    "addx -11"
+    "noop"
+    "noop"
+    "noop"]))
+
+(def d10-02-screen
+  "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....")
+
+(deftest register-values-test
+  (testing "Computes the register value over time given the instructions"
+    (is (= [1 1 1 4 4 -1] (t/register-values d10-01)))))
+
+(deftest sampled-signal-strength-sums-test
+  (testing "Computes the sum of the signal strengths sampled every 40 ticks"
+    (is (= 13140 (t/sampled-signal-strength-sums d10-02)))))
+
+(deftest screen-test
+  (testing "The screen lighting logic produces the test pattern"
+    (is (= d10-02-screen (t/screen d10-02)))))
+
+(deftest day10-part1-soln
+  (testing "Reproduces the answer for day10, part1"
+    (is (= 16020 (t/day10-part1-soln)))))
+
+(deftest day10-part2-soln
+  (testing "Reproduces the answer for day10, part2"
+    (is (= "ECZUZALR" (t/day10-part2-soln)))))
