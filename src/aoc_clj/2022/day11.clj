a31dba3c6ceec020b89c573280e1e304ecd4ffac
Ken Smith <ken.smith@2scientists.net>
2022-12-11 14:32:42 -0700

    2022 Day 11 part 1

--- src/aoc_clj/2022/day11.clj
+++ src/aoc_clj/2022/day11.clj
@@ -1,5 +1,133 @@
-;; (ns aoc-clj.2022.day11
-;;  "Solution to https://adventofcode.com/2022/day/11"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day11
+  "Solution to https://adventofcode.com/2022/day/11"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day11-input (u/puzzle-input "2022/day11-input.txt"))
+(def start-str-len  (count "  Starting items: "))
+(def oper-str-len   (count "  Operation: new = old "))
+(def test-str-len   (count "  Test: divisible by "))
+(def true-str-len   (count "    If true: throw to monkey "))
+(def false-str-len  (count "    If false: throw to monkey "))
+
+(defn parse-num
+  [s skip-length]
+  (read-string (subs s skip-length)))
+
+(defn parse-items [s] (u/firstv [(subs s start-str-len)]))
+(defn parse-test  [s] (parse-num s test-str-len))
+(defn parse-true  [s] (parse-num s true-str-len))
+(defn parse-false [s] (parse-num s false-str-len))
+(defn parse-oper
+  [s]
+  (let [op      (subs s oper-str-len)
+        safe-op (str (subs op 0 1) "' " (subs op 1))]
+    (-> (str "(fn [old] (" safe-op " old))")
+        read-string
+        eval)))
+
+(defn parse-monkey
+  [[_ items oper test true-op false-op]]
+  {:counts    0
+   :items     (parse-items items)
+   :operation (parse-oper oper)
+   :test      (parse-test test)
+   :true-op   (parse-true true-op)
+   :false-op  (parse-false false-op)})
+
+(defn parse
+  [input]
+  (mapv parse-monkey (u/split-at-blankline input)))
+
+(def day11-input (parse (u/puzzle-input "2022/day11-input.txt")))
+
+(def d11-s01
+  (parse
+   ["Monkey 0:"
+    "  Starting items: 79, 98"
+    "  Operation: new = old * 19"
+    "  Test: divisible by 23"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 3"
+    ""
+    "Monkey 1:"
+    "  Starting items: 54, 65, 75, 74"
+    "  Operation: new = old + 6"
+    "  Test: divisible by 19"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 0"
+    ""
+    "Monkey 2:"
+    "  Starting items: 79, 60, 97"
+    "  Operation: new = old * old"
+    "  Test: divisible by 13"
+    "    If true: throw to monkey 1"
+    "    If false: throw to monkey 3"
+    ""
+    "Monkey 3:"
+    "  Starting items: 74"
+    "  Operation: new = old + 3"
+    "  Test: divisible by 17"
+    "    If true: throw to monkey 0"
+    "    If false: throw to monkey 1"]))
+
+(defn monkey-do
+  ([monkeys monkey-id]
+   (monkey-do false monkeys monkey-id))
+  ([worry? monkeys monkey-id]
+   (let [monkey (get monkeys monkey-id)
+         {:keys [items operation test true-op false-op]} monkey
+         item   (first items)
+         foo    (try (operation item)
+                     (catch Exception e (println monkey-id item)))
+         worry  (if worry? foo (quot foo 3))
+         dest   (if (zero? (rem worry test))
+                  true-op
+                  false-op)]
+     (-> monkeys
+         (update-in [monkey-id :items] (comp vec rest))
+         (update-in [monkey-id :counts] inc)
+         (update-in [dest :items] conj worry)))))
+
+(defn monkey-turn
+  ([monkeys monkey-id]
+   (monkey-turn false monkeys monkey-id))
+  ([worry? monkeys monkey-id]
+   (loop [ms monkeys]
+     (if (empty? (get-in ms [monkey-id :items]))
+       ms
+       (recur (monkey-do worry? ms monkey-id))))))
+
+(defn round
+  [monkeys]
+  (reduce monkey-turn monkeys (range (count monkeys))))
+
+(defn round-part2
+  [monkeys]
+  (reduce (partial monkey-turn true) monkeys (range (count monkeys))))
+
+(defn items
+  [monkeys]
+  (map :items monkeys))
+
+(defn counts
+  [monkeys]
+  (map :counts monkeys))
+
+(defn monkey-business
+  [round-fn monkeys rounds]
+  (let [cnts (-> (iterate round-fn monkeys)
+                 (nth rounds)
+                 counts)]
+    (->> (sort > cnts)
+         (take 2)
+         (reduce *))))
+
+(def monkey-business-1 (partial monkey-business round))
+(def monkey-business-2 (partial monkey-business round-part2))
+
+(defn day11-part1-soln
+  "What is the level of monkey business after 20 rounds of stuff-slinging 
+   simian shenanigans?"
+  []
+  (monkey-business-1 day11-input 20))
+
--- test/aoc_clj/2022/day11_test.clj
+++ test/aoc_clj/2022/day11_test.clj
@@ -1,10 +1,89 @@
-;; (ns aoc-clj.2022.day11-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day11 :as t]))
+(ns aoc-clj.2022.day11-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day11 :as t]))
 
-;; (deftest day11-part1-soln
-;;   (testing "Reproduces the answer for day11, part1"
-;;     (is (= 0 (t/day11-part1-soln)))))
+(def d11-s01
+  (t/parse
+   ["Monkey 0:"
+    "  Starting items: 79, 98"
+    "  Operation: new = old * 19"
+    "  Test: divisible by 23"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 3"
+    ""
+    "Monkey 1:"
+    "  Starting items: 54, 65, 75, 74"
+    "  Operation: new = old + 6"
+    "  Test: divisible by 19"
+    "    If true: throw to monkey 2"
+    "    If false: throw to monkey 0"
+    ""
+    "Monkey 2:"
+    "  Starting items: 79, 60, 97"
+    "  Operation: new = old * old"
+    "  Test: divisible by 13"
+    "    If true: throw to monkey 1"
+    "    If false: throw to monkey 3"
+    ""
+    "Monkey 3:"
+    "  Starting items: 74"
+    "  Operation: new = old + 3"
+    "  Test: divisible by 17"
+    "    If true: throw to monkey 0"
+    "    If false: throw to monkey 1"]))
+
+(deftest round-test
+  (testing "Monkeys end up with the right items after each round"
+    (is (= [[20 23 27 26]
+            [2080 25 167 207 401 1046]
+            []
+            []]
+           (t/items (t/round d11-s01))))
+    (is (= [[695 10 71 135 350]
+            [43 49 58 55 362]
+            []
+            []]
+           (t/items (t/round (t/round d11-s01)))))
+    (is (= [[83, 44, 8, 184, 9, 20, 26, 102]
+            [110, 36]
+            []
+            []]
+           (t/items (nth (iterate t/round d11-s01) 15))))
+    (is (= [[10, 12, 14, 26, 34]
+            [245, 93, 53, 199, 115]
+            []
+            []]
+           (t/items (nth (iterate t/round d11-s01) 20))))))
+
+(deftest counts-test
+  (testing "Counts the number of items inspected by each monkey"
+    (is (= [101 95 7 105]
+           (t/counts (nth (iterate t/round d11-s01) 20))))
+    ;; part 2
+    (is (= [2 4 3 6]
+           (t/counts (nth (iterate t/round-part2 d11-s01) 1))))
+    (is (= [99 97 8 103]
+           (t/counts (nth (iterate t/round-part2 d11-s01) 20))))
+    ;; Currently too prohibitively expensive to test
+    ;; (is (= [5204 1792 199 5192]
+    ;;        (t/counts (nth (iterate t/round-part2 d11-s01) 1000))))
+    ;; (is (= [26075 23921 974 26000]
+    ;;        (t/counts (nth (iterate t/round-part2 d11-s01) 5000))))
+    ;; (is (= [52166 47830 1938 52013]
+    ;;        (t/counts (nth (iterate t/round-part2 d11-s01) 10000))))
+    ))
+
+(deftest monkey-business-test
+  (testing "Computes the monkey business score (product of top two 
+            inspection counts)"
+    (is (= 10605 (t/monkey-business-1 d11-s01 20)))
+    ;; Currently too prohibitively expensive to test
+    ;; (is (= 2713310158 (t/monkey-business-2 d11-s01 10000)))
+    ))
+
+(deftest day11-part1-soln
+  (testing "Reproduces the answer for day11, part1"
+    (is (= 112221 (t/day11-part1-soln)))))
 
 ;; (deftest day11-part2-soln
 ;;   (testing "Reproduces the answer for day11, part2"
