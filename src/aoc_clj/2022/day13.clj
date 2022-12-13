770f382528453aec8428f4abebabcbf8927badc6
Ken Smith <ken.smith@2scientists.net>
2022-12-13 07:56:00 -0700

    2022 Day13 passes tests, but wrong answer

--- src/aoc_clj/2022/day13.clj
+++ src/aoc_clj/2022/day13.clj
@@ -1,5 +1,61 @@
-;; (ns aoc-clj.2022.day13
-;;  "Solution to https://adventofcode.com/2022/day/13"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day13
+  "Solution to https://adventofcode.com/2022/day/13"
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day13-input (u/puzzle-input "2022/day13-input.txt"))
+(defn parse
+  [input]
+  (map (partial mapv read-string)
+       (u/split-at-blankline input)))
+
+(def day13-input (parse (u/puzzle-input "2022/day13-input.txt")))
+
+(declare in-order?)
+(defn in-order-int?
+  [a b]
+  (cond
+    (> a b) false
+    (< a b) true
+    (= a b) :noop))
+
+(defn in-order-vector?
+  [a b]
+  (let [in-order-check (map in-order? a b)
+        some-false? (some false? in-order-check)
+        some-true?  (some true? in-order-check)]
+    (if some-false?
+      false
+      (if some-true?
+        true
+        (let [lena (count a)
+              lenb (count b)]
+          (cond
+            (> lena lenb) false
+            (< lena lenb) true
+            (= lena lenb) :noop))))))
+
+(defn in-order?
+  [a b]
+  (cond
+    (and (number? a) (number? b)) (in-order-int? a b)
+    (and (number? a) (vector? b)) (in-order? (vector a) b)
+    (and (vector? a) (number? b)) (in-order? a (vector b))
+    (and (vector? a) (vector? b)) (in-order-vector? a b)))
+
+(defn right-order-packet-id-sum
+  [input]
+  (->>
+   (map-indexed
+    (fn [a b] [(inc a) (apply in-order? b)])
+    input)
+   (filter second)
+   (map first)
+   (reduce +)))
+
+(defn day13-part1-soln
+  "Determine which pairs of packets are already in the right order. 
+   What is the sum of the indices of those pairs?"
+  []
+  (right-order-packet-id-sum day13-input))
+
+(count day13-input)
+(count (filter true? (map (partial apply in-order?) day13-input)))
--- test/aoc_clj/2022/day13_test.clj
+++ test/aoc_clj/2022/day13_test.clj
@@ -1,6 +1,47 @@
-;; (ns aoc-clj.2022.day13-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day13 :as t]))
+(ns aoc-clj.2022.day13-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day13 :as t]))
+
+(def d13-s01
+  (t/parse
+   ["[1,1,3,1,1]"
+    "[1,1,5,1,1]"
+    ""
+    "[[1],[2,3,4]]"
+    "[[1],4]"
+    ""
+    "[9]"
+    "[[8,7,6]]"
+    ""
+    "[[4,4],4,4]"
+    "[[4,4],4,4,4]"
+    ""
+    "[7,7,7,7]"
+    "[7,7,7]"
+    ""
+    "[]"
+    "[3]"
+    ""
+    "[[[]]]"
+    "[[]]"
+    ""
+    "[1,[2,[3,[4,[5,6,7]]]],8,9]"
+    "[1,[2,[3,[4,[5,6,0]]]],8,9]"]))
+
+(deftest in-order?-test
+  (testing "Follows the correct in-order logic for the sample data"
+    (is (true?  (apply t/in-order? (nth d13-s01 0))))
+    (is (true?  (apply t/in-order? (nth d13-s01 1))))
+    (is (false? (apply t/in-order? (nth d13-s01 2))))
+    (is (true?  (apply t/in-order? (nth d13-s01 3))))
+    (is (false? (apply t/in-order? (nth d13-s01 4))))
+    (is (true?  (apply t/in-order? (nth d13-s01 5))))
+    (is (false? (apply t/in-order? (nth d13-s01 6))))
+    (is (false? (apply t/in-order? (nth d13-s01 7))))))
+
+(deftest right-order-packet-id-sum-test
+  (testing "The sum of the packet pair ids for in-order packets"
+    (is (= 13 (t/right-order-packet-id-sum d13-s01)))))
 
 ;; (deftest day13-part1-soln
 ;;   (testing "Reproduces the answer for day13, part1"
