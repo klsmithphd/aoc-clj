575f13299f14c909c43460fb10e5986a3b395612
Ken Smith <ken.smith@2scientists.net>
2022-12-18 16:42:18 -0700

    WIP on 2022 day16

--- src/aoc_clj/2022/day16.clj
+++ src/aoc_clj/2022/day16.clj
@@ -1,6 +1,8 @@
 (ns aoc-clj.2022.day16
   "Solution to https://adventofcode.com/2022/day/16"
-  (:require [aoc-clj.utils.core :as u]))
+  (:require [clojure.math.combinatorics :as combo]
+            [aoc-clj.utils.graph :as graph :refer [->MapGraph]]
+            [aoc-clj.utils.core :as u]))
 
 (defn parse-line
   [line]
@@ -28,3 +30,37 @@
     "Valve JJ has flow rate=21; tunnel leads to valve II"]))
 
 (def day16-input (parse (sort (u/puzzle-input "2022/day16-input.txt"))))
+
+(defn edges
+  [{:keys [valve tunnels]}]
+  {valve (zipmap tunnels (repeat 1))})
+
+(apply merge (map edges d16-s01))
+
+(defn raw-graph
+  [input]
+  (->MapGraph (apply merge (map edges input))))
+
+(defn keeper?
+  [{:keys [valve flow]}]
+  (or (= "AA" valve) (pos? flow)))
+
+(defn valves
+  [input]
+  (let [nodes (filter keeper? input)]
+    (into {} (map #(vector (:valve %) (:flow %)) nodes))))
+
+(defn simpler-graph
+  "Reduce the graph of all possible rooms to just those that have
+   non-zero valve values"
+  [input]
+  (let [g      (raw-graph input)
+        valves (valves input)
+        pairs  (combo/combinations (keys valves) 2)
+        dists  (map #(dec (count (apply graph/dijkstra g %))) pairs)
+        pairs2 (concat pairs (map reverse pairs))]
+    (->> (map #(conj (vec %1) %2) pairs2 (cycle dists))
+         (group-by first)
+         (u/fmap #(into {} (map (comp vec rest)) %))
+         ->MapGraph)))
+
