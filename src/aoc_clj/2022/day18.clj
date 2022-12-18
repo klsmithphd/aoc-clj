7aa70154ae1db422714d895485dc27f5fb36b4ce
Ken Smith <ken.smith@2scientists.net>
2022-12-18 15:11:42 -0700

    2022 Day18 part 1 soln

--- src/aoc_clj/2022/day18.clj
+++ src/aoc_clj/2022/day18.clj
@@ -1,5 +1,46 @@
-;; (ns aoc-clj.2022.day18
-;;  "Solution to https://adventofcode.com/2022/day/18"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day18
+  "Solution to https://adventofcode.com/2022/day/18"
+  (:require [aoc-clj.utils.grid :as grid]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day18-input (u/puzzle-input "2022/day18-input.txt"))
+(defn parse
+  [input]
+  (map u/str->vec input))
+
+(def d18-s01
+  [[1 1 1]
+   [2 1 1]])
+
+(def d18-s02
+  [[2 2 2]
+   [1 2 2]
+   [3 2 2]
+   [2 1 2]
+   [2 3 2]
+   [2 2 1]
+   [2 2 3]
+   [2 2 4]
+   [2 2 6]
+   [1 2 5]
+   [3 2 5]
+   [2 1 5]
+   [2 3 5]])
+
+(def day18-input (parse (u/puzzle-input "2022/day18-input.txt")))
+
+(defn shared-face-count
+  [cubes]
+  (count (filter (set (grid/adj-coords-3d (first cubes))) (rest cubes))))
+
+(defn surface-area
+  "Count the number of sides of each cube that are not immediately 
+   connected to another cube."
+  [cubes]
+  (let [faces (->> (u/rotations cubes)
+                   (map shared-face-count)
+                   (reduce +))]
+    (- (* 6 (count cubes)) faces)))
+
+(defn day18-part1-soln
+  []
+  (surface-area day18-input))
--- test/aoc_clj/2022/day18_test.clj
+++ test/aoc_clj/2022/day18_test.clj
@@ -1,10 +1,34 @@
-;; (ns aoc-clj.2022.day18-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day18 :as t]))
+(ns aoc-clj.2022.day18-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day18 :as t]))
 
-;; (deftest day18-part1-soln
-;;   (testing "Reproduces the answer for day18, part1"
-;;     (is (= 0 (t/day18-part1-soln)))))
+(def d18-s01
+  [[1 1 1]
+   [2 1 1]])
+
+(def d18-s02
+  [[2 2 2]
+   [1 2 2]
+   [3 2 2]
+   [2 1 2]
+   [2 3 2]
+   [2 2 1]
+   [2 2 3]
+   [2 2 4]
+   [2 2 6]
+   [1 2 5]
+   [3 2 5]
+   [2 1 5]
+   [2 3 5]])
+
+(deftest surface-area-test
+  (testing "Counts the number of exposed (non-adjacent) sides of the cubes"
+    (is (= 10 (t/surface-area d18-s01)))
+    (is (= 64 (t/surface-area d18-s02)))))
+
+(deftest day18-part1-soln
+  (testing "Reproduces the answer for day18, part1"
+    (is (= 33900 (t/day18-part1-soln)))))
 
 ;; (deftest day18-part2-soln
 ;;   (testing "Reproduces the answer for day18, part2"
