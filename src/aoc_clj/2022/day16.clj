455aec5af6a1c4118d5be3de708e0a7de06a586f
Ken Smith <ken.smith@2scientists.net>
2022-12-18 11:10:22 -0700

    Parsing logic for 2022 day16 input

--- src/aoc_clj/2022/day16.clj
+++ src/aoc_clj/2022/day16.clj
@@ -1,5 +1,30 @@
-;; (ns aoc-clj.2022.day16
-;;  "Solution to https://adventofcode.com/2022/day/16"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day16
+  "Solution to https://adventofcode.com/2022/day/16"
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day16-input (u/puzzle-input "2022/day16-input.txt"))
+(defn parse-line
+  [line]
+  (let [valves (re-seq #"[A-Z]{2}" line)
+        flow   (read-string (re-find #"\d+" line))]
+    {:valve (first valves)
+     :flow  flow
+     :tunnels (rest valves)}))
+
+(defn parse
+  [input]
+  (map parse-line input))
+
+(def d16-s01
+  (parse
+   ["Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
+    "Valve BB has flow rate=13; tunnels lead to valves CC, AA"
+    "Valve CC has flow rate=2; tunnels lead to valves DD, BB"
+    "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE"
+    "Valve EE has flow rate=3; tunnels lead to valves FF, DD"
+    "Valve FF has flow rate=0; tunnels lead to valves EE, GG"
+    "Valve GG has flow rate=0; tunnels lead to valves FF, HH"
+    "Valve HH has flow rate=22; tunnel leads to valve GG"
+    "Valve II has flow rate=0; tunnels lead to valves AA, JJ"
+    "Valve JJ has flow rate=21; tunnel leads to valve II"]))
+
+(def day16-input (parse (sort (u/puzzle-input "2022/day16-input.txt"))))
--- test/aoc_clj/2022/day16_test.clj
+++ test/aoc_clj/2022/day16_test.clj
@@ -1,6 +1,19 @@
-;; (ns aoc-clj.2022.day16-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day16 :as t]))
+(ns aoc-clj.2022.day16-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day16 :as t]))
+
+(def d16-s01
+  (t/parse
+   ["Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"
+    "Valve BB has flow rate=13; tunnels lead to valves CC, AA"
+    "Valve CC has flow rate=2; tunnels lead to valves DD, BB"
+    "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE"
+    "Valve EE has flow rate=3; tunnels lead to valves FF, DD"
+    "Valve FF has flow rate=0; tunnels lead to valves EE, GG"
+    "Valve GG has flow rate=0; tunnels lead to valves FF, HH"
+    "Valve HH has flow rate=22; tunnel leads to valve GG"
+    "Valve II has flow rate=0; tunnels lead to valves AA, JJ"
+    "Valve JJ has flow rate=21; tunnel leads to valve II"]))
 
 ;; (deftest day16-part1-soln
 ;;   (testing "Reproduces the answer for day16, part1"
