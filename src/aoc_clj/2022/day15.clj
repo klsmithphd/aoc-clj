918dd64616b9da00eb58aaf1abc0a23f75116471
Ken Smith <ken.smith@2scientists.net>
2022-12-18 09:31:21 -0700

    Most of the pieces of 2022 d15 part2

--- src/aoc_clj/2022/day15.clj
+++ src/aoc_clj/2022/day15.clj
@@ -1,6 +1,7 @@
 (ns aoc-clj.2022.day15
   "Solution to https://adventofcode.com/2022/day/15"
-  (:require [aoc-clj.utils.intervals :as ivs]
+  (:require [clojure.math.combinatorics :as combo]
+            [aoc-clj.utils.intervals :as ivs]
             [aoc-clj.utils.math :as math]
             [aoc-clj.utils.core :as u]))
 
@@ -58,6 +59,60 @@
     (- (reduce + (map #(- (inc (second %)) (first %)) intervals))
        beacons-on-line)))
 
+(defn sensor-gap
+  [s1 s2]
+  (- (math/manhattan (:sensor s1) (:sensor s2))
+     (:radius s1)
+     (:radius s2)))
+
+(defn sensor-overlap?
+  [s1 s2]
+  (not (pos? (sensor-gap s1 s2))))
+
+(defn rectangle?
+  [s1 s2 s3 s4]
+  (and (sensor-overlap? s1 s2)
+       (sensor-overlap? s2 s3)
+       (sensor-overlap? s3 s4)
+       (sensor-overlap? s4 s1)
+       (= 2 (sensor-gap s1 s3))
+       (= 2 (sensor-gap s2 s4))))
+
+(defn find-bounding-sensors
+  [sensors]
+  (->>
+   (combo/permuted-combinations sensors 4)
+   (filter #(apply rectangle? %))
+   (map #(map :sensor %))
+   (map sort)
+   distinct))
+
+(defn bounding-direction
+  [[x1 y1] [x2 y2]]
+  (if (> x2 x1)
+    (if (> y2 y1)
+      :ne
+      :se)
+    (if (> y2 y1)
+      :nw
+      :sw)))
+
+(defn gap-position
+  "s1 is the bottom-right rectangle adjacent to the gap
+   s2 is the bottom-left rectangle adjacent to the gap"
+  [s1 s2]
+  (let [[sx1 sy1] (:sensor s1)
+        [sx2 sy2] (:sensor s2)
+        r1        (:radius s1)
+        r2        (:radius s2)
+        x (/ (+ (+ sx1 sx2) (- sy1 sy2) (- r2 r1)) 2)
+        y (+ (- sx1 x) sy1 (- (inc r1)))]
+    [x y]))
+
+(gap-position {:sensor [20 14] :radius 8} {:sensor [12 14] :radius 4})
+
+
+
 (defn tuning-frequency
   "The tuning frequency can be found by multiplying its x coordinate by 4000000
    and then adding its y coordinate."
@@ -69,4 +124,10 @@
    y=2000000, how many positions cannot contain a beacon?"
   []
   (no-beacon-points-in-line day15-input 2000000))
+
+(defn day15-part2-soln
+  "Find the only possible position for the distress beacon. 
+   What is its tuning frequency?"
+  []
+  12)
 
