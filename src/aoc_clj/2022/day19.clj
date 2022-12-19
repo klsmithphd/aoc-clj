b6411b8d00dec5d6763211600e2ff8497eb920b9
Ken Smith <ken.smith@2scientists.net>
2022-12-19 06:05:03 -0700

    Parser for 2022 day19

--- src/aoc_clj/2022/day19.clj
+++ src/aoc_clj/2022/day19.clj
@@ -1,5 +1,23 @@
-;; (ns aoc-clj.2022.day19
-;;  "Solution to https://adventofcode.com/2022/day/19"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day19
+  "Solution to https://adventofcode.com/2022/day/19"
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day19-input (u/puzzle-input "2022/day19-input.txt"))
+(defn parse-line
+  [line]
+  (let [[a b c d e f g] (map read-string (re-seq #"\d+" line))]
+    [a {:ore      {:ore b}
+        :clay     {:ore c}
+        :obsidian {:ore d :clay e}
+        :geode    {:ore f :obsidian g}}]))
+
+(defn parse
+  [input]
+  (into {} (map parse-line input)))
+
+(def d19-s01
+  (parse
+   ["Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian."
+    "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."]))
+
+(def day19-input (parse (u/puzzle-input "2022/day19-input.txt")))
+(def time-limit 24)
