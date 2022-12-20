14c53ea0c5b69432b4211dec7687ebd5472efe3f
Ken Smith <ken.smith@2scientists.net>
2022-12-20 16:47:58 -0700

    2022 Day20 parsing

--- src/aoc_clj/2022/day20.clj
+++ src/aoc_clj/2022/day20.clj
@@ -1,5 +1,28 @@
-;; (ns aoc-clj.2022.day20
-;;  "Solution to https://adventofcode.com/2022/day/20"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day20
+  "Solution to https://adventofcode.com/2022/day/20"
+  (:require [aoc-clj.utils.core :as u]))
 
-;; (def day20-input (u/puzzle-input "2022/day20-input.txt"))
+(defn parse
+  [input]
+  (map read-string input))
+
+(def d20-s01
+  (parse
+   ["1"
+    "2"
+    "-3"
+    "3"
+    "-2"
+    "0"
+    "4"]))
+
+(def day20-input (parse (u/puzzle-input "2022/day20-input.txt")))
+
+(defn grove-coordinates
+  [nums]
+  (let [n (count nums)
+        zero-pos (u/index-of 0 nums)]
+    (+ (nth nums (mod (+ zero-pos 1000) n))
+       (nth nums (mod (+ zero-pos 2000) n))
+       (nth nums (mod (+ zero-pos 3000) n)))))
+
--- test/aoc_clj/2022/day20_test.clj
+++ test/aoc_clj/2022/day20_test.clj
@@ -1,6 +1,13 @@
-;; (ns aoc-clj.2022.day20-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day20 :as t]))
+(ns aoc-clj.2022.day20-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day20 :as t]))
+
+(def d20-s01       [1 2 -3 3 -2 0 4])
+(def d20-s01-mixed [1 2 -3 4 0 3 -2])
+
+(deftest grove-coordinates-test
+  (testing "Computes the `grove coordinates`"
+    (is (= 3 (t/grove-coordinates d20-s01-mixed)))))
 
 ;; (deftest day20-part1-soln
 ;;   (testing "Reproduces the answer for day20, part1"
