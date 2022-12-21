140fb2dcda0d8de00b24e7e7a592e818679c3008
Ken Smith <ken.smith@2scientists.net>
2022-12-21 06:38:29 -0700

    2022 Day 21 part 1 soln

--- src/aoc_clj/2022/day21.clj
+++ src/aoc_clj/2022/day21.clj
@@ -1,5 +1,48 @@
-;; (ns aoc-clj.2022.day21
-;;  "Solution to https://adventofcode.com/2022/day/21"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day21
+  "Solution to https://adventofcode.com/2022/day/21"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
-;; (def day21-input (u/puzzle-input "2022/day21-input.txt"))
+(defn parse-line
+  [line]
+  (let [[l r] (str/split line #": ")
+        args  (str/split r #" ")]
+    [l (if (= 1 (count args))
+         (read-string (first args))
+         args)]))
+
+(defn parse
+  [input]
+  (into {} (map parse-line input)))
+
+(def d21-s01
+  (parse
+   ["root: pppw + sjmn"
+    "dbpl: 5"
+    "cczh: sllz + lgvd"
+    "zczc: 2"
+    "ptdq: humn - dvpt"
+    "dvpt: 3"
+    "lfqf: 4"
+    "humn: 5"
+    "ljgn: 2"
+    "sjmn: drzm * dbpl"
+    "sllz: 4"
+    "pppw: cczh / lfqf"
+    "lgvd: ljgn * ptdq"
+    "drzm: hmdt - zczc"
+    "hmdt: 32"]))
+
+(defn yell
+  [jobs monkey]
+  (let [job (get jobs monkey)]
+    (if (number? job)
+      job
+      (let [[a op b] job]
+        ((eval (read-string op)) (yell jobs a) (yell jobs b))))))
+
+(def day21-input (parse (u/puzzle-input "2022/day21-input.txt")))
+
+(defn day21-part1-soln
+  []
+  (yell day21-input "root"))
--- test/aoc_clj/2022/day21_test.clj
+++ test/aoc_clj/2022/day21_test.clj
@@ -1,10 +1,32 @@
-;; (ns aoc-clj.2022.day21-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day21 :as t]))
+(ns aoc-clj.2022.day21-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day21 :as t]))
 
-;; (deftest day21-part1-soln
-;;   (testing "Reproduces the answer for day21, part1"
-;;     (is (= 0 (t/day21-part1-soln)))))
+(def d21-s01
+  (t/parse
+   ["root: pppw + sjmn"
+    "dbpl: 5"
+    "cczh: sllz + lgvd"
+    "zczc: 2"
+    "ptdq: humn - dvpt"
+    "dvpt: 3"
+    "lfqf: 4"
+    "humn: 5"
+    "ljgn: 2"
+    "sjmn: drzm * dbpl"
+    "sllz: 4"
+    "pppw: cczh / lfqf"
+    "lgvd: ljgn * ptdq"
+    "drzm: hmdt - zczc"
+    "hmdt: 32"]))
+
+(deftest yell-test
+  (testing "Yells out the value for root on the sample data"
+    (is (= 152 (t/yell d21-s01 "root")))))
+
+(deftest day21-part1-soln
+  (testing "Reproduces the answer for day21, part1"
+    (is (= 63119856257960 (t/day21-part1-soln)))))
 
 ;; (deftest day21-part2-soln
 ;;   (testing "Reproduces the answer for day21, part2"
