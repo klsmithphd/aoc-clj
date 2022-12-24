21f2cf803805bfd7b0d83d6eeb692427326b69ac
Ken Smith <ken.smith@2scientists.net>
2022-12-23 17:18:25 -0700

    2022 Day22 start to parsing

--- src/aoc_clj/2022/day22.clj
+++ src/aoc_clj/2022/day22.clj
@@ -1,5 +1,41 @@
-;; (ns aoc-clj.2022.day22
-;;  "Solution to https://adventofcode.com/2022/day/22"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day22
+  "Solution to https://adventofcode.com/2022/day/22"
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.mapgrid :as mapgrid]))
 
-;; (def day22-input (u/puzzle-input "2022/day22-input.txt"))
+(def charmap {\  nil \. :open \# :wall})
+
+;; TODO: This isn't yet correct given that the ascii->MapGrid2D implementation
+;; assumes a rectangular grid (of fixed width and height)
+(defn parse-map
+  [s]
+  (mapgrid/ascii->MapGrid2D charmap s))
+
+(defn parse-path
+  [s]
+  (map read-string (re-seq #"\d+|[LR]" s)))
+
+(defn parse
+  [input]
+  (let [[a b] (u/split-at-blankline input)]
+    {:themap  (parse-map a)
+     :path    (parse-path (first b))}))
+
+(def d22-s01
+  (parse
+   ["        ...#"
+    "        .#.."
+    "        #..."
+    "        ...."
+    "...#.......#"
+    "........#..."
+    "..#....#...."
+    "..........#."
+    "        ...#...."
+    "        .....#.."
+    "        .#......"
+    "        ......#."
+    ""
+    "10R5L5R10L4R5L5"]))
+
+(def day22-input (parse (u/puzzle-input "2022/day22-input.txt")))
--- test/aoc_clj/2022/day22_test.clj
+++ test/aoc_clj/2022/day22_test.clj
@@ -1,6 +1,23 @@
-;; (ns aoc-clj.2022.day22-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day22 :as t]))
+(ns aoc-clj.2022.day22-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day22 :as t]))
+
+(def d22-s01
+  (t/parse
+   ["        ...#"
+    "        .#.."
+    "        #..."
+    "        ...."
+    "...#.......#"
+    "........#..."
+    "..#....#...."
+    "..........#."
+    "        ...#...."
+    "        .....#.."
+    "        .#......"
+    "        ......#."
+    ""
+    "10R5L5R10L4R5L5"]))
 
 ;; (deftest day22-part1-soln
 ;;   (testing "Reproduces the answer for day22, part1"
