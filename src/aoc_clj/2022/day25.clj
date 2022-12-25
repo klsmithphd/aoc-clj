e96178060f2965144d474d58ee3ec2133f8177ba
Ken Smith <ken.smith@2scientists.net>
2022-12-25 08:36:36 -0700

    2022 Day 25 part 1 WIP

--- src/aoc_clj/2022/day25.clj
+++ src/aoc_clj/2022/day25.clj
@@ -1,5 +1,100 @@
-;; (ns aoc-clj.2022.day25
-;;  "Solution to https://adventofcode.com/2022/day/25"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day25
+  "Solution to https://adventofcode.com/2022/day/25"
+  (:require [aoc-clj.utils.core :as u]
+            [clojure.string :as str]))
 
-;; (def day25-input (u/puzzle-input "2022/day25-input.txt"))
+(def d25-s01-decimal
+  [1
+   2
+   3
+   4
+   5
+   6
+   7
+   8
+   9
+   10
+   15
+   20
+   2022
+   12345
+   314159265])
+
+(def d25-s01-snafu
+  ["1"
+   "2"
+   "1="
+   "1-"
+   "10"
+   "11"
+   "12"
+   "2="
+   "2-"
+   "20"
+   "1=0"
+   "1-0"
+   "1=11-2"
+   "1-0---0"
+   "1121-1110-1=0"])
+
+(def d25-s02-decimal
+  [1747
+   906
+   198
+   11
+   201
+   31
+   1257
+   32
+   353
+   107
+   7
+   3
+   37])
+
+(def d25-s02-snafu
+  ["1=-0-2"
+   "12111"
+   "2=0="
+   "21"
+   "2=01"
+   "111"
+   "20012"
+   "112"
+   "1=-1="
+   "1-12"
+   "12"
+   "1="
+   "122"])
+
+(def snafu-map
+  {\2 2
+   \1 1
+   \0 0
+   \- -1
+   \= -2})
+
+(def day25-input (u/puzzle-input "2022/day25-input.txt"))
+
+(defn powers-of-five
+  "Returns a seq of the powers of five up to 5^(n-1) in descending order, i.e.,
+   (5^(n-1) 5^(n-2) ... 5^0)"
+  [n]
+  (->> (drop 1 (range))
+       (reductions (fn [a _] (* 5 a)))
+       (take n)
+       reverse))
+
+(defn snafu->decimal
+  "Convert SNAFU numerals to a base-10 number"
+  [s]
+  (let [n (count s)]
+    (reduce +
+            (map *
+                 (map snafu-map s)
+                 (powers-of-five n)))))
+
+(defn decimal->snafu
+  "Convert a base-10 number into a string of SNAFU numerals"
+  [n]
+  "1")
--- test/aoc_clj/2022/day25_test.clj
+++ test/aoc_clj/2022/day25_test.clj
@@ -1,6 +1,80 @@
-;; (ns aoc-clj.2022.day25-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day25 :as t]))
+(ns aoc-clj.2022.day25-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2022.day25 :as t]))
+
+(def d25-s01-decimal
+  [1
+   2
+   3
+   4
+   5
+   6
+   7
+   8
+   9
+   10
+   15
+   20
+   2022
+   12345
+   314159265])
+
+(def d25-s01-snafu
+  ["1"
+   "2"
+   "1="
+   "1-"
+   "10"
+   "11"
+   "12"
+   "2="
+   "2-"
+   "20"
+   "1=0"
+   "1-0"
+   "1=11-2"
+   "1-0---0"
+   "1121-1110-1=0"])
+
+(def d25-s02-decimal
+  [1747
+   906
+   198
+   11
+   201
+   31
+   1257
+   32
+   353
+   107
+   7
+   3
+   37])
+
+(def d25-s02-snafu
+  ["1=-0-2"
+   "12111"
+   "2=0="
+   "21"
+   "2=01"
+   "111"
+   "20012"
+   "112"
+   "1=-1="
+   "1-12"
+   "12"
+   "1="
+   "122"])
+
+(deftest decimal->snafu-test
+  (testing "Can convert decimal to SNAFU numerals"
+    (is (= d25-s01-snafu (map t/decimal->snafu d25-s01-decimal)))
+    (is (= d25-s02-snafu (map t/decimal->snafu d25-s02-decimal)))))
+
+(deftest snafu->decimal-test
+  (testing "Can convert SNAFU to decimal numerals"
+    (is (= d25-s01-decimal (map t/snafu->decimal d25-s01-snafu)))
+    (is (= d25-s02-decimal (map t/snafu->decimal d25-s02-snafu)))))
 
 ;; (deftest day25-part1-soln
 ;;   (testing "Reproduces the answer for day25, part1"
