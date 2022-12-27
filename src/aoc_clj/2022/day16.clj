256b90d65a96d72998d8766173f714f25f678f0e
Ken Smith <ken.smith@2scientists.net>
2022-12-27 10:36:06 -0700

    2022 Day16 Part 1 WIP

--- src/aoc_clj/2022/day16.clj
+++ src/aoc_clj/2022/day16.clj
@@ -62,54 +62,108 @@
          (u/fmap #(into {} (map (comp vec rest)) %))
          ->MapGraph)))
 
-(defn greedy-next-move
-  [g valves pos time open]
-  (let [candidates (->> (remove (into #{pos} (keys open)) (keys valves))
-                        (filter #(< (get-in g [:graph pos %]) time)))
-        values     (zipmap candidates
-                           (map #(/ (valves %)
-                                    (get-in g [:graph pos %])) candidates))
-        choice     (key (apply max-key val values))
-        dist       (get-in g [:graph pos choice])
-        now-time   (- time dist 1)]
-    [choice now-time (assoc open choice now-time)]))
+;; (defn greedy-next-move
+;;   [g valves pos time open]
+;;   (let [candidates (->> (remove (into #{pos} (keys open)) (keys valves))
+;;                         (filter #(< (get-in g [:graph pos %]) time)))
+;;         values     (zipmap candidates
+;;                            (map #(/ (valves %)
+;;                                     (get-in g [:graph pos %])) candidates))
+;;         choice     (key (apply max-key val values))
+;;         dist       (get-in g [:graph pos choice])
+;;         now-time   (- time dist 1)]
+;;     [choice now-time (assoc open choice now-time)]))
 
-(defn greedy-solve
+;; (defn greedy-solve
+;;   [input]
+;;   (let [valves (valves input)
+;;         g      (simpler-graph input)]
+;;     (loop [pos "AA" time 30 open {}]
+;;       (if (or (zero? time)
+;;               (= (count open) (count valves)))
+;;         [pos time open]
+;;         (let [[p t o] (greedy-next-move g valves pos time open)]
+;;           (recur p t o))))))
+
+;; (defn visit-times
+;;   [{:keys [graph]} nodes]
+;;   (let [steps (partition 2 1 nodes)]
+;;     (->> (map #(inc (get-in graph %)) steps)
+;;          (reductions +)
+;;          (map #(- 30 %))
+;;          (zipmap (rest nodes)))))
+
+;; (defn pressure-released
+;;   [valves open-times]
+;;   (reduce + (map (fn [[k v]] (* (valves k) v)) open-times)))
+
+;; (defn brute-force-best-path
+;;   [input]
+;;   (let [g (simpler-graph input)
+;;         v (valves input)
+;;         nodes (keys (:graph g))
+;;         scenarios (map #(concat ["AA"] %)
+;;                        (combo/permutations (remove #{"AA"} nodes)))]
+;;     (->> scenarios
+;;          (map #(visit-times g %))
+;;          (map #(pressure-released v %))
+;;          (apply max))))
+
+(defn helper
+  [graph valves n1 t p n2]
+  (let [dist      (inc (get-in graph [n1 n2]))
+        newt      (- t dist)
+        pressure  (+ p (* newt (valves n2)))]
+    [n2 newt pressure]))
+
+(defn elapsed-time
+  [h]
+  (nth h 1))
+
+(defn pressure
+  [h]
+  (nth h 2))
+
+(defn best-pressure-subpath
+  [graph valves history]
+  (let [candidates (remove (set (map first history)) (keys graph))]
+    ;; (println history)
+    (if (empty? candidates)
+      history
+      (let [[n t p] (first history)
+            trials (->> candidates
+                        (map #(helper graph valves n t p %))
+                        (filter #(pos? (elapsed-time %))))
+            ;; _ (println n t p trials)
+            subpaths (map
+                      #(best-pressure-subpath graph valves (cons % history))
+                      trials)
+            ;; _ (println n subpaths)
+            ]
+        ;; Return the best subpath
+        (if (empty? subpaths)
+          history
+          (first (sort-by (comp pressure first) > subpaths)))))))
+
+(def g {"AA" {"BB" 1 "CC" 2 "DD" 1}
+        "BB" {"AA" 1 "CC" 1 "DD" 2}
+        "CC" {"AA" 2 "BB" 1 "DD" 1}
+        "DD" {"AA" 1 "BB" 2 "CC" 1}})
+(def v {"AA" 0 "BB" 13 "CC" 2 "DD" 20})
+(best-pressure-subpath g v [["AA" 30 0]])
+
+
+(map #(nth (first %) 2) [[["CC" 26 416] ["BB" 28 364] ["AA" 30 0]] [["BB" 25 379] ["CC" 27 54] ["AA" 30 0]]])
+
+(nth (first [["CC" 26 416] ["BB" 28 364] ["AA" 30 0]]) 2)
+
+(defn best-pressure
   [input]
-  (let [valves (valves input)
-        g      (simpler-graph input)]
-    (loop [pos "AA" time 30 open {}]
-      (if (or (zero? time)
-              (= (count open) (count valves)))
-        [pos time open]
-        (let [[p t o] (greedy-next-move g valves pos time open)]
-          (recur p t o))))))
+  (let [g (:graph (simpler-graph input))
+        v (valves input)]
+    (pressure (first (best-pressure-subpath g v [["AA" 30 0]])))))
 
-(defn visit-times
-  [{:keys [graph]} nodes]
-  (let [steps (partition 2 1 nodes)]
-    (->> (map #(inc (get-in graph %)) steps)
-         (reductions +)
-         (map #(- 30 %))
-         (zipmap (rest nodes)))))
-
-(defn pressure-released
-  [valves open-times]
-  (reduce + (map (fn [[k v]] (* (valves k) v)) open-times)))
-
-(defn brute-force-best-path
-  [input]
-  (let [g (simpler-graph input)
-        v (valves input)
-        nodes (keys (:graph g))
-        scenarios (map #(concat ["AA"] %)
-                       (combo/permutations (remove #{"AA"} nodes)))]
-    (->> scenarios
-         (map #(visit-times g %))
-         (map #(pressure-released v %))
-         (apply max))))
-
-(def best-pressure brute-force-best-path)
+(best-pressure d16-s01)
 
 (defn day16-part1-soln
   []
