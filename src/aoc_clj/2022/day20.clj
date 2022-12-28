9097714200dafbc3bed1c73bf5deda7fad05a11e
Ken Smith <ken.smith@2scientists.net>
2022-12-28 07:00:26 -0700

    2022 day20 part1

--- src/aoc_clj/2022/day20.clj
+++ src/aoc_clj/2022/day20.clj
@@ -4,7 +4,7 @@
 
 (defn parse
   [input]
-  (map read-string input))
+  (mapv read-string input))
 
 (def d20-s01
   (parse
@@ -18,6 +18,40 @@
 
 (def day20-input (parse (u/puzzle-input "2022/day20-input.txt")))
 
+(defn new-vec
+  [v old-pos new-pos]
+  (into
+   []
+   (if (<= old-pos new-pos)
+     (concat (subvec v 0 old-pos)
+             (subvec v (inc old-pos) (inc new-pos))
+             [(get v old-pos)]
+             (subvec v (inc new-pos)))
+     (concat (subvec v 0 new-pos)
+             [(get v old-pos)]
+             (subvec v new-pos old-pos)
+             (subvec v (inc old-pos))))))
+
+(defn bounded-shift
+  [max-idx old shift]
+  (let [newval (+ old shift)]
+    (if (zero? newval)
+      max-idx
+      (mod newval max-idx))))
+
+(defn mix
+  [input order idx]
+  (let [shift   (input idx)
+        old-pos (u/index-of idx order)
+        new-pos (bounded-shift (dec (count input)) old-pos shift)]
+    (new-vec order old-pos new-pos)))
+
+(defn mixed
+  [input]
+  (let [indices (vec (range (count input)))
+        mixer   (partial mix input)]
+    (mapv input (reduce mixer indices indices))))
+
 (defn grove-coordinates
   [nums]
   (let [n (count nums)
@@ -25,4 +59,8 @@
     (+ (nth nums (mod (+ zero-pos 1000) n))
        (nth nums (mod (+ zero-pos 2000) n))
        (nth nums (mod (+ zero-pos 3000) n)))))
+
+(defn day20-part1-soln
+  []
+  (grove-coordinates (mixed day20-input)))
 
--- test/aoc_clj/2022/day20_test.clj
+++ test/aoc_clj/2022/day20_test.clj
@@ -2,16 +2,43 @@
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.2022.day20 :as t]))
 
-(def d20-s01       [1 2 -3 3 -2 0 4])
-(def d20-s01-mixed [1 2 -3 4 0 3 -2])
+(def d20-s01   [1 2 -3 3 -2 0 4])
+(def d20-s01-1 [2 1 -3 3 -2 0 4])
+(def d20-s01-2 [1 -3 2 3 -2 0 4])
+(def d20-s01-3 [1 2 3 -2 -3 0 4])
+(def d20-s01-4 [1 2 -2 -3 0 3 4])
+(def d20-s01-5 [1 2 -3 0 3 4 -2])
+(def d20-s01-6 [1 2 -3 0 3 4 -2])
+(def d20-s01-7 [1 2 -3 4 0 3 -2])
+
+(deftest mix-test
+  (testing "Mixes the values correctly, one at a time"
+    (is (= d20-s01-1
+           (mapv d20-s01 (t/mix d20-s01 (vec (range 7)) 0))))
+    (is (= d20-s01-2
+           (mapv d20-s01-1 (t/mix d20-s01-1 (vec (range 7)) 0))))
+    (is (= d20-s01-3
+           (mapv d20-s01-2 (t/mix d20-s01-2 (vec (range 7)) 1))))
+    (is (= d20-s01-4
+           (mapv d20-s01-3 (t/mix d20-s01-3 (vec (range 7)) 2))))
+    (is (= d20-s01-5
+           (mapv d20-s01-4 (t/mix d20-s01-4 (vec (range 7)) 2))))
+    (is (= d20-s01-6
+           (mapv d20-s01-5 (t/mix d20-s01-5 (vec (range 7)) 3))))
+    (is (= d20-s01-7
+           (mapv d20-s01-6 (t/mix d20-s01-6 (vec (range 7)) 5))))))
+
+(deftest mixed-test
+  (testing "Completes all mixing correctly"
+    (is (= d20-s01-7 (t/mixed d20-s01)))))
 
 (deftest grove-coordinates-test
   (testing "Computes the `grove coordinates`"
-    (is (= 3 (t/grove-coordinates d20-s01-mixed)))))
+    (is (= 3 (t/grove-coordinates d20-s01-7)))))
 
-;; (deftest day20-part1-soln
-;;   (testing "Reproduces the answer for day20, part1"
-;;     (is (= 0 (t/day20-part1-soln)))))
+(deftest ^:slow day20-part1-soln
+  (testing "Reproduces the answer for day20, part1"
+    (is (= 9866 (t/day20-part1-soln)))))
 
 ;; (deftest day20-part2-soln
 ;;   (testing "Reproduces the answer for day20, part2"
