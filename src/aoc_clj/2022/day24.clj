75ebe7f18cc937a50f9dc59485713bedad4789a8
Ken Smith <ken.smith@2scientists.net>
2022-12-30 16:59:59 -0700

    2022 Day 24 WIP

--- src/aoc_clj/2022/day24.clj
+++ src/aoc_clj/2022/day24.clj
@@ -1,5 +1,53 @@
-;; (ns aoc-clj.2022.day24
-;;  "Solution to https://adventofcode.com/2022/day/24"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day24
+  "Solution to https://adventofcode.com/2022/day/24"
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.mapgrid :as mapgrid]))
 
-;; (def day24-input (u/puzzle-input "2022/day24-input.txt"))
+(def charmap {\. :open \# :wall \> :r \^ :u \< :l \v :d})
+
+(defn blizzards
+  [grid]
+  (filter #(#{:r :u :l :d} (val %)) grid))
+
+(defn parse
+  [input]
+  (let [{:keys [width height grid]}
+        (mapgrid/ascii->MapGrid2D charmap input :down true)]
+    {:x-bound (- width 2)
+     :y-bound (- height 2)
+     :blizzards (blizzards grid)}))
+
+(def d24-s01
+  (parse
+   ["#.#####"
+    "#.....#"
+    "#>....#"
+    "#.....#"
+    "#...v.#"
+    "#.....#"
+    "#####.#"]))
+
+(def d24-s02
+  (parse
+   ["#.######"
+    "#>>.<^<#"
+    "#.<..<<#"
+    "#>v.><>#"
+    "#<^v^^>#"
+    "######.#"]))
+
+(def day24-input (parse (u/puzzle-input "2022/day24-input.txt")))
+
+(defn blizzard-update
+  [{:keys [x-bound y-bound]} [[x y] dir]]
+  (let [newpos (case dir
+                 :u (if (= 1 y)       [x y-bound] [x (dec y)])
+                 :d (if (= y-bound y) [x 1]       [x (inc y)])
+                 :l (if (= 1 x)       [x-bound y] [(dec x) y])
+                 :r (if (= x-bound x) [1 y]       [(inc x) y]))]
+    [newpos dir]))
+
+(defn step
+  [{:keys [blizzards] :as state}]
+  (assoc state :blizzards
+         (map #(blizzard-update state %) blizzards)))
