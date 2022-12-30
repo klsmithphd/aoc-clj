0f83e32382a349e4ab970cc81d12aa89684a77ca
Ken Smith <ken.smith@2scientists.net>
2022-12-30 16:00:28 -0700

    2022 Day23 soln

--- src/aoc_clj/2022/day23.clj
+++ src/aoc_clj/2022/day23.clj
@@ -1,5 +1,128 @@
-;; (ns aoc-clj.2022.day23
-;;  "Solution to https://adventofcode.com/2022/day/23"
-;;   (:require [aoc-clj.utils.core :as u]))
+(ns aoc-clj.2022.day23
+  "Solution to https://adventofcode.com/2022/day/23"
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid :as grid]
+            [aoc-clj.utils.grid.mapgrid :as mapgrid]))
 
-;; (def day23-input (u/puzzle-input "2022/day23-input.txt"))
+(def charmap {\. :open \# :elf})
+(def dir-order [:n :s :w :e])
+
+(defn elf? [x] (= :elf x))
+
+(defn parse
+  [input]
+  (->> (mapgrid/ascii->MapGrid2D charmap input)
+       :grid
+       (filter #(elf? (val %)))
+       (map first)
+       (into #{})))
+
+(def d23-s01
+  (parse
+   ["....."
+    "..##."
+    "..#.."
+    "....."
+    "..##."
+    "....."]))
+
+(def d23-s02
+  (parse
+   ["....#.."
+    "..###.#"
+    "#...#.#"
+    ".#...##"
+    "#.###.."
+    "##.#.##"
+    ".#..#.."]))
+
+(def day23-input (parse (u/puzzle-input "2022/day23-input.txt")))
+
+(defn done?
+  [elves pos]
+  (not-any? elves (grid/adj-coords-2d pos :include-diagonals true)))
+
+(defn open-dir
+  [elves [x y] dir]
+  (let [opts (case dir
+               :n [[(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]]
+               :s [[(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]]
+               :w [[(dec x) (dec y)] [(dec x) y] [(dec x) (inc y)]]
+               :e [[(inc x) (dec y)] [(inc x) y] [(inc x) (inc y)]])]
+    (when (not-any? elves opts)
+      (mapv + [x y] (grid/cardinal->offset dir)))))
+
+(defn propose-move
+  [elves dirs pos]
+  (when-let [new-pos (->> (map #(open-dir elves pos %) dirs)
+                          (remove nil?)
+                          first)]
+    [pos new-pos]))
+
+(defn proposed-moves
+  [elves dirs]
+  (let [proposer (partial propose-move elves dirs)
+        elves-to-move (remove #(done? elves %) elves)]
+    (remove #(nil? (second %)) (map proposer elves-to-move))))
+
+(defn duplicates
+  [moves]
+  (->> (map second moves)
+       frequencies
+       (filter #(>= (second %) 2))
+       (map first)
+       set))
+
+(defn deduped-proposals
+  [proposals]
+  (let [duplicate-locs (duplicates proposals)]
+    (remove #(duplicate-locs (second %)) proposals)))
+
+(defn apply-proposal
+  [elves [old new]]
+  (-> (disj elves old)
+      (conj new)))
+
+(defn apply-proposals
+  [elves proposals]
+  (reduce apply-proposal elves proposals))
+
+(defn round
+  [[elves dirs]]
+  [(->> (proposed-moves elves dirs)
+        deduped-proposals
+        (apply-proposals elves))
+   (u/rotate 1 dirs)])
+
+(defn area
+  [elves]
+  (let [[minx maxx] ((juxt #(apply min %) #(apply max %)) (map first elves))
+        [miny maxy] ((juxt #(apply min %) #(apply max %)) (map second elves))]
+    (* (inc (- maxx minx)) (inc (- maxy miny)))))
+
+(defn empty-tiles-after-ten-rounds
+  "Simulate the Elves' process and find the smallest rectangle that 
+   contains the Elves after 10 rounds. How many empty ground tiles does that 
+   rectangle contain?"
+  [elves]
+  (let [final (first (nth (iterate round [elves dir-order]) 10))]
+    (- (area final) (count final))))
+
+(defn rounds-until-static
+  "Figure out where the Elves need to go. What is the number of the first 
+   round where no Elf moves?"
+  [elves]
+  (->> (iterate round [elves dir-order])
+       (map first)
+       (partition 2 1)
+       (take-while #(not= (first %) (second %)))
+       count
+       inc))
+
+(defn day23-part1-soln
+  []
+  (empty-tiles-after-ten-rounds day23-input))
+
+(defn day23-part2-soln
+  []
+  (rounds-until-static day23-input))
--- test/aoc_clj/2022/day23_test.clj
+++ test/aoc_clj/2022/day23_test.clj
@@ -1,11 +1,75 @@
-;; (ns aoc-clj.2022.day23-test
-;;   (:require [clojure.test :refer [deftest testing is]]
-;;             [aoc-clj.2022.day23 :as t]))
+(ns aoc-clj.2022.day23-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2022.day23 :as t]))
 
-;; (deftest day23-part1-soln
-;;   (testing "Reproduces the answer for day23, part1"
-;;     (is (= 0 (t/day23-part1-soln)))))
+(def d23-s01
+  (t/parse
+   ["....."
+    "..##."
+    "..#.."
+    "....."
+    "..##."
+    "....."]))
 
-;; (deftest day23-part2-soln
-;;   (testing "Reproduces the answer for day23, part2"
-;;     (is (= 0 (t/day23-part2-soln)))))
+(def d23-s01-1
+  (t/parse
+   ["..##."
+    "....."
+    "..#.."
+    "...#."
+    "..#.."
+    "....."]))
+
+(def d23-s01-2
+  (t/parse
+   ["....."
+    "..##."
+    ".#..."
+    "....#"
+    "....."
+    "..#.."]))
+
+(def d23-s01-3
+  (t/parse
+   ["..#.."
+    "....#"
+    "#...."
+    "....#"
+    "....."
+    "..#.."]))
+
+(def d23-s02
+  (t/parse
+   ["....#.."
+    "..###.#"
+    "#...#.#"
+    ".#...##"
+    "#.###.."
+    "##.#.##"
+    ".#..#.."]))
+
+(deftest round-test
+  (testing "Returns the new elf locations after one round"
+    (is (= d23-s01-1 (first (t/round [d23-s01 t/dir-order]))))
+    (is (= d23-s01-2 (first (t/round [d23-s01-1 (u/rotate 1 t/dir-order)]))))
+    (is (= d23-s01-3 (first (t/round [d23-s01-2 (u/rotate 2 t/dir-order)]))))))
+
+(deftest empty-tiles-after-ten-rounds-test
+  (testing "Computes the number of empty tiles in the tightest rectangular
+            area around the elves after 10 rounds"
+    (is (= 25  (t/empty-tiles-after-ten-rounds d23-s01)))
+    (is (= 110 (t/empty-tiles-after-ten-rounds d23-s02)))))
+
+(deftest rounds-until-static
+  (testing "Computes the first round where elves will no longer move"
+    (is (= 4 (t/rounds-until-static d23-s01)))
+    (is (= 20 (t/rounds-until-static d23-s02)))))
+
+(deftest day23-part1-soln
+  (testing "Reproduces the answer for day23, part1"
+    (is (= 4172 (t/day23-part1-soln)))))
+
+(deftest day23-part2-soln
+  (testing "Reproduces the answer for day23, part2"
+    (is (= 942 (t/day23-part2-soln)))))
