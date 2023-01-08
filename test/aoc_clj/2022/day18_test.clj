1ad8f6ac49a38fa2ca556876effdb435e46685e1
Ken Smith <ken.smith@2scientists.net>
2023-01-08 10:34:35 -0700

    2022 Day 18 part2

--- test/aoc_clj/2022/day18_test.clj
+++ test/aoc_clj/2022/day18_test.clj
@@ -3,33 +3,39 @@
             [aoc-clj.2022.day18 :as t]))
 
 (def d18-s01
-  [[1 1 1]
-   [2 1 1]])
+  #{[1 1 1]
+    [2 1 1]})
 
 (def d18-s02
-  [[2 2 2]
-   [1 2 2]
-   [3 2 2]
-   [2 1 2]
-   [2 3 2]
-   [2 2 1]
-   [2 2 3]
-   [2 2 4]
-   [2 2 6]
-   [1 2 5]
-   [3 2 5]
-   [2 1 5]
-   [2 3 5]])
+  #{[2 2 2]
+    [1 2 2]
+    [3 2 2]
+    [2 1 2]
+    [2 3 2]
+    [2 2 1]
+    [2 2 3]
+    [2 2 4]
+    [2 2 6]
+    [1 2 5]
+    [3 2 5]
+    [2 1 5]
+    [2 3 5]})
 
 (deftest surface-area-test
   (testing "Counts the number of exposed (non-adjacent) sides of the cubes"
     (is (= 10 (t/surface-area d18-s01)))
     (is (= 64 (t/surface-area d18-s02)))))
 
+(deftest outer-surface-area-test
+  (testing "Counts the number of exposed (non-adjacent) sides of the cubes
+            that are along the outer surface"
+    (is (= 10 (t/outer-surface-area d18-s01)))
+    (is (= 58 (t/outer-surface-area d18-s02)))))
+
 (deftest day18-part1-soln
   (testing "Reproduces the answer for day18, part1"
     (is (= 3390 (t/day18-part1-soln)))))
 
-;; (deftest day18-part2-soln
-;;   (testing "Reproduces the answer for day18, part2"
-;;     (is (= 0 (t/day18-part2-soln)))))
+(deftest day18-part2-soln
+  (testing "Reproduces the answer for day18, part2"
+    (is (= 2058 (t/day18-part2-soln)))))
