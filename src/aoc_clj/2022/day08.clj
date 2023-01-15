798a215b91a1843a44305454d5c24b293b41c123
Ken Smith <ken.smith@2scientists.net>
2023-01-15 21:08:54 +0000

    Tidy 2022 day08 code

--- src/aoc_clj/2022/day08.clj
+++ src/aoc_clj/2022/day08.clj
@@ -13,6 +13,11 @@
 (def day08-input (parse (u/puzzle-input "2022/day08-input.txt")))
 
 (defn visible?
+  "Returns a vec of 0s or 1s, where 0 indicates that the tree is not visible
+   **from the left edge** and 1 indicates that the tree is visible.
+   
+   A tree is visible if all of the other trees between it and an edge of 
+   the grid are shorter than it."
   [s]
   (loop [nxt (rest s) mx (first s) acc [1]]
     (if (empty? nxt)
@@ -24,11 +29,19 @@
                (conj acc (if taller? 1 0)))))))
 
 (defn row-visibility
+  "Returns a vec of 0s or 1s, where 0 indicates that the tree is not visible
+   **from the left or right edge** and 1 indicates that the tree is visible."
   [row]
   (mapv bit-or (visible? row) (reverse (visible? (reverse row)))))
 
 
 (defn grid-visibility
+  "Returns a vec of vecs of 0s or 1s, where 0 indicates that the tree
+   is not visible **from the left or right edges** and 1 indicates that 
+   the tree is visible.
+   
+   The first row and the last row (being at the outer edge) are always
+   visible (i.e. are all 1s)"
   [grid]
   (let [width (count (first grid))]
     (vec
@@ -36,13 +49,24 @@
              (mapv row-visibility (butlast (drop 1 grid)))
              [(vec (repeat width 1))]))))
 
+(defn transpose-fn
+  "Applies a function on the transpose of the grid and then returns the
+   transpose of that result, i.e. (f A^T)^T"
+  [f grid]
+  (u/transpose (f (u/transpose grid))))
+
 (defn visible-trees
+  "Returns a count of all of the trees that are visible from the outside
+   of the grid"
   [input]
   (reduce + (map bit-or
                  (flatten (grid-visibility input))
-                 (flatten (u/transpose (grid-visibility (u/transpose input)))))))
+                 (flatten (transpose-fn grid-visibility input)))))
 
 (defn view-distance
+  "For a given `row`, consider the element at index `i`. Compute the product
+   of the distance to the left before hitting a tree of equal or greater
+   height and the equivalent distance to the right"
   [row i]
   (let [[l [x & r]] (split-at i row)]
     [x l r]
@@ -50,22 +74,30 @@
        (min (count r) (inc (count (take-while #(> x %) r)))))))
 
 (defn row-view-distance
+  "Computes the horizontal view distance products for an entire row"
   [row]
   (map #(view-distance row %) (range (count row))))
 
 (defn grid-view-distance
+  "Computes the horizontal view distance product for an entire grid"
   [grid]
   (map row-view-distance grid))
 
 (defn scenic-score
-  "A tree's scenic score is found by multiplying together its viewing distance 
+  "To measure the viewing distance from a given tree, look up, down, left, and 
+   right from that tree; stop if you reach an edge or at the first tree that is 
+   the same height or taller than the tree under consideration. (If a tree is 
+   right on the edge, at least one of its viewing distances will be zero.)
+   
+   A tree's scenic score is found by multiplying together its viewing distance 
    in each of the four directions."
   [grid]
   (map *
        (flatten (grid-view-distance grid))
-       (flatten (u/transpose (grid-view-distance (u/transpose grid))))))
+       (flatten (transpose-fn grid-view-distance grid))))
 
 (defn max-scenic-score
+  "Find the maximum scenic score of any tree in the grid"
   [input]
   (apply max (scenic-score input)))
 
--- test/aoc_clj/2022/day08_test.clj
+++ test/aoc_clj/2022/day08_test.clj
@@ -10,6 +10,45 @@
     "33549"
     "35390"]))
 
+(deftest parse-test
+  (testing "Correctly parses the sample input"
+    (is (= d08-s01
+           [[3 0 3 7 3]
+            [2 5 5 1 2]
+            [6 5 3 3 2]
+            [3 3 5 4 9]
+            [3 5 3 9 0]]))))
+
+(deftest visible?-test
+  (testing "Determines which trees are visible in a given direction"
+    (is (= [1 0 0 1 0] (t/visible? (nth d08-s01 0))))
+    (is (= [1 1 0 0 0] (t/visible? (nth d08-s01 1))))
+    (is (= [1 0 0 0 0] (t/visible? (nth d08-s01 2))))
+    (is (= [1 0 1 0 1] (t/visible? (nth d08-s01 3))))
+    (is (= [1 1 0 1 0] (t/visible? (nth d08-s01 4))))))
+
+(deftest row-visibility-test
+  (testing "Determines which trees are visible from either edge"
+    (is (= [1 0 0 1 1] (t/row-visibility (nth d08-s01 0))))
+    (is (= [1 1 1 0 1] (t/row-visibility (nth d08-s01 1))))
+    (is (= [1 1 0 1 1] (t/row-visibility (nth d08-s01 2))))
+    (is (= [1 0 1 0 1] (t/row-visibility (nth d08-s01 3))))
+    (is (= [1 1 0 1 1] (t/row-visibility (nth d08-s01 4))))))
+
+(deftest view-distance-test
+  (testing "Determines the view distance product in the horizontal direction"
+    (is (= (* 0 2) (t/view-distance (nth d08-s01 0) 0)))
+    (is (= (* 1 1) (t/view-distance (nth d08-s01 0) 1)))
+    (is (= (* 2 1) (t/view-distance (nth d08-s01 0) 2)))
+    (is (= (* 3 1) (t/view-distance (nth d08-s01 0) 3)))
+    (is (= (* 1 0) (t/view-distance (nth d08-s01 0) 4)))
+
+    (is (= (* 0 1) (t/view-distance (nth d08-s01 1) 0)))
+    (is (= (* 1 1) (t/view-distance (nth d08-s01 1) 1)))
+    (is (= (* 1 2) (t/view-distance (nth d08-s01 1) 2)))
+    (is (= (* 1 1) (t/view-distance (nth d08-s01 1) 3)))
+    (is (= (* 2 0) (t/view-distance (nth d08-s01 1) 4)))))
+
 (deftest visible-trees-test
   (testing "Counts the number of visible trees in the sample data"
     (is (= 21 (t/visible-trees d08-s01)))))
