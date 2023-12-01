1ff23ceb4d075231d789992b9d28b8a95e28d591
Ken Smith <ken@klsmith.me>
2023-12-01 14:06:42 +0000

    2023 day01 part2 partial soln

--- src/aoc_clj/2023/day01.clj
+++ src/aoc_clj/2023/day01.clj
@@ -6,22 +6,46 @@
 
 (def day01-input (u/parse-puzzle-input parse 2023 1))
 
+(def numbers
+  {"one"   1
+   "two"   2
+   "three" 3
+   "four"  4
+   "five"  5
+   "six"   6
+   "seven" 7
+   "eight" 8
+   "nine"  9})
+
 (defn digits
   [s]
   (re-seq #"\d" s))
 
-(defn calibration-value
+(defn digits2
   [s]
-  (let [ds (digits s)
-        a  (read-string (first ds))
-        b  (read-string (last ds))]
+  (re-seq #"\d|one|two|three|four|five|six|seven|eight|nine" s))
+
+(defn s->int
+  [s]
+  (if (= 1 (count s))
+    (read-string s)
+    (numbers s)))
+
+(defn calibration-value
+  [digits-fn s]
+  (let [ds (digits-fn s)
+        a  (s->int (first ds))
+        b  (s->int (last ds))]
     (+ (* 10 a) b)))
 
 (defn calibration-value-sum
-  [input]
-  (reduce + (map calibration-value input)))
-
+  [digits-fn input]
+  (reduce + (map #(calibration-value digits-fn %) input)))
 
 (defn day01-part1-soln
   []
-  (calibration-value-sum day01-input))
+  (calibration-value-sum digits day01-input))
+
+(defn day01-part2-soln
+  []
+  (calibration-value-sum digits2 day01-input))
--- test/aoc_clj/2023/day01_test.clj
+++ test/aoc_clj/2023/day01_test.clj
@@ -8,15 +8,41 @@
    "a1b2c3d4e5f"
    "treb7uchet"])
 
+(def d01-s02
+  ["two1nine"
+   "eightwothree"
+   "abcone2threexyz"
+   "xtwone3four"
+   "4nineeightseven2"
+   "zoneight234"
+   "7pqrstsixteen"])
+
+(def hard-cases
+  ["sevenine"])
+
+(t/digits2 (get d01-s02 0))
+
 (deftest calibration-value-test
   (testing "Computes a calibration value (first and last digit)"
     (is (= [12 38 15 77]
-           (mapv t/calibration-value d01-s01)))))
+           (mapv #(t/calibration-value t/digits %) d01-s01)))
+    (is (= [29, 83, 13, 24, 42, 14, 76]
+           (mapv #(t/calibration-value t/digits2 %) d01-s02)))
+    (is (= [11 27 33 58 71 92 87 51 76 99 94 14 47]
+           (mapv #(t/calibration-value t/digits2 %) (take 13 t/day01-input))))
+    ;; (is (= [79]
+    ;;        (mapv #(t/calibration-value t/digits2 %) hard-cases))) 
+    ))
 
 (deftest calibration-value-sum-test
   (testing "Computes the calibration value sum"
-    (is (= 142 (t/calibration-value-sum d01-s01)))))
+    (is (= 142 (t/calibration-value-sum t/digits d01-s01)))
+    (is (= 281 (t/calibration-value-sum t/digits2 d01-s02)))))
 
 (deftest day01-part1-soln
   (testing "Reproduces the answer for day01, part1"
     (is (= 55172 (t/day01-part1-soln)))))
+
+;; (deftest day01-part2-soln
+;;   (testing "Reproduces the answer for day01, part2"
+;;     (is (= 54953 (t/day01-part2-soln)))))
