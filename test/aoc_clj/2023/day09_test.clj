8740f2cd967c37edc93d070129167f71135a3a7e
Ken Smith <ken@klsmith.me>
2023-12-09 21:57:49 +0000

    2023 Day09 Part2 soln

--- test/aoc_clj/2023/day09_test.clj
+++ test/aoc_clj/2023/day09_test.clj
@@ -17,13 +17,20 @@
 
 (deftest extrapolate-test
   (testing "Appends the correct next value in the sequence"
-    (is (= [0 3 6 9 12 15 18]     (t/extrapolate (nth d09-s01 0))))
-    (is (= [1 3 6 10 15 21 28]    (t/extrapolate (nth d09-s01 1))))
-    (is (= [10 13 16 21 30 45 68] (t/extrapolate (nth d09-s01 2))))))
+    (is (= [0 3 6 9 12 15 18]     (t/right-extrapolate (nth d09-s01 0))))
+    (is (= [1 3 6 10 15 21 28]    (t/right-extrapolate (nth d09-s01 1))))
+    (is (= [10 13 16 21 30 45 68] (t/right-extrapolate (nth d09-s01 2))))))
+
+(deftest left-extrapolate-test
+  (testing "Prepends the correct next value in the sequence"
+    (is (= [-3 0 3 6 9 12 15]    (t/left-extrapolate (nth d09-s01 0))))
+    (is (= [0 1 3 6 10 15 21]    (t/left-extrapolate (nth d09-s01 1))))
+    (is (= [5 10 13 16 21 30 45] (t/left-extrapolate (nth d09-s01 2))))))
 
 (deftest extrapolation-sum-test
   (testing "Returns the sum of the extrapolated values"
-    (is (= 114 (t/extrapolation-sum d09-s01)))))
+    (is (= 114 (t/extrapolation-sum d09-s01)))
+    (is (= 2   (t/extrapolation-sum d09-s01 true)))))
 
 (def day09-input (u/parse-puzzle-input t/parse 2023 9))
 
@@ -31,3 +38,6 @@
   (testing "Reproduces the answer for day09, part1"
     (is (= 2162 (t/day09-part1-soln day09-input)))))
 
+(deftest day09-part2-soln
+  (testing "Reproduces the answer for day09, part2"
+    (is (= 864 (t/day09-part2-soln day09-input)))))
