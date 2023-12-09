de3daf2081577bcc191c57c3fe28e9178d980cee
Ken Smith <ken@klsmith.me>
2023-12-09 20:10:01 +0000

    2023 Day08 parsing

--- src/aoc_clj/2023/day08.clj
+++ src/aoc_clj/2023/day08.clj
@@ -1,1 +1,17 @@
-(ns aoc-clj.2023.day08)
+(ns aoc-clj.2023.day08
+  (:require [aoc-clj.utils.core :as u]))
+
+(defn parse-node
+  [s]
+  (let [[node left right] (re-seq #"\w{3}" s)]
+    [node {:left left :right right}]))
+
+(defn parse-nodes
+  [nodes]
+  (into {} (map parse-node nodes)))
+
+(defn parse
+  [input]
+  (let [chunks (u/split-at-blankline input)]
+    {:instructions (ffirst chunks)
+     :nodes (parse-nodes (first (rest chunks)))}))
--- test/aoc_clj/2023/day08_test.clj
+++ test/aoc_clj/2023/day08_test.clj
@@ -1,1 +1,40 @@
-(ns aoc-clj.2023.day08-test)
+(ns aoc-clj.2023.day08-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2023.day08 :as t]))
+
+(def d08-s01-raw ["RL"
+                  ""
+                  "AAA = (BBB, CCC)"
+                  "BBB = (DDD, EEE)"
+                  "CCC = (ZZZ, GGG)"
+                  "DDD = (DDD, DDD)"
+                  "EEE = (EEE, EEE)"
+                  "GGG = (GGG, GGG)"
+                  "ZZZ = (ZZZ, ZZZ)"])
+
+(def d08-s02-raw ["LLR"
+                  ""
+                  "AAA = (BBB, BBB)"
+                  "BBB = (AAA, ZZZ)"
+                  "ZZZ = (ZZZ, ZZZ)"])
+
+(def d08-s01
+  {:instructions "RL"
+   :nodes {"AAA" {:left "BBB" :right "CCC"}
+           "BBB" {:left "DDD" :right "EEE"}
+           "CCC" {:left "ZZZ" :right "GGG"}
+           "DDD" {:left "DDD" :right "DDD"}
+           "EEE" {:left "EEE" :right "EEE"}
+           "GGG" {:left "GGG" :right "GGG"}
+           "ZZZ" {:left "ZZZ" :right "ZZZ"}}})
+
+(def d08-s02
+  {:instructions "LLR"
+   :nodes {"AAA" {:left "BBB" :right "BBB"}
+           "BBB" {:left "AAA" :right "ZZZ"}
+           "ZZZ" {:left "ZZZ" :right "ZZZ"}}})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d08-s01 (t/parse d08-s01-raw)))
+    (is (= d08-s02 (t/parse d08-s02-raw)))))
