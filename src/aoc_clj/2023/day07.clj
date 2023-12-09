c2c006078c1ec7c539c8169d128f1e886e67e3dd
Ken Smith <ken@klsmith.me>
2023-12-09 18:33:30 +0000

    2023 Day07 Part1 soln

--- src/aoc_clj/2023/day07.clj
+++ src/aoc_clj/2023/day07.clj
@@ -1,6 +1,15 @@
 (ns aoc-clj.2023.day07
   (:require [clojure.string :as str]))
 
+(def hand-type-ranks
+  {:five-of-a-kind  6
+   :four-of-a-kind  5
+   :full-house      4
+   :three-of-a-kind 3
+   :two-pair        2
+   :one-pair        1
+   :high-card       0})
+
 (defn card-map
   [ch]
   (case ch
@@ -11,12 +20,51 @@
     \T 10
     (read-string (str ch))))
 
+(defn parse-hand
+  [s]
+  (mapv card-map s))
+
 (defn parse-row
   [row]
   (let [[hand-str bid-str] (str/split row #" ")]
-    {:hand (mapv card-map hand-str)
+    {:hand (parse-hand hand-str)
      :bid  (read-string bid-str)}))
 
 (defn parse
   [input]
   (map parse-row input))
+
+
+(defn hand-type
+  [hand]
+  (let [freqs (vec (sort (vals (frequencies hand))))]
+    (case freqs
+      [5]         :five-of-a-kind
+      [1 4]       :four-of-a-kind
+      [2 3]       :full-house
+      [1 1 3]     :three-of-a-kind
+      [1 2 2]     :two-pair
+      [1 1 1 2]   :one-pair
+      [1 1 1 1 1] :high-card)))
+
+
+(defn compare-hands
+  [a b]
+  (let [hand-a (:hand a)
+        hand-b (:hand b)
+        rank-a (hand-type-ranks (hand-type hand-a))
+        rank-b (hand-type-ranks (hand-type hand-b))]
+    (if (= rank-a rank-b)
+      (compare hand-a hand-b)
+      (compare rank-a rank-b))))
+
+(defn winnings
+  [hands]
+  (->> (sort compare-hands hands)
+       (map-indexed #(* (inc %1) (:bid %2)))
+       (reduce +)))
+
+(defn day07-part1-soln
+  [input]
+  (winnings input))
+
--- test/aoc_clj/2023/day07_test.clj
+++ test/aoc_clj/2023/day07_test.clj
@@ -19,5 +19,31 @@
   (testing "Correctly parses the input"
     (is (= d07-s01 (t/parse d07-s01-raw)))))
 
+(deftest hand-type-test
+  (testing "Determines the type of hand"
+    (is (= :five-of-a-kind  (t/hand-type "AAAAA")))
+    (is (= :four-of-a-kind  (t/hand-type "AA8AA")))
+    (is (= :full-house      (t/hand-type "23332")))
+    (is (= :three-of-a-kind (t/hand-type "TTT98")))
+    (is (= :two-pair        (t/hand-type "23432")))
+    (is (= :one-pair        (t/hand-type "A23A4")))
+    (is (= :high-card       (t/hand-type "23456")))))
+
+(deftest sort-test
+  (testing "Applies the correct sort logic"
+    (is (= [{:hand [3 2 10 3 13] :bid 765}
+            {:hand [13 10 11 11 10] :bid 220}
+            {:hand [13 13 6 7 7] :bid 28}
+            {:hand [10 5 5 11 5] :bid 684}
+            {:hand [12 12 12 11 14] :bid 483}]
+           (sort t/compare-hands d07-s01)))))
+
+(deftest winnings-test
+  (testing "Computes the overall winnings"
+    (is (= 6440 (t/winnings d07-s01)))))
 
 (def day07-input (u/parse-puzzle-input t/parse 2023 7))
+
+(deftest day07-part1-soln
+  (testing "Reproduces the answer for day07, part1"
+    (is (= 248396258 (t/day07-part1-soln day07-input)))))
