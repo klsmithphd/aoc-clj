e62ea07b389ec329cdc8c6d14e5e07530a691b26
Ken Smith <ken@klsmith.me>
2023-12-09 13:31:02 +0000

    2023 Day05 parse logic

--- src/aoc_clj/2023/day05.clj
+++ src/aoc_clj/2023/day05.clj
@@ -1,1 +1,25 @@
-(ns aoc-clj.2023.day05)
+(ns aoc-clj.2023.day05
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
+
+(defn str->numbers
+  [s]
+  (map read-string (str/split s #"\s")))
+
+(defn parse-map
+  [chunk]
+  (map str->numbers (rest chunk)))
+
+(defn parse-seeds
+  [seeds-str]
+  (-> (str/split seeds-str #": ")
+      second
+      str->numbers))
+
+(defn parse
+  [input]
+  (let [chunks (u/split-at-blankline input)]
+    {:seeds (parse-seeds (ffirst chunks))
+     :maps  (mapv parse-map (rest chunks))}))
+
+
