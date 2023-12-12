9d2db993201dc05b2baf3ea366247b26b26d4867
Ken Smith <ken@klsmith.me>
2023-12-12 11:29:41 +0000

    2023 Day12 Part1 soln

--- src/aoc_clj/2023/day12.clj
+++ src/aoc_clj/2023/day12.clj
@@ -3,10 +3,44 @@
 
 (defn parse-line
   [line]
-  (let [[l r] (str/split line #" ")]
-    {:springs l
-     :groups (read-string (str "[" r "]"))}))
+  (let [[left right] (str/split line #" ")]
+    [left (read-string (str "[" right "]"))]))
 
 (defn parse
   [input]
   (map parse-line input))
+
+(defn num-arrangements
+  [[spring-str groups]]
+  ;; Trim any leading "." indicating operating springs.
+  (let [springs (str/replace spring-str #"^\.+" "")]
+    (cond
+    ;; If the string is empty and there are no more groups to be found, that's an option
+      (empty? springs) (if (empty? groups) 1 0)
+    ;; If there are no more groups to be found, the string cannot contain "#"
+      (empty? groups) (if (str/includes? springs "#") 0 1)
+      (str/starts-with? springs "#")
+      (cond
+        ;; The string is shorter than required
+        (< (count springs) (first groups)) 0
+        ;; The string contains a dot before reach the size 
+        (str/includes? (subs springs 0 (first groups)) ".") 0
+        ;; The string is as long as the group
+        (= (count springs) (first groups)) (if (= (count groups) 1) 1 0)
+        ;; The next character after the matching string cannot be a "#"
+        (str/starts-with? (subs springs (first groups)) "#") 0
+        ;; This spring was a match
+        :else (num-arrangements [(subs springs (inc (first groups))) (rest groups)]))
+      ;; The first character is a "?" 
+      ;; So we try replacing it with a "#" and see how that goes or with a "."
+      ;; (which will get stripped, so we just skip it)
+      :else (+ (num-arrangements [(str "#" (subs springs 1)) groups])
+               (num-arrangements [(subs springs 1) groups])))))
+
+(defn num-arrangements-sum
+  [input]
+  (reduce + (map num-arrangements input)))
+
+(defn day12-part1-soln
+  [input]
+  (num-arrangements-sum input))
--- test/aoc_clj/2023/day12_test.clj
+++ test/aoc_clj/2023/day12_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2023.day12-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2023.day12 :as t]))
 
 (def d12-s01-raw
@@ -11,13 +12,42 @@
    "?###???????? 3,2,1"])
 
 (def d12-s01
-  [{:springs "???.###" :groups [1 1 3]}
-   {:springs ".??..??...?##." :groups [1 1 3]}
-   {:springs "?#?#?#?#?#?#?#?" :groups [1 3 1 6]}
-   {:springs "????.#...#..." :groups [4 1 1]}
-   {:springs "????.######..#####." :groups [1 6 5]}
-   {:springs "?###????????" :groups [3 2 1]}])
+  [["???.###"             [1 1 3]]
+   [".??..??...?##."      [1 1 3]]
+   ["?#?#?#?#?#?#?#?"     [1 3 1 6]]
+   ["????.#...#..."       [4 1 1]]
+   ["????.######..#####." [1 6 5]]
+   ["?###????????"        [3 2 1]]])
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d12-s01 (t/parse d12-s01-raw)))))
+
+(deftest num-arrangements-test
+  (testing "Counts the number of possible arrangements that satisfy
+            the group conditions"
+    (is (= 0 (t/num-arrangements ["" [1]])))
+    (is (= 1 (t/num-arrangements ["" []])))
+    (is (= 1 (t/num-arrangements ["?" [1]])))
+    (is (= 1 (t/num-arrangements [".?" [1]])))
+    (is (= 1 (t/num-arrangements [".?....." [1]])))
+    (is (= 1 (t/num-arrangements [".#" [1]])))
+    (is (= 2 (t/num-arrangements ["??" [1]])))
+    (is (= 0 (t/num-arrangements ["##?#?#?#?" [1 6]])))
+
+    (is (= 1 (t/num-arrangements (nth d12-s01 0))))
+    (is (= 4 (t/num-arrangements (nth d12-s01 1))))
+    (is (= 1 (t/num-arrangements (nth d12-s01 2))))
+    (is (= 1 (t/num-arrangements (nth d12-s01 3))))
+    (is (= 4 (t/num-arrangements (nth d12-s01 4))))
+    (is (= 10 (t/num-arrangements (nth d12-s01 5))))))
+
+(deftest num-arrangements-sum-test
+  (testing "Sum of all the valid arrangement counts"
+    (is (= 21 (t/num-arrangements-sum d12-s01)))))
+
+(def day12-input (u/parse-puzzle-input t/parse 2023 12))
+
+(deftest day12-part1-soln
+  (testing "Reproduces the answer for day12, part1"
+    (is (= 1 (t/day12-part1-soln day12-input)))))
