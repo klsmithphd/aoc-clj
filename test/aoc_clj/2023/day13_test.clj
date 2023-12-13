f830b8f07b5d3488743645cf4e64054b2046dd0a
Ken Smith <ken@klsmith.me>
2023-12-13 15:01:08 +0000

    2023 Day13 Part2 soln

--- test/aoc_clj/2023/day13_test.clj
+++ test/aoc_clj/2023/day13_test.clj
@@ -38,16 +38,24 @@
 
 (deftest mirror-pos-test
   (testing "Returns the position of the reflection point"
-    (is (= {:type :vertical :pos 5} (t/mirror-pos d13-s01)))
-    (is (= {:type :horizontal :pos 4} (t/mirror-pos d13-s02)))
-    (is (= {:type :vertical :pos 16} (t/mirror-pos d13-s03)))))
+    (is (= {:type :vertical :pos 5}   (t/mirror-pos 0 d13-s01)))
+    (is (= {:type :horizontal :pos 4} (t/mirror-pos 0 d13-s02)))
+    (is (= {:type :vertical :pos 16}  (t/mirror-pos 0 d13-s03)))
+
+    (is (= {:type :horizontal :pos 3} (t/mirror-pos 1 d13-s01)))
+    (is (= {:type :horizontal :pos 1} (t/mirror-pos 1 d13-s02)))))
 
 (deftest summarize-test
   (testing "Computes the summarized sum"
-    (is (= 405 (t/summarize [d13-s01 d13-s02])))))
+    (is (= 405 (t/summarize 0 [d13-s01 d13-s02])))
+    (is (= 400 (t/summarize 1 [d13-s01 d13-s02])))))
 
 (def day13-input (u/parse-puzzle-input t/parse 2023 13))
 
 (deftest day13-part1-soln
   (testing "Reproduces the answer for day13, part1"
-    (is (= 55172 (t/day13-part1-soln day13-input)))))
+    (is (= 33195 (t/day13-part1-soln day13-input)))))
+
+(deftest day13-part2-soln
+  (testing "Reproduces the answer for day13, part2"
+    (is (= 31836 (t/day13-part2-soln day13-input)))))
