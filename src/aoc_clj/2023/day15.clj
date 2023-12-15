7c6b5d5e8ecf4d482379e8ee8d70c74b4dc0ae3c
Ken Smith <ken@klsmith.me>
2023-12-15 14:04:23 +0000

    2023 Day15 Part2 soln

--- src/aoc_clj/2023/day15.clj
+++ src/aoc_clj/2023/day15.clj
@@ -19,6 +19,65 @@
   [input]
   (reduce + (map hash-alg input)))
 
+;; TODO change the utility fn to take a predicate instead of a value
+(defn index-of
+  [pred coll]
+  (ffirst (filter (comp pred second) (map-indexed vector coll))))
+
+(defn match-lens
+  [label [lens-label _]]
+  (= label lens-label))
+
+(defn seq-add
+  [boxes step]
+  (let [[label r] (str/split step #"=")
+        box-id (hash-alg label)
+        lens (read-string r)
+        index (index-of (partial match-lens label) (boxes box-id))]
+    (if index
+      (update boxes box-id #(assoc % index [label lens]))
+      (update boxes box-id #(if (seq %) (conj % [label lens]) [[label lens]])))))
+
+(defn vec-without
+  [v index]
+  (let [[l r] (split-at index v)]
+    (into [] (concat l (rest r)))))
+
+(defn seq-remove
+  [boxes step]
+  (let [label (subs step 0 (dec (count step)))
+        box-id (hash-alg label)
+        index (index-of (partial match-lens label) (boxes box-id))]
+    (if index
+      (update boxes box-id #(vec-without % index))
+      boxes)))
+
+(defn sequence-step
+  [boxes step]
+  (if (some #{\=} step)
+    (seq-add boxes step)
+    (seq-remove boxes step)))
+
+(defn lens-arrangement
+  [steps]
+  (reduce sequence-step {} steps))
+
+(defn box-focusing-power
+  [[box-id lenses]]
+  (* (inc box-id)
+     (reduce +
+             (map-indexed (fn [idx [_ lens]] (* (inc idx) lens)) lenses))))
+
+(defn focusing-power
+  [steps]
+  (->> (lens-arrangement steps)
+       (map box-focusing-power)
+       (reduce +)))
+
 (defn day15-part1-soln
   [input]
   (hash-sum input))
+
+(defn day15-part2-soln
+  [input]
+  (focusing-power input))
--- test/aoc_clj/2023/day15_test.clj
+++ test/aoc_clj/2023/day15_test.clj
@@ -28,8 +28,57 @@
   (testing "Adds the HASH values of the input strings"
     (is (= 1320 (t/hash-sum d15-s01)))))
 
+(deftest sequence-step-test
+  (testing "Incrementally processing the sequence steps"
+    (is (= {0 [["rn" 1]]}
+           (t/sequence-step {}
+                            (nth d15-s01 0))))
+    (is (= {0 [["rn" 1]]}
+           (t/sequence-step {0 [["rn" 1]]}
+                            (nth d15-s01 1))))
+    (is (= {0 [["rn" 1]] 1 [["qp" 3]]}
+           (t/sequence-step {0 [["rn" 1]]}
+                            (nth d15-s01 2))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [["qp" 3]]}
+           (t/sequence-step {0 [["rn" 1]] 1 [["qp" 3]]}
+                            (nth d15-s01 3))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 []}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [["qp" 3]]}
+                            (nth d15-s01 4))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 []}
+                            (nth d15-s01 5))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4] ["ot" 9]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4]]}
+                            (nth d15-s01 6))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4] ["ot" 9] ["ab" 5]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4] ["ot" 9]]}
+                            (nth d15-s01 7))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 9] ["ab" 5]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["pc" 4] ["ot" 9] ["ab" 5]]}
+                            (nth d15-s01 8))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 9] ["ab" 5] ["pc" 6]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 9] ["ab" 5]]}
+                            (nth d15-s01 9))))
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 7] ["ab" 5] ["pc" 6]]}
+           (t/sequence-step {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 9] ["ab" 5] ["pc" 6]]}
+                            (nth d15-s01 10))))))
+
+(deftest lens-arrangement-test
+  (testing "Final lens arrangement"
+    (is (= {0 [["rn" 1] ["cm" 2]] 1 [] 3 [["ot" 7] ["ab" 5] ["pc" 6]]}
+           (t/lens-arrangement d15-s01)))))
+
+(deftest focusing-power-test
+  (testing "Focusing power"
+    (is (= 145 (t/focusing-power d15-s01)))))
+
 (def day15-input (u/parse-puzzle-input t/parse 2023 15))
 
 (deftest day15-part1-soln
   (testing "Reproduces the answer for day15, part1"
     (is (= 511215 (t/day15-part1-soln day15-input)))))
+
+(deftest day15-part2-soln
+  (testing "Reproduces the answer for day15, part2"
+    (is (= 236057 (t/day15-part2-soln day15-input)))))
