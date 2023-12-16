eb9fe6fa6b023761a5ff4a89c8a57496f6e1d8d6
Ken Smith <ken@klsmith.me>
2023-12-16 17:36:30 +0000

    2023 Day04 cleanup

--- src/aoc_clj/2023/day04.clj
+++ src/aoc_clj/2023/day04.clj
@@ -2,75 +2,68 @@
   (:require [clojure.set :as set]
             [clojure.string :as str]))
 
-(def day04_s01_raw
-  ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-   "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
-   "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
-   "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"
-   "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
-   "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"])
-
-(def day04_s01
-  [{:winning #{41 48 83 86 17} :card #{83 86  6 31 17  9 48 53}}
-   {:winning #{13 32 20 16 61} :card #{61 30 68 82 17 32 24 19}}
-   {:winning #{1 21 53 59 44}  :card #{69 82 63 72 16 21 14  1}}
-   {:winning #{41 92 73 84 69} :card #{59 84 76 51 58  5 54 83}}
-   {:winning #{87 83 26 28 32} :card #{88 30 70 12 93 22 82 36}}
-   {:winning #{31 18 13 56 72} :card #{74 77 10 23 35 67 36 11}}])
-
 (defn parse-line
   [line]
-  (let [nums (second (str/split line #":\s+"))
+  (let [[_ nums]       (str/split line #":\s+")
         [winning card] (str/split nums #"\s+\|\s+")]
     {:winning (into #{} (map read-string (str/split winning #"\s+")))
-     :card (into #{} (map read-string (str/split card #"\s+")))}))
+     :card    (into #{} (map read-string (str/split card #"\s+")))}))
 
 (defn parse
   [input]
   (mapv parse-line input))
 
-(defn card-score
+(defn winning-matches
+  "Computes how many values overlap between the winning numbers and the card"
   [{:keys [winning card]}]
   (count (set/intersection winning card)))
 
 (defn points
+  "The first match makes the card worth one point and each match after the 
+   first doubles the point value of that card."
   [card]
-  (let [matches (card-score card)]
+  (let [matches (winning-matches card)]
     (if (zero? matches)
       0
       (int (Math/pow 2 (dec matches))))))
 
 (defn points-sum
+  "Returns the total of all the point scores for each of the cards"
   [cards]
   (reduce + (map points cards)))
 
 (defn update-card-count
+  "Applies the rules of part2 to update the number of remaining cards
+   based on the number of winning number matches on the current card,
+   i.e., the current card's `score`.
+   
+   You win copies of the scratchcards below the winning card equal 
+   to the number of matches."
   [{:keys [card-cnt total]} score]
-  (let [this-count (first card-cnt)
-        new-counts (map #(+ this-count %) (take score (rest card-cnt)))]
-    {:card-cnt (concat new-counts (drop score (rest card-cnt)))
+  (let [this-count (first card-cnt)]
+    {:card-cnt (concat
+                (map #(+ this-count %) (take score (rest card-cnt)))
+                (drop score (rest card-cnt)))
      :total    (+ total this-count)}))
 
-(update-card-count {:card-cnt [1 1 1 1 1 1] :total 0} 4)
-(update-card-count {:card-cnt [2 2 2 2 1] :total 1} 2)
-(update-card-count {:card-cnt [4 4 2 1] :total 3} 2)
-(update-card-count {:card-cnt [8 6 1] :total 7} 1)
-(update-card-count {:card-cnt [14 1] :total 15} 0)
-(update-card-count {:card-cnt [1] :total 29} 0)
-
-(reduce update-card-count {:card-cnt [1 1 1 1 1 1] :total 0} [4 2 2 1 0 0])
-
 (defn total-cards
+  "Determines the total number of scratchcards you have applying
+   the logic of part2"
   [cards]
-  (let [scores (map card-score cards)
-        card-cnt (repeat (count cards) 1)]
-    (:total (reduce update-card-count {:card-cnt card-cnt :total 0} scores))))
-
+  (let [scores   (map winning-matches cards)
+        card-cnt (repeat (count cards) 1)
+        init     {:card-cnt card-cnt :total 0}]
+    (:total (reduce update-card-count init scores))))
 
 (defn day04-part1-soln
+  "Take a seat in the large pile of colorful cards.
+   How many points are they worth in total?"
   [input]
   (points-sum input))
 
 (defn day04-part2-soln
+  "Process all of the original and copied scratchcards until no more 
+   scratchcards are won. Including the original set of scratchcards, 
+   how many total scratchcards do you end up with?"
   [input]
   (total-cards input))
--- test/aoc_clj/2023/day04_test.clj
+++ test/aoc_clj/2023/day04_test.clj
@@ -4,7 +4,7 @@
             [aoc-clj.2023.day04 :as t]))
 
 
-(def day04_s01_raw
+(def d04-s01-raw
   ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
@@ -12,7 +12,7 @@
    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"])
 
-(def day04_s01
+(def d04-s01
   [{:winning #{41 48 83 86 17} :card #{83 86  6 31 17  9 48 53}}
    {:winning #{13 32 20 16 61} :card #{61 30 68 82 17 32 24 19}}
    {:winning #{1 21 53 59 44}  :card #{69 82 63 72 16 21 14  1}}
@@ -22,15 +22,39 @@
 
 (deftest parse-test
   (testing "Can parse the input correctly"
-    (is (= day04_s01 (t/parse day04_s01_raw)))))
+    (is (= d04-s01 (t/parse d04-s01-raw)))))
+
+(deftest winning-matches-test
+  (testing "Determines how many winning matches a card has"
+    (is (= [4 2 2 1 0 0] (map t/winning-matches d04-s01)))))
 
 (deftest points-test
   (testing "Scores the card correctly with the number of points"
-    (is (= [8 2 2 1 0 0] (map t/points day04_s01)))))
+    (is (= [8 2 2 1 0 0] (map t/points d04-s01)))))
 
 (deftest points-sum-test
   (testing "Computes the sum of the points of each card"
-    (is (= 13 (t/points-sum day04_s01)))))
+    (is (= 13 (t/points-sum d04-s01)))))
+
+(deftest update-card-count-test
+  (testing "Determines how many of each of the lower cards there are based
+            on the current number of matches"
+    (is (= {:card-cnt [2 2 2 2 1] :total 1}
+           (t/update-card-count {:card-cnt [1 1 1 1 1 1] :total 0} 4)))
+    (is (= {:card-cnt [4 4 2 1] :total 3}
+           (t/update-card-count {:card-cnt [2 2 2 2 1] :total 1} 2)))
+    (is (= {:card-cnt [8 6 1] :total 7}
+           (t/update-card-count {:card-cnt [4 4 2 1] :total 3} 2)))
+    (is (= {:card-cnt [14 1] :total 15}
+           (t/update-card-count {:card-cnt [8 6 1] :total 7} 1)))
+    (is (= {:card-cnt [1] :total 29}
+           (t/update-card-count {:card-cnt [14 1] :total 15} 0)))
+    (is (= {:card-cnt [] :total 30}
+           (t/update-card-count {:card-cnt [1] :total 29} 0)))))
+
+(deftest total-cards-test
+  (testing "Computes the total number of cards you have in part2"
+    (is (= 30 (t/total-cards d04-s01)))))
 
 (def day04-input (u/parse-puzzle-input t/parse 2023 4))
 
@@ -41,22 +65,3 @@
 (deftest day04-part2-soln
   (testing "Reproduces the answer for day04, part2"
     (is (= 5744979 (t/day04-part2-soln day04-input)))))
-
-
-;; ;; Scores 
-;; [4 2 2 1 0 0]
-
-;; ;; Card Count
-;; [1 1 1 1 1 1] ;; START
-;; ;; idx = 0, score = 4, cnt = 1, add 1 to next 4
-;; [1 2 2 2 2 1]
-;; ;; idx = 1, score = 2, cnt = 2, add 2 to next 2
-;; [1 2 4 4 2 1]
-;; ;; idx = 2, score = 2, cnt = 4, add 4 to next 2
-;; [1 2 4 8 6 1]
-;; ;; idx = 3, score = 1, cnt = 8, add 8 to next 1
-;; [1 2 4 8 14 1]
-;; ;; idx = 4, score = 0, cnt = 14, do nothing
-;; [1 2 4 8 14 1]
-;; ;; idx = 5, score = 0, cnt = 1, do nothing
-;; [1 2 4 8 14 1] ;; FINAL
