e3ba9ca215b8e0781bee209012634a15967240e4
Ken Smith <ken@klsmith.me>
2023-12-17 17:14:20 +0000

    2023 Day17 Part1 soln

--- src/aoc_clj/2023/day17.clj
+++ src/aoc_clj/2023/day17.clj
@@ -1,5 +1,8 @@
 (ns aoc-clj.2023.day17
   (:require [clojure.string :as str]
+            [aoc-clj.utils.graph :as g :refer [Graph]]
+            [aoc-clj.utils.grid :as grid :refer [width height value]]
+            [aoc-clj.utils.grid.vecgrid :as vg]
             [aoc-clj.utils.core :as u]))
 
 (defn parse-line
@@ -8,4 +11,70 @@
 
 (defn parse
   [input]
-  (mapv parse-line input))
+  (vg/->VecGrid2D (mapv parse-line input)))
+
+(def allowed-headings
+  {:R [:R :U :D]
+   :L [:L :U :D]
+   :U [:U :L :R]
+   :D [:D :L :R]})
+
+;; TODO - Copied from Day 16 -- should be a library fn
+(defn next-cell
+  "Returns the next cell from the position along the `heading` direction"
+  [[x y] heading]
+  (case heading
+    :U [x (dec y)]
+    :R [(inc x) y]
+    :D [x (inc y)]
+    :L [(dec x) y]))
+
+;; TODO - Copied from Day 16 -- should be a library fn
+(defn in-grid?
+  "Returns true if the position is contained within the `grid`"
+  [grid [x y]]
+  (let [h (dec (height grid))
+        w (dec (width grid))]
+    (and (<= 0 x w) (<= 0 y h))))
+
+(defn new-vertex
+  [pos heading count]
+  {:pos pos :heading heading :count count})
+
+(defn next-vertices
+  [grid {:keys [pos heading count]}]
+  (let [headings  (allowed-headings heading)
+        positions (map #(next-cell pos %) headings)
+        counts    [(inc count) 0 0]
+        vertices  (map new-vertex positions headings counts)]
+    (->> vertices
+         (filter #(in-grid? grid (:pos %)))
+         (remove #(= 3 (:count %))))))
+
+(defrecord CityGrid [grid]
+  Graph
+  (edges
+    [_ v]
+    (next-vertices grid v))
+  (distance
+    [_ _ {:keys [pos]}]
+    (value grid pos)))
+
+(defn min-heat-loss
+  [grid]
+  (let [ex (dec (width grid))
+        ey (dec (height grid))
+        graph (->CityGrid grid)]
+    (->>
+     (g/dijkstra graph
+                 {:pos [0 0] :heading :R :count 0}
+                 #(= [ex ey] (:pos %))
+                 :limit 10000000)
+     (drop 1)
+     (map :pos)
+     (map #(value grid %))
+     (reduce +))))
+
+(defn day17-part1-soln
+  [input]
+  (min-heat-loss input))
--- test/aoc_clj/2023/day17_test.clj
+++ test/aoc_clj/2023/day17_test.clj
@@ -1,5 +1,7 @@
 (ns aoc-clj.2023.day17-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.vecgrid :as vg]
             [aoc-clj.2023.day17 :as t]))
 
 (def d17-s01-raw
@@ -18,20 +20,56 @@
    "4322674655533"])
 
 (def d17-s01
-  [[2 4 1 3 4 3 2 3 1 1 3 2 3]
-   [3 2 1 5 4 5 3 5 3 5 6 2 3]
-   [3 2 5 5 2 4 5 6 5 4 2 5 4]
-   [3 4 4 6 5 8 5 8 4 5 4 5 2]
-   [4 5 4 6 6 5 7 8 6 7 5 3 6]
-   [1 4 3 8 5 9 8 7 9 8 4 5 4]
-   [4 4 5 7 8 7 6 9 8 7 7 6 6]
-   [3 6 3 7 8 7 7 9 7 9 6 5 3]
-   [4 6 5 4 9 6 7 9 8 6 8 8 7]
-   [4 5 6 4 6 7 9 9 8 6 4 5 3]
-   [1 2 2 4 6 8 6 8 6 5 5 6 3]
-   [2 5 4 6 5 4 8 8 8 7 7 3 5]
-   [4 3 2 2 6 7 4 6 5 5 5 3 3]])
+  (vg/->VecGrid2D
+   [[2 4 1 3 4 3 2 3 1 1 3 2 3]
+    [3 2 1 5 4 5 3 5 3 5 6 2 3]
+    [3 2 5 5 2 4 5 6 5 4 2 5 4]
+    [3 4 4 6 5 8 5 8 4 5 4 5 2]
+    [4 5 4 6 6 5 7 8 6 7 5 3 6]
+    [1 4 3 8 5 9 8 7 9 8 4 5 4]
+    [4 4 5 7 8 7 6 9 8 7 7 6 6]
+    [3 6 3 7 8 7 7 9 7 9 6 5 3]
+    [4 6 5 4 9 6 7 9 8 6 8 8 7]
+    [4 5 6 4 6 7 9 9 8 6 4 5 3]
+    [1 2 2 4 6 8 6 8 6 5 5 6 3]
+    [2 5 4 6 5 4 8 8 8 7 7 3 5]
+    [4 3 2 2 6 7 4 6 5 5 5 3 3]]))
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d17-s01 (t/parse d17-s01-raw)))))
+
+(deftest next-vertices-test
+  (testing "Determines the next allowed vertices, and doesn't allow
+            three straight-line segments in a row"
+    (is (= [{:pos [1 0] :heading :R :count 1}
+            {:pos [0 1] :heading :D :count 0}]
+           (t/next-vertices d17-s01 {:pos [0 0] :heading :R :count 0})))
+    (is (= [{:pos [2 0] :heading :R :count 2}
+            {:pos [1 1] :heading :D :count 0}]
+           (t/next-vertices d17-s01 {:pos [1 0] :heading :R :count 1})))
+    (is (= [{:pos [0 2] :heading :D :count 1}
+            {:pos [1 1] :heading :R :count 0}]
+           (t/next-vertices d17-s01 {:pos [0 1] :heading :D :count 0})))
+    (is (= [{:pos [2 1] :heading :D :count 0}]
+           (t/next-vertices d17-s01 {:pos [2 0] :heading :R :count 2})))
+    (is (= [{:pos [1 2] :heading :D :count 1}
+            {:pos [0 1] :heading :L :count 0}
+            {:pos [2 1] :heading :R :count 0}]
+           (t/next-vertices d17-s01 {:pos [1 1] :heading :D :count 0})))
+    (is (= [{:pos [1 2] :heading :R :count 0}]
+           (t/next-vertices d17-s01 {:pos [0 2] :heading :D :count 2})))
+    (is (= [{:pos [2 1] :heading :R :count 1}
+            {:pos [1 0] :heading :U :count 0}
+            {:pos [1 2] :heading :D :count 0}]
+           (t/next-vertices d17-s01  {:pos [1 1] :heading :R :count 0})))))
+
+(deftest min-heat-loss-test
+  (testing "Computes the minimum possible heat loss"
+    (is (= 102 (t/min-heat-loss d17-s01)))))
+
+(def day17-input (u/parse-puzzle-input t/parse 2023 17))
+
+(deftest day17-part1-soln
+  (testing "Reproduces the answer for day17, part1"
+    (is (= 843 (t/day17-part1-soln day17-input)))))
