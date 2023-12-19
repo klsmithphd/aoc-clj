0d634b90d13c49469cd2ebcc706c7f405fd7196d
Ken Smith <ken@klsmith.me>
2023-12-19 16:38:46 +0000

    2023 Day19 Part1 soln

--- src/aoc_clj/2023/day19.clj
+++ src/aoc_clj/2023/day19.clj
@@ -2,31 +2,90 @@
   (:require [clojure.string :as str]
             [aoc-clj.utils.core :as u]))
 
-(defn parse-cond
-  [cond-str]
-  (if (nil? (str/index-of cond-str ":"))
-    {:outcome cond-str}
-    (let [value (subs cond-str 0 1)
-          oper  (subs cond-str 1 2)
-          [num outcome] (str/split (subs cond-str 2) #":")]
+(defn parse-rule
+  [rule-str]
+  (if (nil? (str/index-of rule-str ":"))
+    {:outcome (keyword rule-str)}
+    (let [value (subs rule-str 0 1)
+          oper  (subs rule-str 1 2)
+          [num outcome] (str/split (subs rule-str 2) #":")]
       {:value value
        :oper oper
        :num (read-string num)
-       :outcome outcome})))
+       :outcome (keyword outcome)})))
 
-(defn parse-rule
-  [rule-str]
-  (let [[id rules]    (str/split rule-str #" ")
-        trimmed-rules (subs rules 1 (dec (count rules)))]
-    [id (map parse-cond (str/split trimmed-rules #","))]))
+(defn parse-workflow
+  [workflow-str]
+  (let [[id rules]    (str/split workflow-str #"\{")
+        trimmed-rules (subs rules 0 (dec (count rules)))]
+    [(keyword id) (map parse-rule (str/split trimmed-rules #","))]))
 
-(defn parse-rating
-  [rating-str]
-  (mapv read-string (re-seq #"\d+" rating-str)))
+(defn parse-parts
+  [input-str]
+  (mapv read-string (re-seq #"\d+" input-str)))
 
 (defn parse
   [input]
-  (let [[rules ratings] (u/split-at-blankline input)]
-    {:rules   (into {} (map parse-rule rules))
+  (let [[workflows inputs] (u/split-at-blankline input)]
+    {:workflows (into {} (map parse-workflow workflows))
+     :parts     (map parse-parts inputs)}))
 
-     :ratings (map parse-rating ratings)}))
+(defn cond->str
+  "Construct a cond clause as a string for each of the non-terminal 
+   workflow rules"
+  [{:keys [value oper num outcome]}]
+  (str "(" oper " " value " " num ")" " " outcome))
+
+(defn workflow->fn-str
+  "Create a valid clojure function definition as a string based on the
+   rules structure"
+  [rules]
+  (let [opening ["(fn [[x m a s]]"
+                 "(cond"]
+        body    (into opening (map cond->str (butlast rules)))
+        closing (into body [(str ":else "
+                                 (:outcome (last rules))
+                                 "))")])]
+    (str/join " " closing)))
+
+(defn workflow->fn
+  "Create clojure functions for each of the workflow rules structures"
+  [rules]
+  (eval (read-string (workflow->fn-str rules))))
+
+(defn functionized-input
+  "Augments the input data structure with a `workflow-fns` key that
+   has the compiled workflow logic"
+  [{:keys [workflows] :as input}]
+  (assoc input :workflow-fns (u/fmap workflow->fn workflows)))
+
+(defn apply-workflows
+  "Executes the workflow functions against the part ratings data
+   to determine whether it is accepted (`:A`) or rejected (`:R`)"
+  [{:keys [workflow-fns]} part]
+  (loop [workflow (get workflow-fns :in)]
+    (let [outcome (workflow part)]
+      (if (or (= :A outcome) (= :R outcome))
+        outcome
+        (recur (get workflow-fns outcome))))))
+
+(defn accepted-parts
+  "Returns the parts data for the items that are accepted by the workflows"
+  [{:keys [parts] :as input}]
+  (let [input-fns (functionized-input input)]
+    (filter #(= :A (apply-workflows input-fns %)) parts)))
+
+(defn accepted-parts-sum
+  "Computes the sum of all the ratings values for all the parts that
+   are accepted by the workflow"
+  [input]
+  (->> (accepted-parts input)
+       (map (partial reduce +))
+       (reduce +)))
+
+(defn day19-part1-soln
+  "Sort through all of the parts you've been given; what do you get if you 
+   add together all of the rating numbers for all of the parts that 
+   ultimately get accepted?"
+  [input]
+  (accepted-parts-sum input))
--- test/aoc_clj/2023/day19_test.clj
+++ test/aoc_clj/2023/day19_test.clj
@@ -4,17 +4,17 @@
             [aoc-clj.2023.day19 :as t]))
 
 (def d19-s01-raw
-  ["px {a<2006:qkq,m>2090:A,rfg}"
-   "pv {a>1716:R,A}"
-   "lnx {m>1548:A,A}"
-   "rfg {s<537:gd,x>2440:R,A}"
-   "qs {s>3448:A,lnx}"
-   "qkq {x<1416:A,crn}"
-   "crn {x>2662:A,R}"
-   "in {s<1351:px,qqz}"
-   "qqz {s>2770:qs,m<1801:hdj,R}"
-   "gd {a>3333:R,R}"
-   "hdj {m>838:A,pv}"
+  ["px{a<2006:qkq,m>2090:A,rfg}"
+   "pv{a>1716:R,A}"
+   "lnx{m>1548:A,A}"
+   "rfg{s<537:gd,x>2440:R,A}"
+   "qs{s>3448:A,lnx}"
+   "qkq{x<1416:A,crn}"
+   "crn{x>2662:A,R}"
+   "in{s<1351:px,qqz}"
+   "qqz{s>2770:qs,m<1801:hdj,R}"
+   "gd{a>3333:R,R}"
+   "hdj{m>838:A,pv}"
    ""
    "{x=787,m=2655,a=1222,s=2876}"
    "{x=1679,m=44,a=2067,s=496}"
@@ -23,37 +23,71 @@
    "{x=2127,m=1623,a=2188,s=1013}"])
 
 (def d19-s01
-  {:rules {"px"  [{:value "a", :oper "<", :num 2006, :outcome "qkq"}
-                  {:value "m", :oper ">", :num 2090, :outcome "A"}
-                  {:outcome "rfg"}],
-           "pv"  [{:value "a", :oper ">", :num 1716, :outcome "R"}
-                  {:outcome "A"}],
-           "lnx" [{:value "m", :oper ">", :num 1548, :outcome "A"}
-                  {:outcome "A"}],
-           "rfg" [{:value "s", :oper "<", :num 537, :outcome "gd"}
-                  {:value "x", :oper ">", :num 2440, :outcome "R"}
-                  {:outcome "A"}],
-           "qs"  [{:value "s", :oper ">", :num 3448, :outcome "A"}
-                  {:outcome "lnx"}],
-           "qkq" [{:value "x", :oper "<", :num 1416, :outcome "A"}
-                  {:outcome "crn"}],
-           "crn" [{:value "x", :oper ">", :num 2662, :outcome "A"}
-                  {:outcome "R"}],
-           "in"  [{:value "s", :oper "<", :num 1351, :outcome "px"}
-                  {:outcome "qqz"}]
-           "qqz" [{:value "s", :oper ">", :num 2770, :outcome "qs"}
-                  {:value "m", :oper "<", :num 1801, :outcome "hdj"}
-                  {:outcome "R"}],
-           "gd"  [{:value "a", :oper ">", :num 3333, :outcome "R"}
-                  {:outcome "R"}],
-           "hdj" [{:value "m", :oper ">", :num 838, :outcome "A"}
-                  {:outcome "pv"}]}
-   :ratings [[787 2655 1222 2876]
-             [1679 44 2067 496]
-             [2036 264 79 2244]
-             [2461 1339 466 291]
-             [2127 1623 2188 1013]]})
+  {:workflows {:px  [{:value "a", :oper "<", :num 2006, :outcome :qkq}
+                     {:value "m", :oper ">", :num 2090, :outcome :A}
+                     {:outcome :rfg}],
+               :pv  [{:value "a", :oper ">", :num 1716, :outcome :R}
+                     {:outcome :A}],
+               :lnx [{:value "m", :oper ">", :num 1548, :outcome :A}
+                     {:outcome :A}],
+               :rfg [{:value "s", :oper "<", :num 537, :outcome :gd}
+                     {:value "x", :oper ">", :num 2440, :outcome :R}
+                     {:outcome :A}],
+               :qs  [{:value "s", :oper ">", :num 3448, :outcome :A}
+                     {:outcome :lnx}],
+               :qkq [{:value "x", :oper "<", :num 1416, :outcome :A}
+                     {:outcome :crn}],
+               :crn [{:value "x", :oper ">", :num 2662, :outcome :A}
+                     {:outcome :R}],
+               :in  [{:value "s", :oper "<", :num 1351, :outcome :px}
+                     {:outcome :qqz}]
+               :qqz [{:value "s", :oper ">", :num 2770, :outcome :qs}
+                     {:value "m", :oper "<", :num 1801, :outcome :hdj}
+                     {:outcome :R}],
+               :gd  [{:value "a", :oper ">", :num 3333, :outcome :R}
+                     {:outcome :R}],
+               :hdj [{:value "m", :oper ">", :num 838, :outcome :A}
+                     {:outcome :pv}]}
+   :parts [[787 2655 1222 2876]
+           [1679 44 2067 496]
+           [2036 264 79 2244]
+           [2461 1339 466 291]
+           [2127 1623 2188 1013]]})
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d19-s01 (t/parse d19-s01-raw)))))
+
+(deftest workflow->fn-str-test
+  (testing "Converts the rules in a workflow into a string representation of
+            a clojure function"
+    (is (= "(fn [[x m a s]] (cond (< a 2006) :qkq (> m 2090) :A :else :rfg))"
+           (t/workflow->fn-str (get-in d19-s01 [:workflows :px]))))
+    (is (= "(fn [[x m a s]] (cond (> m 838) :A :else :pv))"
+           (t/workflow->fn-str (get-in d19-s01 [:workflows :hdj]))))))
+
+(def d19-s01-fns (t/functionized-input d19-s01))
+(deftest outcome-test
+  (testing "Runs the workflows and returns the correct verdict for each part"
+    (is (= :A (t/apply-workflows d19-s01-fns (nth (:parts d19-s01) 0))))
+    (is (= :R (t/apply-workflows d19-s01-fns (nth (:parts d19-s01) 1))))
+    (is (= :A (t/apply-workflows d19-s01-fns (nth (:parts d19-s01) 2))))
+    (is (= :R (t/apply-workflows d19-s01-fns (nth (:parts d19-s01) 3))))
+    (is (= :A (t/apply-workflows d19-s01-fns (nth (:parts d19-s01) 4))))))
+
+(deftest accepted-parts-test
+  (testing "Returns the parts that are accepted"
+    (is (= [[787 2655 1222 2876]
+            [2036 264 79 2244]
+            [2127 1623 2188 1013]]
+           (t/accepted-parts d19-s01)))))
+
+(deftest accepted-parts-sum-test
+  (testing "Returns the sum of all the ratings of all the accepted parts"
+    (is (= 19114 (t/accepted-parts-sum d19-s01)))))
+
+(def day19-input (u/parse-puzzle-input t/parse 2023 19))
+
+(deftest day19-part1-soln
+  (testing "Reproduces the answer for day19, part1"
+    (is (= 319062 (t/day19-part1-soln day19-input)))))
