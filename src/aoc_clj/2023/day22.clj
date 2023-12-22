3dcf90c7fadc9f929b95ebf889863770e8e25aae
Ken Smith <ken@klsmith.me>
2023-12-22 17:50:33 +0000

    2023 Day22 Part1 WIP too high

--- src/aoc_clj/2023/day22.clj
+++ src/aoc_clj/2023/day22.clj
@@ -11,50 +11,75 @@
   [input]
   (sort-by (comp peek first) (map parse-line input)))
 
-(defn expanded-brick-coords
-  [[[sx sy sz] [ex ey ez]]]
-  (for [z (range sz (inc ez))
-        y (range sy (inc ey))
-        x (range sx (inc ex))]
-    [x y z]))
+;; (defn expanded-brick-coords
+;;   [[[sx sy sz] [ex ey ez]]]
+;;   (for [z (range sz (inc ez))
+;;         y (range sy (inc ey))
+;;         x (range sx (inc ex))]
+;;     [x y z]))
 
-(defn xy-vals
-  [brick]
-  (set (map (comp vec #(take 2 %)) brick)))
+;; (defn xy-vals
+;;   [brick]
+;;   (set (map (comp vec #(take 2 %)) brick)))
 
 (defn lowest-z
   [brick]
-  (peek (first brick)))
+  (apply min (keys brick)))
+
+(defn brick-z-rep
+  [[[sx sy sz] [ex ey ez]]]
+  (into {} (for [z (range sz (inc ez))]
+             [z (set (for [y (range sy (inc ey))
+                           x (range sx (inc ex))]
+                       [x y]))])))
 
 (defn at-rest?
   [z-index brick]
-  (if (= 1 (lowest-z brick))
-    true
-    (seq (set/intersection (xy-vals brick)
-                           (z-index (dec (lowest-z brick)))))))
+  (let [brick-lowest-z (lowest-z brick)]
+    (if (= 1 brick-lowest-z)
+      true
+      (seq (set/intersection (first (vals brick))
+                             (z-index (dec brick-lowest-z)))))))
 
 (defn lower-brick
   [z-index brick]
   (loop [next-brick brick]
     (if (at-rest? z-index next-brick)
       next-brick
-      (recur (mapv #(update % 2 dec) next-brick)))))
+      (recur (u/kmap dec next-brick)))))
 
-(defn update-z-index
-  [index brick]
-  (merge-with into index (->> (group-by peek brick)
-                              (u/fmap xy-vals))))
+;; (defn update-z-index
+;;   [index brick]
+;;   (merge-with into index brick))
 
 (defn place-brick
   [{:keys [z-index bricks]} brick]
   (let [updated-brick (lower-brick z-index brick)]
-    {:z-index (update-z-index z-index updated-brick)
+    {:z-index (merge-with into z-index updated-brick)
      :bricks  (conj bricks updated-brick)}))
 
 (defn place-bricks
   [bricks]
   (reduce place-brick {:z-index {} :bricks []} bricks))
 
+(defn disintegratable?
+  [{:keys [z-index bricks]} brick]
+  (let [brick-z      (lowest-z brick)
+        above-bricks (filter #(= (inc brick-z) (lowest-z %)) bricks)
+        z-index-wo-brick (merge-with set/difference z-index brick)]
+    (or (empty? above-bricks)
+        (every? #(at-rest? z-index-wo-brick %) above-bricks))))
+
+(defn disintegratable-count
+  [bricks]
+  (let [placed-bricks (place-bricks (map brick-z-rep bricks))]
+    (->> (:bricks placed-bricks)
+         (filter #(disintegratable? placed-bricks %))
+         count)))
+
+(defn day22-part1-soln
+  [input]
+  (disintegratable-count input))
 
 
 
--- test/aoc_clj/2023/day22_test.clj
+++ test/aoc_clj/2023/day22_test.clj
@@ -21,79 +21,105 @@
    [[0 1 6] [2 1 6]]
    [[1 1 8] [1 1 9]]])
 
-(def d22-s01-expanded
-  [[[1 0 1] [1 1 1] [1 2 1]]
-   [[0 0 2] [1 0 2] [2 0 2]]
-   [[0 2 3] [1 2 3] [2 2 3]]
-   [[0 0 4] [0 1 4] [0 2 4]]
-   [[2 0 5] [2 1 5] [2 2 5]]
-   [[0 1 6] [1 1 6] [2 1 6]]
-   [[1 1 8] [1 1 9]]])
+(def d22-s01-z-rep
+  [{1 #{[1 0] [1 1] [1 2]}}
+   {2 #{[0 0] [1 0] [2 0]}}
+   {3 #{[0 2] [1 2] [2 2]}}
+   {4 #{[0 0] [0 1] [0 2]}}
+   {5 #{[2 0] [2 1] [2 2]}}
+   {6 #{[0 1] [1 1] [2 1]}}
+   {8 #{[1 1]}
+    9 #{[1 1]}}])
+
+;; (def d22-s01-expanded
+;;   [[[1 0 1] [1 1 1] [1 2 1]]
+;;    [[0 0 2] [1 0 2] [2 0 2]]
+;;    [[0 2 3] [1 2 3] [2 2 3]]
+;;    [[0 0 4] [0 1 4] [0 2 4]]
+;;    [[2 0 5] [2 1 5] [2 2 5]]
+;;    [[0 1 6] [1 1 6] [2 1 6]]
+;;    [[1 1 8] [1 1 9]]])
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d22-s01 (t/parse d22-s01-raw)))))
 
-(deftest expanded-brick-coords-test
-  (testing "Expands the input into occupied coordinate cells"
-    (is (= d22-s01-expanded (map t/expanded-brick-coords d22-s01)))))
+;; (deftest expanded-brick-coords-test
+;;   (testing "Expands the input into occupied coordinate cells"
+;;     (is (= d22-s01-expanded (map t/expanded-brick-coords d22-s01)))))
 
-(deftest xy-vals-test
-  (testing "Returns only the xy positions of a brick"
-    (is (= [#{[1 0] [1 1] [1 2]}
-            #{[0 0] [1 0] [2 0]}
-            #{[0 2] [1 2] [2 2]}
-            #{[0 0] [0 1] [0 2]}
-            #{[2 0] [2 1] [2 2]}
-            #{[0 1] [1 1] [2 1]}
-            #{[1 1]}]
-           (map t/xy-vals d22-s01-expanded)))))
+;; (deftest xy-vals-test
+;;   (testing "Returns only the xy positions of a brick"
+;;     (is (= [#{[1 0] [1 1] [1 2]}
+;;             #{[0 0] [1 0] [2 0]}
+;;             #{[0 2] [1 2] [2 2]}
+;;             #{[0 0] [0 1] [0 2]}
+;;             #{[2 0] [2 1] [2 2]}
+;;             #{[0 1] [1 1] [2 1]}
+;;             #{[1 1]}]
+;;            (map t/xy-vals d22-s01-expanded)))))
 
 (deftest lowest-z-test
   (testing "Computes the lowest z position of a brick"
-    (is (= [1 2 3 4 5 6 8] (map t/lowest-z d22-s01-expanded)))))
+    (is (= [1 2 3 4 5 6 8] (map t/lowest-z d22-s01-z-rep)))))
+
+(deftest brick-z-rep-test
+  (testing "Converts the brick expression into occupied cells at each z-level"
+    (is (= d22-s01-z-rep (map t/brick-z-rep d22-s01)))))
+
 
 (deftest place-brick-test
   (testing "Places each brick in correct location in sample data"
     (is (= {:z-index {1 #{[1 0] [1 1] [1 2]}}
-            :bricks [[[1 0 1] [1 1 1] [1 2 1]]]}
-           (t/place-brick {:z-index {} :bricks []} (nth d22-s01-expanded 0))))
+            :bricks [{1 #{[1 0] [1 1] [1 2]}}]}
+           (t/place-brick {:z-index {} :bricks []} (nth d22-s01-z-rep 0))))
 
     (is (= {:z-index {1 #{[1 0] [1 1] [1 2]}
                       2 #{[0 0] [1 0] [2 0]}}
-            :bricks [[[1 0 1] [1 1 1] [1 2 1]]
-                     [[0 0 2] [1 0 2] [2 0 2]]]}
+            :bricks [{1 #{[1 0] [1 1] [1 2]}}
+                     {2 #{[0 0] [1 0] [2 0]}}]}
            (t/place-brick {:z-index {1 #{[1 0] [1 1] [1 2]}}
-                           :bricks [[[1 0 1] [1 1 1] [1 2 1]]]}
-                          (nth d22-s01-expanded 1))))
+                           :bricks [{1 #{[1 0] [1 1] [1 2]}}]}
+                          (nth d22-s01-z-rep 1))))
+
     (is (= {:z-index {1 #{[1 0] [1 1] [1 2]}
-                      2 #{[2 2] [0 0] [1 0] [0 2] [2 0] [1 2]}}
-            :bricks [[[1 0 1] [1 1 1] [1 2 1]]
-                     [[0 0 2] [1 0 2] [2 0 2]]
-                     [[0 2 2] [1 2 2] [2 2 2]]]}
+                      2 #{[0 0] [1 0] [2 0] [0 2] [1 2] [2 2]}}
+            :bricks [{1 #{[1 0] [1 1] [1 2]}}
+                     {2 #{[0 0] [1 0] [2 0]}}
+                     {2 #{[0 2] [1 2] [2 2]}}]}
            (t/place-brick {:z-index {1 #{[1 0] [1 1] [1 2]}
                                      2 #{[0 0] [1 0] [2 0]}}
-                           :bricks [[[1 0 1] [1 1 1] [1 2 1]]
-                                    [[0 0 2] [1 0 2] [2 0 2]]]}
-                          (nth d22-s01-expanded 2))))))
+                           :bricks [{1 #{[1 0] [1 1] [1 2]}}
+                                    {2 #{[0 0] [1 0] [2 0]}}]}
+                          (nth d22-s01-z-rep 2))))))
 
+(def d22-s01-placed-bricks (t/place-bricks d22-s01-z-rep))
 (deftest place-bricks-test
   (testing "Correctly determines resting place/coords of each brick"
-    (is (= [[[1 0 1] [1 1 1] [1 2 1]]
-            [[0 0 2] [1 0 2] [2 0 2]]
-            [[0 2 2] [1 2 2] [2 2 2]]
-            [[0 0 3] [0 1 3] [0 2 3]]
-            [[2 0 3] [2 1 3] [2 2 3]]
-            [[0 1 4] [1 1 4] [2 1 4]]
-            [[1 1 5] [1 1 6]]]
-           (:bricks (t/place-bricks d22-s01-expanded))))))
+    (is (= [{1 #{[1 0] [1 1] [1 2]}}
+            {2 #{[0 0] [1 0] [2 0]}}
+            {2 #{[0 2] [1 2] [2 2]}}
+            {3 #{[0 0] [0 1] [0 2]}}
+            {3 #{[2 0] [2 1] [2 2]}}
+            {4 #{[0 1] [1 1] [2 1]}}
+            {5 #{[1 1]}
+             6 #{[1 1]}}]
+           (:bricks d22-s01-placed-bricks)))))
 
+(deftest distintegratable?-test
+  (testing "Determines whether a brick can be distintegrated
+            without affecting any bricks above it"
+    (is (= [false true true true true false true]
+           (map #(t/disintegratable? d22-s01-placed-bricks %)
+                (:bricks d22-s01-placed-bricks))))))
+
+(deftest disintegratable-count-test
+  (testing "Counts how many placed bricks can be disintegrated"
+    (is (= 5 (t/disintegratable-count d22-s01)))))
 
 (def day22-input (u/parse-puzzle-input t/parse 2023 22))
 
-(t/place-brick {:z-index {} :bricks []} (nth d22-s01-expanded 0))
-(t/place-brick {:z-index {1 #{[1 0] [1 1] [1 2]}}
-                :bricks [[[1 0 1] [1 1 1] [1 2 1]]]}
-               (nth d22-s01-expanded 1))
-
-
+(deftest day22-part1-soln
+  (testing "Reproduces the answer for day22, part1"
+    ;; 461 is too high
+    (is (= 461 (t/day22-part1-soln day22-input)))))
