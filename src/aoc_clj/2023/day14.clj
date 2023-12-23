aa6013d4aadd7996b4798f7266cce918d729b20a
Ken Smith <ken@klsmith.me>
2023-12-23 21:23:23 +0000

    2023 14 Part2 soln but somehow broke part1

--- src/aoc_clj/2023/day14.clj
+++ src/aoc_clj/2023/day14.clj
@@ -1,7 +1,17 @@
 (ns aoc-clj.2023.day14
-  (:require [aoc-clj.utils.core :as u]))
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid :refer [width height]]
+            [aoc-clj.utils.grid.vecgrid :as vg]))
 
-(def parse identity)
+(def charmap
+  {\O :r ;; rolling rock
+   \. :s ;; space
+   \# :w ;; wall --- fixed, square rock
+   })
+
+(defn parse
+  [input]
+  (vg/ascii->VecGrid2D charmap input :down true))
 
 (defn rev-compare
   [a b]
@@ -14,36 +24,67 @@
   [dir row]
   (let [sort-fn (if (zero? dir) sort (partial sort rev-compare))]
     (->> row
-         (partition-by #{\#})
+         (partition-by #{:w})
          (map sort-fn)
          flatten)))
 
-(def roll-east  #(map (partial roll-row 0) %))
-(def roll-west  #(map (partial roll-row 1) %))
-(def roll-south #(map (partial roll-row 0) %))
-(def roll-north #(map (partial roll-row 1) %))
+(def roll-east  #(map (partial roll-row 1) %))
+(def roll-west  #(map (partial roll-row 0) %))
+(def roll-south #(map (partial roll-row 1) %))
+(def roll-north #(map (partial roll-row 0) %))
 
 (defn roll-north-solo
   "Roll the round rocks toward the north (top) end of the grid"
-  [input]
-  (-> input u/transpose roll-north u/transpose))
+  [{:keys [v]}]
+  (-> v u/transpose roll-north u/transpose vg/->VecGrid2D))
 
 (defn spin-cycle
   "Each cycle tilts the platform four times so that the rounded rocks roll 
    north, then west, then south, then east. After each tilt, 
    the rounded rocks roll as far as they can before the platform 
    tilts in the next direction."
-  [rows]
-  (->> rows
-       u/transpose
-       roll-north
-       u/transpose
+  [{:keys [v]}]
+  (->> v
+       u/transpose roll-north u/transpose
        roll-west
-       u/transpose
-       roll-south
-       u/transpose
-       roll-east))
+       u/transpose roll-south u/transpose
+       roll-east
+       vg/->VecGrid2D))
 
+(defn round-rock-positions
+  [{:keys [v] :as grid}]
+  (let [h (height grid)
+        w (width grid)
+        values (zipmap (for [y (range h) x (range w)] [x y])
+                       (flatten v))]
+    (set (keys (filter #(= :r (val %)) values)))))
+
+(defn cycle-params
+  [{:keys [state-set states idx]} state]
+  (if (state-set state)
+    (let [start (u/index-of (u/equals? state) states)]
+      (reduced [start (- idx start)]))
+    {:state-set (conj state-set state)
+     :states    (conj states state)
+     :idx       (inc idx)}))
+
+(defn spin-cycle-params
+  [grid]
+  (->> (iterate spin-cycle grid)
+       (map round-rock-positions)
+       (reduce cycle-params {:state-set #{} :states [] :idx 0})))
+
+;; (defn total-load
+;;   "The amount of load caused by a single rounded rock (O) is equal to the 
+;;    number of rows from the rock to the south edge of the platform, 
+;;    including the row the rock is on.
+
+;;    The total load is the sum of the load caused by all of the rounded rocks."
+;;   [{:keys [v] :as grid}]
+;;   (let [h (height grid)]
+;;     (->> (map #(count (filter #{:r} %)) v)
+;;          (map * (range h 0 -1))
+;;          (reduce +))))
 
 (defn total-load
   "The amount of load caused by a single rounded rock (O) is equal to the 
@@ -51,15 +92,28 @@
    including the row the rock is on.
    
    The total load is the sum of the load caused by all of the rounded rocks."
-  [rows]
-  (let [height (count rows)]
+  [{:keys [v]}]
+  (let [height (count v)]
     (reduce + (map-indexed (fn [idx row]
                              (* (- height idx)
-                                (count (filter #{\O} row)))) rows))))
+                                (count (filter #{:r} row)))) v))))
 
+
+(defn total-load-after-n-spin-cycles
+  [grid n]
+  (let [[start length] (spin-cycle-params grid)]
+    (if (< n start)
+      (total-load (nth (iterate spin-cycle grid) n))
+      (total-load (nth (iterate spin-cycle grid) (+ start (mod (- n start) length)))))))
 
 (defn day14-part1-soln
   "Tilt the platform so that the rounded rocks all roll north. 
    Afterward, what is the total load on the north support beams?"
   [input]
-  (total-load (roll-north input)))
+  (total-load (roll-north-solo input)))
+
+(defn day14-part2-soln
+  "Run the spin cycle for 1000000000 cycles. 
+   Afterward, what is the total load on the north support beams?"
+  [input]
+  (total-load-after-n-spin-cycles input 1000000000))
--- test/aoc_clj/2023/day14_test.clj
+++ test/aoc_clj/2023/day14_test.clj
@@ -1,9 +1,10 @@
 (ns aoc-clj.2023.day14-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.vecgrid :as vg]
             [aoc-clj.2023.day14 :as t]))
 
-(def d14-s01
+(def d14-s01-raw
   ["O....#...."
    "O.OO#....#"
    ".....##..."
@@ -15,73 +16,112 @@
    "#....###.."
    "#OO..#...."])
 
+(def d14-s01
+  (vg/->VecGrid2D
+   [[:r :s :s :s :s :w :s :s :s :s]
+    [:r :s :r :r :w :s :s :s :s :w]
+    [:s :s :s :s :s :w :w :s :s :s]
+    [:r :r :s :w :r :s :s :s :s :r]
+    [:s :r :s :s :s :s :s :r :w :s]
+    [:r :s :w :s :s :r :s :w :s :w]
+    [:s :s :r :s :s :w :r :s :s :r]
+    [:s :s :s :s :s :s :s :r :s :s]
+    [:w :s :s :s :s :w :w :w :s :s]
+    [:w :r :r :s :s :w :s :s :s :s]]))
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d14-s01 (t/parse d14-s01-raw)))))
+
 (def d14-s01-north
-  ["OOOO.#.O.."
-   "OO..#....#"
-   "OO..O##..O"
-   "O..#.OO..."
-   "........#."
-   "..#....#.#"
-   "..O..#.O.O"
-   "..O......."
-   "#....###.."
-   "#....#...."])
+  (t/parse
+   ["OOOO.#.O.."
+    "OO..#....#"
+    "OO..O##..O"
+    "O..#.OO..."
+    "........#."
+    "..#....#.#"
+    "..O..#.O.O"
+    "..O......."
+    "#....###.."
+    "#....#...."]))
 
 (def d14-s01-cycle1
-  [".....#...."
-   "....#...O#"
-   "...OO##..."
-   ".OO#......"
-   ".....OOO#."
-   ".O#...O#.#"
-   "....O#...."
-   "......OOOO"
-   "#...O###.."
-   "#..OO#...."])
+  (t/parse
+   [".....#...."
+    "....#...O#"
+    "...OO##..."
+    ".OO#......"
+    ".....OOO#."
+    ".O#...O#.#"
+    "....O#...."
+    "......OOOO"
+    "#...O###.."
+    "#..OO#...."]))
 
 (def d14-s01-cycle2
-  [".....#...."
-   "....#...O#"
-   ".....##..."
-   "..O#......"
-   ".....OOO#."
-   ".O#...O#.#"
-   "....O#...O"
-   ".......OOO"
-   "#..OO###.."
-   "#.OOO#...O"])
+  (t/parse
+   [".....#...."
+    "....#...O#"
+    ".....##..."
+    "..O#......"
+    ".....OOO#."
+    ".O#...O#.#"
+    "....O#...O"
+    ".......OOO"
+    "#..OO###.."
+    "#.OOO#...O"]))
 
 (def d14-s01-cycle3
-  [".....#...."
-   "....#...O#"
-   ".....##..."
-   "..O#......"
-   ".....OOO#."
-   ".O#...O#.#"
-   "....O#...O"
-   ".......OOO"
-   "#...O###.O"
-   "#.OOO#...O"])
+  (t/parse
+   [".....#...."
+    "....#...O#"
+    ".....##..."
+    "..O#......"
+    ".....OOO#."
+    ".O#...O#.#"
+    "....O#...O"
+    ".......OOO"
+    "#...O###.O"
+    "#.OOO#...O"]))
 
 (deftest roll-north-test
   (testing "Modifies the input to have the round rocks roll north"
-    (is (= d14-s01-north (map #(apply str %) (t/roll-north-solo d14-s01))))))
+    (is (= d14-s01-north (t/roll-north-solo d14-s01)))))
 
 (deftest spin-cycle-test
   (testing "Runs the input data through a spin cycle (N-W-S-E)"
-    (is (= d14-s01-cycle1 (map #(apply str %)
-                               (t/spin-cycle d14-s01))))
-    (is (= d14-s01-cycle2 (map #(apply str %)
-                               (t/spin-cycle (t/spin-cycle d14-s01)))))
-    (is (= d14-s01-cycle3 (map #(apply str %)
-                               (t/spin-cycle (t/spin-cycle (t/spin-cycle d14-s01))))))))
+    (is (= d14-s01-cycle1 (-> d14-s01 t/spin-cycle)))
+    (is (= d14-s01-cycle2 (-> d14-s01 t/spin-cycle t/spin-cycle)))
+    (is (= d14-s01-cycle3 (-> d14-s01 t/spin-cycle t/spin-cycle t/spin-cycle)))))
 
 (deftest total-load-test
   (testing "Computes the total load of the rocks"
-    (is (= 136 (t/total-load d14-s01-north)))))
+    (is (= 136 (t/total-load (t/roll-north-solo d14-s01))))))
+
+(deftest round-rock-positions-test
+  (testing "Returns the positions of the round rocks"
+    (is (= #{[8 1] [3 2] [4 2] [1 3] [2 3] [5 4] [6 4] [7 4] [1 5]
+             [6 5] [4 6] [6 7] [7 7] [8 7] [9 7] [4 8] [3 9] [4 9]}
+           (t/round-rock-positions d14-s01-cycle1)))
+    (is (= #{[8 1] [2 3] [7 4] [5 4] [6 4] [6 5] [1 5] [4 6] [9 6]
+             [7 7] [8 7] [9 7] [3 8] [4 8] [2 9] [3 9] [4 9] [9 9]}
+           (t/round-rock-positions d14-s01-cycle2)))))
+
+(deftest spin-cycle-params-test
+  (testing "Determines when the spin cycle begins repeating itself"
+    (is (= [3 7] (t/spin-cycle-params d14-s01)))))
+
+(deftest total-load-after-n-spin-cycles-test
+  (testing "Determines the total load after n spin cycles"
+    (is (= 64 (t/total-load-after-n-spin-cycles d14-s01 1000000000)))))
 
 (def day14-input (u/parse-puzzle-input t/parse 2023 14))
 
 (deftest day14-part1-soln
   (testing "Reproduces the answer for day14, part1"
     (is (= 55172 (t/day14-part1-soln day14-input)))))
+
+(deftest day14-part2-soln
+  (testing "Reproduces the answer for day14, part2"
+    (is (= 106390 (t/day14-part2-soln day14-input)))))
