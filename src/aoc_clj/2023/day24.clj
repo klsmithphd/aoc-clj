e7a2559c83841c5c6284b29a3881c5b28adc0032
Ken Smith <ken@klsmith.me>
2023-12-24 22:46:12 +0000

    2023 Day24 Part1 soln

--- src/aoc_clj/2023/day24.clj
+++ src/aoc_clj/2023/day24.clj
@@ -1,4 +1,5 @@
-(ns aoc-clj.2023.day24)
+(ns aoc-clj.2023.day24
+  (:require [clojure.math.combinatorics :as combo]))
 
 (defn parse-line
   [line]
@@ -8,3 +9,44 @@
 (defn parse
   [input]
   (map parse-line input))
+
+(defn slope-and-intercept-xy
+  [[[px py _] [vx vy _]]]
+  (let [slope (/ vy vx)]
+    [slope (- py (* slope px))]))
+
+(defn in-past?
+  [x [[px _ _] [vx _ _]]]
+  (neg? (/ (- x px) vx)))
+
+(defn hailstone-intersect-xy
+  [[h1 h2]]
+  (let [[m1 b1] (slope-and-intercept-xy h1)
+        [m2 b2] (slope-and-intercept-xy h2)]
+    (if (= m1 m2)
+      :parallel
+      (let [x (/ (- b2 b1) (- m1 m2))]
+        (if (or (in-past? x h1) (in-past? x h2))
+          :past
+          [x (+ (* m1 x) b1)])))))
+
+(defn within-area?
+  [[mn mx] [x y]]
+  (and (<= mn x mx) (<= mn y mx)))
+
+(defn intersections-within-area
+  [limits hailstones]
+  (->> (combo/combinations hailstones 2)
+       (map hailstone-intersect-xy)
+       (remove keyword?)
+       (filter (partial within-area? limits))
+       count))
+
+(def part1-limits [200000000000000 400000000000000])
+
+(defn day24-part1-soln
+  [input]
+  (intersections-within-area part1-limits input))
+
+
+
--- test/aoc_clj/2023/day24_test.clj
+++ test/aoc_clj/2023/day24_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2023.day24-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2023.day24 :as t]))
 
 (def d24-s01-raw
@@ -19,3 +20,27 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d24-s01 (t/parse d24-s01-raw)))))
+
+(deftest hailstone-intersect-xy-test
+  (testing "Tests the comparisons of where hailstones will intersect"
+    (is (= [43/3 46/3] (t/hailstone-intersect-xy [(nth d24-s01 0) (nth d24-s01 1)])))
+    (is (= [35/3 50/3] (t/hailstone-intersect-xy [(nth d24-s01 0) (nth d24-s01 2)])))
+    (is (= [31/5 97/5] (t/hailstone-intersect-xy [(nth d24-s01 0) (nth d24-s01 3)])))
+    (is (= :past       (t/hailstone-intersect-xy [(nth d24-s01 0) (nth d24-s01 4)])))
+    (is (= :parallel   (t/hailstone-intersect-xy [(nth d24-s01 1) (nth d24-s01 2)])))
+    (is (= [-6 -5]     (t/hailstone-intersect-xy [(nth d24-s01 1) (nth d24-s01 3)])))
+    (is (= :past       (t/hailstone-intersect-xy [(nth d24-s01 1) (nth d24-s01 4)])))
+    (is (= [-2 3]      (t/hailstone-intersect-xy [(nth d24-s01 2) (nth d24-s01 3)])))
+    (is (= :past       (t/hailstone-intersect-xy [(nth d24-s01 2) (nth d24-s01 4)])))
+    (is (= :past       (t/hailstone-intersect-xy [(nth d24-s01 3) (nth d24-s01 4)])))))
+
+(deftest intersections-within-area-test
+  (testing "Counts the number of hailstone intersections within a prescribed area"
+    (is (= 2 (t/intersections-within-area [7 27] d24-s01)))))
+
+
+(def day24-input (u/parse-puzzle-input t/parse 2023 24))
+
+(deftest day24-part1-soln
+  (testing "Reproduces the answer for day24, part1"
+    (is (= 16172 (t/day24-part1-soln day24-input)))))
