101c381a86ac4ea6d9ea65c49c03a4c3b13b9c51
Ken Smith <ken@klsmith.me>
2023-12-30 18:56:44 +0000

    2023 Day25 WIP

--- test/aoc_clj/2023/day25_test.clj
+++ test/aoc_clj/2023/day25_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2023.day25-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2023.day25 :as t]))
 
 (def d25-s01-raw
@@ -32,6 +33,22 @@
    "rzs" ["qnr" "cmg" "lsr" "rsh"]
    "frs" ["qnr" "lhk" "lsr"]})
 
+;; Determined by plotting the graph with graphviz and seeing what links crossed
+;; between the two clusters
+(def d25-s01-cuts
+  [["hfx" "pzl"]
+   ["bvb" "cmg"]
+   ["nvd" "jqt"]])
+
+;; Determined by plotting the graph with graphviz and seeing what links crossed
+;; between the two clusters
+(def day25-input-cuts
+  [["mhb" "zqg"]
+   ["sjr" "jlt"]
+   ["fjn" "mzb"]])
+
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d25-s01 (t/parse d25-s01-raw)))))
+
+(def day25-input (u/parse-puzzle-input t/parse 2023 25))
