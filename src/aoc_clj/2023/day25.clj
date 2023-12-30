00223aa87a9e9de4565115f6b0cbbf07a5428922
Ken Smith <ken@klsmith.me>
2023-12-30 20:41:57 +0000

    2023 Day25 part1 mostly solution

--- src/aoc_clj/2023/day25.clj
+++ src/aoc_clj/2023/day25.clj
@@ -1,7 +1,8 @@
 (ns aoc-clj.2023.day25
   (:require [clojure.string :as str]
             [aoc-clj.utils.graph :as graph]
-            [aoc-clj.utils.core :as u]))
+            [aoc-clj.utils.core :as u]
+            [clojure.set :as set]))
 
 (defn parse-line
   [line]
@@ -12,7 +13,38 @@
   [input]
   (into {} (map parse-line input)))
 
+(defn flatten-graph-line
+  [[a coll]]
+  (map #(set [a %]) coll))
 
+(defn flatten-graph
+  [input]
+  (mapcat flatten-graph-line input))
+
+(defn graph-without-nodes
+  [graph without-edges]
+  (remove (set without-edges) graph))
+
+(defn neighbors
+  [graph node]
+  (let [matches (->> (filter #(seq (set/intersection #{node} %)) graph)
+                     (apply set/union))]
+    (disj matches node)))
+
+(defn connected-nodes
+  [graph node]
+  (loop [queue (u/queue [node])
+         visited #{node}]
+    (if (not (seq queue))
+      (count visited)
+      (recur (into (pop queue) (remove visited (neighbors graph (peek queue))))
+             (conj visited (peek queue))))))
+
+(defn clique-sizes
+  [input to-remove]
+  (let [graph (-> (flatten-graph input)
+                  (graph-without-nodes to-remove))]
+    (map (partial connected-nodes graph) (first to-remove))))
 
 (defn graphviz-line
   [[a coll]]
@@ -21,3 +53,14 @@
 (defn graphviz-rep
   [input]
   (str/join "\n" (mapcat graphviz-line input)))
+
+;; Determined by plotting the graph with graphviz and seeing what links crossed
+;; between the two clusters
+(def day25-input-cuts
+  [#{"mhb" "zqg"}
+   #{"sjr" "jlt"}
+   #{"fjn" "mzb"}])
+
+(defn day25-part1-soln
+  [input]
+  (clique-sizes input day25-input-cuts))
--- test/aoc_clj/2023/day25_test.clj
+++ test/aoc_clj/2023/day25_test.clj
@@ -18,6 +18,10 @@
    "rzs: qnr cmg lsr rsh"
    "frs: qnr lhk lsr"])
 
+(def d25-s02-raw
+  ["jqt: rhn xhk nvd"
+   "rsh: frs pzl lsr"])
+
 (def d25-s01
   {"jqt" ["rhn" "xhk" "nvd"]
    "rsh" ["frs" "pzl" "lsr"]
@@ -33,22 +37,53 @@
    "rzs" ["qnr" "cmg" "lsr" "rsh"]
    "frs" ["qnr" "lhk" "lsr"]})
 
-;; Determined by plotting the graph with graphviz and seeing what links crossed
-;; between the two clusters
-(def d25-s01-cuts
-  [["hfx" "pzl"]
-   ["bvb" "cmg"]
-   ["nvd" "jqt"]])
+(def d25-s02
+  {"jqt" ["rhn" "xhk" "nvd"]
+   "rsh" ["frs" "pzl" "lsr"]})
 
-;; Determined by plotting the graph with graphviz and seeing what links crossed
-;; between the two clusters
-(def day25-input-cuts
-  [["mhb" "zqg"]
-   ["sjr" "jlt"]
-   ["fjn" "mzb"]])
+(def d25-s02-flattened
+  [#{"jqt" "rhn"}
+   #{"jqt" "xhk"}
+   #{"jqt" "nvd"}
+   #{"rsh" "frs"}
+   #{"rsh" "pzl"}
+   #{"rsh" "lsr"}])
 
 (deftest parse-test
   (testing "Correctly parses the input"
-    (is (= d25-s01 (t/parse d25-s01-raw)))))
+    (is (= d25-s01 (t/parse d25-s01-raw)))
+    (is (= d25-s02 (t/parse d25-s02-raw)))))
+
+(deftest flatten-graph-test
+  (testing "Flattens the graph to just edge sets"
+    (is (= d25-s02-flattened (t/flatten-graph d25-s02)))))
+
+(deftest graph-without-nodes-test
+  (testing "Removes edge sets from the graph"
+    (is (= [#{"jqt" "xhk"}
+            #{"jqt" "nvd"}
+            #{"rsh" "pzl"}
+            #{"rsh" "lsr"}]
+           (t/graph-without-nodes d25-s02-flattened [#{"jqt" "rhn"}
+                                                     #{"rsh" "frs"}])))))
+
+(deftest neighbors-test
+  (testing "Computes the neighboring (directly connected) nodes"
+    (is (= #{"rhn" "xhk" "nvd"} (t/neighbors d25-s02-flattened "jqt")))))
+
+;; Determined by plotting the graph with graphviz and seeing what links crossed
+;; between the two clusters
+(def d25-s01-cuts
+  [#{"hfx" "pzl"}
+   #{"bvb" "cmg"}
+   #{"nvd" "jqt"}])
+
+(deftest clique-sizes-test
+  (testing "Computes the sizes of the two subgraphs after removing 3 edges"
+    (is (= #{9 6} (set (t/clique-sizes d25-s01 d25-s01-cuts))))))
 
 (def day25-input (u/parse-puzzle-input t/parse 2023 25))
+
+(deftest day25-part1-soln
+  (testing "Reproduces the answer for day25, part1"
+    (is (= 606062 (t/day25-part1-soln day25-input)))))
