7d9b20772047c8558458e29e3a67883b3bf1d185
Ken Smith <ken@klsmith.me>
2024-01-05 13:25:55 +0000

    Tidy 2015 Day1

--- src/aoc_clj/2015/day01.clj
+++ src/aoc_clj/2015/day01.clj
@@ -1,29 +1,34 @@
 (ns aoc-clj.2015.day01
-  "Solution to https://adventofcode.com/2015/day/1"
-  (:require [aoc-clj.utils.core :as u]))
+  "Solution to https://adventofcode.com/2015/day/1")
 
-(def parse first)
-
-(defn instructions
+;; Input parsing
+(defn parse
   [input]
-  (map {\( 1 \) -1} input))
+  (map {\( 1 \) -1} (first input)))
 
+;; Puzzle logic
 (defn final-floor
   [input]
-  (->> (instructions input)
-       (reduce + 0)))
+  (reduce + 0 input))
 
 (defn first-pos-in-basement
   [input]
-  (->> (instructions input)
-       (reductions + 0)
-       (u/index-of (u/equals? -1))))
+  (->> (reductions + 0 input)
+       (take-while #(not= -1 %))
+       count))
 
+;; Puzzle solutions
 (defn day01-part1-soln
+  "Interepting `(` as going up one floor and `)` as going down
+   one floor what floor do you end up on given a string of 
+   parentheses as input"
   [input]
   (final-floor input))
 
 (defn day01-part2-soln
+  "Still interpreting `(` as up one floor and `)` as down one
+   floor, what's the first character in the input string that 
+   causes the elevator to reach the basement (floor -1) "
   [input]
   (first-pos-in-basement input))
 
--- test/aoc_clj/2015/day01_test.clj
+++ test/aoc_clj/2015/day01_test.clj
@@ -1,31 +1,31 @@
 (ns aoc-clj.2015.day01-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day01 :as t]))
+            [aoc-clj.2015.day01 :as d01]))
 
 (deftest final-floor
   (testing "Correctly determines the final floor"
-    (is (= 0   (t/final-floor "(())")))
-    (is (= 0   (t/final-floor "()()")))
-    (is (= 3   (t/final-floor "(((")))
-    (is (= 3   (t/final-floor "(()(()(")))
-    (is (= 3   (t/final-floor "))(((((")))
-    (is (= -1  (t/final-floor "())")))
-    (is (= -1  (t/final-floor "))(")))
-    (is (= -3  (t/final-floor ")))")))
-    (is (= -3  (t/final-floor ")())())")))))
+    (is (= 0  (d01/final-floor "(())")))
+    (is (= 0  (d01/final-floor "()()")))
+    (is (= 3  (d01/final-floor "(((")))
+    (is (= 3  (d01/final-floor "(()(()(")))
+    (is (= 3  (d01/final-floor "))(((((")))
+    (is (= -1 (d01/final-floor "())")))
+    (is (= -1 (d01/final-floor "))(")))
+    (is (= -3 (d01/final-floor ")))")))
+    (is (= -3 (d01/final-floor ")())())")))))
 
 (deftest first-pos-in-basement
   (testing "Finds the first position where the sum equals -1"
-    (is (= 1 (t/first-pos-in-basement ")")))
-    (is (= 5 (t/first-pos-in-basement "()())")))))
+    (is (= 1 (d01/first-pos-in-basement ")")))
+    (is (= 5 (d01/first-pos-in-basement "()())")))))
 
-(def day01-input (u/parse-puzzle-input t/parse 2015 1))
+(def day01-input (u/parse-puzzle-input d01/parse 2015 1))
 
 (deftest day01-part1-soln
   (testing "Reproduces the answer for day01, part1"
-    (is (= 138 (t/day01-part1-soln day01-input)))))
+    (is (= 138 (d01/day01-part1-soln day01-input)))))
 
 (deftest day01-part2-soln
   (testing "Reproduces the answer for day01, part2"
-    (is (= 1771 (t/day01-part2-soln day01-input)))))
+    (is (= 1771 (d01/day01-part2-soln day01-input)))))
