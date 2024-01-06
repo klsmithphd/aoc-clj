beb685d05f54b77ed5540d2d42f20ba20b998fd3
Ken Smith <ken@klsmith.me>
2024-01-06 00:13:50 +0000

    Cleanup 2015 Day02

--- src/aoc_clj/2015/day02.clj
+++ src/aoc_clj/2015/day02.clj
@@ -2,25 +2,25 @@
   "Solution to https://adventofcode.com/2015/day/2"
   (:require [clojure.string :as str]))
 
+;; Input parsing
 (defn parse-line
   [line]
-  (map read-string (str/split line #"x")))
+  (sort (map read-string (str/split line #"x"))))
 
 (defn parse
   [input]
   (map parse-line input))
 
+;; Puzzle logic
 (defn wrapping-paper-area
-  [[l w h]]
-  (let [extra (take 2 (sort [l w h]))]
-    (+ (* 2 l w) (* 2 l h) (* 2 w h) (apply * extra))))
+  [[a b c]]
+  (+ (* 2 a b) (* 2 a c) (* 2 b c) (* a b)))
 
 (defn ribbon-length
-  [[l w h]]
-  (let [smallest (take 2 (sort [l w h]))]
-    (+ (* 2 (reduce + smallest))
-       (* l w h))))
+  [[a b c]]
+  (+ (* 2 (+ a b)) (* a b c)))
 
+;; Puzzle solutions
 (defn part1
   [input]
   (reduce + (map wrapping-paper-area input)))
--- test/aoc_clj/2015/day02_test.clj
+++ test/aoc_clj/2015/day02_test.clj
@@ -1,24 +1,31 @@
 (ns aoc-clj.2015.day02-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day02 :as t]))
+            [aoc-clj.2015.day02 :as d02]))
+
+(def d02-s00-raw ["2x3x4" "1x1x10" "9x8x7"])
+(def d02-s00     [[2 3 4] [1 1 10] [7 8 9]])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d02-s00 (d02/parse d02-s00-raw)))))
 
 (deftest wrapping-paper-area
   (testing "Computes the correct area of wrapping paper"
-    (is (= 58 (t/wrapping-paper-area [2 3 4])))
-    (is (= 43 (t/wrapping-paper-area [1 1 10])))))
+    (is (= 58 (d02/wrapping-paper-area (nth d02-s00 0))))
+    (is (= 43 (d02/wrapping-paper-area (nth d02-s00 1))))))
 
 (deftest ribbon-length
   (testing "Computes the length of the ribbon from the examples given"
-    (is (= 34 (t/ribbon-length [2 3 4])))
-    (is (= 14 (t/ribbon-length [1 1 10])))))
+    (is (= 34 (d02/ribbon-length (nth d02-s00 0))))
+    (is (= 14 (d02/ribbon-length (nth d02-s00 1))))))
 
-(def day02-input (u/parse-puzzle-input t/parse 2015 2))
+(def day02-input (u/parse-puzzle-input d02/parse 2015 2))
 
 (deftest part1-test
   (testing "Reproduces the answer for day02, part1"
-    (is (= 1598415 (t/part1 day02-input)))))
+    (is (= 1598415 (d02/part1 day02-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day02, part2"
-    (is (= 3812909 (t/part2 day02-input)))))
+    (is (= 3812909 (d02/part2 day02-input)))))
