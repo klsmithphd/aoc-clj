fee97fb9d5df273f79100d69ca63c9ab13a45b3f
Ken Smith <ken@klsmith.me>
2024-01-07 18:59:35 +0000

    Cleanup of 2015 Day07

--- test/aoc_clj/2015/day07_test.clj
+++ test/aoc_clj/2015/day07_test.clj
@@ -3,40 +3,44 @@
             [aoc-clj.utils.core :as u]
             [aoc-clj.2015.day07 :as t]))
 
-(def day07-sample ["123 -> x"
-                   "456 -> y"
-                   "x AND y -> d"
-                   "x OR y -> e"
-                   "x LSHIFT 2 -> f"
-                   "y RSHIFT 2 -> g"
-                   "NOT x -> h"
-                   "NOT y -> i"
-                   "f -> j"])
+(def d07-s00-raw
+  ["123 -> x"
+   "456 -> y"
+   "x AND y -> d"
+   "x OR y -> e"
+   "x LSHIFT 2 -> f"
+   "y RSHIFT 2 -> g"
+   "NOT x -> h"
+   "NOT y -> i"
+   ;; Added to the sample to reflect what's in the real puzzle data
+   "f -> j"])
+
+(def d07-s00
+  {"x" {:op :assign :args 123}
+   "y" {:op :assign :args 456}
+   "d" {:op :and    :args ["x" "y"]}
+   "e" {:op :or     :args ["x" "y"]}
+   "f" {:op :lshift :args ["x" 2]}
+   "g" {:op :rshift :args ["y" 2]}
+   "h" {:op :not    :args ["x"]}
+   "i" {:op :not    :args ["y"]}
+   "j" {:op :assign :args "f"}})
 
 (deftest parse-test
   (testing "Parser logic works correctly"
-    (is (= (t/parse-line (nth day07-sample 0))  ["x" {:op :assign :args 123}]))
-    (is (= (t/parse-line (nth day07-sample 1))  ["y" {:op :assign :args 456}]))
-    (is (= (t/parse-line (nth day07-sample 2))  ["d" {:op :and :args ["x" "y"]}]))
-    (is (= (t/parse-line (nth day07-sample 3))  ["e" {:op :or :args ["x" "y"]}]))
-    (is (= (t/parse-line (nth day07-sample 4))  ["f" {:op :lshift :args ["x" 2]}]))
-    (is (= (t/parse-line (nth day07-sample 5))  ["g" {:op :rshift :args ["y" 2]}]))
-    (is (= (t/parse-line (nth day07-sample 6))  ["h" {:op :not :args ["x"]}]))
-    (is (= (t/parse-line (nth day07-sample 7))  ["i" {:op :not :args ["y"]}]))
-    (is (= (t/parse-line (nth day07-sample 8))  ["j" {:op :assign :args "f"}]))))
+    (is (= d07-s00 (t/parse d07-s00-raw)))))
 
 (deftest wire-val-test
   (testing "Computes the wire values correctly"
-    (let [circuit (into {} (map t/parse-line day07-sample))]
-      (is (= (t/wire-val circuit "d") 72))
-      (is (= (t/wire-val circuit "e") 507))
-      (is (= (t/wire-val circuit "f") 492))
-      (is (= (t/wire-val circuit "g") 114))
-      (is (= (t/wire-val circuit "h") 65412))
-      (is (= (t/wire-val circuit "i") 65079))
-      (is (= (t/wire-val circuit "x") 123))
-      (is (= (t/wire-val circuit "y") 456))
-      (is (= (t/wire-val circuit "j") 492)))))
+    (is (= (t/wire-val d07-s00 "d") 72))
+    (is (= (t/wire-val d07-s00 "e") 507))
+    (is (= (t/wire-val d07-s00 "f") 492))
+    (is (= (t/wire-val d07-s00 "g") 114))
+    (is (= (t/wire-val d07-s00 "h") 65412))
+    (is (= (t/wire-val d07-s00 "i") 65079))
+    (is (= (t/wire-val d07-s00 "x") 123))
+    (is (= (t/wire-val d07-s00 "y") 456))
+    (is (= (t/wire-val d07-s00 "j") 492))))
 
 (def day07-input (u/parse-puzzle-input t/parse 2015 7))
 
