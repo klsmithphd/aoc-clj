639c51fd7f416c94b3aeb2a1e728e636f0f4c58f
Ken Smith <ken@klsmith.me>
2024-01-07 22:45:12 +0000

    Not fast, but marking fixed #5 regardless

--- src/aoc_clj/2015/day10.clj
+++ src/aoc_clj/2015/day10.clj
@@ -2,18 +2,30 @@
   "Solution to https://adventofcode.com/2015/day/10"
   (:require [clojure.string :as str]))
 
+;; Input parsing
 (def parse first)
 
+;; Puzzle logic
 (defn look-and-say
+  "Implements the Look-and-say sequence
+   https://en.wikipedia.org/wiki/Look-and-say_sequence"
   [s]
   (let [chunks (partition-by identity s)
-        counts (map #(vector (str  (count %)) (str (first %))) chunks)]
-    (str/join  (flatten  counts))))
+        counts (map #(vector (str (count %)) (str (first %))) chunks)]
+    (str/join (flatten counts))))
 
+(defn nth-length
+  "Starting from `seed`, run the look_and_say fn `n` times"
+  [n seed]
+  (count (nth (iterate look-and-say seed) n)))
+
+;; Puzzle solutions
 (defn part1
+  "Length of string after 40 iterations given the puzzle input seed"
   [input]
-  (count (nth (iterate look-and-say input) 40)))
+  (nth-length 40 input))
 
 (defn part2
+  "Length of the string after 50 iterations given the puzzle input seed"
   [input]
-  (count (nth (iterate look-and-say input) 50)))
+  (nth-length 50 input))
--- test/aoc_clj/2015/day10_test.clj
+++ test/aoc_clj/2015/day10_test.clj
@@ -1,23 +1,22 @@
 (ns aoc-clj.2015.day10-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day10 :as t]))
+            [aoc-clj.2015.day10 :as d10]))
 
 (deftest look-and-say
   (testing "Returns next string given input"
-    (is (= "11" (t/look-and-say "1")))
-    (is (= "21" (t/look-and-say "11")))
-    (is (= "1211" (t/look-and-say "21")))
-    (is (= "111221" (t/look-and-say "1211")))))
+    (is (= "11" (d10/look-and-say "1")))
+    (is (= "21" (d10/look-and-say "11")))
+    (is (= "1211" (d10/look-and-say "21")))
+    (is (= "111221" (d10/look-and-say "1211")))
+    (is (= "312211" (d10/look-and-say "111221")))))
 
-(def day10-input (u/parse-puzzle-input t/parse 2015 10))
+(def day10-input (u/parse-puzzle-input d10/parse 2015 10))
 
 (deftest part1-test
   (testing "Reproduces the answer for day10, part1"
-    (is (= 252594 (t/part1 day10-input)))))
+    (is (= 252594 (d10/part1 day10-input)))))
 
-;; FIXME: 2015.day10 solution is slow
-;; https://github.com/Ken-2scientists/aoc-clj/issues/5
-(deftest ^:slow part2
+(deftest part2-test
   (testing "Reproduces the answer for day10, part2"
-    (is (= 3579328 (t/part2 day10-input)))))
+    (is (= 3579328 (d10/part2 day10-input)))))
