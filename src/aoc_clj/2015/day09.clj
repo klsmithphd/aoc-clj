2ffd14a2804869ba45a25d420f6619001807249c
Ken Smith <ken@klsmith.me>
2024-01-07 21:57:10 +0000

    Cleanup 2015 Day09

--- src/aoc_clj/2015/day09.clj
+++ src/aoc_clj/2015/day09.clj
@@ -4,43 +4,52 @@
             [clojure.math.combinatorics :as combo]
             [aoc-clj.utils.core :as u]))
 
+;; Input parsing
 (defn parse-line
   [line]
   (let [[a _ b _ c] (str/split line #" ")
         dist (read-string c)]
-    [[a {b dist}]
-     [b {a dist}]]))
+    [[a b dist]
+     [b a dist]]))
 
 (defn parse
   [input]
   (->> (mapcat parse-line input)
        (group-by first)
-       (u/fmap #(apply merge (map second %)))))
+       (u/fmap #(into {} (map (fn [v] (subvec v 1)) %)))))
 
+;; Puzzle logic
 (defn route-distance
+  "Computes the distance along a route denoted as a sequence of nodes"
   [dists route]
   (let [pairs (partition 2 1 route)
         steps (map (partial get-in dists) pairs)]
     (reduce + steps)))
 
 (defn route-distances
+  "Computes all possible route distances"
   [dists]
   (let [nodes (keys dists)
         routes (combo/permutations nodes)]
     (map (partial route-distance dists) routes)))
 
 (defn shortest-route-distance
+  "Finds the distance of the shortest route"
   [dists]
   (apply min (route-distances dists)))
 
 (defn longest-route-distance
+  "Finds the distance of the longest route"
   [dists]
   (apply max (route-distances dists)))
 
+;; Puzzle solutions
 (defn part1
+  "Distance of shortest route visiting every location once"
   [input]
   (shortest-route-distance input))
 
 (defn part2
+  "Distance of longest route visiting every location once"
   [input]
   (longest-route-distance input))
--- test/aoc_clj/2015/day09_test.clj
+++ test/aoc_clj/2015/day09_test.clj
@@ -1,36 +1,45 @@
 (ns aoc-clj.2015.day09-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day09 :as t]))
+            [aoc-clj.2015.day09 :as d09]))
 
-(def d09-s00 (t/parse
-              ["London to Dublin = 464"
-               "London to Belfast = 518"
-               "Dublin to Belfast = 141"]))
+(def d09-s00-raw
+  ["London to Dublin = 464"
+   "London to Belfast = 518"
+   "Dublin to Belfast = 141"])
+
+(def d09-s00
+  {"London"  {"Dublin" 464 "Belfast" 518}
+   "Dublin"  {"London" 464 "Belfast" 141}
+   "Belfast" {"London" 518 "Dublin" 141}})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d09-s00 (d09/parse d09-s00-raw)))))
 
 (deftest route-distances
   (testing "Returns the distance along an identified route"
-    (is (= 982 (t/route-distance d09-s00 ["Dublin" "London" "Belfast"])))
-    (is (= 605 (t/route-distance d09-s00 ["London" "Dublin" "Belfast"])))
-    (is (= 659 (t/route-distance d09-s00 ["London" "Belfast" "Dublin"])))
-    (is (= 659 (t/route-distance d09-s00 ["Dublin" "Belfast" "London"])))
-    (is (= 605 (t/route-distance d09-s00 ["Belfast" "Dublin" "London"])))
-    (is (= 982 (t/route-distance d09-s00 ["Belfast" "London" "Dublin"])))))
+    (is (= 982 (d09/route-distance d09-s00 ["Dublin" "London" "Belfast"])))
+    (is (= 605 (d09/route-distance d09-s00 ["London" "Dublin" "Belfast"])))
+    (is (= 659 (d09/route-distance d09-s00 ["London" "Belfast" "Dublin"])))
+    (is (= 659 (d09/route-distance d09-s00 ["Dublin" "Belfast" "London"])))
+    (is (= 605 (d09/route-distance d09-s00 ["Belfast" "Dublin" "London"])))
+    (is (= 982 (d09/route-distance d09-s00 ["Belfast" "London" "Dublin"])))))
 
 (deftest shortest-route-distance
   (testing "Returns the distance of the shortest route"
-    (is (= 605 (t/shortest-route-distance d09-s00)))))
+    (is (= 605 (d09/shortest-route-distance d09-s00)))))
 
 (deftest longest-route-distance
   (testing "Returns the distance of the longest route"
-    (is (= 982 (t/longest-route-distance d09-s00)))))
+    (is (= 982 (d09/longest-route-distance d09-s00)))))
 
-(def day09-input (u/parse-puzzle-input t/parse 2015 9))
+(def day09-input (u/parse-puzzle-input d09/parse 2015 9))
 
 (deftest part1-test
   (testing "Reproduces the answer for day09, part1"
-    (is (= 207 (t/part1 day09-input)))))
+    (is (= 207 (d09/part1 day09-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day09, part2"
-    (is (= 804 (t/part2 day09-input)))))
+    (is (= 804 (d09/part2 day09-input)))))
