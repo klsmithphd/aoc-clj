350f4d320633a8ac84973fe300a5b6fcd52e248d
Ken Smith <ken@klsmith.me>
2024-01-14 22:49:18 +0000

    Tidying 2015 Day12

--- src/aoc_clj/2015/day12.clj
+++ src/aoc_clj/2015/day12.clj
@@ -1,33 +1,46 @@
 (ns aoc-clj.2015.day12
   "Solution to https://adventofcode.com/2015/day/11"
-  (:require [cheshire.core :as c]))
+  (:require [cheshire.core :as c]
+            [aoc-clj.utils.core :as u]))
 
+;; Input parsing
 (def parse first)
 
+;; Puzzle logic
 (defn all-numbers
+  "Simply read all the numbers out of the strings"
   [s]
   (map read-string (re-seq #"-?\d+" s)))
 
-(defn drop-red
-  [s]
-  (if (map? s)
-    (if (some #{"red"} (vals s))
-      {}
-      (zipmap (keys s)
-              (map drop-red (vals s))))
-
-    (if (vector? s)
-      (mapv drop-red s)
-      s)))
-
-(defn part1
+(defn all-numbers-total
+  "Computes the sum of the numbers read out of the JSON docs"
   [input]
   (reduce + (all-numbers input)))
 
-(defn part2
+(defn drop-red
+  "Drop any map (JSON object) that has any property whose value is `red`"
+  [obj]
+  (cond
+    (map? obj)    (if (some #{"red"} (vals obj))
+                    {}
+                    (u/fmap drop-red obj))
+    (vector? obj) (mapv drop-red obj)
+    :else obj))
+
+(defn JSON-without-red
+  "Parse the JSON document string, strip out the objects with properties whose 
+   value is red, and then re-emit as JSON string"
   [input]
-  (->> (c/parse-string input)
-       drop-red
-       c/generate-string
-       all-numbers
-       (reduce +)))
+  (-> input c/parse-string drop-red c/generate-string))
+
+;; Puzzle solutions
+(defn part1
+  "Computes the sum of the numbers in the document"
+  [input]
+  (all-numbers-total input))
+
+(defn part2
+  "Computes the sum of the numbers in the doc after objects with properties
+   whose values equal red are dropped"
+  [input]
+  (-> input JSON-without-red all-numbers-total))
--- test/aoc_clj/2015/day12_test.clj
+++ test/aoc_clj/2015/day12_test.clj
@@ -1,21 +1,41 @@
 (ns aoc-clj.2015.day12-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day12 :as t]))
+            [aoc-clj.2015.day12 :as d12]))
+
+(def d12-s00 "[1,2,3]")
+(def d12-s01 "{\"a\":2,\"b\":4}")
+(def d12-s02 "[[[3]]]")
+(def d12-s03 "{\"a\":{\"b\":4},\"c\":-1}")
+(def d12-s04 "{\"a\":[-1,1]}")
+(def d12-s05 "[-1,{\"a\":1}]")
+(def d12-s06 "[]")
+(def d12-s07 "{}")
+
+(deftest all-numbers-total-test
+  (testing "Computes the sum of the numbers in each JSON doc"
+    (is (= 6 (d12/all-numbers-total d12-s00)))
+    (is (= 6 (d12/all-numbers-total d12-s01)))
+    (is (= 3 (d12/all-numbers-total d12-s02)))
+    (is (= 3 (d12/all-numbers-total d12-s03)))
+    (is (= 0 (d12/all-numbers-total d12-s04)))
+    (is (= 0 (d12/all-numbers-total d12-s05)))
+    (is (= 0 (d12/all-numbers-total d12-s06)))
+    (is (= 0 (d12/all-numbers-total d12-s07)))))
 
 (deftest drop-red-test
   (testing "Correctly drops sub-maps with any value that is 'red'"
-    (is (= [1 2 3]     (t/drop-red [1,2,3])))
-    (is (= [1 {} 3]    (t/drop-red [1,{"c" "red" "b" 2},3])))
-    (is (= {}          (t/drop-red {"d" "red","e" [1,2,3,4],"f" 5})))
-    (is (= [1 "red" 5] (t/drop-red [1, "red", 5])))))
+    (is (= [1 2 3]     (d12/drop-red [1,2,3])))
+    (is (= [1 {} 3]    (d12/drop-red [1,{"c" "red" "b" 2},3])))
+    (is (= {}          (d12/drop-red {"d" "red","e" [1,2,3,4],"f" 5})))
+    (is (= [1 "red" 5] (d12/drop-red [1, "red", 5])))))
 
-(def day12-input (u/parse-puzzle-input t/parse 2015 12))
+(def day12-input (u/parse-puzzle-input d12/parse 2015 12))
 
 (deftest part1-test
   (testing "Reproduces the answer for day12, part1"
-    (is (= 111754 (t/part1 day12-input)))))
+    (is (= 111754 (d12/part1 day12-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day12, part2"
-    (is (= 65402 (t/part2 day12-input)))))
+    (is (= 65402 (d12/part2 day12-input)))))
