08abeae55c256d671b9e9b89e1ff3aed6b09292a
Ken Smith <ken@klsmith.me>
2024-01-15 21:20:25 +0000

    Cleanup and speedup of 2015 Day13 soln

--- src/aoc_clj/2015/day13.clj
+++ src/aoc_clj/2015/day13.clj
@@ -4,6 +4,7 @@
             [clojure.math.combinatorics :as combo]
             [aoc-clj.utils.core :as u]))
 
+;; Input parsing
 (defn parse-line
   [line]
   (let [bits (str/split line #" ")
@@ -20,34 +21,60 @@
        (group-by first)
        (u/fmap #(into {} (map second %)))))
 
-(defn adjacents
-  [order]
-  (map vector (u/rotate 1 order) (u/rotate -1 order)))
-
+;; Puzzle logic
 (defn happiness
-  [table guest neighbors]
-  (+  (get-in table [guest (first  neighbors)])
-      (get-in table [guest (second neighbors)])))
+  "Compute a happiness score for a given guest based on who their
+   neighbors are and their known preferences"
+  [prefs [n1 guest n2]]
+  (+  (get-in prefs [guest n1])
+      (get-in prefs [guest n2])))
+
+(defn table-circle
+  "Takes a collection of n elements and makes it into a collection 2 items
+   longer, wrapping around the first and second elements to the end, i.e.,
+   it take [1, 2, 3, 4] and turns it into [1, 2, 3, 4, 1, 2]. This can
+   then be used to take 3 elements at a time and compute the happiness
+   of the center person"
+  [coll]
+  (concat coll (take 2 coll)))
 
 (defn happiness-total
-  [table order]
-  (let [neighbors (adjacents order)]
-    (reduce + (map (partial happiness table) order neighbors))))
+  "Compute the total happiness score for the given ordering of guests"
+  [prefs ordering]
+  (->> (table-circle ordering)
+       (partition 3 1)
+       (map (partial happiness prefs))
+       (reduce +)))
+
+(defn arrangements
+  "Returns the (n-1)! possible arrangements of the guests around the table.
+   
+   Technically there should be only half of that number to account because
+   the clockwise/counter-clockwise orderings around the table are equivalent"
+  [prefs]
+  (let [guests (keys prefs)]
+    (map #(into [(first guests)] %) (combo/permutations (rest guests)))))
 
 (defn max-happiness
-  [table]
-  (let [orders (combo/permutations (keys table))]
-    (apply max (map (partial  happiness-total table) orders))))
+  "Find the maximum happiness of any possible configuration of guests"
+  [prefs]
+  (->> (arrangements prefs)
+       (map (partial happiness-total prefs))
+       (apply max)))
 
 (defn add-me
-  [table]
-  (let [new-table (u/fmap #(assoc % "Me" 0) table)]
-    (assoc new-table "Me" (zipmap (keys new-table) (repeat 0)))))
+  "Insert a new guest (me) into the mix, whose happiness scores are all zero"
+  [prefs]
+  (let [new-prefs (u/fmap #(assoc % "Me" 0) prefs)]
+    (assoc new-prefs "Me" (zipmap (keys prefs) (repeat 0)))))
 
+;; Puzzle solutions
 (defn part1
+  "Find the maximum happiness score possible for all guests"
   [input]
   (max-happiness input))
 
 (defn part2
+  "Find the maximum happiness score including me in the seating plan"
   [input]
   (max-happiness (add-me input)))
--- test/aoc_clj/2015/day13_test.clj
+++ test/aoc_clj/2015/day13_test.clj
@@ -1,9 +1,9 @@
 (ns aoc-clj.2015.day13-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day13 :as t]))
+            [aoc-clj.2015.day13 :as d13]))
 
-(def d13-s00
+(def d13-s00-raw
   ["Alice would gain 54 happiness units by sitting next to Bob."
    "Alice would lose 79 happiness units by sitting next to Carol."
    "Alice would lose 2 happiness units by sitting next to David."
@@ -17,16 +17,35 @@
    "David would lose 7 happiness units by sitting next to Bob."
    "David would gain 41 happiness units by sitting next to Carol."])
 
+(def d13-s00 {"Alice" {"Bob" 54    "Carol" -79 "David" -2}
+              "Bob"   {"Alice" 83  "Carol" -7  "David" -63}
+              "Carol" {"Alice" -62 "Bob" 60    "David" 55}
+              "David" {"Alice" 46  "Bob" -7    "Carol" 41}})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d13-s00 (d13/parse d13-s00-raw)))))
+
+(deftest arrangements-test
+  (testing "Computes all the possible (round table) arrangements"
+    (is (= [["Alice" "Bob" "Carol" "David"]
+            ["Alice" "Bob" "David" "Carol"]
+            ["Alice" "Carol" "Bob" "David"]
+            ["Alice" "Carol" "David" "Bob"]
+            ["Alice" "David" "Bob" "Carol"]
+            ["Alice" "David" "Carol" "Bob"]]
+           (d13/arrangements d13-s00)))))
+
 (deftest max-happiness-test
   (testing "Finds the maximum happiness value for the guest list"
-    (is (= 330 (t/max-happiness (t/parse d13-s00))))))
+    (is (= 330 (d13/max-happiness d13-s00)))))
 
-(def day13-input (u/parse-puzzle-input t/parse 2015 13))
+(def day13-input (u/parse-puzzle-input d13/parse 2015 13))
 
 (deftest part1-test
   (testing "Reproduces the answer for day13, part1"
-    (is (= 709 (t/part1 day13-input)))))
+    (is (= 709 (d13/part1 day13-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day13, part2"
-    (is (= 668 (t/part2 day13-input)))))
+    (is (= 668 (d13/part2 day13-input)))))
