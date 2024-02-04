5042fb019fa39570e2590a270b063a35b2c11df4
Ken Smith <ken@klsmith.me>
2024-02-04 23:32:56 +0000

    2015 Day16 cleanup and docs

--- src/aoc_clj/2015/day16.clj
+++ src/aoc_clj/2015/day16.clj
@@ -2,6 +2,7 @@
   "Solution to https://adventofcode.com/2015/day/16"
   (:require [clojure.string :as str]))
 
+;; Constants
 (def criteria
   {:children 3
    :cats 7
@@ -14,47 +15,59 @@
    :cars 2
    :perfumes 1})
 
+;; Input parsing
 (defn parse-line
   [line]
   (let [[aunt props] (str/split line #": " 2)
-        [_ aunt-no] (str/split aunt #" ")
-        pairs (str/split props #", ")
-        attrs (mapv (fn [x]
-                      (let [[k v] (str/split x #": ")]
-                        [(keyword k) (read-string v)])) pairs)]
-    [(read-string aunt-no) (into {} attrs)]))
+        aunt-num     (read-string (subs aunt 4 (count aunt)))
+        pairs        (str/split props #", ")
+        attrs        (mapv (fn [pair]
+                             (let [[k v] (str/split pair #": ")]
+                               [(keyword k) (read-string v)])) pairs)]
+    [aunt-num (into {} attrs)]))
 
 (defn parse
   [input]
   (into {} (mapv parse-line input)))
 
+;; Puzzle logic
 (defn exact-match?
+  "Checks if the given aunt's properties exactly match thos of the criteria"
   [[_ props]]
-  (let [sub-criteria (select-keys criteria (keys props))]
-    (= props sub-criteria)))
+  (= props (select-keys criteria (keys props))))
 
 (defn attr-compare
-  [attr expected-val actual-val]
-  (case attr
-    :cats        (> actual-val expected-val)
-    :trees       (> actual-val expected-val)
-    :pomeranians (< actual-val expected-val)
-    :goldfish    (< actual-val expected-val)
-    (= actual-val expected-val)))
+  "For four properties (cats, trees, pomerians, and goldfish), the value
+   must be greater than or less than, not equal to the expected value. 
+   For all others, we're looking for an exact match"
+  [[attr actual-val]]
+  (let [expected-val (criteria attr)]
+    (case attr
+      :cats        (> actual-val expected-val)
+      :trees       (> actual-val expected-val)
+      :pomeranians (< actual-val expected-val)
+      :goldfish    (< actual-val expected-val)
+      (= actual-val expected-val))))
 
 (defn range-match?
+  "Use the logic of part 2 where some attributes need to be greater
+   than or less than, not just equal, to determine whether an aunt
+   matches the criteria"
   [[_ props]]
-  (let [attrs (keys props)]
-    (every? true? (map #(attr-compare % (criteria %) (props %)) attrs))))
+  (every? true? (map attr-compare props)))
 
 (defn matching-aunt
+  "Use the supplied criteria filter-"
   [criteria aunts]
-  (ffirst (filter criteria aunts)))
+  (-> (filter criteria aunts) first key))
 
+;; Puzzle solutions
 (defn part1
+  "Find the aunt whose properties exactly match the search criteria"
   [input]
   (matching-aunt exact-match? input))
 
 (defn part2
+  "Find the aunt whose properties satisfied the range comparisonsß"
   [input]
   (matching-aunt range-match? input))
--- test/aoc_clj/2015/day16_test.clj
+++ test/aoc_clj/2015/day16_test.clj
@@ -1,14 +1,32 @@
 (ns aoc-clj.2015.day16-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day16 :as t]))
+            [aoc-clj.2015.day16 :as d16]))
 
-(def day16-input (u/parse-puzzle-input t/parse 2015 16))
+(def d16-s00-raw
+  ["Sue 1: children: 1, cars: 8, vizslas: 7"
+   "Sue 2: akitas: 10, perfumes: 10, children: 5"
+   "Sue 3: cars: 5, pomeranians: 4, vizslas: 1"
+   "Sue 4: goldfish: 5, children: 8, perfumes: 3"
+   "Sue 5: vizslas: 2, akitas: 7, perfumes: 6"])
+
+(def d16-s00
+  {1 {:children 1 :cars 8 :vizslas 7}
+   2 {:akitas 10  :perfumes 10 :children 5}
+   3 {:cars 5 :pomeranians 4 :vizslas 1}
+   4 {:goldfish 5 :children 8 :perfumes 3}
+   5 {:vizslas 2 :akitas 7 :perfumes 6}})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d16-s00 (d16/parse d16-s00-raw)))))
+
+(def day16-input (u/parse-puzzle-input d16/parse 2015 16))
 
 (deftest part1-test
   (testing "Reproduces the answer for day16, part1"
-    (is (= 213 (t/part1 day16-input)))))
+    (is (= 213 (d16/part1 day16-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day16, part2"
-    (is (= 323 (t/part2 day16-input)))))
+    (is (= 323 (d16/part2 day16-input)))))
