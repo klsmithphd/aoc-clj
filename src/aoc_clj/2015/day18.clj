df6181bd02b8c1bcc5de9868fc857e3d26a77b41
Ken Smith <ken@klsmith.me>
2024-02-05 23:53:57 +0000

    Add docstrings

--- src/aoc_clj/2015/day18.clj
+++ src/aoc_clj/2015/day18.clj
@@ -2,11 +2,16 @@
   "Solution to https://adventofcode.com/2015/day/18"
   (:require [aoc-clj.utils.grid.mapgrid :as mapgrid]))
 
+;; Constants
+(def soln-steps 100)
+
+;; Input parsing
 (def char-map {\. :off \# :on})
 (defn parse
   [input]
   (mapgrid/ascii->MapGrid2D char-map input))
 
+;; Puzzle logic
 (defn adj-coords
   [[x y]]
   (filter #(not= [x y] %) (for [y (range (dec y) (+ y 2))
@@ -19,6 +24,7 @@
     (zipmap locs (map grid locs))))
 
 (defn corner?
+  "Is the given position one of the four corner cells?"
   [{:keys [width height]} pos]
   (or (= pos [0 0])
       (= pos [0 (dec height)])
@@ -26,6 +32,7 @@
       (= pos [(dec width) (dec height)])))
 
 (defn corners-on
+  "Force set the four corners to an on position"
   [{:keys [width height grid] :as input}]
   (assoc input :grid (assoc grid
                             [0 0] :on
@@ -34,6 +41,8 @@
                             [(dec width) (dec height)] :on)))
 
 (defn update-lights
+  "Update the light value in the grid at the given `position`, based
+   on current neighbor values"
   ([input pos]
    (update-lights false input pos))
   ([corners-on? {:keys [grid] :as input} pos]
@@ -50,6 +59,7 @@
                 :off))))))
 
 (defn step
+  "Update all lights in the grid based on their neighbors."
   ([input]
    (step false input))
   ([corners-on? {:keys [grid] :as input}]
@@ -58,18 +68,24 @@
                   (map (partial update-lights corners-on? input) (keys grid))))))
 
 (defn lights-on-at-step-n
+  "Evolve the light grid to the nth step"
   ([n input]
    (lights-on-at-step-n false n input))
   ([corners-on? n input]
-   (->>  (nth (iterate (partial step corners-on?) input) n)
+   (->>  (iterate (partial step corners-on?) input)
+         (drop n)
+         first
          :grid
          (filter #(= :on (val %)))
          count)))
 
+;; Puzzle solutions
 (defn part1
+  "How many lights are on after 100 steps"
   [input]
-  (lights-on-at-step-n 100 input))
+  (lights-on-at-step-n soln-steps input))
 
 (defn part2
+  "How many lights are on after 100 steps when the corners are kept always on"
   [input]
-  (lights-on-at-step-n true 100 (corners-on input)))
+  (lights-on-at-step-n true soln-steps (corners-on input)))
