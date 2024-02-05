f2e1cf5de7b5040a09e7eb76cf0041b785d2958b
Ken Smith <ken@klsmith.me>
2024-02-05 18:31:03 +0000

    Much faster

--- src/aoc_clj/2015/day17.clj
+++ src/aoc_clj/2015/day17.clj
@@ -1,6 +1,5 @@
 (ns aoc-clj.2015.day17
-  "Solution to https://adventofcode.com/2015/day/17"
-  (:require [clojure.math.combinatorics :as combo]))
+  "Solution to https://adventofcode.com/2015/day/17")
 
 ;; Constants
 (def target-qty 150)
@@ -8,58 +7,53 @@
 ;; Input parsing
 (defn parse
   [input]
-  (map read-string input))
+  (sort > (map read-string input)))
 
 ;; Puzzle logic
+(defn combinations
+  ([total containers]
+   (combinations total containers []))
+  ([total containers used]
+   (if (zero? total)
+     ;; If we get down to zero for the target total, we've succeeded, so
+     ;; return the containers we used to get here
+     [used]
+     (if (or (neg? total) (> total (reduce + containers)))
+       ;; If the remaining containers aren't enough to meet the target, 
+       ;; or if we've overshot the target and gone negative, don't look any 
+       ;; further and return an empty vec indicating a dead branch 
+       []
+       ;; Otherwise, we explore the sub-problem of picking each container
+       ;; (in descending size order), removing its size from the total, and
+       ;; computing the total options for meeting the new smaller target total
+       ;; from the remaining containers.
+       ;;
+       ;; First, filter to only containers that are less than or equal to 
+       ;; our target
+       (let [ctrs (filter #(<= % total) containers)]
+         (->> (range (count ctrs))
+              (mapcat #(combinations
+                        (- total (nth ctrs %))
+                        (drop (inc %) ctrs)
+                        (conj used (nth ctrs %))))))))))
+
 (defn total-options
   [total containers]
-  (if (zero? total)
-    ;; If we get down to zero for the target total, we've succeeded, so this
-    ;; counts as one option
-    1
-    (if (or (neg? total) (> total (reduce + containers)))
-      ;; If the remaining containers aren't enough to meet the target, 
-      ;; or if we've overshot the target and gone negative, don't look any 
-      ;; further and return zero indicating a dead branch
-      0
-      ;; Otherwise, we explore the sub-problem of picking each container
-      ;; (in descending size order), removing its size from the total, and
-      ;; computing the total options for meeting the new smaller target total
-      ;; from the remaining containers.
-      ;;
-      ;; First, filter to only containers that are less than or equal to 
-      ;; our target
-      (let [ctrs (filter #(<= % total) containers)]
-        (->> (range (count ctrs))
-             (map #(total-options (- total (nth ctrs %)) (drop (inc %) ctrs)))
-             (reduce +))))))
+  (count (combinations total containers)))
 
-
-
-(defn sums-to?
-  [sum coll]
-  (= sum (reduce + (map second coll))))
-
-(defn combinations
-  [sum containers]
-  (->> (map-indexed vector containers)
-       (combo/subsets)
-       (filter (partial sums-to? sum))
-       (map (partial map  second))))
-
-(defn minimal-combinations
-  [sum containers]
-  (let [combos (combinations sum containers)
-        min-containers (apply min (map count combos))]
-    (filter #(= min-containers (count %)) combos)))
+(defn min-container-total-options
+  [total containers]
+  (let [freqs (->> (combinations total containers)
+                   (map count)
+                   (frequencies))
+        min-key (apply min (keys freqs))]
+    (freqs min-key)))
 
 ;; Puzzle solutions
 (defn part1
   [input]
-  (total-options target-qty input)
-  ;; (count (combinations 150 input))
-  )
+  (total-options target-qty input))
 
 (defn part2
   [input]
-  (count (minimal-combinations target-qty input)))
+  (min-container-total-options target-qty input))
--- test/aoc_clj/2015/day17_test.clj
+++ test/aoc_clj/2015/day17_test.clj
@@ -5,7 +5,17 @@
 
 (def d17-s00 [20 15 10 5 5])
 
-(def total-options-test
+(deftest combinations-test
+  (testing "Returns the possible combinations that meet a target"
+    (is (= [] (t/combinations 10 [5])))
+    (is (= [[8]] (t/combinations 8 [8])))
+    (is (= [[5]] (t/combinations 0 [4 3 2 1] [5])))
+    (is (= [[3 2]] (t/combinations 5 [3 2])))
+    (is (= [[3 2] [3 2]] (t/combinations 5 [3 2 2])))
+
+    (is (= [[20 5] [20 5] [15 10] [15 5 5]] (t/combinations 25 d17-s00)))))
+
+(deftest total-options-test
   (testing "Computes the total options to meet a target"
     (is (= 0 (t/total-options 10 [5])))
     (is (= 1 (t/total-options 8 [8])))
@@ -15,15 +25,9 @@
 
     (is (= 4 (t/total-options 25 d17-s00)))))
 
-
-
-(deftest combinations-test
-  (testing "Identifies the correct combinations, allowing for dupes"
-    (is (= '((20 5) (20 5) (15 10) (15 5 5)) (t/combinations 25 d17-s00)))))
-
-(deftest min-combinations-test
-  (testing "Identifies the correct minimal combinations"
-    (is (= '((20 5) (20 5) (15 10)) (t/minimal-combinations 25 d17-s00)))))
+(deftest min-container-total-options-test
+  (testing "Computes the number of options that use a minimum number of containers"
+    (is (= 3 (t/min-container-total-options 25 d17-s00)))))
 
 (def day17-input (u/parse-puzzle-input t/parse 2015 17))
 
@@ -33,6 +37,6 @@
   (testing "Reproduces the answer for day17, part1"
     (is (= 654 (t/part1 day17-input)))))
 
-(deftest ^:slow part2
+(deftest part2
   (testing "Reproduces the answer for day17, part2"
     (is (= 57 (t/part2 day17-input)))))
