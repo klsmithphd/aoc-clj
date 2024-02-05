e76076d4073e7a663637e340d009a0fb6c927884
Ken Smith <ken@klsmith.me>
2024-02-05 17:12:22 +0000

    Use a dynamic programming approach

--- src/aoc_clj/2015/day17.clj
+++ src/aoc_clj/2015/day17.clj
@@ -2,9 +2,36 @@
   "Solution to https://adventofcode.com/2015/day/17"
   (:require [clojure.math.combinatorics :as combo]))
 
+;; Input parsing
 (defn parse
   [input]
   (map read-string input))
+
+;; Puzzle logic
+(defn total-options
+  [total containers]
+  (if (zero? total)
+    ;; If we get down to zero for the target total, we've succeeded, so this
+    ;; counts as one option
+    1
+    (if (or (neg? total) (> total (reduce + containers)))
+      ;; If the remaining containers aren't enough to meet the target, 
+      ;; or if we've overshot the target and gone negative, don't look any 
+      ;; further and return zero indicating a dead branch
+      0
+      ;; Otherwise, we explore the sub-problem of picking each container
+      ;; (in descending size order), removing its size from the total, and
+      ;; computing the total options for meeting the new smaller target total
+      ;; from the remaining containers.
+      ;;
+      ;; First, filter to only containers that are less than or equal to 
+      ;; our target
+      (let [ctrs (filter #(<= % total) containers)]
+        (->> (range (count ctrs))
+             (map #(total-options (- total (nth ctrs %)) (drop (inc %) ctrs)))
+             (reduce +))))))
+
+
 
 (defn sums-to?
   [sum coll]
@@ -23,9 +50,12 @@
         min-containers (apply min (map count combos))]
     (filter #(= min-containers (count %)) combos)))
 
+;; Puzzle solutions
 (defn part1
   [input]
-  (count (combinations 150 input)))
+  (total-options 150 input)
+  ;; (count (combinations 150 input))
+  )
 
 (defn part2
   [input]
--- test/aoc_clj/2015/day17_test.clj
+++ test/aoc_clj/2015/day17_test.clj
@@ -5,6 +5,18 @@
 
 (def d17-s00 [20 15 10 5 5])
 
+(def total-options-test
+  (testing "Computes the total options to meet a target"
+    (is (= 0 (t/total-options 10 [5])))
+    (is (= 1 (t/total-options 8 [8])))
+    (is (= 1 (t/total-options 0 [1 2 3 4])))
+    (is (= 1 (t/total-options 5 [3 2])))
+    (is (= 2 (t/total-options 5 [3 2 2])))
+
+    (is (= 4 (t/total-options 25 d17-s00)))))
+
+
+
 (deftest combinations-test
   (testing "Identifies the correct combinations, allowing for dupes"
     (is (= '((20 5) (20 5) (15 10) (15 5 5)) (t/combinations 25 d17-s00)))))
@@ -17,7 +29,7 @@
 
 ;; FIXME: 2015.day17 solution is slow
 ;; https://github.com/Ken-2scientists/aoc-clj/issues/6
-(deftest ^:slow part1
+(deftest part1
   (testing "Reproduces the answer for day17, part1"
     (is (= 654 (t/part1 day17-input)))))
 
