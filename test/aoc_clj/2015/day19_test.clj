0e375d3fd00f3fcdfc0c6f18a848a8e9f166a901
Ken Smith <ken@klsmith.me>
2024-02-07 22:49:59 +0000

    Cleanup/refactor of 2015 Day19

--- test/aoc_clj/2015/day19_test.clj
+++ test/aoc_clj/2015/day19_test.clj
@@ -1,44 +1,69 @@
 (ns aoc-clj.2015.day19-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day19 :as t]))
+            [aoc-clj.2015.day19 :as d19]))
+
+(def d19-s00-raw
+  ["e => H"
+   "e => O"
+   "H => HO"
+   "H => OH"
+   "O => HH"
+   ""
+   "HOH"])
+
+(def d19-s01-raw
+  ["e => H"
+   "e => O"
+   "H => HO"
+   "H => OH"
+   "O => HH"
+   ""
+   "HOHOHO"])
 
 (def d19-s00
-  (t/parse
-   ["e => H"
-    "e => O"
-    "H => HO"
-    "H => OH"
-    "O => HH"
-    ""
-    "HOH"]))
+  {:replacements [["e" "H"]
+                  ["e" "O"]
+                  ["H" "HO"]
+                  ["H" "OH"]
+                  ["O" "HH"]]
+   :molecule "HOH"})
 
-(def d19-s01
-  (t/parse
-   ["e => H"
-    "e => O"
-    "H => HO"
-    "H => OH"
-    "O => HH"
-    ""
-    "HOHOHO"]))
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d19-s00 (d19/parse d19-s00-raw)))))
 
-(deftest distinct-replacements-test
+(def d19-s01 (d19/parse d19-s01-raw))
+
+(deftest match-indices-test
+  (testing "Finds the indices of all matching substrings in a string"
+    (is (= [[0 2] [2 4] [4 6]] (d19/match-ranges "HOHOHO" "HO")))
+    (is (= [[1 3] [3 5]]       (d19/match-ranges "HOHOHO" "OH")))
+    (is (= []                  (d19/match-ranges "HOHOHO" "e")))
+    (is (= [[1 4] [3 6]]       (d19/match-ranges "HOHOHO" "OHO")))))
+
+(deftest single-replacements-test
+  (testing "Returns all possible new strings for a given replacement pair"
+    (is (= ["HOOH" "HOHO"] (d19/single-replacements "HOH" ["H" "HO"])))
+    (is (= ["OHOH" "HOOH"] (d19/single-replacements "HOH" ["H" "OH"])))
+    (is (= ["HHHH"]        (d19/single-replacements "HOH" ["O" "HH"])))))
+
+(deftest distinct-molecules-test
   (testing "Can count the correct number of distinct molecules generated"
-    (is (= 4 (count (t/distinct-molecules d19-s00))))
-    (is (= 7 (count (t/distinct-molecules d19-s01))))))
+    (is (= 4 (count (d19/distinct-molecules d19-s00))))
+    (is (= 7 (count (d19/distinct-molecules d19-s01))))))
 
 (deftest fabrication-steps-test
   (testing "Can count the minimum number of steps to fabricate the molecule"
-    (is (= 3 (count (t/fabrication-steps d19-s00))))
-    (is (= 6 (count (t/fabrication-steps d19-s01))))))
+    (is (= 3 (count (d19/fabrication-steps d19-s00))))
+    (is (= 6 (count (d19/fabrication-steps d19-s01))))))
 
-(def day19-input (u/parse-puzzle-input t/parse 2015 19))
+(def day19-input (u/parse-puzzle-input d19/parse 2015 19))
 
 (deftest part1-test
   (testing "Reproduces the answer for day19, part1"
-    (is (= 518 (t/part1 day19-input)))))
+    (is (= 518 (d19/part1 day19-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day19, part2"
-    (is (= 200 (t/part2 day19-input)))))
+    (is (= 200 (d19/part2 day19-input)))))
