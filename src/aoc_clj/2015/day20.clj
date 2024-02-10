568047caf17c7ff97e44434c3904730e3de2adf1
Ken Smith <ken@klsmith.me>
2024-02-10 19:29:50 +0000

    Added sigma_one fn, but it's not helping with speed

--- src/aoc_clj/2015/day20.clj
+++ src/aoc_clj/2015/day20.clj
@@ -5,12 +5,35 @@
 (def parse (comp read-string first))
 
 ;; Puzzle logic
+(defn pos-neg-range
+  []
+  (interleave (drop 1 (range)) (map #(* -1 %) (drop 1 (range)))))
+
+(defn gen-pentagonal-nums
+  "[Generalized pentagonal numbers](https://en.wikipedia.org/wiki/Pentagonal_number)"
+  []
+  (for [i (pos-neg-range)]
+    (/ (- (* 3 i i) i) 2)))
+
+(def sum-of-divisors
+  "The [sum-of-divisors function](https://en.wikipedia.org/wiki/Divisor_function)
+   for z=1, aka sigma_1(n)
+   
+   This implementation uses Euler's recurrence relationship from:
+   https://en.wikipedia.org/wiki/Divisor_function#Other_properties_and_identities"
+  (memoize
+   (fn [n]
+     (let [recurrences (take-while #(<= % n) (gen-pentagonal-nums))]
+       (->> (map #(if (zero? (- n %)) n (sum-of-divisors (- n %))) recurrences)
+            (partition 2 2 [])
+            (map #(reduce + %))
+            (map #(* %1 %2) (cycle [1 -1]))
+            (reduce +))))))
+
 (defn house-presents
   "Computes the number of presents delivered to the house numbered `house`"
   [house]
-  (let [factors (->> (range 1 (inc house))
-                     (filter #(zero? (rem house %))))]
-    (* 10 (reduce + factors))))
+  (* 10 (sum-of-divisors house)))
 
 (defn house-presents-part2
   "Computes the number of presents delivered to teh house numbered `house`
@@ -34,7 +57,7 @@
 (defn part1
   "The lowest house number that gets at least as many presents as input"
   [input]
-  (first-house-with-n-presents house-presents 776159 input))
+  (first-house-with-n-presents house-presents 0 input))
 
 (defn part2
   "The lowest house number that gets at least as many presents as input
--- test/aoc_clj/2015/day20_test.clj
+++ test/aoc_clj/2015/day20_test.clj
@@ -3,6 +3,17 @@
             [aoc-clj.utils.core :as u]
             [aoc-clj.2015.day20 :as d20]))
 
+(def A000203-first70
+  "https://oeis.org/A00020"
+  [1 3 4 7 6 12 8 15 13 18 12 28 14 24 24 31 18 39 20 42 32 36 24 60
+   31 42 40 56 30 72 32 63 48 54 48 91 38 60 56 90 42 96 44 84 78 72
+   48 124 57 93 72 98 54 120 72 120 80 90 60 168 62 96 104 127 84 144
+   68 126 96 144])
+
+(deftest sum-of-divisors-test
+  (testing "Computes the sum-of-divisors for integer n"
+    (is (= A000203-first70 (map d20/sum-of-divisors (range 1 71))))))
+
 (deftest presents-test
   (testing "Counts the right number of presents at the first few houses"
     (is (= [10 30 40 70 60 120 80 150 130]
