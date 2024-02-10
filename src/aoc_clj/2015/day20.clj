ff8b989bde692826e6a4e18fb53341e79faea868
Ken Smith <ken@klsmith.me>
2024-02-10 18:34:40 +0000

    Add docs to 2015 day 20

--- src/aoc_clj/2015/day20.clj
+++ src/aoc_clj/2015/day20.clj
@@ -1,29 +1,43 @@
 (ns aoc-clj.2015.day20
   "Solution to https://adventofcode.com/2015/day/20")
 
+;; Input parsing
 (def parse (comp read-string first))
 
+;; Puzzle logic
 (defn house-presents
+  "Computes the number of presents delivered to the house numbered `house`"
   [house]
   (let [factors (->> (range 1 (inc house))
                      (filter #(zero? (rem house %))))]
     (* 10 (reduce + factors))))
 
-(defn first-house-with-n-presents
-  [present-logic start input]
-  (ffirst (filter #(>= (second %) input) (map #(vector % (present-logic %)) (range start input)))))
-
 (defn house-presents-part2
+  "Computes the number of presents delivered to teh house numbered `house`
+   using the rules of part2"
   [house]
   (let [start   (int (Math/ceil (/ house 50)))
         factors (->> (range (if (zero? start) 1 start) (inc house))
                      (filter #(zero? (rem house %))))]
     (* 11 (reduce + factors))))
 
+(defn first-house-with-n-presents
+  "The first house number that has at least `n` presents delivered to it,
+   using the present-distribution logic fn `present-logic`."
+  [present-logic start n]
+  (->> (range start n)
+       (map #(vector % (present-logic %)))
+       (filter #(>= (second %) n))
+       ffirst))
+
+;; Puzzle solutions
 (defn part1
+  "The lowest house number that gets at least as many presents as input"
   [input]
   (first-house-with-n-presents house-presents 776159 input))
 
 (defn part2
+  "The lowest house number that gets at least as many presents as input
+   using the part2 present distribution logic"
   [input]
   (first-house-with-n-presents house-presents-part2 786239 input))
--- test/aoc_clj/2015/day20_test.clj
+++ test/aoc_clj/2015/day20_test.clj
@@ -1,19 +1,19 @@
 (ns aoc-clj.2015.day20-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day20 :as t]))
+            [aoc-clj.2015.day20 :as d20]))
 
 (deftest presents-test
   (testing "Counts the right number of presents at the first few houses"
     (is (= [10 30 40 70 60 120 80 150 130]
-           (map t/house-presents (range 1 10))))))
+           (map d20/house-presents (range 1 10))))))
 
-(def day20-input (u/parse-puzzle-input t/parse 2015 20))
+(def day20-input (u/parse-puzzle-input d20/parse 2015 20))
 
 (deftest part1-test
   (testing "Reproduces the answer for day20, part1"
-    (is (= 776160 (t/part1 day20-input)))))
+    (is (= 776160 (d20/part1 day20-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day20, part2"
-    (is (= 786240 (t/part2 day20-input)))))
+    (is (= 786240 (d20/part2 day20-input)))))
