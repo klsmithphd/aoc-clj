7f9c66f4d67d90c74a08f1769f42b83ac452ac03
Ken Smith <ken@klsmith.me>
2024-02-15 21:48:43 +0000

    Cleanup and docs, 2015 Day23

--- src/aoc_clj/2015/day23.clj
+++ src/aoc_clj/2015/day23.clj
@@ -2,12 +2,18 @@
   "Solution to https://adventofcode.com/2015/day/23"
   (:require [clojure.string :as str]))
 
+;; Constants
+(def init-state {:a 0 :b 0 :next-inst 0})
+
+;; Input parsing
 (defn parse-line
   [line]
   (let [args       (str/split line #", ")
         [inst val] (str/split (first args) #" ")
         base       {:inst (keyword inst)
-                    :arg1 (if (number? (read-string val)) (read-string val) val)}]
+                    :arg1 (if (number? (read-string val))
+                            (read-string val)
+                            (keyword val))}]
     (if (> (count args) 1)
       (assoc base :arg2 (read-string (second args)))
       base)))
@@ -16,42 +22,52 @@
   [input]
   (mapv parse-line input))
 
+;; Puzzle logic
 (defn apply-inst
+  "Updates the registers' state given the next instruction from 
+   `instructions`"
   [instructions {:keys [next-inst] :as state}]
-  (let [{:keys [inst arg1 arg2]} (nth instructions next-inst)
-        reg (keyword arg1)]
+  (let [{:keys [inst arg1 arg2]} (nth instructions next-inst)]
     (case inst
       :hlf (-> state
-               (update reg #(int (/ % 2)))
+               (update arg1 #(quot % 2))
                (update :next-inst inc))
       :tpl (-> state
-               (update reg * 3)
+               (update arg1 * 3)
                (update :next-inst inc))
       :inc (-> state
-               (update reg inc)
+               (update arg1 inc)
                (update :next-inst inc))
       :jmp (update state :next-inst + arg1)
-      :jie (if (even? (get state reg))
+      :jie (if (even? (get state arg1))
              (update state :next-inst + arg2)
              (update state :next-inst inc))
-      :jio (if (= 1 (get state reg))
+      :jio (if (= 1 (get state arg1))
              (update state :next-inst + arg2)
              (update state :next-inst inc)))))
 
 (defn not-done?
+  "The program is not complete while the next instruction is still
+  within the bounds of the ones provided"
   [max-inst next-inst]
-  (and (>= next-inst 0)
-       (< next-inst max-inst)))
+  (< -1 next-inst max-inst))
 
 (defn run-program
-  [instructions init]
+  "Execute the logic in `instructions` against the starting `state`"
+  [instructions state]
   (let [max-inst (count instructions)]
-    (first (drop-while #(not-done? max-inst (:next-inst %)) (iterate (partial apply-inst instructions) init)))))
+    (->> (iterate #(apply-inst instructions %) state)
+         (drop-while #(not-done? max-inst (:next-inst %)))
+         first)))
 
+;; Puzzle solutions
 (defn part1
+  "The value in register b after executing the program"
   [input]
-  (:b (run-program input {:a 0 :b 0 :next-inst 0})))
+  (:b (run-program input init-state)))
 
 (defn part2
+  "The value in register b after executing the program with register a
+   starting as 1"
   [input]
-  (:b (run-program input {:a 1 :b 0 :next-inst 0})))
+  (:b (run-program input (assoc init-state :a 1))))
--- test/aoc_clj/2015/day23_test.clj
+++ test/aoc_clj/2015/day23_test.clj
@@ -1,26 +1,34 @@
 (ns aoc-clj.2015.day23-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day23 :as t]))
+            [aoc-clj.2015.day23 :as d23]))
+
+(def d23-s00-raw
+  ["inc a"
+   "jio a, +2"
+   "tpl a"
+   "inc a"])
 
 (def d23-s00
-  (t/parse
-   ["inc a"
-    "jio a, +2"
-    "tpl a"
-    "inc a"]))
+  [{:inst :inc :arg1 :a}
+   {:inst :jio :arg1 :a :arg2 2}
+   {:inst :tpl :arg1 :a}
+   {:inst :inc :arg1 :a}])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d23-s00 (d23/parse d23-s00-raw)))))
 
 (deftest run-program-test
   (testing "Runs on the simple input"
-    (is (= {:a 2 :b 0 :next-inst 4}
-           (t/run-program d23-s00 {:a 0 :b 0 :next-inst 0})))))
+    (is (= {:a 2 :b 0 :next-inst 4} (d23/run-program d23-s00 d23/init-state)))))
 
-(def day23-input (u/parse-puzzle-input t/parse 2015 23))
+(def day23-input (u/parse-puzzle-input d23/parse 2015 23))
 
 (deftest part1-test
   (testing "Reproduces the answer for day23, part1"
-    (is (= 184 (t/part1 day23-input)))))
+    (is (= 184 (d23/part1 day23-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day23, part2"
-    (is (= 231 (t/part2 day23-input)))))
+    (is (= 231 (d23/part2 day23-input)))))
