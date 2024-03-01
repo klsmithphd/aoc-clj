5c5cf14c8c2954fe135d1a33e67ae9b4edc97c0c
Ken Smith <ken@klsmith.me>
2024-03-01 17:16:42 +0000

    Cleanup and document 2015 Day 24

--- src/aoc_clj/2015/day24.clj
+++ src/aoc_clj/2015/day24.clj
@@ -2,36 +2,54 @@
   "Solution to https://adventofcode.com/2015/day/24"
   (:require [clojure.math.combinatorics :as combo]))
 
+;; Input parsing
 (defn parse
   [input]
   (map read-string input))
 
-(defn smallest-groups
-  [groups input]
-  (let [target (/ (reduce + input) groups)]
-    (loop [size 1 partitions []]
+;; Puzzle logic
+(defn smallest-passenger-packages
+  "Returns the valid combination of package `weights` in the passenger 
+   compartment that will balance the weight across all `n` compartments but 
+   results in the smallest number of packages in the passenger compartment."
+  [n weights]
+  ;; We don't need to actually work out the distribution of packages across
+  ;; the other compartments --- every compartment needs an equal weight, so
+  ;; we just take the total weight, divide by the number of compartments,
+  ;; and return the combinations of items for the first (passenger) compartment
+  ;; that match the compartment weight target, starting with the fewest
+  ;; items possible (two)
+  (let [target (/ (reduce + weights) n)]
+    (loop [size 2 partitions []]
       (if (seq partitions)
         partitions
-        (recur (inc size) (filter #(= target (reduce + %)) (combo/combinations input size)))))))
+        (recur
+         (inc size)
+         (filter #(= target (reduce + %)) (combo/combinations weights size)))))))
 
 (defn best-quantum-entanglement
+  "Finds the best (minimum) quantum entanglement, which is just the product
+   of the packages' weights"
   [partitions]
-  (apply min (map (partial reduce *) partitions)))
+  (->> (map (partial reduce *) partitions)
+       (apply min)))
 
-(defn best-qe-thirds
-  [input]
-  (->> (smallest-groups 3 input)
+(defn ideal-quantum-entanglement
+  "Returns the ideal quantum entanglement value for the packages divided
+   into `n` compartments"
+  [n weights]
+  (->> (smallest-passenger-packages n weights)
        best-quantum-entanglement))
 
-(defn best-qe-fourths
-  [input]
-  (->> (smallest-groups 4 input)
-       best-quantum-entanglement))
-
+;; Puzzle solutions
 (defn part1
+  "Quantum entanglement of the packages in the passenger compartment for
+   packages divided among 3 compartments"
   [input]
-  (best-qe-thirds input))
+  (ideal-quantum-entanglement 3 input))
 
 (defn part2
+  "Quantum entanglement of the packages in the passenger compartment for
+   packages divided among 4 compartments"
   [input]
-  (best-qe-fourths input))
+  (ideal-quantum-entanglement 4 input))
--- test/aoc_clj/2015/day24_test.clj
+++ test/aoc_clj/2015/day24_test.clj
@@ -1,21 +1,27 @@
 (ns aoc-clj.2015.day24-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2015.day24 :as t]))
+            [aoc-clj.2015.day24 :as d24]))
 
 (def d24-s00 (concat (range 1 6) (range 7 12)))
 
-(deftest best-qe-test
-  (testing "Correctly computes the lowest quantum entanglement for the sample data"
-    (is (= 99 (t/best-qe-thirds d24-s00)))
-    (is (= 44 (t/best-qe-fourths d24-s00)))))
+(deftest smallest-passenger-packages-test
+  (testing "Computes the combinations of packages for the passenger compartment
+            with the smallest number of packages"
+    (is (= [[9 11]] (d24/smallest-passenger-packages 3 d24-s00)))
+    (is (= [[4 11] [5 10] [7 8]] (d24/smallest-passenger-packages 4 d24-s00)))))
 
-(def day24-input (u/parse-puzzle-input t/parse 2015 24))
+(deftest ideal-quantum-entanglement-test
+  (testing "Correctly computes the lowest quantum entanglement for the sample data"
+    (is (= 99 (d24/ideal-quantum-entanglement 3 d24-s00)))
+    (is (= 44 (d24/ideal-quantum-entanglement 4 d24-s00)))))
+
+(def day24-input (u/parse-puzzle-input d24/parse 2015 24))
 
 (deftest part1-test
   (testing "Reproduces the answer for day24, part1"
-    (is (= 11846773891 (t/part1 day24-input)))))
+    (is (= 11846773891 (d24/part1 day24-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day24, part1"
-    (is (= 80393059 (t/part2 day24-input)))))
+    (is (= 80393059 (d24/part2 day24-input)))))
