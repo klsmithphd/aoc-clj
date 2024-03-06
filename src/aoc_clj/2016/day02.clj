d91050968feae78ba9118d53d646fb43f7377100
Ken Smith <ken@klsmith.me>
2024-03-06 23:15:40 +0000

    Cleanup and document 2016-02

--- src/aoc_clj/2016/day02.clj
+++ src/aoc_clj/2016/day02.clj
@@ -1,21 +1,27 @@
 (ns aoc-clj.2016.day02
   "Solution to https://adventofcode.com/2016/day/2")
 
-(def parse identity)
+;; Constants
+(def first-pos [0 0])
 
 (def square-keypad
   {[-1 -1] 1  [0 -1] 2  [1 -1] 3
    [-1  0] 4  [0  0] 5  [1  0] 6
    [-1  1] 7  [0  1] 8  [1  1] 9})
 
-(def diagonal-keypad
+(def diamond-keypad
   {[2 -2] 1
    [1 -1] 2  [2 -1] 3  [3 -1] 4
    [0  0] 5  [1  0] 6  [2  0] 7  [3  0] 8  [4  0] 9
    [1  1] "A"  [2  1] "B"  [3  1] "C"
    [2 2] "D"})
 
+;; Input parsing
+(def parse identity)
+
+;; Puzzle logic
 (defn intended-step
+  "Determine the grid position that `cmd` would have you try to move to next"
   [[x y] cmd]
   (case cmd
     \U [x (dec y)]
@@ -23,32 +29,43 @@
     \R [(inc x) y]
     \L [(dec x) y]))
 
-(defn step
+(defn move
+  "Apply the given `cmd` to move (or not move if impossible) to the next
+   position on the keypad"
   [keypad pos cmd]
   (let [goto (intended-step pos cmd)]
     (if (keypad goto)
       goto
       pos)))
 
-(defn move-digit
+(defn digit-pos
+  "Determine where you end up following the instructions (`cmds`) for a single
+   digit, starting at `start`"
   [keypad start cmds]
-  (reduce (partial step keypad) start cmds))
+  (reduce (partial move keypad) start cmds))
 
-(defn all-digits
+(defn all-digits-pos
+  "Determine the grid positions of all digits, given the sequential cmds
+   for each one"
   [keypad cmds]
-  (reductions (partial move-digit keypad) [0 0] cmds))
+  (reductions (partial digit-pos keypad) first-pos cmds))
 
 (defn bathroom-code
+  "Compute the bathroom code for the given `keyboard` layout and set of 
+   `cmds`"
   [keypad cmds]
-  (->> cmds (all-digits keypad) rest (map keypad) (apply str)))
+  (->> cmds (all-digits-pos keypad) rest (map keypad) (apply str)))
 
-(def square-bathroom-code   (partial bathroom-code square-keypad))
-(def diagonal-bathroom-code (partial bathroom-code diagonal-keypad))
+(def square-bathroom-code  (partial bathroom-code square-keypad))
+(def diamond-bathroom-code (partial bathroom-code diamond-keypad))
 
+;; Puzzle solutions
 (defn part1
+  "What's the bathroom code given the provided instructions (square keypad)"
   [input]
   (square-bathroom-code input))
 
 (defn part2
+  "What's the bathroom code given the provided instructions (diamond keypad)"
   [input]
-  (diagonal-bathroom-code input))
+  (diamond-bathroom-code input))
--- test/aoc_clj/2016/day02_test.clj
+++ test/aoc_clj/2016/day02_test.clj
@@ -1,7 +1,7 @@
 (ns aoc-clj.2016.day02-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day02 :as t]))
+            [aoc-clj.2016.day02 :as d02]))
 
 (def d02-s00
   ["ULL"
@@ -11,16 +11,16 @@
 
 (deftest bathroom-code
   (testing "Computes the bathroom code on a square 9-digit keypad"
-    (is (= "1985" (t/square-bathroom-code d02-s00))))
+    (is (= "1985" (d02/square-bathroom-code d02-s00))))
   (testing "Computes the bathroom code on a diagonal keypad"
-    (is (= "5DB3" (t/diagonal-bathroom-code d02-s00)))))
+    (is (= "5DB3" (d02/diamond-bathroom-code d02-s00)))))
 
-(def day02-input (u/parse-puzzle-input t/parse 2016 2))
+(def day02-input (u/parse-puzzle-input d02/parse 2016 2))
 
 (deftest part1-test
   (testing "Reproduces the answer for day02, part1"
-    (is (= "18843" (t/part1 day02-input)))))
+    (is (= "18843" (d02/part1 day02-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day02, part2"
-    (is (= "67BB9" (t/part2 day02-input)))))
+    (is (= "67BB9" (d02/part2 day02-input)))))
