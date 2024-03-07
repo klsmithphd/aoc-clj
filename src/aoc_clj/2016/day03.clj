63a4d7bdb6c7200ba5e49c50cdf381ee9a4ce5f7
Ken Smith <ken@klsmith.me>
2024-03-07 23:11:04 +0000

    Cleanup and document 2016 Day3

--- src/aoc_clj/2016/day03.clj
+++ src/aoc_clj/2016/day03.clj
@@ -1,6 +1,8 @@
 (ns aoc-clj.2016.day03
-  "Solution to https://adventofcode.com/2016/day/3")
+  "Solution to https://adventofcode.com/2016/day/3"
+  (:require [aoc-clj.utils.core :as u]))
 
+;; Input parsing
 (defn parse-line
   [line]
   (read-string (str "[" line "]")))
@@ -9,27 +11,36 @@
   [input]
   (map parse-line input))
 
+;; Puzzle logic
 (defn valid-triangle?
+  "A triangle is valid if the sum of two sides are strictly greater than
+   the the remaining side"
   [[a b c]]
   (and (> (+ a b) c)
        (> (+ b c) a)
        (> (+ c a) b)))
 
 (defn count-of-valid-triangles
-  [input]
-  (->> input (filter valid-triangle?) count))
+  "Count of triangles that are considered valid"
+  [triangles]
+  (->> triangles (filter valid-triangle?) count))
 
 (defn group-by-columns
+  "Re-interpret data with triangles spanning three consecutive rows
+   in the same column."
   [input]
-  (->> (concat (map #(nth % 0) input)
-               (map #(nth % 1) input)
-               (map #(nth % 2) input))
-       (partition 3)))
+  ;; This implementation looks at rows in chunks of 3 and then just
+  ;; transposes rows to columns
+  (->> (partition 3 input)
+       (mapcat u/transpose)))
 
+;; Puzzle solutions
 (defn part1
+  "How many triangles are valid"
   [input]
   (count-of-valid-triangles input))
 
 (defn part2
+  "How many triangles are valid re-interpreting the data as spanning 3 rows"
   [input]
   (-> input group-by-columns count-of-valid-triangles))
--- test/aoc_clj/2016/day03_test.clj
+++ test/aoc_clj/2016/day03_test.clj
@@ -1,7 +1,7 @@
 (ns aoc-clj.2016.day03-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day03 :as t]))
+            [aoc-clj.2016.day03 :as d02]))
 
 (def d03-s00
   [[101 301 501]
@@ -11,22 +11,27 @@
    [202 402 602]
    [203 403 603]])
 
+(deftest test-valid-triangle?
+  (testing "Correctly determines whether a triangle is valid"
+    (is (= false (d02/valid-triangle? [5 10 25])))
+    (is (= true  (d02/valid-triangle? [5 12 13])))))
+
 (deftest group-by-columns
   (testing "Reorders the triangles to be read by columns instead of rows"
-    (is (= '((101 102 103)
-             (201 202 203)
-             (301 302 303)
-             (401 402 403)
-             (501 502 503)
-             (601 602 603))
-           (t/group-by-columns d03-s00)))))
+    (is (= (set [[101 102 103]
+                 [201 202 203]
+                 [301 302 303]
+                 [401 402 403]
+                 [501 502 503]
+                 [601 602 603]])
+           (set (d02/group-by-columns d03-s00))))))
 
-(def day03-input (u/parse-puzzle-input t/parse 2016 3))
+(def day03-input (u/parse-puzzle-input d02/parse 2016 3))
 
 (deftest part1-test
   (testing "Reproduces the answer for day03, part1"
-    (is (= 869 (t/part1 day03-input)))))
+    (is (= 869 (d02/part1 day03-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day03, part2"
-    (is (= 1544 (t/part2 day03-input)))))
+    (is (= 1544 (d02/part2 day03-input)))))
