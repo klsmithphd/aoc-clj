5996b541bea7de29f58c08c61a21d5a4b6d68215
Ken Smith <ken@klsmith.me>
2024-03-10 00:01:30 +0000

    Tidying and documenation for 2016 Day04

--- src/aoc_clj/2016/day04.clj
+++ src/aoc_clj/2016/day04.clj
@@ -3,48 +3,62 @@
   (:require [clojure.string :as str]
             [aoc-clj.utils.core :as u]))
 
+;; Constants
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 
+;; Input parsing
 (defn parse-line
   [line]
   (let [[l r] (str/split line #"\[")
         name (str/split l #"-")]
     {:encrypted-name (butlast name)
      :sector-id (read-string (last name))
-     :checksum (butlast r)}))
+     :checksum (subs r 0 5)}))
 
 (defn parse
   [input]
   (map parse-line input))
 
+;; Puzzle logic
 (defn freq-letter-compare
+  "A compare function for sorting letter-frequency key-value pairs.
+   Frequencies should be sorted in descending order first, and if
+   there are ties, characters should be sorted alphabetically"
   [x y]
-  (let [freq-compare (compare (val x) (val y))]
+  (let [freq-compare (compare (val y) (val x))]
     (if (zero? freq-compare)
       (compare (key x) (key y))
-      (- freq-compare))))
+      freq-compare)))
 
 (defn real-room?
+  "A room is real if the checksum is the five most common characters
+   in the enrcrypted name, in order, with ties broken by alphabetization"
   [{:keys [:encrypted-name checksum]}]
   (let [freqs (->> (apply str encrypted-name)
                    frequencies
                    (sort freq-letter-compare)
-                   (map first)
-                   (take 5))]
+                   (map key)
+                   (take 5)
+                   (apply str))]
     (= freqs checksum)))
 
 (defn real-room-sector-id-sum
+  "The sum of the sector ids of all the real rooms"
   [input]
   (->> (filter real-room? input)
        (map :sector-id)
        (reduce +)))
 
 (defn decipher
+  "Update the room to add a decrypted name to all room names"
   [{:keys [encrypted-name sector-id] :as room}]
-  (let [mapping (zipmap alphabet (u/rotate (mod sector-id 26) alphabet))]
-    (assoc room :decrypted-name (map #(apply str (map mapping %)) encrypted-name))))
+  (let [charmap (zipmap alphabet (u/rotate (mod sector-id 26) alphabet))
+        decode  (fn [s] (apply str (map charmap s)))]
+    (assoc room :decrypted-name (map decode encrypted-name))))
 
 (defn north-pole-objects-room
+  "Find all the valid rooms, decrypt their names, and find the one
+   that matches `northpole object storage`."
   [input]
   (->> (filter real-room? input)
        (map decipher)
@@ -52,10 +66,13 @@
        first
        :sector-id))
 
+;; Puzzle solutions
 (defn part1
+  "Sum of the sector ids of the real rooms"
   [input]
   (real-room-sector-id-sum input))
 
 (defn part2
+  "Sector id of the room where North Pole objects are stored"
   [input]
   (north-pole-objects-room input))
--- test/aoc_clj/2016/day04_test.clj
+++ test/aoc_clj/2016/day04_test.clj
@@ -1,30 +1,47 @@
 (ns aoc-clj.2016.day04-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day04 :as t]))
+            [aoc-clj.2016.day04 :as d04]))
+
+(def d04-s00-raw
+  ["aaaaa-bbb-z-y-x-123[abxyz]"
+   "a-b-c-d-e-f-g-h-987[abcde]"
+   "not-a-real-room-404[oarel]"
+   "totally-real-room-200[decoy]"])
 
 (def d04-s00
-  (map t/parse-line
-       ["aaaaa-bbb-z-y-x-123[abxyz]"
-        "a-b-c-d-e-f-g-h-987[abcde]"
-        "not-a-real-room-404[oarel]"
-        "totally-real-room-200[decoy]"]))
+  [{:encrypted-name ["aaaaa" "bbb" "z" "y" "x"]
+    :sector-id 123
+    :checksum "abxyz"}
+   {:encrypted-name ["a" "b" "c" "d" "e" "f" "g" "h"]
+    :sector-id 987
+    :checksum "abcde"}
+   {:encrypted-name ["not" "a" "real" "room"]
+    :sector-id 404
+    :checksum "oarel"}
+   {:encrypted-name ["totally" "real" "room"]
+    :sector-id 200
+    :checksum "decoy"}])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d04-s00 (d04/parse d04-s00-raw)))))
 
 (deftest real-room?
   (testing "Identifies which rooms are real (not decoys)"
     (is (= [true true true false]
-           (map t/real-room? d04-s00)))))
+           (map d04/real-room? d04-s00)))))
 
 (deftest real-room-sector-id-sum
   (testing "Adds up the sector ids of all the real rooms"
-    (is (= 1514 (t/real-room-sector-id-sum d04-s00)))))
+    (is (= 1514 (d04/real-room-sector-id-sum d04-s00)))))
 
-(def day04-input (u/parse-puzzle-input t/parse 2016 4))
+(def day04-input (u/parse-puzzle-input d04/parse 2016 4))
 
 (deftest part1-test
   (testing "Reproduces the answer for day04, part1"
-    (is (= 361724 (t/part1 day04-input)))))
+    (is (= 361724 (d04/part1 day04-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day04, part2"
-    (is (= 482 (t/part2 day04-input)))))
+    (is (= 482 (d04/part2 day04-input)))))
