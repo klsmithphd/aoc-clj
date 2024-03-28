b261b6d7fc1f54aa3907248b828c8d58c4503f3a
Ken Smith <ken@klsmith.me>
2024-03-28 15:30:30 +0000

    Cleanup and documation of 2016-07

--- src/aoc_clj/2016/day07.clj
+++ src/aoc_clj/2016/day07.clj
@@ -1,50 +1,69 @@
 (ns aoc-clj.2016.day07
-  "Solution to https://adventofcode.com/2016/day/7"
-  (:require [clojure.string :as str]))
+  "Solution to https://adventofcode.com/2016/day/7")
 
+;; Input parsing
 (defn parse-line
   [line]
-  (let [chunks (str/split line #"\[")
-        splits (map #(str/split % #"\]") (rest chunks))]
-    {:supernets (into [(first chunks)] (map second splits))
-     :hypernets (mapv first splits)}))
+  (let [nets (re-seq #"\w+" line)]
+    {:supernets (take-nth 2 nets)
+     :hypernets (take-nth 2 (rest nets))}))
 
 (defn parse
   [input]
   (map parse-line input))
 
+;; Puzzle logic
 (defn abba?
+  "An ABBA is any four-character sequence which consists of a pair of
+   two different characters followed by the reverse of that pair, 
+   such as xyyx or abba"
   [s]
   (re-seq #"(.)(?!\1)(.)\2\1" s))
 
 (defn supports-tls?
+  "An IP supports TLS if it has an ABBA sequence among its supernets
+   but not among its hypernets"
   [{:keys [supernets hypernets]}]
-  (if (some identity (filter abba? hypernets))
+  (if (some abba? hypernets)
     false
-    (boolean (some identity (filter abba? supernets)))))
+    (boolean (some abba? supernets))))
 
 (defn all-abas
+  "Returns all ABA sequences.
+   
+   An ABA is any three-character sequence which consists of the same 
+   character twice with a different character between them, 
+   such as xyx or aba."
   [s]
   (re-seq #"(?=((.)(?!\2)(.)\2))" s))
 
 (defn bab
+  "Given an ABA sequence, construct a regex pattern to find an exact
+   literal BAB sequence"
   [[_ _ a b]]
   (re-pattern (str b a b)))
 
-(defn supports-ssl?
-  [{:keys [supernets hypernets]}]
-  (->> (mapcat all-abas supernets)
-       (map bab)
-       (filter (fn [re]
-                 (some identity (mapcat (partial re-find re) hypernets))))
-       (some identity)
-       boolean))
+(defn has-bab
+  "Whether any of the hypernets matches the BAB regex"
+  [hypernets bab]
+  (some #(re-find bab %) hypernets))
 
+(defn supports-ssl?
+  "An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, 
+   anywhere in the supernet sequences (outside any square bracketed sections), 
+   and a corresponding Byte Allocation Block, or BAB, 
+   anywhere in the hypernet sequences."
+  [{:keys [supernets hypernets]}]
+  (let [babs (->> (mapcat all-abas supernets) (map bab))]
+    (boolean (some #(has-bab hypernets %) babs))))
+
+;; Puzzle solutions
 (defn part1
+  "How many IPs support TLS"
   [input]
   (count (filter supports-tls? input)))
 
 (defn part2
+  "How many IPs support SLS"
   [input]
   (count (filter supports-ssl? input)))
-
--- test/aoc_clj/2016/day07_test.clj
+++ test/aoc_clj/2016/day07_test.clj
@@ -1,45 +1,65 @@
 (ns aoc-clj.2016.day07-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day07 :as t]))
+            [aoc-clj.2016.day07 :as d07]))
 
-(def day07-sample1
-  (map t/parse-line
-       ["abba[mnop]qrst"
-        "abcd[bddb]xyyx"
-        "aaaa[qwer]tyui"
-        "ioxxoj[asdfgh]zxcvbn"]))
+(def d07-s00-raw
+  ["abba[mnop]qrst"
+   "abcd[bddb]xyyx"
+   "aaaa[qwer]tyui"
+   "ioxxoj[asdfgh]zxcvbn"])
 
-(def day07-sample2
-  (map t/parse-line
-       ["aba[bab]xyz"
-        "xyx[xyx]xyx"
-        "aaa[kek]eke"
-        "zazbz[bzb]cdb"]))
+(def d07-s01-raw
+  ["aba[bab]xyz"
+   "xyx[xyx]xyx"
+   "aaa[kek]eke"
+   "zazbz[bzb]cdb"])
+
+(def d07-s02-raw
+  ["gdlrknrmexvaypu[crqappbbcaplkkzb]vhvkjyadjsryysvj[nbvypeadikilcwg]jwxlimrgakadpxu[dgoanojvdvwfabtt]yqsalmulblolkgsheo"])
+
+(d07/parse d07-s02-raw)
+
+(def d07-s00
+  [{:supernets ["abba" "qrst"] :hypernets ["mnop"]}
+   {:supernets ["abcd" "xyyx"] :hypernets ["bddb"]}
+   {:supernets ["aaaa" "tyui"] :hypernets ["qwer"]}
+   {:supernets ["ioxxoj" "zxcvbn"] :hypernets ["asdfgh"]}])
+
+(def d07-s01
+  [{:supernets ["aba" "xyz"] :hypernets ["bab"]}
+   {:supernets ["xyx" "xyx"] :hypernets ["xyx"]}
+   {:supernets ["aaa" "eke"] :hypernets ["kek"]}
+   {:supernets ["zazbz" "cdb"] :hypernets ["bzb"]}])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d07-s00 (d07/parse d07-s00-raw)))
+    (is (= d07-s01 (d07/parse d07-s01-raw)))))
 
 (deftest abba?
   (testing "Correct logic for detecting ABBA patterns"
-    (is (nil?  (t/abba? "abbc")))
-    (is (nil?  (t/abba? "aaaa")))
-    (is (some? (t/abba? "abba")))
-    (is (some? (t/abba? "aaaaxyyx")))))
+    (is (nil?  (d07/abba? "abbc")))
+    (is (nil?  (d07/abba? "aaaa")))
+    (is (some? (d07/abba? "abba")))
+    (is (some? (d07/abba? "aaaaxyyx")))))
 
 (deftest supports-tls?
   (testing "Matches sample data determination about supporting TLS"
     (is (= [true false false true]
-           (map t/supports-tls? day07-sample1)))))
+           (map d07/supports-tls? d07-s00)))))
 
 (deftest supports-ssl?
   (testing "Matches sample data determination about supporting SSL"
     (is (= [true false true true]
-           (map t/supports-ssl? day07-sample2)))))
+           (map d07/supports-ssl? d07-s01)))))
 
-(def day07-input (u/parse-puzzle-input t/parse 2016 7))
+(def day07-input (u/parse-puzzle-input d07/parse 2016 7))
 
 (deftest part1-test
   (testing "Reproduces the answer for day07, part1"
-    (is (= 110 (t/part1 day07-input)))))
+    (is (= 110 (d07/part1 day07-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day07, part2"
-    (is (= 242 (t/part2 day07-input)))))
+    (is (= 242 (d07/part2 day07-input)))))
