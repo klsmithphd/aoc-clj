3fad3c1bc6f2b34d2110418c21b654941ab0a539
Ken Smith <ken@klsmith.me>
2024-03-28 14:48:44 +0000

    Cleanup 2016 Day06

--- src/aoc_clj/2016/day06.clj
+++ src/aoc_clj/2016/day06.clj
@@ -1,25 +1,33 @@
 (ns aoc-clj.2016.day06
-  "Solution to https://adventofcode.com/2016/day/6")
+  "Solution to https://adventofcode.com/2016/day/6"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
+;; Puzzle input
 (def parse identity)
 
+;; Puzzle logic
+
 (defn frequent-chars
-  [max-or-min-f input]
-  (let [size (count input)]
-    (->> (apply interleave input)
-         (partition size)
-         (map frequencies)
-         (map (partial apply max-or-min-f val))
-         (map first)
-         (apply str))))
+  "A string of the most or least frequent characters in each column of
+   the code strings"
+  [max-or-min-f codes]
+  (->> (u/transpose codes)
+       (map frequencies)
+       (map (partial apply max-or-min-f val))
+       (map key)
+       str/join))
 
 (def most-frequent-chars (partial frequent-chars max-key))
 (def least-frequent-chars (partial frequent-chars min-key))
 
+;; Puzzle solutions
 (defn part1
+  "The string composed of the most frequent chars in each column"
   [input]
   (most-frequent-chars input))
 
 (defn part2
+  "The string composed of the least frequent chars in each column"
   [input]
   (least-frequent-chars input))
--- test/aoc_clj/2016/day06_test.clj
+++ test/aoc_clj/2016/day06_test.clj
@@ -1,7 +1,7 @@
 (ns aoc-clj.2016.day06-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day06 :as t]))
+            [aoc-clj.2016.day06 :as d06]))
 
 (def d06-s00
   ["eedadn"
@@ -21,20 +21,20 @@
    "dvrsen"
    "enarar"])
 
-(deftest most-frequent-chars
+(deftest most-frequent-chars-test
   (testing "Decodes the most frequent character in each position"
-    (is (= "easter" (t/most-frequent-chars d06-s00)))))
+    (is (= "easter" (d06/most-frequent-chars d06-s00)))))
 
-(deftest least-frequent-chars
+(deftest least-frequent-chars-test
   (testing "Decodes the least frequent character in each position"
-    (is (= "advent" (t/least-frequent-chars d06-s00)))))
+    (is (= "advent" (d06/least-frequent-chars d06-s00)))))
 
-(def day06-input (u/parse-puzzle-input t/parse 2016 6))
+(def day06-input (u/parse-puzzle-input d06/parse 2016 6))
 
 (deftest part1-test
   (testing "Reproduces the answer for day06, part1"
-    (is (= "agmwzecr" (t/part1 day06-input)))))
+    (is (= "agmwzecr" (d06/part1 day06-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day06, part2"
-    (is (= "owlaxqvq" (t/part2 day06-input)))))
+    (is (= "owlaxqvq" (d06/part2 day06-input)))))
