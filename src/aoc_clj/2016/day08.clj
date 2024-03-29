483f0780e192b94c7baebcb157e29a1936c42d33
Ken Smith <ken@klsmith.me>
2024-03-29 22:58:14 +0000

    Cleanup and docs 2016 Day08

--- src/aoc_clj/2016/day08.clj
+++ src/aoc_clj/2016/day08.clj
@@ -1,24 +1,28 @@
-(ns aoc-clj.2016.day08
-  "Solution to https://adventofcode.com/2016/day/8"
-  (:require [clojure.string :as str]
-            [aoc-clj.utils.core :as u]
-            [aoc-clj.utils.grid :as grid]))
+- (ns aoc-clj.2016.day08
+    "Solution to https://adventofcode.com/2016/day/8"
+    (:require [clojure.string :as str]
+              [aoc-clj.utils.core :as u]
+              [aoc-clj.utils.grid :as grid]
+              [aoc-clj.utils.grid.mapgrid :as mg]))
 
+;; Constants
+(def screen-width 50)
+(def screen-height 6)
+
+;; Input parsing
 (defn parse-rect
-  [s]
-  (let [[_ dims] (str/split s #" ")
-        [w h]    (str/split dims #"x")]
+  [rect-str]
+  (let [[w h] (re-seq #"\d+" rect-str)]
     {:cmd :rect
-     :width (read-string w)
+     :width  (read-string w)
      :height (read-string h)}))
 
 (defn parse-rotate
-  [s]
-  (let [[_ type rowcol _ amt] (str/split s #" ")
-        [_ idx] (str/split rowcol #"=")]
+  [rot-str]
+  (let [[_ type pos amt] (re-find #"(row|column) [x|y]=(\d+) by (\d+)" rot-str)]
     {:cmd :rotate
      :type (keyword type)
-     :pos  (read-string idx)
+     :pos  (read-string pos)
      :amount (read-string amt)}))
 
 (defn parse-line
@@ -31,66 +35,83 @@
   [input]
   (map parse-line input))
 
-(defn apply-rect
-  [state {:keys [width height]}]
-  (update state :grid merge
-          (into {} (for [y (range height)
-                         x (range width)]
-                     [[x y] :on]))))
+;; Puzzle logic
+(defn grid-set
+  "Return a (w x h) grid map with all values set to `val`"
+  [width height val]
+  (into {} (for [y (range height)
+                 x (range width)]
+             [[x y] val])))
 
 (defn get-slice
-  [{:keys [grid]} type pos]
+  "Retrieve a slice of the `grid` of `type` (`:row|:column`) at
+   index `pos` (0-indexed)"
+  [grid type pos]
   (let [coord (case type
                 :column first
                 :row second)]
     (into (sorted-map) (filter #(= pos (-> % key coord)) grid))))
 
-(defn rotate
+(defn rotate-slice
+  "Cycle the values of the slice by `amount`"
   [slice amount]
   (zipmap (keys slice) (u/rotate (- amount) (vals slice))))
 
 (defn apply-rotate
-  [state {:keys [type pos amount]}]
-  (update state :grid merge (-> (get-slice state type pos)
-                                (rotate amount))))
+  "Apply a `rotate` instruction to update the display grid"
+  [grid {:keys [type pos amount]}]
+  (merge grid (-> (get-slice grid type pos)
+                  (rotate-slice amount))))
 
-(defn apply-cmd
-  [state {:keys [cmd] :as instruction}]
+(defn apply-rect
+  "Apply a `rect` instruction to update the display grid"
+  [grid {:keys [width height]}]
+  (merge grid (grid-set width height 1)))
+
+(defn step
+  "Update the `grid` following one instruction "
+  [grid {:keys [cmd] :as instruction}]
   (case cmd
-    :rect   (apply-rect state instruction)
-    :rotate (apply-rotate state instruction)))
+    :rect   (apply-rect grid instruction)
+    :rotate (apply-rotate grid instruction)))
 
-(defn make-grid
+(defn init-grid
+  "Intialize an empty grid (w x h) to all pixels off"
   [width height]
-  {:width width
-   :height height
-   :grid
-   (zipmap (for [y (range height)
-                 x (range width)]
-             [x y])
-           (repeat :off))})
+  (grid-set width height 0))
 
-(defn apply-instructions
+(defn final-state
+  "Iteratively apply the instructions to a (w x h) display grid"
   [width height cmds]
-  (reduce apply-cmd (make-grid width height) cmds))
+  (reduce step (init-grid width height) cmds))
 
 (defn lit-pixels
+  "How many pixels are lit up on the (w x h) display after
+   executing each of the `cmds` sequentially"
   [width height cmds]
-  (->> (apply-instructions width height cmds)
-       :grid
+  (->> (final-state width height cmds)
        vals
-       (filter #{:on})
+       (filter pos?)
        count))
 
+;; Puzzle solutions
 (defn part1
+  "How many pixels are lit up after following the instructions"
   [input]
-  (lit-pixels 50 6 input))
+  (lit-pixels screen-width screen-height input))
 
 (defn part2
+  "What string do the lit up pixels spell after following the instructions"
   [input]
-  ;; Execute the code in the comment to print the message
+  ;; Print the grid so as to be able to read the block characters
+  (println (grid/Grid2D->ascii
+            {\  0 \# 1}
+            (mg/->MapGrid2D
+             screen-width
+             screen-height
+             (final-state screen-width screen-height input))
+            :down true))
   (comment
-    (println (grid/Grid2D->ascii {\  :off \# :on} (apply-instructions 50 6 input)))
     "####  ##   ##  ###   ##  ###  #  # #   # ##   ##  "
     "#    #  # #  # #  # #  # #  # #  # #   ##  # #  # "
     "###  #  # #  # #  # #    #  # ####  # # #  # #  # "
--- test/aoc_clj/2016/day08_test.clj
+++ test/aoc_clj/2016/day08_test.clj
@@ -1,30 +1,70 @@
 (ns aoc-clj.2016.day08-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day08 :as t]))
+            [aoc-clj.2016.day08 :as d08]))
+
+(def d08-s00-raw
+  ["rect 3x2"
+   "rotate column x=1 by 1"
+   "rotate row y=0 by 4"
+   "rotate column x=1 by 1"])
 
 (def d08-s00
-  (map t/parse-line
-       ["rect 3x2"
-        "rotate column x=1 by 1"
-        "rotate row y=0 by 4"
-        "rotate column x=1 by 1"]))
+  [{:cmd :rect, :width 3, :height 2}
+   {:cmd :rotate, :type :column, :pos 1, :amount 1}
+   {:cmd :rotate, :type :row, :pos 0, :amount 4}
+   {:cmd :rotate, :type :column, :pos 1, :amount 1}])
 
-(deftest apply-instructions
+(def d08-s00-grid-0
+  {[0 0] 0 [1 0] 0 [2 0] 0 [3 0] 0 [4 0] 0 [5 0] 0 [6 0] 0
+   [0 1] 0 [1 1] 0 [2 1] 0 [3 1] 0 [4 1] 0 [5 1] 0 [6 1] 0
+   [0 2] 0 [1 2] 0 [2 2] 0 [3 2] 0 [4 2] 0 [5 2] 0 [6 2] 0})
+
+(def d08-s00-grid-1
+  {[0 0] 1 [1 0] 1 [2 0] 1 [3 0] 0 [4 0] 0 [5 0] 0 [6 0] 0
+   [0 1] 1 [1 1] 1 [2 1] 1 [3 1] 0 [4 1] 0 [5 1] 0 [6 1] 0
+   [0 2] 0 [1 2] 0 [2 2] 0 [3 2] 0 [4 2] 0 [5 2] 0 [6 2] 0})
+
+(def d08-s00-grid-2
+  {[0 0] 1 [1 0] 0 [2 0] 1 [3 0] 0 [4 0] 0 [5 0] 0 [6 0] 0
+   [0 1] 1 [1 1] 1 [2 1] 1 [3 1] 0 [4 1] 0 [5 1] 0 [6 1] 0
+   [0 2] 0 [1 2] 1 [2 2] 0 [3 2] 0 [4 2] 0 [5 2] 0 [6 2] 0})
+
+(def d08-s00-grid-3
+  {[0 0] 0 [1 0] 0 [2 0] 0 [3 0] 0 [4 0] 1 [5 0] 0 [6 0] 1
+   [0 1] 1 [1 1] 1 [2 1] 1 [3 1] 0 [4 1] 0 [5 1] 0 [6 1] 0
+   [0 2] 0 [1 2] 1 [2 2] 0 [3 2] 0 [4 2] 0 [5 2] 0 [6 2] 0})
+
+(def d08-s00-grid-4
+  {[0 0] 0 [1 0] 1 [2 0] 0 [3 0] 0 [4 0] 1 [5 0] 0 [6 0] 1
+   [0 1] 1 [1 1] 0 [2 1] 1 [3 1] 0 [4 1] 0 [5 1] 0 [6 1] 0
+   [0 2] 0 [1 2] 1 [2 2] 0 [3 2] 0 [4 2] 0 [5 2] 0 [6 2] 0})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d08-s00 (d08/parse d08-s00-raw)))))
+
+(deftest step-test
+  (testing "Updates the grid with each instruction"
+    (is (= d08-s00-grid-1 (d08/step d08-s00-grid-0 (nth d08-s00 0))))
+    (is (= d08-s00-grid-2 (d08/step d08-s00-grid-1 (nth d08-s00 1))))
+    (is (= d08-s00-grid-3 (d08/step d08-s00-grid-2 (nth d08-s00 2))))
+    (is (= d08-s00-grid-4 (d08/step d08-s00-grid-3 (nth d08-s00 3))))))
+
+(deftest final-state-test
   (testing "Can apply the instructions for the sample data"
     (is (= #{[1 0] [4 0] [6 0] [0 1] [2 1] [1 2]}
-           (->>  (t/apply-instructions 7 3 d08-s00)
-                 :grid
-                 (filter #(= :on (val %)))
+           (->>  (d08/final-state 7 3 d08-s00)
+                 (filter #(pos? (val %)))
                  keys
                  set)))))
 
-(def day08-input (u/parse-puzzle-input t/parse 2016 8))
+(def day08-input (u/parse-puzzle-input d08/parse 2016 8))
 
 (deftest part1-test
   (testing "Reproduces the answer for day08, part1"
-    (is (= 128 (t/part1 day08-input)))))
+    (is (= 128 (d08/part1 day08-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day08, part2"
-    (is (= "EOARGPHYAO" (t/part2 day08-input)))))
+    (is (= "EOARGPHYAO" (d08/part2 day08-input)))))
