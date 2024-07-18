d72f5512a0e06953e739d57394a778cefddade55
Ken Smith <ken@klsmith.me>
2024-07-18 19:58:17 +0000

    Preliminary re-formatting/documentation for 2016 day 09

--- src/aoc_clj/2016/day09.clj
+++ src/aoc_clj/2016/day09.clj
@@ -2,8 +2,11 @@
   "Solution to https://adventofcode.com/2016/day/9"
   (:require [clojure.string :as str]))
 
+;; Input parsing
 (def parse first)
 
+
+;; Puzzle logic
 (defn repeat-coll
   [n coll]
   (flatten (repeat n coll)))
@@ -12,7 +15,7 @@
   [s]
   (loop [c (first s) cs (rest s) output []]
     (if (not c)
-      (apply str output)
+      (str/join output)
       (if (not= \( c)
         (recur (first cs) (rest cs) (conj output c))
         (let [splits (str/split (apply str cs) #"\)")
@@ -24,6 +27,8 @@
                  (rest remainder)
                  (into output (repeat-coll repeats repeat-seq))))))))
 
+
+;; TODO --- remove the duplicated logic between v2 and v1 of the format
 (defn decompress-count
   [s]
   (loop [char-count 0 remaining s]
@@ -39,10 +44,13 @@
           (recur (+ (* (decompress-count repeat-string) repeats)
                     char-count open-marker-loc) new))))))
 
+;; Puzzle solutions
 (defn part1
+  "Length of the decompressed input"
   [input]
   (count (decompress input)))
 
 (defn part2
+  "Length of the decompressed input using version 2 of the format"
   [input]
   (decompress-count input))
--- test/aoc_clj/2016/day09_test.clj
+++ test/aoc_clj/2016/day09_test.clj
@@ -1,30 +1,30 @@
 (ns aoc-clj.2016.day09-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day09 :as t]))
+            [aoc-clj.2016.day09 :as d09]))
 
-(deftest decompress
+(deftest decompress-test
   (testing "Decompresses the example data correctly"
-    (is (= "ADVENT" (t/decompress "ADVENT")))
-    (is (= "ABBBBBC" (t/decompress "A(1x5)BC")))
-    (is (= "XYZXYZXYZ" (t/decompress "(3x3)XYZ")))
-    (is (= "ABCBCDEFEFG" (t/decompress "A(2x2)BCD(2x2)EFG")))
-    (is (= "(1x3)A" (t/decompress "(6x1)(1x3)A")))
-    (is (= "X(3x3)ABC(3x3)ABCY" (t/decompress "X(8x2)(3x3)ABCY")))))
+    (is (= "ADVENT" (d09/decompress "ADVENT")))
+    (is (= "ABBBBBC" (d09/decompress "A(1x5)BC")))
+    (is (= "XYZXYZXYZ" (d09/decompress "(3x3)XYZ")))
+    (is (= "ABCBCDEFEFG" (d09/decompress "A(2x2)BCD(2x2)EFG")))
+    (is (= "(1x3)A" (d09/decompress "(6x1)(1x3)A")))
+    (is (= "X(3x3)ABC(3x3)ABCY" (d09/decompress "X(8x2)(3x3)ABCY")))))
 
-(deftest decompress-count
+(deftest decompress-count-test
   (testing "Can count the decompressed size for example data"
-    (is (= 9 (t/decompress-count "(3x3)XYZ")))
-    (is (= 20 (t/decompress-count "X(8x2)(3x3)ABCY")))
-    (is (= 241920 (t/decompress-count "(27x12)(20x12)(13x14)(7x10)(1x12)A")))
-    (is (= 445 (t/decompress-count "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")))))
+    (is (= 9 (d09/decompress-count "(3x3)XYZ")))
+    (is (= 20 (d09/decompress-count "X(8x2)(3x3)ABCY")))
+    (is (= 241920 (d09/decompress-count "(27x12)(20x12)(13x14)(7x10)(1x12)A")))
+    (is (= 445 (d09/decompress-count "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")))))
 
-(def day09-input (u/parse-puzzle-input t/parse 2016 9))
+(def day09-input (u/parse-puzzle-input d09/parse 2016 9))
 
 (deftest part1-test
   (testing "Reproduces the answer for day09, part1"
-    (is (= 112830 (t/part1 day09-input)))))
+    (is (= 112830 (d09/part1 day09-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day09, part2"
-    (is (= 10931789799 (t/part2 day09-input)))))
+    (is (= 10931789799 (d09/part2 day09-input)))))
