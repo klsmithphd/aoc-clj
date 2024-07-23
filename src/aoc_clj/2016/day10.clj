a571089ae210f8ba5358673ec4494fced390bbd3
Ken Smith <ken@klsmith.me>
2024-07-23 19:41:56 +0000

    Clean up and document 2016 Day10

--- src/aoc_clj/2016/day10.clj
+++ src/aoc_clj/2016/day10.clj
@@ -3,6 +3,7 @@
   (:require [clojure.string :as str]
             [aoc-clj.utils.core :as u]))
 
+;; Input parsing
 (defn parse-assignment
   [s]
   (let [[_ value bot] (re-find #"value (\d+) goes to bot (\d+)" s)]
@@ -24,22 +25,21 @@
     (parse-comparison s)))
 
 (defn assign
+  "Update the `state` to assign `value` to the given `bot` number"
   [state {:keys [value bot]}]
   (if (get state bot)
     (update state bot conj value)
     (assoc state bot #{value})))
 
-(defn assignments
-  [cmds]
-  (filter #(= :assignment (:type %)) cmds))
-
 (defn establish-comparison
   [{:keys [bot low high]}]
   [bot {:low low :high high}])
 
-(defn comparisons
-  [cmds]
-  (filter #(= :comparison (:type %)) cmds))
+(defn cmd-filter
+  [k]
+  (fn [cmds] (filter #(= k (:type %)) cmds)))
+(def assignments (cmd-filter :assignment))
+(def comparisons (cmd-filter :comparison))
 
 (defn parse
   [input]
@@ -47,11 +47,16 @@
     {:assignments (reduce assign {} (assignments cmds))
      :comparisons (into {} (map establish-comparison (comparisons cmds)))}))
 
+;; Puzzle logic
 (defn ready-bots
+  "Determine the names of the bot(s) that have two microchips to compare"
   [{:keys [assignments]}]
   (keys (filter #(= 2 (count (val %))) assignments)))
 
 (defn apply-logic
+  "For a `bot` that is currently comparing two microchips, hand over
+   the chips to the intended recipients defined in `comparisons` and
+   return the new assignments data"
   [comparisons assignments bot]
   (let [logic (get comparisons bot)
         [low high] (sort (get assignments bot))]
@@ -61,20 +66,26 @@
         (assoc bot #{}))))
 
 (defn step
+  "Given the current state of bot assignments, advance one step in time
+   given the logic defined in `state`'s `:comparisons` value"
   [{:keys [assignments comparisons] :as state}]
   (let [ready (ready-bots state)]
     (assoc state :assignments
            (reduce (partial apply-logic comparisons) assignments ready))))
 
 (defn bot-that-compares
-  [state values]
-  (loop [s state]
-    (let [matches (filter #(= values (val %)) (:assignments s))]
+  "The number of the bot that ends up comparing the two values in `values`
+   given the assignments and comparison logic in `init-state`"
+  [init-state values]
+  (loop [state init-state]
+    (let [matches (filter #(= values (val %)) (:assignments state))]
       (if (not-empty matches)
         (read-string (subs (ffirst matches) 3))
-        (recur (step s))))))
+        (recur (step state))))))
 
 (defn output-values
+  "Product of the values in outputs 0, 1, and 2 after all bots have
+   handed off their microchips"
   [state]
   (let [outputs (-> (u/converge step state)
                     last
@@ -85,10 +96,13 @@
          (map first)
          (reduce *))))
 
+;; Puzzle solutions
 (defn part1
+  "The number of the bot that compares 61 and 17"
   [input]
   (bot-that-compares input #{17 61}))
 
 (defn part2
-  [input]
+  "The product of the values of outputs 0, 1, and 2"
+  [input] 
   (output-values input))
--- test/aoc_clj/2016/day10_test.clj
+++ test/aoc_clj/2016/day10_test.clj
@@ -1,29 +1,50 @@
 (ns aoc-clj.2016.day10-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day10 :as t]))
+            [aoc-clj.2016.day10 :as d10]))
+
+(def d10-s00-raw
+  ["value 5 goes to bot 2"
+   "bot 2 gives low to bot 1 and high to bot 0"
+   "value 3 goes to bot 1"
+   "bot 1 gives low to output 1 and high to bot 0"
+   "bot 0 gives low to output 2 and high to output 0"
+   "value 2 goes to bot 2"])
 
 (def d10-s00
-  (t/parse
-   ["value 5 goes to bot 2"
-    "bot 2 gives low to bot 1 and high to bot 0"
-    "value 3 goes to bot 1"
-    "bot 1 gives low to output 1 and high to bot 0"
-    "bot 0 gives low to output 2 and high to output 0"
-    "value 2 goes to bot 2"]))
+  {:assignments {"bot2" #{5 2}
+                 "bot1" #{3}}
+   :comparisons {"bot2" {:low "bot1" :high "bot0"}
+                 "bot1" {:low "output1" :high "bot0"}
+                 "bot0" {:low "output2" :high "output0"}}})
 
-(deftest bot-that-compares
+(def d10-s00-assignments-1 {"bot0" #{5} "bot1" #{2 3} "bot2" #{}})
+(def d10-s00-assignments-2 {"bot0" #{3 5} "bot1" #{} "bot2" #{} "output1" #{2}})
+(def d10-s00-assignments-3 {"bot0" #{} "bot1" #{} "bot2" #{} 
+                            "output0" #{5} "output1" #{2} "output2" #{3}})
+
+(deftest parse-test
+  (testing "Correctly parses the sample input"
+    (is (= d10-s00 (d10/parse d10-s00-raw)))))
+
+(deftest step-test
+  (testing "Checks each of the steps result in the correct next set of assignments"
+    (is (= d10-s00-assignments-1 (:assignments (d10/step d10-s00))))
+    (is (= d10-s00-assignments-2 (:assignments (d10/step (d10/step d10-s00)))))
+    (is (= d10-s00-assignments-3 (:assignments (d10/step (d10/step (d10/step d10-s00))))))))
+
+(deftest bot-that-compares-test
   (testing "Find the bots that will compare the values in the sample data"
-    (is (= 2 (t/bot-that-compares d10-s00 #{2 5})))
-    (is (= 1 (t/bot-that-compares d10-s00 #{2 3})))
-    (is (= 0 (t/bot-that-compares d10-s00 #{3 5})))))
+    (is (= 2 (d10/bot-that-compares d10-s00 #{2 5})))
+    (is (= 1 (d10/bot-that-compares d10-s00 #{2 3})))
+    (is (= 0 (d10/bot-that-compares d10-s00 #{3 5})))))
 
-(def day10-input (u/parse-puzzle-input t/parse 2016 10))
+(def day10-input (u/parse-puzzle-input d10/parse 2016 10))
 
 (deftest part1-test
   (testing "Reproduces the answer for day10, part1"
-    (is (= 93 (t/part1 day10-input)))))
+    (is (= 93 (d10/part1 day10-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day10, part2"
-    (is (= 47101 (t/part2 day10-input)))))
+    (is (= 47101 (d10/part2 day10-input)))))
