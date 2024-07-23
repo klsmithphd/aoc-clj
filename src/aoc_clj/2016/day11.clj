d16a3b4a1feb447c9b5fdfe57a534c77e27e63c2
Ken Smith <ken@klsmith.me>
2024-07-23 20:33:17 +0000

    Add parse methods to 2016 Day11

--- src/aoc_clj/2016/day11.clj
+++ src/aoc_clj/2016/day11.clj
@@ -5,22 +5,31 @@
             [aoc-clj.utils.core :as u]
             [aoc-clj.utils.graph :as g :refer [Graph]]))
 
-;; TODO - Should add a parse method
-
-(defn endstate
-  [state]
-  {:e 4
-   1 #{}
-   2 #{}
-   3 #{}
-   4 (set/union (state 1) (state 2) (state 3) (state 4))})
-
+;; Constants
 (def elevator-options
   {1 [2]
    2 [1 3]
    3 [2 4]
    4 [3]})
 
+;; Input parsing
+(defn get-generators 
+  [s]
+  (map (comp #(vector :g %) second) (re-seq #"(\w+) generator" s)))
+
+(defn get-microchips
+  [s]
+  (map (comp #(vector :m %) second) (re-seq #"(\w+)-compatible microchip" s)))
+
+(defn parse-line
+  [line]
+  (into #{} (concat (get-generators line) (get-microchips line))))
+
+(defn parse
+  [input]
+  (merge (zipmap [1 2 3 4] (map parse-line input)) {:e 1}))
+
+;; Puzzle logic
 (defn move
   [state from to objects]
   (assoc state
@@ -76,6 +85,16 @@
     [_ _ _]
     1))
 
+(defn endstate
+  [state]
+  {:e 4
+   1 #{}
+   2 #{}
+   3 #{}
+   4 (->> (select-keys state [1 2 3 4])
+         vals
+         (apply set/union))})
+
 (defn move-count
   [input]
   (->> (g/dijkstra
@@ -88,8 +107,9 @@
 
 (defn extra-items
   [state]
-  (update state 1 conj [:m "El"] [:g "El"] [:m "Li2"] [:g "Li2"]))
+  (update state 1 conj [:m "elerium"] [:g "elerium"] [:m "dilithium"] [:g "dilithium"]))
 
+;; Puzzle solutions
 (defn part1
   [input]
   (move-count input))
--- test/aoc_clj/2016/day11_test.clj
+++ test/aoc_clj/2016/day11_test.clj
@@ -1,42 +1,47 @@
 (ns aoc-clj.2016.day11-test
   (:require [clojure.test :refer [deftest testing is]]
-            ;; [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day11 :as t]))
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2016.day11 :as d11]))
 
-"The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip."
-"The second floor contains a hydrogen generator."
-"The third floor contains a lithium generator."
-"The fourth floor contains nothing relevant."
+(def d11-s00-raw
+  ["The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip."
+   "The second floor contains a hydrogen generator."
+   "The third floor contains a lithium generator."
+   "The fourth floor contains nothing relevant."])
+
 (def d11-s00
   {:e 1
-   1 #{[:m "H"] [:m "Li"]}
-   2 #{[:g "H"]}
-   3 #{[:g "Li"]}
+   1 #{[:m "hydrogen"] [:m "lithium"]}
+   2 #{[:g "hydrogen"]}
+   3 #{[:g "lithium"]}
    4 #{}})
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d11-s00 (d11/parse d11-s00-raw)))))
+
+(deftest endstate-test
+  (testing "Defines the desired endstate (with all objects on the fourth floor), given the initial state"
+    (is (= {:e 4
+            1 #{}
+            2 #{}
+            3 #{}
+            4 #{[:m "hydrogen"] [:m "lithium"] [:g "hydrogen"] [:g "lithium"]}}
+           (d11/endstate d11-s00)))))
 
 (deftest move-count
   (testing "Counts the minimum number of moves "
-    (is (= 11 (t/move-count d11-s00)))))
+    (is (= 11 (d11/move-count d11-s00)))))
 
-"The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, and a strontium generator.
-The second floor contains a plutonium-compatible microchip and a strontium-compatible microchip.
-The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, and a ruthenium-compatible microchip.
-The fourth floor contains nothing relevant."
-(def day11-input
-  {:e 1
-  ;;  :move 0
-   1 #{[:m "Tm"] [:g "Tm"] [:g "Pu"] [:g "Sr"]}
-   2 #{[:m "Pu"] [:m "Sr"]}
-   3 #{[:m "Pm"] [:m "Ru"] [:g "Pm"] [:g "Ru"]}
-   4 #{}})
-;; (def day11-input (u/parse-puzzle-input t/parse 2016 11))
+
+(def day11-input (u/parse-puzzle-input d11/parse 2016 11))
 
 ;; FIXME: Implementation is too slow
 ;; https://github.com/Ken-2scientists/aoc-clj/issues/31
 (deftest ^:slow part1
   (testing "Reproduces the answer for day11, part1"
-    (is (= 31 (t/part1 day11-input)))))
+    (is (= 31 (d11/part1 day11-input)))))
 
 (deftest ^:slow part2
   (testing "Reproduces the answer for day11, part2"
-    (is (= 55 (t/part2 day11-input)))))
+    (is (= 55 (d11/part2 day11-input)))))
