21c2442e90c669eaff8a48a2a6fa353c24351d3f
Ken Smith <ken@klsmith.me>
2024-07-24 21:04:33 +0000

    2016 Day14 part1 soln

--- src/aoc_clj/2016/day14.clj
+++ src/aoc_clj/2016/day14.clj
@@ -1,2 +1,45 @@
 (ns aoc-clj.2016.day14
-  "Solution to https://adventofcode.com/2016/day/14")
+  "Solution to https://adventofcode.com/2016/day/14"
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.digest :as d]))
+
+;; Input parsing
+(def parse first)
+
+;; Puzzle logic
+(defn has-triple-chars?
+  [s]
+  (some? (re-find #"(.)\1{2}" s)))
+
+(defn triple-char-key-candidates
+  [salt]
+  (->> (range)
+       (map-indexed (fn [idx itm] [idx (d/md5-str (str salt itm))]))
+       (filter #(has-triple-chars? (second %)))))
+
+(defn fivepeat-in-thousand
+  [[[idx hashstr] & other-candidates]]
+  (let [trip-ch (second (re-find #"(.)\1{2}" hashstr))
+        patt    (re-pattern (str/join (repeat 5 trip-ch)))]
+    (->> other-candidates
+         (filter #(<= (first %) (+ 1000 idx)))
+         (some #(re-find patt (second %))))))
+
+(defn pad-keys
+  [salt]
+  (->> (triple-char-key-candidates salt)
+       (partition 1001 1)
+       (filter fivepeat-in-thousand)
+       (map first)))
+
+(defn last-pad-key
+  [salt]
+  (->> (pad-keys salt)
+       (take 64)
+       last
+       first))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (last-pad-key input))
--- test/aoc_clj/2016/day14_test.clj
+++ test/aoc_clj/2016/day14_test.clj
@@ -2,3 +2,13 @@
   (:require [clojure.test :refer [deftest is testing]]
             [aoc-clj.utils.core :as u]
             [aoc-clj.2016.day14 :as d14]))
+
+(deftest last-pad-key-test
+  (testing "Finds the index that produces the 64 one-time pad key"
+    (is (= 22728 (d14/last-pad-key "abc")))))
+
+(def day14-input (u/parse-puzzle-input d14/parse 2016 14))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day13, part1"
+    (is (= 15168 (d14/part1 day14-input)))))
