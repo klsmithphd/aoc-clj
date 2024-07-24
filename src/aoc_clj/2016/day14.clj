dd39422b7d5487fa4e50e634fa5ad87ae85a8425
Ken Smith <ken@klsmith.me>
2024-07-24 19:52:29 +0000

    Stubs for 2016 Day14 solns

---
+++ src/aoc_clj/2016/day14.clj
@@ -0,0 +1,2 @@
+(ns aoc-clj.2016.day14
+  "Solution to https://adventofcode.com/2016/day/14")
---
+++ test/aoc_clj/2016/day14_test.clj
@@ -0,0 +1,4 @@
+(ns aoc-clj.2016.day14-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2016.day14 :as d14]))
