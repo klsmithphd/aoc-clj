1307b6821583398d77e7c9184dae5a80f84ef16c
Ken Smith <ken@klsmith.me>
2024-07-27 21:43:26 +0000

    Skeleton files

---
+++ src/aoc_clj/2016/day16.clj
@@ -0,0 +1,5 @@
+(ns aoc-clj.2016.day16
+  "Solution to https://adventofcode.com/2016/day/16")
+
+;; Input parsing
+(def parse first)
---
+++ test/aoc_clj/2016/day16_test.clj
@@ -0,0 +1,2 @@
+(ns aoc-clj.2016.day16-test
+  (:require [aoc-clj.2016.day16 :as d16]))
