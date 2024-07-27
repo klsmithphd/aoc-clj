1dc4f28e7e41c8ce521a1ef5534fcab8ea9d3f86
Ken Smith <ken@klsmith.me>
2024-07-27 22:16:20 +0000

    fill and checksum logic

--- src/aoc_clj/2016/day16.clj
+++ src/aoc_clj/2016/day16.clj
@@ -1,5 +1,32 @@
 (ns aoc-clj.2016.day16
-  "Solution to https://adventofcode.com/2016/day/16")
+  "Solution to https://adventofcode.com/2016/day/16"
+  (:require [clojure.string :as str]))
 
 ;; Input parsing
 (def parse first)
+
+;; Puzzle logic
+(defn flip
+  [s]
+  (map {\1 \0 \0 \1} s))
+
+(defn fill
+  [s]
+  (concat s [\0] (flip (reverse s))))
+
+(def checksum-mapping
+  {[\1 \1] \1
+   [\0 \0] \1
+   [\0 \1] \0
+   [\1 \0] \0})
+
+(defn checksum-pass
+  [s]
+  (map checksum-mapping (partition 2 s)))
+
+(defn checksum
+  [s]
+  (let [cs (checksum-pass s)]
+    (if (odd? (count cs))
+      (str/join cs)
+      (checksum cs))))
--- test/aoc_clj/2016/day16_test.clj
+++ test/aoc_clj/2016/day16_test.clj
@@ -1,2 +1,17 @@
 (ns aoc-clj.2016.day16-test
-  (:require [aoc-clj.2016.day16 :as d16]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2016.day16 :as d16]))
+
+(deftest fill-test
+  (testing "Implements the fill logic correctly"
+    (is (= [\1 \0 \0] (d16/fill "1")))
+    (is (= [\0 \0 \1] (d16/fill "0")))
+    (is (= [\1 \1 \1 \1 \1 \0 \0 \0 \0 \0 \0]
+           (d16/fill "11111")))
+    (is (= [\1 \1 \1 \1 \0 \0 \0 \0 \1 \0 \1 \0 \0 \1 \0 \1 \0 \1 \1 \1 \1 \0 \0 \0 \0]
+           (d16/fill "111100001010")))))
+
+(deftest checksum-test
+  (testing "Computes the checksum for a bit string"
+    (is (= "100"   (d16/checksum "110010110100")))
+    (is (= "01100" (d16/checksum "10000011110010000111")))))
