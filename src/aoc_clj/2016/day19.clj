25964eedea4a18ba88ef09035286fc3dc62c1cc8
Ken Smith <ken@klsmith.me>
2024-07-29 21:29:00 +0000

    Add notes for how I arrived at the solns

--- src/aoc_clj/2016/day19.clj
+++ src/aoc_clj/2016/day19.clj
@@ -19,90 +19,49 @@
   ;; the winning elf is always the next odd number counting up from 1.
   ;;
   ;; In other words, if n_elves = 2^i + x (where x < 2^i),
-  ;; the winner is (2*i + 1)
+  ;; the winner is (2*x + 1)
   (let [power-of-two  (math/floor (/ (math/log n-elves) (math/log 2)))
         nearest-power (int (math/pow 2 power-of-two))
         rem           (- n-elves nearest-power)]
     (+ (* 2 rem) 1)))
 
-;; 1 = 3^0     :   1
-;; 2 = 2*3^0 + 0:   1
-;; 3 = 3^1     :   3
-;; 4 = 3^1 +  1:   1
-;; 5 = 3^1 +  2:   2
-;; 6 = 2*3^1 + 0:   3   (2* 3^1)
-;; 7 = 2*3^1 + 1:   5
-;; 8 = 2*3^1 +  2;   7
-;; 9 = 3^2     ;   9
-;; 10 = 3^2 + 1:   1
-;; 11 = 3^2 + 2:   2
-;; 12 = 3^2 + 3:   3
-;; 13 = 3^2 + 4:   4
-;; 14 = 3^2 + 5:   5
-;; 15 = 3^2 + 6:   6
-;; 16 = 3^2 + 7:   7
-;; 17 = 3^2 + 8:   8
-;; 18 = 2*3^2 + 0: 9  
-;; 19 = 2*3^2 + 1: 11
-;; 20 = 2*3^2 + 2: 13
-;; ...
-;; 26 = 2*3^2 + 8: 25
-;; 27 = 3^3      : 27
-;; 28 = 3^3   + 1: 1
-;; 29 = 3^3   + 2: 2
-;; ...
-;; 53 = 3^3   + 26: 26
-;; 54 = 2*3^3     : 27
-;; 55 = 2*3^3 + 1 : 29
-;; 56 = 2*3^3 + 2 : 31
-;; ...
-;; 80 = 2*3^3 + 26: 79
-;; 81 = 3^4       : 81
-;; 82 = 3^4   +  1: 1
-
-
-
-(defn vec-dissoc
-  "Return a new vector from `v` without the element at `idx`"
-  [v idx]
-  (into (subvec v 0 idx) (subvec v (inc idx))))
-
-(defn next-elf
-  [pos to-be-removed]
-  (if (< to-be-removed pos)
-    pos
-    (inc pos)))
-
-(defn part2-sim
-  "A literal implementation of the part2 stealing logic to walk through
-   generating the pattern (if any)"
-  [elves pos]
-  (let [n-elves (count elves)] 
-    (if (= 1 n-elves)
-      (first elves)
-      (let [to-be-removed (mod (+ pos (quot n-elves 2)) n-elves)]
-        (part2-sim 
-         (vec-dissoc elves to-be-removed)
-         (mod (next-elf pos to-be-removed) (dec n-elves)))))))
-
-(defn test-pattern
-  []
-  (->> (range 1 100)
-       (map #(part2-sim (vec (range 1 (inc %))) 0))
-       (map-indexed #(vector (inc %1) %2))))
-
 (defn part2-winning-elf
+  "Determines which elf ends up winning all the presents using the 
+   stealing logic from part2"
   [n-elves]
+  ;; This pattern takes a bit more to work out than that of part1
+  ;; You can sketch out the outcomes by hand, but I ended up coding
+  ;; up the helper simulations that are in the comments at the end
+  ;; of this file. It turns out that the winning elf can be found
+  ;; by looking to see where they fall in the interval 3^i to 3^(i+1)
+  ;;
+  ;; If n-elves exactly equals 3^i, that's the winning elf
+  ;; (i.e., 1, 3, 9, 27, 81, ...)
+  ;;
+  ;; If n-elves is between 3^i and 2*3^i, then the winning elf
+  ;; is just how far n-elves is away from 3^i, e.g. 
+  ;; n = 4, winner = 1
+  ;; n = 5, winner = 2
+  ;; n = 6, winner = 3
+  ;; n = 28, winner = 1
+  ;; n = 29, winner = 2
+  ;; n = 30, winner = 3
+  ;; 
+  ;; Finally, when n-elves is greater than 2*3^i, each additional
+  ;; elf increases the winner by 2. 
+  ;; n = 7, winner = 5
+  ;; n = 8, winner = 7
+  ;; n = 9, winner = 9 
+  ;; n = 55, winner = 29
+  ;; n = 56, winner = 31
+  ;; n = 57, winner = 33
   (let [power-of-three (math/floor (/ (math/log n-elves) (math/log 3)))
         nearest-power  (int (math/pow 3 power-of-three))
         rem            (- n-elves nearest-power)]
-    (println n-elves nearest-power rem)
-    (if (zero? rem)
-      nearest-power
-      (if (<= rem nearest-power)
-        rem
-        (- (* 2 rem) nearest-power)))))
-
+    (cond
+      (zero? rem) nearest-power
+      (<= rem nearest-power) rem
+      :else (- (* 2 rem) nearest-power))))
 
 ;; Puzzle solutions
 (defn part1
@@ -114,3 +73,38 @@
   "Which elf gets all the presents with the part2 rules?"
   [input]
   (part2-winning-elf input))
+
+
+;; Supplemental
+(comment
+  (defn- vec-dissoc
+    "Return a new vector from `v` without the element at `idx`"
+    [v idx]
+    (into (subvec v 0 idx) (subvec v (inc idx))))
+  
+  (defn- next-elf
+    [pos to-be-removed]
+    (if (< to-be-removed pos)
+      pos
+      (inc pos))) 
+
+  (defn- part2-sim
+    "A literal implementation of the part2 stealing logic to walk through
+   generating the pattern (if any)"
+    [elves pos]
+    (let [n-elves (count elves)] 
+      (if (= 1 n-elves)
+        (first elves)
+        (let [to-be-removed (mod (+ pos (quot n-elves 2)) n-elves)]
+          (part2-sim 
+           (vec-dissoc elves to-be-removed)
+           (mod (next-elf pos to-be-removed) (dec n-elves))))))) 
+
+  (defn- test-pattern
+    []
+    (->> (range 1 100)
+         (map #(part2-sim (vec (range 1 (inc %))) 0))
+         (map-indexed #(vector (inc %1) %2))))
+  
+  (print test-pattern))
+
--- test/aoc_clj/2016/day19_test.clj
+++ test/aoc_clj/2016/day19_test.clj
@@ -4,7 +4,7 @@
             [aoc-clj.2016.day19 :as d19]))
 
 
-(deftest winning-elf-test
+(deftest part1-winning-elf-test
   (testing "Finds the winning elf in the exchange"
     (is (= 1 (d19/part1-winning-elf 1)))
     (is (= 1 (d19/part1-winning-elf 2)))
@@ -22,6 +22,30 @@
     (is (= 13 (d19/part1-winning-elf 14)))
     (is (= 15 (d19/part1-winning-elf 15)))
     (is (= 1 (d19/part1-winning-elf 16)))))
+
+(deftest part2-winning-elf-test
+  (testing "Finds the winning elf in the exchange"
+    (is (= 1 (d19/part2-winning-elf 1)))
+    (is (= 1 (d19/part2-winning-elf 2)))
+    (is (= 3 (d19/part2-winning-elf 3)))
+    (is (= 1 (d19/part2-winning-elf 4)))
+    (is (= 2 (d19/part2-winning-elf 5)))
+    (is (= 3 (d19/part2-winning-elf 6)))
+    (is (= 5 (d19/part2-winning-elf 7)))
+    (is (= 7 (d19/part2-winning-elf 8)))
+    (is (= 9 (d19/part2-winning-elf 9)))
+    (is (= 1 (d19/part2-winning-elf 10)))
+    (is (= 2 (d19/part2-winning-elf 11)))
+    (is (= 3 (d19/part2-winning-elf 12)))
+    (is (= 9 (d19/part2-winning-elf 18)))
+    (is (= 11 (d19/part2-winning-elf 19)))
+    (is (= 25 (d19/part2-winning-elf 26)))
+    (is (= 27 (d19/part2-winning-elf 27)))
+    (is (= 1  (d19/part2-winning-elf 28)))
+    (is (= 27 (d19/part2-winning-elf 54)))
+    (is (= 29 (d19/part2-winning-elf 55)))
+    (is (= 79 (d19/part2-winning-elf 80)))
+    (is (= 81 (d19/part2-winning-elf 81)))))
 
 (def day19-input (u/parse-puzzle-input d19/parse 2016 19))
 
