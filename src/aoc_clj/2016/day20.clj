93f3e3db6f209f45b49bbc1d80b6920b0c9f4565
Ken Smith <ken@klsmith.me>
2024-07-29 22:12:05 +0000

    parts 1 and 2 solved

--- src/aoc_clj/2016/day20.clj
+++ src/aoc_clj/2016/day20.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2016.day20
-  "Solution to https://adventofcode.com/2016/day/20")
+  "Solution to https://adventofcode.com/2016/day/20"
+  (:require [aoc-clj.utils.intervals :as intervals]))
 
 ;; Input parsing
 (defn parse-line
@@ -9,3 +10,35 @@
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn lowest-unblocked-ip
+  [blocked-ranges]
+  (->> (intervals/simplify blocked-ranges) 
+       first
+       second
+       inc))
+
+(defn- gap-size
+  [[[_ r1] [l2 _]]]
+  (- l2 r1 1))
+
+(defn num-allowed-ips
+  [blocked-ranges]
+  (let [ranges (intervals/simplify blocked-ranges)
+        first-gap (- (first (first ranges)) 0)
+        last-gap (- 4294967295 (second (last ranges)))]
+    (->> (intervals/simplify blocked-ranges)
+         (partition 2 1)
+         (map gap-size)
+         (reduce +)
+         (+ first-gap last-gap))))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (lowest-unblocked-ip input))
+
+(defn part2
+  [input]
+  (num-allowed-ips input))
--- test/aoc_clj/2016/day20_test.clj
+++ test/aoc_clj/2016/day20_test.clj
@@ -1,7 +1,8 @@
 (ns aoc-clj.2016.day20-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2016.day20 :as d20]))
+            [aoc-clj.2016.day20 :as d20]
+            [aoc-clj.utils.intervals :as ivals]))
 
 (def d20-s00-raw
   ["5-8"
@@ -17,3 +18,16 @@
   (testing "Correctly parses the input"
     (is (= d20-s00 (d20/parse d20-s00-raw)))))
 
+(deftest lowest-unblocked-ip-test
+  (testing "Finds the smallest unblocked IP address"
+    (is (= 3 (d20/lowest-unblocked-ip d20-s00)))))
+
+(def day20-input (u/parse-puzzle-input d20/parse 2016 20))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day20, part1"
+    (is (= 14975795 (d20/part1 day20-input)))))
+
+(deftest part2-test
+  (testing "Reproduces the answer for day20, part2"
+    (is (= 101 (d20/part2 day20-input)))))
