be67438e7bf74f6e2aacf40f8679e92b2dc08de9
Ken Smith <ken@klsmith.me>
2024-08-04 20:27:59 +0000

    Input parsing

--- src/aoc_clj/2016/day22.clj
+++ src/aoc_clj/2016/day22.clj
@@ -1,2 +1,16 @@
 (ns aoc-clj.2016.day22
   "Solution to https://adventofcode.com/2016/day/22")
+
+;; Input parsing
+(defn parse-line
+  [line]
+  (let [[x y size used avail usepct] (map read-string (re-seq #"\d+" line))]
+    {:pos [x y]
+     :size size
+     :used used
+     :avail avail
+     :usepct usepct}))
+
+(defn parse
+  [input]
+  (map parse-line (drop 2 input)))
