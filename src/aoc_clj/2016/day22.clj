149f7379dac36c63b4730b09d6cb83795fba8b70
Ken Smith <ken@klsmith.me>
2024-08-04 20:22:03 +0000

    Stub for day22

---
+++ src/aoc_clj/2016/day22.clj
@@ -0,0 +1,2 @@
+(ns aoc-clj.2016.day22
+  "Solution to https://adventofcode.com/2016/day/22")
---
+++ test/aoc_clj/2016/day22_test.clj
@@ -0,0 +1,4 @@
+(ns aoc-clj.2016.day22-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2016.day22 :as d22]))
