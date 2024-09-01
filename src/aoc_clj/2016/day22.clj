becaae73013b52348f454eca95a51d2fa65ec079
Ken Smith <ken@klsmith.me>
2024-09-01 19:14:42 +0000

    Soln by hand for part2

--- src/aoc_clj/2016/day22.clj
+++ src/aoc_clj/2016/day22.clj
@@ -40,3 +40,22 @@
   "How many viable pairs of nodes are there?"
   [input]
   (viable-pair-count input))
+
+
+(defn part2
+  "What is the fewest number of steps required to move your goal
+   data to node-x0-y0?"
+  [_]
+  ;; Looking at the data visually, it takes 52 steps in my puzzle input
+  ;; to move the empty node up to x=36, y=0, and then it takes a sequence
+  ;; of five moves to move the goal data left one cell at a time, from
+  ;; x=35 to x=0.
+  ;; 
+  ;; How to solve programmatically...
+  ;; Find shortest allowed path from empty node to upper-right
+  ;; (Say, use Dijkstra's). This will need to navigate past the wall of
+  ;; immovable data.
+  ;; Then, presuming there are no vertical obstacles of immovable data,
+  ;; it takes a sequence of five moves to shift the target data left by one,
+  ;; so the remainder is just five times the number of cells to move.
+  (+ (* 35 5) 52))
--- test/aoc_clj/2016/day22_test.clj
+++ test/aoc_clj/2016/day22_test.clj
@@ -10,6 +10,19 @@
    "/dev/grid/node-x0-y1     93T   72T    21T   77%"
    "/dev/grid/node-x0-y2     87T   67T    20T   77%"])
 
+(def d22-s01-raw
+  ["root@ebhq-gridcenter# df -h"
+   "Filesystem            Size  Used  Avail  Use%"
+   "/dev/grid/node-x0-y0   10T    8T     2T   80%"
+   "/dev/grid/node-x0-y1   11T    6T     5T   54%"
+   "/dev/grid/node-x0-y2   32T   28T     4T   87%"
+   "/dev/grid/node-x1-y0    9T    7T     2T   77%"
+   "/dev/grid/node-x1-y1    8T    0T     8T    0%"
+   "/dev/grid/node-x1-y2   11T    7T     4T   63%"
+   "/dev/grid/node-x2-y0   10T    6T     4T   60%"
+   "/dev/grid/node-x2-y1    9T    8T     1T   88%"
+   "/dev/grid/node-x2-y2    9T    6T     3T   66%"])
+
 (def d22-s00
   [{:pos [0 0] :size 87 :used 71 :avail 16 :usepct 81}
    {:pos [0 1] :size 93 :used 72 :avail 21 :usepct 77}
@@ -24,3 +37,7 @@
 (deftest part1-test
   (testing "Reproduces the answer for day22, part1"
     (is (= 892 (d22/part1 day22-input)))))
+
+(deftest part2-test
+  (testing "Reproduces the answer for day22, part2"
+    (is (= 227 (d22/part2 day22-input)))))
