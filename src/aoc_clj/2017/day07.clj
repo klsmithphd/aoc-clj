3e109b6ee0f4abbbd197e0c51b4accf3409dbb6e
Ken Smith <ken@klsmith.me>
2024-09-07 23:02:14 +0000

    Skeleton for day07

---
+++ src/aoc_clj/2017/day07.clj
@@ -0,0 +1,2 @@
+(ns aoc-clj.2017.day07
+  "Solution to https://adventofcode.com/2017/day/7")
---
+++ test/aoc_clj/2017/day07_test.clj
@@ -0,0 +1,31 @@
+(ns aoc-clj.2017.day07-test)
+
+(def d07-s00-raw
+  ["pbga (66)"
+   "xhth (57)"
+   "ebii (61)"
+   "havc (66)"
+   "ktlj (57)"
+   "fwft (72) -> ktlj, cntj, xhth"
+   "qoyq (66)"
+   "padx (45) -> pbga, havc, qoyq"
+   "tknk (41) -> ugml, padx, fwft"
+   "jptl (61)"
+   "ugml (68) -> gyxo, ebii, jptl"
+   "gyxo (61)"
+   "cntj (57)"])
+
+(def d07-s00
+  {"pbga" {:weight 66 :children []}
+   "xhth" {:weight 57 :children []}
+   "ebii" {:weight 61 :children []}
+   "havc" {:weight 66 :children []}
+   "ktlj" {:weight 57 :children []}
+   "fwft" {:weight 72 :children ["ktlj" "cntj" "xhth"]}
+   "qoyq" {:weight 66 :children []}
+   "padx" {:weight 45 :children ["pbga" "havc" "qoyq"]}
+   "tknk" {:weight 41 :children ["ugml" "padx" "fwft"]}
+   "jptl" {:weight 61 :children []}
+   "ugml" {:weight 68 :children ["gyxo" "ebii" "jptl"]}
+   "gyxo" {:weight 61 :children []}
+   "cntj" {:weight 57 :children []}})
