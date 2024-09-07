7e02feb9fd8404773fd5cbbb3c613eea10724dc0
Ken Smith <ken@klsmith.me>
2024-09-07 17:59:38 +0000

    Part1

--- src/aoc_clj/2017/day05.clj
+++ src/aoc_clj/2017/day05.clj
@@ -4,4 +4,29 @@
 ;; Input parsing
 (defn parse
   [input]
-  (map read-string input))
+  (mapv read-string input))
+
+;; Puzzle logic
+(defn step
+  [{:keys [jumps idx] :as state}]
+  (-> state
+      (update :idx + (get jumps idx))
+      (update-in [:jumps idx] inc)
+      (update :steps inc)))
+
+(defn not-escaped?
+  [{:keys [jumps idx]}]
+  (<= 0 idx (dec (count jumps))))
+
+(defn steps-to-escape
+  [jumps]
+  (->> (iterate step {:jumps jumps :idx 0 :steps 0})
+       (drop-while not-escaped?)
+       first
+       :steps))
+
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (steps-to-escape input))
--- test/aoc_clj/2017/day05_test.clj
+++ test/aoc_clj/2017/day05_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2017.day05-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day05 :as d05]))
 
 (def d05-s00-raw
@@ -15,3 +16,13 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d05-s00 (d05/parse d05-s00-raw)))))
+
+(deftest steps-to-escape-test
+  (testing "Counts the number of steps it takes to escape"
+    (is (= 5 (d05/steps-to-escape d05-s00)))))
+
+(def day05-input (u/parse-puzzle-input d05/parse 2017 5))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day05, part1"
+    (is (= 339351 (d05/part1 day05-input)))))
