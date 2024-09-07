f314d597e705530b269f3b544adfb76b59d4c17d
Ken Smith <ken@klsmith.me>
2024-09-07 17:33:22 +0000

    Docstrings

--- src/aoc_clj/2017/day04.clj
+++ src/aoc_clj/2017/day04.clj
@@ -10,6 +10,7 @@
 
 ;; Puzzle logic
 (defn is-valid-part1?
+  "A passphrase is valid in part1 if there are no repeated words"
   [passphrase]
   (->> passphrase
        frequencies
@@ -18,24 +19,30 @@
        (= 1)))
 
 (defn not-anagrams
+  "Checks that two strings a and b are not anagrams of one another"
   [[a b]]
   (not= (frequencies a) (frequencies b)))
 
 (defn is-valid-part2?
+  "A passphrase is valid in part2 if there are no words that are anagrams
+   of one another"
   [passphrase]
   (->> (combo/combinations passphrase 2)
        (every? not-anagrams)))
 
 (defn valid-passphrase-count
+  "Counts how many passphrases satisfy the validity condition"
   [is-valid? passphrases]
   (count (filter is-valid? passphrases)))
 
 ;; Puzzle solutions
 (defn part1
+  "How many passphrases are valid given part1's validity rules?"
   [input]
   (valid-passphrase-count is-valid-part1? input))
 
 (defn part2
+  "How many passphrases are valid given part2's validity rules?"
   [input]
   (valid-passphrase-count is-valid-part2? input))
 
