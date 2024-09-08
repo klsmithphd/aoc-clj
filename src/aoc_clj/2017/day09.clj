61ac0c9f525b6e5c724d31ed7ab57f79aa68ef94
Ken Smith <ken@klsmith.me>
2024-09-08 23:05:59 +0000

    Redo with regexes and part2

--- src/aoc_clj/2017/day09.clj
+++ src/aoc_clj/2017/day09.clj
@@ -1,46 +1,38 @@
 (ns aoc-clj.2017.day09
-  "Solution to https://adventofcode.com/2017/day/9")
+  "Solution to https://adventofcode.com/2017/day/9"
+  (:require [clojure.string :as str]))
+
+;; Constants
+(def cancel-ptn #"!.")
+(def garbage-ptn #"\<[^\>]*\>")
 
 ;; Input parsing
 (def parse first)
 
 ;; Puzzle logic
-(defn drop-garbage
-  "Given a character sequence `s` starting with the first character
-   inside a garbage sequence, continuously drop characters until
-   reaching the end of the garbage sequence"
+(defn without-canceled
+  "Return a new version of the string without any canceled sequences"
   [s]
-  (loop [[ch & nxt] s]
-    (if (= ch \>)
-      nxt
-      (if (= ch \!)
-        (recur (rest nxt))
-        (recur nxt)))))
+  (str/replace s cancel-ptn ""))
 
 (defn without-garbage
-  "Given a character sequence `s`, return a new sequence with all
-   the garbage sequences removed"
+  "Given a string, return all non-overlapping garbage sequences"
   [s]
-  (if (nil? s)
-    '()
-    (lazy-seq
-     (let [[ch & nxt] s]
-       (if (= ch \<)
-         (without-garbage (drop-garbage nxt))
-         (cons ch (without-garbage nxt)))))))
+  (str/replace s garbage-ptn ""))
+
+(defn without-commas
+  "Return a new string stripped of all commas"
+  [s]
+  (str/replace s "," ""))
 
 (defn cleaned
-  "Return a string with all the garbage sequences and commas removed.
-   
+  "Return a string with the canceled, garbage sequence, and commas removed.  
    This string should only contain open or closed brace characters"
   [s]
-  (->> (without-garbage s)
-       (remove #{\,})
-       (apply str)))
+  (-> s without-canceled without-garbage without-commas))
 
 (defn scores
-  "Compute the depth score for each open-close brace pair
-   
+  "Compute the depth score for each open-close brace pair.   
    Returns in depth-first order."
   [s]
   (loop [depths [] depth 0 chars (cleaned s)]
@@ -55,8 +47,22 @@
   [s]
   (reduce + (scores s)))
 
+(defn garbage-count
+  "Counts the total number of non-canceled garbage characters to 
+   remove from the string"
+  [s]
+  (->> (without-canceled s)
+       (re-seq garbage-ptn)
+       (map (comp dec dec count))
+       (reduce +)))
+
 ;; Puzzle solutions
 (defn part1
   "What is the total score for all the groups in your input?"
   [input]
   (total-score input))
+
+(defn part2
+  "How many non-canceld characters are within the garbage in your input?"
+  [input]
+  (garbage-count input))
--- test/aoc_clj/2017/day09_test.clj
+++ test/aoc_clj/2017/day09_test.clj
@@ -3,57 +3,74 @@
             [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day09 :as d09]))
 
-(def d09-s00 "{}")
-(def d09-s01 "{{{}}}")
-(def d09-s02 "{{},{}}")
-(def d09-s03 "{{{},{},{{}}}}")
-(def d09-s04 "{<{},{},{{}}>}")
-(def d09-s05 "{<a>,<a>,<a>,<a>}")
-(def d09-s06 "{{<a>},{<a>},{<a>},{<a>}}")
-(def d09-s07 "{{<!>},{<!>},{<!>},{<a>}}")
+(def d09-s00 "<>")
+(def d09-s01 "<random characters>")
+(def d09-s02 "<<<<>")
+(def d09-s03 "<{!>}>")
+(def d09-s04 "<!!>")
+(def d09-s05 "<!!!>>")
+(def d09-s06 "<{o\"i!a,<{i<a>")
+
+(def d09-s07 "{}")
+(def d09-s08 "{{{}}}")
+(def d09-s09 "{{},{}}")
+(def d09-s10 "{{{},{},{{}}}}")
+(def d09-s11 "{<{},{},{{}}>}")
+(def d09-s12 "{<a>,<a>,<a>,<a>}")
+(def d09-s13 "{{<a>},{<a>},{<a>},{<a>}}")
+(def d09-s14 "{{<!>},{<!>},{<!>},{<a>}}")
 
 (deftest cleaned-test
   (testing "Strips the garbage from the character stream"
-    (is (= "" (d09/cleaned "<>")))
-    (is (= "" (d09/cleaned "<random characters>")))
-    (is (= "" (d09/cleaned "<<<<>")))
-    (is (= "" (d09/cleaned "<{!>}>")))
-    (is (= "" (d09/cleaned "<!!>")))
-    (is (= "" (d09/cleaned "<!!!>>")))
-    (is (= "" (d09/cleaned "<{o\"i!a,<{i<a>")))
+    (is (= "" (d09/cleaned d09-s00)))
+    (is (= "" (d09/cleaned d09-s01)))
+    (is (= "" (d09/cleaned d09-s02)))
+    (is (= "" (d09/cleaned d09-s03)))
+    (is (= "" (d09/cleaned d09-s04)))
+    (is (= "" (d09/cleaned d09-s05)))
+    (is (= "" (d09/cleaned d09-s06)))
 
-    (is (= "{}"           (d09/cleaned d09-s00)))
-    (is (= "{{{}}}"       (d09/cleaned d09-s01)))
-    (is (= "{{}{}}"       (d09/cleaned d09-s02)))
-    (is (= "{{{}{}{{}}}}" (d09/cleaned d09-s03)))
-    (is (= "{}"           (d09/cleaned d09-s04)))
-    (is (= "{}"           (d09/cleaned d09-s05)))
-    (is (= "{{}{}{}{}}"   (d09/cleaned d09-s06)))
-    (is (= "{{}}"         (d09/cleaned d09-s07)))))
+    (is (= "{}"           (d09/cleaned d09-s07)))
+    (is (= "{{{}}}"       (d09/cleaned d09-s08)))
+    (is (= "{{}{}}"       (d09/cleaned d09-s09)))
+    (is (= "{{{}{}{{}}}}" (d09/cleaned d09-s10)))
+    (is (= "{}"           (d09/cleaned d09-s11)))
+    (is (= "{}"           (d09/cleaned d09-s12)))
+    (is (= "{{}{}{}{}}"   (d09/cleaned d09-s13)))
+    (is (= "{{}}"         (d09/cleaned d09-s14)))))
 
 (deftest scores-test
   (testing "Computes the depth scores correctly"
-    (is (= [1]           (d09/scores d09-s00)))
-    (is (= [3 2 1]       (d09/scores d09-s01)))
-    (is (= [2 2 1]       (d09/scores d09-s02)))
-    (is (= [3 3 4 3 2 1] (d09/scores d09-s03)))
-    (is (= [1]           (d09/scores d09-s04)))
-    (is (= [1]           (d09/scores d09-s05)))
-    (is (= [2 2 2 2 1]   (d09/scores d09-s06)))
-    (is (= [2 1]         (d09/scores d09-s07)))))
+    (is (= [1]           (d09/scores d09-s07)))
+    (is (= [3 2 1]       (d09/scores d09-s08)))
+    (is (= [2 2 1]       (d09/scores d09-s09)))
+    (is (= [3 3 4 3 2 1] (d09/scores d09-s10)))
+    (is (= [1]           (d09/scores d09-s11)))
+    (is (= [1]           (d09/scores d09-s12)))
+    (is (= [2 2 2 2 1]   (d09/scores d09-s13)))
+    (is (= [2 1]         (d09/scores d09-s14)))))
 
 (deftest total-score-test
   (testing "Computes the total depth score"
-    (is (= 1  (d09/total-score d09-s00)))
-    (is (= 6  (d09/total-score d09-s01)))
-    (is (= 5  (d09/total-score d09-s02)))
-    (is (= 16 (d09/total-score d09-s03)))
-    (is (= 1  (d09/total-score d09-s05)))
-    (is (= 9  (d09/total-score d09-s06)))
-    (is (= 3  (d09/total-score d09-s07)))))
+    (is (= 1  (d09/total-score d09-s07)))
+    (is (= 6  (d09/total-score d09-s08)))
+    (is (= 5  (d09/total-score d09-s09)))
+    (is (= 16 (d09/total-score d09-s10)))
+    (is (= 1  (d09/total-score d09-s12)))
+    (is (= 9  (d09/total-score d09-s13)))
+    (is (= 3  (d09/total-score d09-s14)))))
+
+(deftest garbage-count-test
+  (testing "Counts number of non-canceled garbage chars removed"
+    (is (= 0 (d09/garbage-count "<>")))
+    (is (= 17 (d09/garbage-count "<random characters>")))))
 
 (def day09-input (u/parse-puzzle-input d09/parse 2017 9))
 
 (deftest part1-test
   (testing "Reproduces the answer for day09, part1"
     (is (= 16689 (d09/part1 day09-input)))))
+
+(deftest part2-test
+  (testing "Reproduces the answer for day09, part2"
+    (is (= 7982 (d09/part2 day09-input)))))
