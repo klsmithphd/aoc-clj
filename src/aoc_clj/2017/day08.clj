04ebafd96734c3728cb7e329fd906f4541e5a051
Ken Smith <ken@klsmith.me>
2024-09-08 16:38:42 +0000

    Parsing logic

--- src/aoc_clj/2017/day08.clj
+++ src/aoc_clj/2017/day08.clj
@@ -1,2 +1,21 @@
 (ns aoc-clj.2017.day08
   "Solution to https://adventofcode.com/2017/day/8")
+
+;; Input parsing
+(defn ineq-map
+  [s]
+  (case s
+    "==" "="
+    "!=" "not="
+    s))
+
+(def pattern #"(\w+) (\w+) (-?\d+) if (\w+) (\S+) (-?\d+)")
+
+(defn parse-line
+  [line]
+  (let [[reg op amt if-reg ineq val] (rest (re-find pattern line))]
+    [reg op (read-string amt) if-reg (ineq-map ineq) (read-string val)]))
+
+(defn parse
+  [input]
+  (map parse-line input))
--- test/aoc_clj/2017/day08_test.clj
+++ test/aoc_clj/2017/day08_test.clj
@@ -1,1 +1,27 @@
-(ns aoc-clj.2017.day08-test)
+(ns aoc-clj.2017.day08-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.2017.day08 :as d08]))
+
+(def d08-s00-raw
+  ["b inc 5 if a > 1"
+   "a inc 1 if b < 5"
+   "c dec -10 if a >= 1"
+   "c inc -20 if c == 10"])
+
+(def d08-s01-raw
+  ["x dec 315 if y != -910"])
+
+(def d08-s00
+  [["b" "inc" 5 "a" ">" 1]
+   ["a" "inc" 1 "b" "<" 5]
+   ["c" "dec" -10 "a" ">=" 1]
+   ["c" "inc" -20 "c" "=" 10]])
+
+(def d08-s01
+  [["x" "dec" 315 "y" "not=" -910]])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d08-s00 (d08/parse d08-s00-raw)))
+    (is (= d08-s01 (d08/parse d08-s01-raw)))))
+
