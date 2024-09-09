f7e635b98b6fc3e4d1e4fa0a6705081bc8f089d3
Ken Smith <ken@klsmith.me>
2024-09-09 22:53:48 +0000

    Part1

--- src/aoc_clj/2017/day10.clj
+++ src/aoc_clj/2017/day10.clj
@@ -1,7 +1,30 @@
 (ns aoc-clj.2017.day10
   "Solution to https://adventofcode.com/2017/day/10"
-  (:require [aoc-clj.utils.core :as u]))
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.math :as math]))
 
 ;; Input parsing
 (def parse u/firstv)
 
+;; Puzzle logic
+(defn twist
+  [{:keys [v pos skip] :as state} len]
+  (let [shifted (vec (u/rotate pos v))
+        new-v (concat (reverse (subvec shifted 0 len))
+                      (subvec shifted len))]
+    (-> state
+        (assoc :v (u/rotate (- pos) new-v))
+        (update :pos #(math/mod-add (count v) % (+ len skip)))
+        (update :skip inc))))
+
+(defn first-two-nums-prod
+  [loop-size lengths]
+  (->> (reduce twist {:v (range loop-size) :pos 0 :skip 0} lengths)
+       :v
+       (take 2)
+       (reduce *)))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (first-two-nums-prod 256 input))
--- test/aoc_clj/2017/day10_test.clj
+++ test/aoc_clj/2017/day10_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2017.day10-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day10 :as d10]))
 
 (def d10-s00-raw ["3, 4, 1, 5"])
@@ -8,3 +9,24 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d10-s00 (d10/parse d10-s00-raw)))))
+
+(deftest twist-test
+  (testing "Executes one twist instruction and returns the new state"
+    (is (= {:v [2 1 0 3 4] :pos 3 :skip 1}
+           (d10/twist {:v [0 1 2 3 4] :pos 0 :skip 0} 3)))
+    (is (= {:v [4 3 0 1 2] :pos 3 :skip 2}
+           (d10/twist {:v [2 1 0 3 4] :pos 3 :skip 1} 4)))
+    (is (= {:v [4 3 0 1 2] :pos 1 :skip 3}
+           (d10/twist {:v [4 3 0 1 2] :pos 3 :skip 2} 1)))
+    (is (= {:v [3 4 2 1 0] :pos 4 :skip 4}
+           (d10/twist {:v [4 3 0 1 2] :pos 1 :skip 3} 5)))))
+
+(deftest first-two-nums-prod-test
+  (testing "Applies all the twists and then computes product of first two nums"
+    (is (= 12 (d10/first-two-nums-prod 5 d10-s00)))))
+
+(def day10-input (u/parse-puzzle-input d10/parse 2017 10))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day10, part1"
+    (is (= 38628 (d10/part1 day10-input)))))
