924c2ff209d1082045055383168ad085eeffc0cb
Ken Smith <ken@klsmith.me>
2024-09-14 19:56:47 +0000

    Part1

--- src/aoc_clj/2017/day11.clj
+++ src/aoc_clj/2017/day11.clj
@@ -1,8 +1,37 @@
 (ns aoc-clj.2017.day11
   "Solution to https://adventofcode.com/2017/day/11"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.vectors :as v]))
 
 ;; Input parsing
 (defn parse
   [input]
   (str/split (first input) #","))
+
+;; Puzzle logic
+(def cube-coords
+  "Mapping between move directions and Cube coordinates.
+   
+   See https://www.redblobgames.com/grids/hexagons/#coordinates"
+  {"n" [0 -1 1]
+   "s" [0 1 -1]
+   "ne" [1 -1 0]
+   "se" [1 0 -1]
+   "nw" [-1 0 1]
+   "sw" [-1 1 0]})
+
+(defn hex-distance
+  "Move distance away from origin
+   
+   See https://www.redblobgames.com/grids/hexagons/#distances"
+  [moves]
+  (->> (map cube-coords moves)
+       (reduce v/vec-add)
+       (map abs)
+       (apply max)))
+
+;; Puzzle solutions
+(defn part1
+  "How many steps away is the child process?"
+  [input]
+  (hex-distance input))
--- test/aoc_clj/2017/day11_test.clj
+++ test/aoc_clj/2017/day11_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2017.day11-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day11 :as d11]))
 
 (def d11-s00-raw
@@ -13,3 +14,16 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d11-s00 (d11/parse d11-s00-raw)))))
+
+(deftest hex-distance-test
+  (testing "Computes the distance to the end position"
+    (is (= 3 (d11/hex-distance d11-s00)))
+    (is (= 0 (d11/hex-distance d11-s01)))
+    (is (= 2 (d11/hex-distance d11-s02)))
+    (is (= 3 (d11/hex-distance d11-s03)))))
+
+(def day11-input (u/parse-puzzle-input d11/parse 2017 11))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day11, part1"
+    (is (= 761 (d11/part1 day11-input)))))
