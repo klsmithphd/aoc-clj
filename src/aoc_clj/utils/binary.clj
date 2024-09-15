94e98f3ac7404cd7330e2454f760ecbb7641b90e
Ken Smith <ken@klsmith.me>
2024-09-15 20:14:38 +0000

    New helper methods in binary ns

--- src/aoc_clj/utils/binary.clj
+++ src/aoc_clj/utils/binary.clj
@@ -1,12 +1,19 @@
-(ns aoc-clj.utils.binary)
+(ns aoc-clj.utils.binary
+  (:require [clojure.pprint :refer [cl-format]]))
 
 (defn bitstr->int
   "Converts a bit string (e.g. '1101') to the corresponding integer (may be a long or bigint)"
   [s]
   (->> s (str "2r") read-string))
 
+(defn hexstr->int
+  "Converts a hex string (e.g. 'ff00aa' to an integer value"
+  [s]
+  (->> s (str "0x") read-string))
+
 (defn int->bitstr
   "Converts any integer (or long or bigint) to a bit string"
-  [x]
-  (let [bi (biginteger x)]
-    (.toString bi 2)))
+  ([x]
+   (cl-format nil "~b" x))
+  ([width x]
+   (cl-format nil (str "~" width ",'0b") x)))
