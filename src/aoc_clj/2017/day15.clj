146312e5422ef776cf398897ac8752c6bc79c905
Ken Smith <ken@klsmith.me>
2024-09-15 21:37:40 +0000

    Part1

--- src/aoc_clj/2017/day15.clj
+++ src/aoc_clj/2017/day15.clj
@@ -1,5 +1,13 @@
 (ns aoc-clj.2017.day15
-  "Solution to https://adventofcode.com/2017/day/15")
+  "Solution to https://adventofcode.com/2017/day/15"
+  (:require [aoc-clj.utils.math :as math]))
+
+;; Constants
+(def lower-sixteen 65535)
+(def a-mul 16807)
+(def b-mul 48271)
+(def modulus 2147483647)
+(def sample-size 40000000)
 
 ;; Input parsing
 (defn parse
@@ -8,3 +16,28 @@
        (map read-string)))
 
 ;; Puzzle logic
+(defn gen-a
+  [num]
+  (math/mod-mul modulus a-mul num))
+
+(defn gen-b
+  [num]
+  (math/mod-mul modulus b-mul num))
+
+(defn lower-bits-match?
+  [[a b]]
+  (= (bit-and a lower-sixteen) (bit-and b lower-sixteen)))
+
+(defn judge-count
+  [limit [start-a start-b]]
+  (->> (map vector
+            (drop 1 (iterate gen-a start-a))
+            (drop 1 (iterate gen-b start-b)))
+       (take limit)
+       (filter lower-bits-match?)
+       (count)))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (judge-count sample-size input))
--- test/aoc_clj/2017/day15_test.clj
+++ test/aoc_clj/2017/day15_test.clj
@@ -1,15 +1,24 @@
 (ns aoc-clj.2017.day15-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day15 :as d15]))
-
 
 (def d15-s00-raw
   ["Generator A uses 65"
    "Generator B uses 8921"])
 
-(def d15-s00
-  [65 8921])
+(def d15-s00 [65 8921])
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d15-s00 (d15/parse d15-s00-raw)))))
+
+(deftest judge-count-test
+  (testing "How many values match within the sample size"
+    (is (= 588 (d15/judge-count d15/sample-size d15-s00)))))
+
+(def day15-input (u/parse-puzzle-input d15/parse 2017 15))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day15, part1"
+    (is (= 594 (d15/part1 day15-input)))))
