63c39309158ffc9e12f21e4f5ef9d3b5f858eaee
Ken Smith <ken@klsmith.me>
2024-09-15 21:01:25 +0000

    Part2

--- test/aoc_clj/2017/day14_test.clj
+++ test/aoc_clj/2017/day14_test.clj
@@ -9,8 +9,16 @@
   (testing "Computes the number of used squares in the grid"
     (is (= 8108 (d14/used-squares d14-s00)))))
 
+(deftest ^:slow region-count-test
+  (testing "Counts the number of interconnected regions in the grid"
+    (is (= 1242 (d14/region-count d14-s00)))))
+
 (def day14-input (u/parse-puzzle-input d14/parse 2017 14))
 
-(deftest part1-test
+(deftest ^:slow part1-test
   (testing "Reproduces the answer for day14, part1"
     (is (= 8222 (d14/part1 day14-input)))))
+
+(deftest ^:slow part2-test
+  (testing "Reproduces the answer for day14, part2"
+    (is (= 1086 (d14/part2 day14-input)))))
