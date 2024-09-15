27ed651a2e14eaf367e2a152e65c9fa1f69dfc88
Ken Smith <ken@klsmith.me>
2024-09-15 22:31:51 +0000

    Part 1

--- src/aoc_clj/2017/day16.clj
+++ src/aoc_clj/2017/day16.clj
@@ -1,6 +1,10 @@
 (ns aoc-clj.2017.day16
   "Solution to https://adventofcode.com/2017/day/16"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
+
+;; Constants
+(def programs ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p"])
 
 ;; Input parsing
 (defn parse-inst
@@ -13,3 +17,26 @@
 (defn parse
   [input]
   (map parse-inst (str/split (first input) #",")))
+
+;; Puzzle logic
+(defn swap
+  [state [pos-a pos-b]]
+  (-> state
+      (assoc pos-a (state pos-b))
+      (assoc pos-b (state pos-a))))
+
+(defn move
+  [state [cmd args]]
+  (case cmd
+    :spin (vec (u/rotate (- args) state))
+    :exchange (swap state args)
+    :partner (swap state (map #(u/index-of (u/equals? %) state) args))))
+
+(defn dance
+  [init-state moves]
+  (apply str (reduce move init-state moves)))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (dance programs input))
--- test/aoc_clj/2017/day16_test.clj
+++ test/aoc_clj/2017/day16_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2017.day16-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day16 :as d16]))
 
 (def d16-s00-raw ["s1,x3/4,pe/b"])
@@ -11,3 +12,22 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d16-s00 (d16/parse d16-s00-raw)))))
+
+(deftest move-test
+  (testing "Correctly moves the programs for each instruction"
+    (is (= ["e" "a" "b" "c" "d"]
+           (d16/move ["a" "b" "c" "d" "e"] (nth d16-s00 0))))
+    (is (= ["e" "a" "b" "d" "c"]
+           (d16/move ["e" "a" "b" "c" "d"] (nth d16-s00 1))))
+    (is (= ["b" "a" "e" "d" "c"]
+           (d16/move ["e" "a" "b" "d" "c"] (nth d16-s00 2))))))
+
+(deftest dance-test
+  (testing "Performs all the dance steps and returns the final state"
+    (is (= "baedc" (d16/dance ["a" "b" "c" "d" "e"] d16-s00)))))
+
+(def day16-input (u/parse-puzzle-input d16/parse 2017 16))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day15, part1"
+    (is (= "olgejankfhbmpidc" (d16/part1 day16-input)))))
