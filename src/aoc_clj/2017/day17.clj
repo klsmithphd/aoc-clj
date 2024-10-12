ce1872988edf0ca06201fab85ee0c8c7bb36954c
Ken Smith <ken@klsmith.me>
2024-10-12 23:00:40 +0000

    Part1 soln

--- src/aoc_clj/2017/day17.clj
+++ src/aoc_clj/2017/day17.clj
@@ -1,10 +1,42 @@
 (ns aoc-clj.2017.day17
-  "Solution to https://adventofcode.com/2017/day/17")
+  "Solution to https://adventofcode.com/2017/day/17"
+  (:require [aoc-clj.utils.core :as u]))
+
+;; Constants
+(def part1-limit 2017)
+(def part2-limit 50000000)
 
 ;; Input parsing
 (def parse (comp read-string first))
 
 ;; Puzzle logic
+(defn spin-step
+  [size [buffer pos nxt-val]]
+  (let [len (count buffer)
+        new-pos (inc (mod (+ pos size) len))]
+    [(into [] (concat (subvec buffer 0 new-pos)
+                      [nxt-val]
+                      (if (< new-pos len)
+                        (subvec buffer new-pos)
+                        [])))
+     new-pos
+     (inc nxt-val)]))
+
+(defn final-state
+  [limit size]
+  (->> [[0] 0 1]
+       (iterate #(spin-step size %))
+       (drop limit)
+       first))
+
+(defn val-after-target
+  [target limit size]
+  (let [final (first (final-state limit size))
+        idx (u/index-of (u/equals? target) final)]
+    (get final (mod (inc idx) (inc limit)))))
 
 
 ;; Puzzle solutions
+(defn part1
+  [input]
+  (val-after-target part1-limit part1-limit input))
--- test/aoc_clj/2017/day17_test.clj
+++ test/aoc_clj/2017/day17_test.clj
@@ -1,7 +1,26 @@
 (ns aoc-clj.2017.day17-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day17 :as d17]))
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= 307 (d17/parse ["307"])))))
+
+(deftest spin-step-test
+  (testing "Performs each sequential step of the spinlock logic"
+    (is (= [[0 1] 1 2] (d17/spin-step 3 [[0] 0 1])))
+    (is (= [[0 2 1] 1 3] (d17/spin-step 3 [[0 1] 1 2])))
+    (is (= [[0 2 3 1] 2 4] (d17/spin-step 3 [[0 2 1] 1 3])))
+    (is (= [[0 2 4 3 1] 2 5] (d17/spin-step 3 [[0 2 3 1] 2 4])))
+    (is (= [[0 5 2 4 3 1] 1 6] (d17/spin-step 3 [[0 2 4 3 1] 2 5])))))
+
+(deftest val-after-target
+  (testing "Finds the value one past the 2017 target value"
+    (is (= 638 (d17/val-after-target d17/part1-limit d17/part1-limit 3)))))
+
+(def day17-input (u/parse-puzzle-input d17/parse 2017 17))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day17, part1"
+    (is (= 1244 (d17/part1 day17-input)))))
