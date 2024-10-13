70ac1dbdba3b085ff6df576fce46bc11deee2d8a
Ken Smith <ken@klsmith.me>
2024-10-13 22:32:31 +0000

    Add Docstrings

--- src/aoc_clj/2017/day17.clj
+++ src/aoc_clj/2017/day17.clj
@@ -10,42 +10,57 @@
 (def parse (comp read-string first))
 
 ;; Puzzle logic
-(defn spin-step
-  [size [buffer pos nxt-val]]
-  (let [len (count buffer)
-        new-pos (inc (mod (+ pos size) len))]
-    [(into [] (concat (subvec buffer 0 new-pos)
-                      [nxt-val]
-                      (if (< new-pos len)
-                        (subvec buffer new-pos)
-                        [])))
-     new-pos
-     (inc nxt-val)]))
+(defn next-pos
+  "Given a step size `step`, the last position `pos`, and the last `num`
+   inserted, returns the position that `num` will occupy and the next num
+   to be added in a subsequent iteration."
+  [step-size [pos num]]
+  ;; Because the buffer is circular, take the current position, add
+  ;; the step size, and then mod it by the size of the buffer to compute
+  ;; the new position. 
+  ;; The buffer size is always one more than the number being inserted. 
+  ;; For example, at start, num = 0, and the buffer size is 1.
+  [(inc (mod (+ pos step-size) (inc num))) (inc num)])
 
-(defn final-state
-  [limit size]
-  (->> [[0] 0 1]
-       (iterate #(spin-step size %))
-       (drop limit)
-       first))
+(defn spin-step
+  "Evolves the state one iteration, returning a new circular buffer,
+   and vec of the newly inserted position and newly inserted value."
+  [step-size [buffer state]]
+  (let [[nxt-pos nxt-num :as new-state] (next-pos step-size state)]
+    [(u/vec-insert buffer nxt-pos nxt-num) new-state]))
+
+(defn final-buffer
+  "Returns the circular buffer after allowing `n` steps to evolve."
+  [step-size n]
+  (let [step (partial spin-step step-size)]
+    (->> [[0] [0 0]]
+         (iterate step)
+         (drop n)
+         ffirst)))
 
 (defn val-after-2017
-  [size]
-  (let [final (first (final-state part1-limit size))
-        idx (u/index-of (u/equals? part1-limit) final)]
-    (get final (mod (inc idx) (inc part1-limit)))))
-
-(defn next-pos
-  [size [pos num]]
-  [(inc (mod (+ pos size) (inc num))) (inc num)])
+  "After letting the spinlock insert 2017 values into the circular buffer,
+   return the value sequentially after 2017 in the buffer."
+  [step-size]
+  (let [buff (final-buffer step-size part1-limit)
+        idx  (u/index-of (u/equals? part1-limit) buff)]
+    (buff (inc idx))))
 
 (defn val-after-zero
-  [limit size]
-  (let [next-pos-step (partial next-pos size)]
-    (->> (iterate next-pos-step [0 0])
+  "For a large number of steps `n`, return the value in the circular
+   buffer sequentially after the value zero, which will always be at index 0."
+  [step-size n]
+  (let [step (partial next-pos step-size)]
+    (->> [0 0]
+         (iterate step)
+         ;; Elements to the right of zero will have index = 1
          (filter #(= 1 (first %)))
-         (take-while #(<= (second %) limit))
+         ;; Stop iterating once we've gone past the limit we're seeking
+         (take-while #(<= (second %) n))
+         ;; The last element will have the element that was last
+         ;; inserted into the buffer
          last
+         ;; The number in that position is the second in the vec
          second)))
 
 ;; Puzzle solutions
@@ -57,4 +72,4 @@
 (defn part2
   "What is the value after 0 the moment 50000000 is inserted?"
   [input]
-  (val-after-zero part2-limit input))
+  (val-after-zero input part2-limit))
--- test/aoc_clj/2017/day17_test.clj
+++ test/aoc_clj/2017/day17_test.clj
@@ -9,11 +9,17 @@
 
 (deftest spin-step-test
   (testing "Performs each sequential step of the spinlock logic"
-    (is (= [[0 1] 1 2] (d17/spin-step 3 [[0] 0 1])))
-    (is (= [[0 2 1] 1 3] (d17/spin-step 3 [[0 1] 1 2])))
-    (is (= [[0 2 3 1] 2 4] (d17/spin-step 3 [[0 2 1] 1 3])))
-    (is (= [[0 2 4 3 1] 2 5] (d17/spin-step 3 [[0 2 3 1] 2 4])))
-    (is (= [[0 5 2 4 3 1] 1 6] (d17/spin-step 3 [[0 2 4 3 1] 2 5])))))
+    (is (= [[0 1] [1 1]]         (d17/spin-step 3 [[0] [0 0]])))
+    (is (= [[0 2 1] [1 2]]       (d17/spin-step 3 [[0 1] [1 1]])))
+    (is (= [[0 2 3 1] [2 3]]     (d17/spin-step 3 [[0 2 1] [1 2]])))
+    (is (= [[0 2 4 3 1] [2 4]]   (d17/spin-step 3 [[0 2 3 1] [2 3]])))
+    (is (= [[0 5 2 4 3 1] [1 5]] (d17/spin-step 3 [[0 2 4 3 1] [2 4]])))))
+
+(deftest final-buffer-test
+  (testing "Returns the buffer after n steps"
+    (is (= [0 1]                 (d17/final-buffer 3 1)))
+    (is (= [0 5 2 4 3 1]         (d17/final-buffer 3 5)))
+    (is (= [0 9 5 7 2 4 3 8 6 1] (d17/final-buffer 3 9)))))
 
 (deftest val-after-2017-test
   (testing "Finds the value one past the 2017 target value"
@@ -21,12 +27,12 @@
 
 (deftest val-after-zero-test
   (testing "Finds the value in the position to the right of zero after x steps"
-    (is (= 1 (d17/val-after-zero 1 3)))
-    (is (= 2 (d17/val-after-zero 2 3)))
-    (is (= 5 (d17/val-after-zero 8 3)))
-    (is (= 9 (d17/val-after-zero 9 3)))
-    (is (= 9 (d17/val-after-zero 11 3)))
-    (is (= 12 (d17/val-after-zero 12 3)))))
+    (is (= 1 (d17/val-after-zero 3 1)))
+    (is (= 2 (d17/val-after-zero 3 2)))
+    (is (= 5 (d17/val-after-zero 3 8)))
+    (is (= 9 (d17/val-after-zero 3 9)))
+    (is (= 9 (d17/val-after-zero 3 11)))
+    (is (= 12 (d17/val-after-zero 3 12)))))
 
 (def day17-input (u/parse-puzzle-input d17/parse 2017 17))
 
