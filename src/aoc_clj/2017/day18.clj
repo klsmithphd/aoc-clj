d8e7da6dbd847fd161c6748910afc13f8a75b891
Ken Smith <ken@klsmith.me>
2024-10-14 20:02:33 +0000

    Document the part1 code

--- src/aoc_clj/2017/day18.clj
+++ src/aoc_clj/2017/day18.clj
@@ -20,48 +20,62 @@
 
 ;; Puzzle logic
 (defn arg-val
+  "Parse the argument. If it's a literal (number), return the number,
+   else return the value stored in the register or zero if the register
+   is unset"
   [{:keys [regs]} x]
   (if (number? x)
     x
     (get regs x 0)))
 
 (defn add-cmd
+  "`add X Y` **increases** register X by the value of Y"
   [state [x y]]
   (-> state
       (assoc-in [:regs x] (+ (arg-val state x) (arg-val state y)))
       (update :pos inc)))
 
 (defn jgz-cmd
+  "`jgz X Y` **jumps** with an offset of the value of Y, but only if 
+   the value of X is greater than zero."
   [state [x y]]
   (if (> (arg-val state x) 0)
     (update state :pos + y)
     (update state :pos inc)))
 
 (defn mod-cmd
+  "`mod X Y` sets register X to the **remainder** of dividing the value 
+   contained in register X by the value of Y"
   [state [x y]]
   (-> state
       (assoc-in [:regs x] (mod (arg-val state x) (arg-val state y)))
       (update :pos inc)))
 
 (defn mul-cmd
+  "`mul X Y` sets register X to the result of **multiplying** the value 
+   contained in register X by the value of Y."
   [state [x y]]
   (-> state
       (assoc-in [:regs x] (* (arg-val state x) (arg-val state y)))
       (update :pos inc)))
 
 (defn rcv-cmd
+  "`rcv X` **recovers** the frequency of the last sound played, but only when 
+   the value of X is not zero. (If it is zero, the command does nothing.)"
   [state [x]]
   (if (zero? (arg-val state x))
     (update state :pos inc)
     (assoc state :pos -1)))
 
 (defn set-cmd
+  "`set X Y` **sets** register X to the value of Y."
   [state [x y]]
   (-> state
       (assoc-in [:regs x] (arg-val state y))
       (update :pos inc)))
 
 (defn snd-cmd
+  "`snd X` **plays a sound** with a frequency equal to the value of X."
   [state [x]]
   (-> state
       (assoc :snd (arg-val state x))
@@ -77,6 +91,7 @@
    "snd" snd-cmd})
 
 (defn step
+  "Evolve the state by one step based on the instruction indicated by `pos`"
   [{:keys [insts pos snd] :as state}]
   (if (< -1 pos (count insts))
     (let [[cmd args] (insts pos)]
@@ -84,6 +99,7 @@
     snd))
 
 (defn execute
+  "Execute the assembly programs instructions until it returns a recover value"
   [insts]
   (->> {:insts insts :regs {} :pos 0}
        (iterate step)
@@ -92,5 +108,7 @@
 
 ;; Puzzle solutions
 (defn part1
+  "What is the value of the recovered frequency the first time a `rcv` 
+   instruction is executed with a non-zero value?"
   [input]
   (execute input))
