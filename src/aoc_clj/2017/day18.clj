921e7fba3f6a25f97a7fdfa9adfa48f6a54159b6
Ken Smith <ken@klsmith.me>
2024-10-19 16:51:09 +0000

    Back to returning full state instead of state changes

--- src/aoc_clj/2017/day18.clj
+++ src/aoc_clj/2017/day18.clj
@@ -29,92 +29,146 @@
     (get state x 0)))
 
 (defn add-cmd
-  "Returns the state change for an `add` instruction, which updates
+  "Returns the new state for an `add` instruction, which updates
    the first argument's register value by adding the second argument's value."
-  [{:keys [pos] :as state} [x y]]
-  {x (+ (arg-val state x) (arg-val state y))
-   :pos (inc pos)})
+  [state [x y]]
+  (-> state
+      (assoc x (+ (arg-val state x) (arg-val state y)))
+      (update :pos inc)))
 
 (defn jgz-cmd
-  "Returns the state change for an `jgz` instruction, which jumps
+  "Returns the new state for a `jgz` instruction, which jumps
    in instruction position by the offset of the second argument only
    if the value in the first argument's register is greater than zero."
-  [{:keys [pos] :as state} [x y]]
+  [state [x y]]
   (if (> (arg-val state x) 0)
-    {:pos (+ pos y)}
-    {:pos (inc pos)}))
+    (update state :pos + y)
+    (update state :pos inc)))
 
 (defn mod-cmd
-  "Returns the state change for a `mod` instruction, which updates
+  "Returns the new state for a `mod` instruction, which updates
    the first argument's register value by modding it with the second
    argument's value"
-  [{:keys [pos] :as state} [x y]]
-  {x (mod (arg-val state x) (arg-val state y))
-   :pos (inc pos)})
+  [state [x y]]
+  (-> state
+      (assoc x (mod (arg-val state x) (arg-val state y)))
+      (update :pos inc)))
 
 (defn mul-cmd
-  "Returns the state change for a `mul` instruction, which updates
+  "Returns the new state for a `mul` instruction, which updates
    the first argument's register value by multiplying it by the second
    argument's value"
-  [{:keys [pos] :as state} [x y]]
-  {x (* (arg-val state x) (arg-val state y))
-   :pos (inc pos)})
+  [state [x y]]
+  (-> state
+      (assoc x (* (arg-val state x) (arg-val state y)))
+      (update :pos inc)))
 
 (defn set-cmd
-  "Returns the state change for a `set` instruction, which sets the
+  "Returns the new state for a `set` instruction, which sets the
    first argument's register value to the second argument's value"
-  [{:keys [pos] :as state} [x y]]
-  {x (arg-val state y) :pos (inc pos)})
+  [state [x y]]
+  (-> state
+      (assoc x (arg-val state y))
+      (update :pos inc)))
 
 (defn snd-cmd-p1
-  "Returns the state change for a `snd` instruction using the interpretation
+  "Returns the new state for a `snd` instruction using the interpretation
    in part 1. It sets the `:snd` key to the value of the argument's register,
    indicating that frequency has been played as a sound"
-  [{:keys [pos] :as state} [x]]
-  {:snd (arg-val state x) :pos (inc pos)})
+  [state [x]]
+  (-> state
+      (assoc :snd (arg-val state x))
+      (update :pos inc)))
 
 (defn rcv-cmd-p1
-  "Returns the state change for a `rcv` instruction using the interpretation
+  "Returns the new state for a `rcv` instruction using the interpretation
    in part 1. If the value of the argument's register is zero, it does nothing,
    but if non-zero, it sets the instruction position out of bounds to
    stop the program from running."
-  [{:keys [pos] :as state} [x]]
+  [state [x]]
   (if (zero? (arg-val state x))
-    {:pos (inc pos)}
-    {:pos -1}))
+    (update state :pos inc)
+    (assoc state :pos -1)))
 
-;; (defn part2-snd-cmd
-;;   "`snd X` **sends** the value of X to the other program. These values
-;;    wait in a queue until that program is ready to receive them."
-;;   [{:keys [id] :as state} [x]]
-;;   (-> state
-;;       (update-in [(bit-xor 1 id) :queue] (arg-val state x))
-;;       (update :pos inc)))
+(defn snd-cmd-p2
+  "Returns the new state for a `snd` instruction using the interpretation
+   in part 2. It adds the value of the argument's register to the queue
+   of the other program."
+  [p-id state [x]]
+  (let [other-p-id (bit-xor 1 p-id)
+        snd-val    (arg-val (get-in state [:progs p-id]) x)]
+    (-> state
+        (assoc-in [:progs other-p-id :waiting] false)
+        (update-in [:progs other-p-id :queue] conj snd-val)
+        (update-in [:progs p-id :snd-cnt] inc)
+        (update-in [:progs p-id :pos] inc))))
 
-(def cmd-map-p1
+(defn rcv-cmd-p2
+  "Returns the state change for a `rcv` instruction using the interpretation
+   in part 2. It sets the value of the argument's register to the current
+   value in the program's queue."
+  [{:keys [queue] :as state} [x]]
+  (if (empty? queue)
+    (assoc state :waiting true)
+    (-> state
+        (update :queue #(subvec % 1))
+        (assoc x (first queue))
+        (update :pos inc))))
+
+(def common-cmds
   {"add" add-cmd
    "jgz" jgz-cmd
    "mod" mod-cmd
    "mul" mul-cmd
-   "rcv" rcv-cmd-p1
-   "set" set-cmd
-   "snd" snd-cmd-p1})
+   "set" set-cmd})
+
+(def cmd-map-p1
+  (merge common-cmds
+         {"rcv" rcv-cmd-p1
+          "snd" snd-cmd-p1}))
+
+(def cmd-map-p2
+  (merge common-cmds
+         {"rcv" rcv-cmd-p2
+          "snd" snd-cmd-p2}))
+
+(defn in-bounds?
+  [{:keys [insts pos]}]
+  (< -1 pos (count insts)))
+
+(def running-p1? in-bounds?)
 
 (defn step-p1
   "Evolve the state by one step based on the instruction indicated by `pos`"
-  [{:keys [insts pos snd] :as state}]
-  (if (< -1 pos (count insts))
-    (let [[cmd args] (insts pos)]
-      (merge state ((cmd-map-p1 cmd) state args)))
-    snd))
+  [{:keys [insts pos] :as state}]
+  (let [[cmd args] (insts pos)
+        cmd-fn (cmd-map-p1 cmd)]
+    (cmd-fn state args)))
+
+(defn step-p2
+  [{:keys [insts progs] :as state}]
+  ;; Because the speed of the programs doesn't matter, we always attempt
+  ;; to execute program 0 if we can (if it's not waiting for a value to 
+  ;; receive).
+  (let [prog (if (get-in progs [0 :waiting]) 1 0)
+        [cmd args] (insts (get-in state [:progs prog :pos]))
+        cmd-fn (cmd-map-p2 cmd)]
+    (if (= "snd" cmd)
+      (cmd-fn prog state args)
+      (update-in state [:progs prog] merge (cmd-fn (get-in state [:progs prog]) args)))))
 
 (defn execute-p1
   "Execute the assembly programs instructions until it returns a recover value"
   [insts]
   (->> {:insts insts :pos 0}
        (iterate step-p1)
-       (drop-while map?)
-       first))
+       (drop-while running-p1?)
+       first
+       :snd))
+
+(defn execute-p2
+  [insts]
+  (->> {:insts insts}))
 
 ;; Puzzle solutions
 (defn part1
@@ -131,13 +185,27 @@
 ;; The register values and positions are unique to each program now.
 ;;
 ;; Here's a possible structure for the state
-(comment
-  {:insts []
-   0 {:pos 0
+(def p2-init-progs
+  {0 {:pos 0
+      :snd-cnt 0
       :queue []
-      :snd-count 0
+      :waiting false
       "p" 0}
    1 {:pos 0
+      :snd-cnt 0
       :queue []
-      :snd-count 0
+      :waiting false
       "p" 1}})
+
+(comment
+  {:insts []
+   :progs {0 {:pos 0
+              :queue []
+              :snd-count 0
+              :waiting false
+              "p" 0}
+           1 {:pos 0
+              :queue []
+              :snd-count 0
+              :waiting false
+              "p" 1}}})
--- test/aoc_clj/2017/day18_test.clj
+++ test/aoc_clj/2017/day18_test.clj
@@ -44,29 +44,29 @@
   (testing "`add X Y` increases register X by the value of Y"
     (is (= {"a" 10 :pos 1}
            (d18/add-cmd {"a" 0 :pos 0} ["a" 10])))
-    (is (= {"a" 7  :pos 1}
+    (is (= {"a" 7 "b" 4 :pos 1}
            (d18/add-cmd {"a" 3 "b" 4 :pos 0} ["a" "b"])))))
 
 (deftest jgz-cmd-test
   (testing "`jgz X Y` jumps with an offset of the value of Y, only if
             the value of X is greater than zero"
-    (is (= {:pos 1}
+    (is (= {"x" 0 :pos 1}
            (d18/jgz-cmd {"x" 0 :pos 0} ["x" 10])))
-    (is (= {:pos 10}
+    (is (= {"x" 1 :pos 10}
            (d18/jgz-cmd {"x" 1 :pos 0} ["x" 10])))))
 
 (deftest mod-cmd-test
   (testing "`mod X Y` sets register X to the remainder dividing X by Y"
     (is (= {"a" 2 :pos 1}
            (d18/mod-cmd {"a" 5 :pos 0} ["a" 3])))
-    (is (= {"a" 3 :pos 1}
+    (is (= {"a" 3 "b" 4 :pos 1}
            (d18/mod-cmd {"a" 7 "b" 4 :pos 0} ["a" "b"])))))
 
 (deftest mul-cmd-test
   (testing "`mul X Y` sets register X to the product of X and Y"
     (is (= {"a" 15 :pos 1}
            (d18/mul-cmd {"a" 5 :pos 0} ["a" 3])))
-    (is (= {"a" 28 :pos 1}
+    (is (= {"a" 28 "b" 4 :pos 1}
            (d18/mul-cmd {"a" 7 "b" 4 :pos 0} ["a" "b"])))
     (is (= {"a" 0 :pos 1}
            (d18/mul-cmd {:pos 0} ["a" 5])))))
@@ -75,20 +75,20 @@
   (testing "`set X Y` sets register X to the value of Y"
     (is (= {"a" 1 :pos 1}
            (d18/set-cmd {:pos 0} ["a" 1])))
-    (is (= {"a" 10 :pos 1}
+    (is (= {"a" 10 "b" 10 :pos 1}
            (d18/set-cmd {"b" 10 :pos 0} ["a" "b"])))))
 
 (deftest rcv-cmd-p1-test
   (testing "`rcv X` recovers the frequency of last sound played, but
             only when X is not zero"
-    (is (= {:pos 1}
+    (is (= {"a" 0 :pos 1}
            (d18/rcv-cmd-p1 {"a" 0 :pos 0} ["a"])))
-    (is (= {:pos -1}
-           (d18/rcv-cmd-p1 {"a" 1 :pos 0 :snd 5} ["a"])))))
+    (is (= {"a" 1 :pos -1}
+           (d18/rcv-cmd-p1 {"a" 1 :pos 0} ["a"])))))
 
 (deftest snd-cmd-p1-test
   (testing "`snd X Y` plays a sound with frequency equal to X"
-    (is (= {:pos 1 :snd 5}
+    (is (= {"a" 5 :pos 1 :snd 5}
            (d18/snd-cmd-p1 {"a" 5 :pos 0} ["a"])))))
 
 (deftest step-p1-test
@@ -116,9 +116,137 @@
     (is (= {:insts d18-s00 :pos 6 "a" 1 :snd 4}
            (d18/step-p1 {:insts d18-s00 :pos 7 "a" 1 :snd 4})))
     (is (= {:insts d18-s00 :pos -1 "a" 1 :snd 4}
-           (d18/step-p1 {:insts d18-s00 :pos 6 "a" 1 :snd 4})))
-    (is (= 4
-           (d18/step-p1 {:insts d18-s00 :pos -1 "a" 1 :snd 4})))))
+           (d18/step-p1 {:insts d18-s00 :pos 6 "a" 1 :snd 4})))))
+
+(deftest step-p2-test
+  (testing "Advances the state by one step using part2 logic"
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 1
+                       :snd-cnt 1
+                       :queue []
+                       :waiting false
+                       "p" 0}
+                    1 {:pos 0
+                       :snd-cnt 0
+                       :queue [1]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01 :progs d18/p2-init-progs})))
+
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 2
+                       :snd-cnt 2
+                       :queue []
+                       :waiting false
+                       "p" 0}
+                    1 {:pos 0
+                       :snd-cnt 0
+                       :queue [1 2]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01
+                         :progs {0 {:pos 1
+                                    :snd-cnt 1
+                                    :queue []
+                                    :waiting false
+                                    "p" 0}
+                                 1 {:pos 0
+                                    :snd-cnt 0
+                                    :queue [1]
+                                    :waiting false
+                                    "p" 1}}})))
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 3
+                       :snd-cnt 3
+                       :queue []
+                       :waiting false
+                       "p" 0}
+                    1 {:pos 0
+                       :snd-cnt 0
+                       :queue [1 2 0]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01
+                         :progs {0 {:pos 2
+                                    :snd-cnt 2
+                                    :queue []
+                                    :waiting false
+                                    "p" 0}
+                                 1 {:pos 0
+                                    :snd-cnt 0
+                                    :queue [1 2]
+                                    :waiting false
+                                    "p" 1}}})))
+
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 3
+                       :snd-cnt 3
+                       :queue []
+                       :waiting true
+                       "p" 0}
+                    1 {:pos 0
+                       :snd-cnt 0
+                       :queue [1 2 0]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01
+                         :progs {0 {:pos 3
+                                    :snd-cnt 3
+                                    :queue []
+                                    :waiting false
+                                    "p" 0}
+                                 1 {:pos 0
+                                    :snd-cnt 0
+                                    :queue [1 2 0]
+                                    :waiting false
+                                    "p" 1}}})))
+
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 3
+                       :snd-cnt 3
+                       :queue [1]
+                       :waiting false
+                       "p" 0}
+                    1 {:pos 1
+                       :snd-cnt 1
+                       :queue [1 2 0]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01
+                         :progs {0 {:pos 3
+                                    :snd-cnt 3
+                                    :queue []
+                                    :waiting true
+                                    "p" 0}
+                                 1 {:pos 0
+                                    :snd-cnt 0
+                                    :queue [1 2 0]
+                                    :waiting false
+                                    "p" 1}}})))
+
+    (is (= {:insts d18-s01
+            :progs {0 {:pos 4
+                       :snd-cnt 3
+                       :queue []
+                       :waiting false
+                       "p" 0
+                       "a" 1}
+                    1 {:pos 1
+                       :snd-cnt 1
+                       :queue [1 2 0]
+                       :waiting false
+                       "p" 1}}}
+           (d18/step-p2 {:insts d18-s01
+                         :progs {0 {:pos 3
+                                    :snd-cnt 3
+                                    :queue [1]
+                                    :waiting false
+                                    "p" 0}
+                                 1 {:pos 1
+                                    :snd-cnt 1
+                                    :queue [1 2 0]
+                                    :waiting false
+                                    "p" 1}}})))))
 
 (deftest execute-p1-test
   (testing "Runs the instructions and returns the value recovered"
