35ca22f9c5830fbc5cc1d194a2c20398ecdacb2e
Ken Smith <ken@klsmith.me>
2024-10-26 22:50:26 +0000

    Part1 soln

--- src/aoc_clj/2017/day22.clj
+++ src/aoc_clj/2017/day22.clj
@@ -1,5 +1,10 @@
 (ns aoc-clj.2017.day22
-  "Solution to https://adventofcode.com/2017/day/22")
+  "Solution to https://adventofcode.com/2017/day/22"
+  (:require [aoc-clj.utils.grid :as grid]))
+
+;; Constants
+(def part1-burst-count 10000)
+(def part2-burst-count 10000000)
 
 ;; Input parsing
 (defn parse
@@ -7,10 +12,57 @@
   (let [size (count input)
         dim  (quot size 2)]
     (->>
-     (zipmap (for [y (range dim (dec (- dim)) -1)
+     (zipmap (for [y (reverse (range (- dim) (inc dim)))
                    x (range (- dim) (inc dim))]
                [x y])
              (apply concat input))
      (filter #(= \# (val %)))
      keys
      set)))
+
+;; Puzzle logic
+(defn turn
+  "Return a new state with the virus turned based the infected state of
+   the cell it's currently on"
+  [{:keys [infected pos] :as state}]
+  (if (infected pos)
+    (grid/turn state :right)
+    (grid/turn state :left)))
+
+(defn infect-or-clean
+  "Return a new state with the cell toggled from infected or cleaned"
+  [{:keys [infected pos] :as state}]
+  (if (infected pos)
+    (update state :infected disj pos)
+    (-> state
+        (update :infected conj pos)
+        (update :infect-cnt inc))))
+
+(defn step
+  "Return a new state for a single interation"
+  [state]
+  (-> state
+      turn
+      infect-or-clean
+      (grid/forward 1)))
+
+(defn init-state
+  "Create an initial state map based on what cells are initially infected"
+  [infected]
+  {:infected infected :infect-cnt 0 :pos [0 0] :heading :n})
+
+(defn infections-caused-at-n
+  "Returns the number of times an infection was caused after n bursts"
+  [infections n]
+  (->> (init-state infections)
+       (iterate step)
+       (drop n)
+       first
+       :infect-cnt))
+
+;; Puzzle solutions
+(defn part1
+  "After 10000 bursts of activity, how many bursts cause a node to
+   become infected?"
+  [input]
+  (infections-caused-at-n input part1-burst-count))
--- test/aoc_clj/2017/day22_test.clj
+++ test/aoc_clj/2017/day22_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2017.day22-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2017.day22 :as d22]))
 
 (def d22-s00-raw
@@ -13,3 +14,33 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d22-s00 (d22/parse d22-s00-raw)))))
+
+(deftest step-test
+  (testing "Evolves the state forward one iteration"
+    (is (= {:infected #{[0 0] [-1 0] [1 1]}
+            :infect-cnt 1 :pos [-1 0] :heading :w}
+           (d22/step (d22/init-state d22-s00))))
+
+    (is (= {:infected #{[0 0] [1 1]}
+            :infect-cnt 1 :pos [-1 1] :heading :n}
+           (d22/step
+            {:infected #{[0 0] [-1 0] [1 1]}
+             :infect-cnt 1 :pos [-1 0] :heading :w})))
+
+    (is (= {:infected #{[0 0] [1 1] [-1 1]}
+            :infect-cnt 2 :pos [-2 1] :heading :w}
+           (d22/step
+            {:infected #{[0 0] [1 1]}
+             :infect-cnt 1 :pos [-1 1] :heading :n})))))
+
+(deftest infections-caused-at-n-test
+  (testing "Counts how many infections were caused after n bursts"
+    (is (= 5 (d22/infections-caused-at-n d22-s00 7)))
+    (is (= 41 (d22/infections-caused-at-n d22-s00 70)))
+    (is (= 5587 (d22/infections-caused-at-n d22-s00 10000)))))
+
+(def day22-input (u/parse-puzzle-input d22/parse 2017 22))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day22, part1"
+    (is (= 5280 (d22/part1 day22-input)))))
