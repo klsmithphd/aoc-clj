9ce4b913113a02215ad6a789cc06fbb6c9d165d2
Ken Smith <ken@klsmith.me>
2024-11-09 18:00:17 +0000

    Cleanup and refactor 2018 Day04

--- src/aoc_clj/2018/day04.clj
+++ src/aoc_clj/2018/day04.clj
@@ -1,52 +1,80 @@
 (ns aoc-clj.2018.day04
   "Solution to https://adventofcode.com/2018/day/4"
-  (:require [clojure.string :as str]
-            [aoc-clj.utils.core :as u]))
+  (:require [aoc-clj.utils.core :as u]))
+
+;; Input parsing
+(defn last-number
+  "Returns the last multi-digit number contained in the string as an integer"
+  [s]
+  (->> s
+       (re-seq #"\d+")
+       last
+       ;; Use parseInt instead of read-string because read-string
+       ;; attempts to interpret '09' as an octal value, which is invalid.
+       Integer/parseInt))
 
 (defn parse-shift
-  [shift]
-  (let [guard-id (Integer/parseInt (subs (re-find #"#[0-9]+" (first shift)) 1))
-        cycles (map (comp #(Integer/parseInt %)
-                          #(subs % 15 17)) (rest shift))]
-    [guard-id cycles]))
+  [[[shift-start] sleep-cycles]]
+  ;; The last number in the shift-start is the Guard id
+  ;; The last number in every other line is the minutes after the hour
+  {(last-number shift-start) (map last-number sleep-cycles)})
 
 (defn parse
   [input]
-  (->> (str/split (str/join "\n" (sort input)) #"\[.{16}\] Guard ")
-       (map #(str/split % #"\n"))
-       rest
-       (map parse-shift)
-       (group-by first)
-       (u/fmap (comp
-                (partial partition 2)
-                (partial mapcat second)))))
+  (->>
+   ;; Puzzle input isn't chronologically sorted
+   (sort input)
+   ;; Break seq whenever a new Guard is declared
+   (partition-by #(re-find #"Guard" %))
+   ;; Combine the guard and the sleep/wake times
+   (partition 2)
+   (map parse-shift)
+   ;; Combine the different shifts over different days into one coll
+   (apply merge-with concat)
+   ;; Update the guard
+   (u/fmap #(partition 2 %))))
 
+;; Puzzle logic
 (defn guard-sleep-minutes
+  "Returns an unordered seq of all the minutes the guard was ever asleep"
   [windows]
-  (mapcat #(range (first %) (second %)) windows))
+  (mapcat #(apply range %) windows))
 
 (defn sleepiest-guard-and-optimal-minute
+  "Returns the id of the guard who sleeps the most and which
+   minute they were asleep the most"
   [shifts]
-  (let [sleep-sched (u/fmap guard-sleep-minutes shifts)
+  (let [sleep-sched     (u/fmap guard-sleep-minutes shifts)
         sleepiest-guard (u/max-val (u/fmap count sleep-sched))
-        optimal-minute (->> (get sleep-sched sleepiest-guard)
-                            frequencies
-                            u/max-val)]
+        optimal-minute  (->> (get sleep-sched sleepiest-guard)
+                             frequencies
+                             u/max-val)]
     [sleepiest-guard optimal-minute]))
 
+(defn sleepiest-minute
+  "Returns the minute and the number of times that any given guard slept
+   the most"
+  [windows]
+  (->> windows
+       guard-sleep-minutes
+       frequencies
+       (apply max-key val)))
+
 (defn guard-most-frequently-asleep-at-minute
+  "Returns which guard was asleep at the same minute the most times 
+   and the minute they slept the most"
   [shifts]
-  (let [max-minutes (->> (u/fmap (comp frequencies guard-sleep-minutes) shifts)
-                         (filter #(seq (val %)))
-                         (into {})
-                         (u/fmap (partial apply max-key val)))
+  (let [max-minutes        (u/fmap sleepiest-minute shifts)
         [guard [minute _]] (apply max-key (comp second val) max-minutes)]
     [guard minute]))
 
+;; Puzzle solutions
 (defn part1
+  "What is the ID of the guard you chose multiplied by the minute you chose?"
   [input]
   (reduce * (sleepiest-guard-and-optimal-minute input)))
 
 (defn part2
+  "What is the ID of the guard you chose multiplied by the minute you chose?"
   [input]
   (reduce * (guard-most-frequently-asleep-at-minute input)))
--- test/aoc_clj/2018/day04_test.clj
+++ test/aoc_clj/2018/day04_test.clj
@@ -1,9 +1,9 @@
 (ns aoc-clj.2018.day04-test
   (:require [clojure.test :refer [deftest testing is]]
             [aoc-clj.utils.core :as u]
-            [aoc-clj.2018.day04 :as t]))
+            [aoc-clj.2018.day04 :as d04]))
 
-(def d04-s00
+(def d04-s00-raw
   ["[1518-11-01 00:00] Guard #10 begins shift"
    "[1518-11-01 00:05] falls asleep"
    "[1518-11-01 00:25] wakes up"
@@ -22,27 +22,33 @@
    "[1518-11-05 00:45] falls asleep"
    "[1518-11-05 00:55] wakes up"])
 
+(def d04-s00
+  {10 [[5 25] [30 55] [24 29]]
+   99 [[40 50] [36 46] [45 55]]})
+
+(->> (u/fmap (comp #(apply max-key val %) frequencies d04/guard-sleep-minutes) d04-s00)
+     (filter #(seq (val %))))
+
 (deftest parse-test
   (testing "Can identify the guard-id, and sleep windows"
-    (is (= {10 '((5 25) (30 55) (24 29)), 99 '((40 50) (36 46) (45 55))}
-           (t/parse d04-s00)))))
+    (is (= d04-s00 (d04/parse d04-s00-raw)))))
 
 (deftest strategy1
   (testing "Identify the guard who sleeps the most and the minute they sleep the most"
     (is (= [10 24]
-           (t/sleepiest-guard-and-optimal-minute (t/parse d04-s00))))))
+           (d04/sleepiest-guard-and-optimal-minute d04-s00)))))
 
 (deftest strategy2
   (testing "Identify the guard who sleeps the most at any given minute"
     (is (= [99 45]
-           (t/guard-most-frequently-asleep-at-minute (t/parse d04-s00))))))
+           (d04/guard-most-frequently-asleep-at-minute d04-s00)))))
 
-(def day04-input (u/parse-puzzle-input t/parse 2018 4))
+(def day04-input (u/parse-puzzle-input d04/parse 2018 4))
 
 (deftest part1-test
   (testing "Reproduces the answer for day04, part1"
-    (is (= 74743 (t/part1 day04-input)))))
+    (is (= 74743 (d04/part1 day04-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day04, part2"
-    (is (= 132484 (t/part2 day04-input)))))
+    (is (= 132484 (d04/part2 day04-input)))))
