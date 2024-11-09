33235a423bb7de892d37d266b3138af3c20c7658
Ken Smith <ken@klsmith.me>
2024-11-09 20:50:43 +0000

    Part1 soln

--- src/aoc_clj/2018/day06.clj
+++ src/aoc_clj/2018/day06.clj
@@ -1,5 +1,8 @@
 (ns aoc-clj.2018.day06
-  "Solution to https://adventofcode.com/2018/day/6")
+  "Solution to https://adventofcode.com/2018/day/6"
+  (:require
+   [aoc-clj.utils.grid :as grid]
+   [aoc-clj.utils.vectors :as vec]))
 
 ;; Input parsing
 (defn parse-line
@@ -9,3 +12,63 @@
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn bounds
+  "Returns the outer bounds of the provided coordinates as 
+   [min-x, max-x], [min-y, max-y]"
+  [coords]
+  [[(apply min (map first coords))
+    (apply max (map first coords))]
+   [(apply min (map second coords))
+    (apply max (map second coords))]])
+
+(defn strictly-inside?
+  "Returns true if the provided position is strictly contained
+   within the provided bounds"
+  [[[min-x max-x] [min-y max-y]] [x y]]
+  (and (< min-x x max-x)
+       (< min-y y max-y)))
+
+(defn inner-coords
+  "Returns the coordinates that are strictly inside the bounds
+   set by the other coordinates"
+  [coords]
+  (let [bnds (bounds coords)]
+    (filter #(strictly-inside? bnds %) coords)))
+
+(defn closer?
+  "Returns true if `pos` is strictly closer to `coord` than any
+   other coord in `others`, via Manhattan distance"
+  [others coord pos]
+  (let [dist (vec/manhattan coord pos)]
+    (every? #(> (vec/manhattan pos %) dist) others)))
+
+(defn area
+  "Returns the number of points that are closer to `coord` than
+   any other point in `cooords`, or zero if that area would
+   expand out into infinity."
+  [coords coord]
+  (let [not-inside? #(not (strictly-inside? (bounds coords) %))
+        others (remove #{coord} coords)]
+    (loop [front (grid/adj-coords-2d coord) in-area #{}]
+      (if (or (empty? front) (some not-inside? in-area))
+        (if (some not-inside? in-area)
+          0
+          (count in-area))
+        (let [keepers (filter #(closer? others coord %) front)]
+          (recur (remove in-area (set (mapcat grid/adj-coords-2d keepers)))
+                 (into in-area keepers)))))))
+
+(defn largest-area
+  "Computes the largest area around any coordinate."
+  [coords]
+  (->> (inner-coords coords)
+       (map #(area coords %))
+       (apply max)))
+
+;; Puzzle solutions
+(defn part1
+  "What is the size of the largest area that isn't infinite?"
+  [input]
+  (largest-area input))
--- test/aoc_clj/2018/day06_test.clj
+++ test/aoc_clj/2018/day06_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2018.day06-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2018.day06 :as d06]))
 
 (def d06-s00-raw
@@ -21,3 +22,28 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d06-s00 (d06/parse d06-s00-raw)))))
+
+(deftest bounds-test
+  (testing "Determines the outer bounds of the coordinates"
+    (is (= [[1 8] [1 9]] (d06/bounds d06-s00)))))
+
+(deftest inner-coords-test
+  (testing "Returns the coordinates contained within the bounds set by all 
+            the others"
+    (is (= [[3 4] [5 5]] (d06/inner-coords d06-s00)))))
+
+(deftest area-test
+  (testing "Returns the number of points that are closer to this coord
+            than any other coord"
+    (is (= 9 (d06/area d06-s00 [3 4])))
+    (is (= 17 (d06/area d06-s00 [5 5])))))
+
+(deftest largest-area-test
+  (testing "Finds the largest area around any coordinate"
+    (is (= 17 (d06/largest-area d06-s00)))))
+
+(def day06-input (u/parse-puzzle-input d06/parse 2018 6))
+
+(deftest ^:slow part1-test
+  (testing "Reproduces the answer for day06, part1"
+    (is (= 4290 (d06/part1 day06-input)))))
