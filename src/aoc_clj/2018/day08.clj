7e6a643c06c6ed93a2813e1e2796bda04ef22751
Ken Smith <ken@klsmith.me>
2024-11-10 00:02:10 +0000

    Part1

--- src/aoc_clj/2018/day08.clj
+++ src/aoc_clj/2018/day08.clj
@@ -4,3 +4,42 @@
 
 ;; Input parsing
 (def parse u/firstv)
+
+;; Puzzle logic
+(defn node-size
+  "Computes the number of data elements the node occupies
+   including its children, recursively"
+  [{:keys [children metadata]}]
+  (if (empty? children)
+    (+ 2 (count metadata))
+    (+ 2 (count metadata) (reduce + (map node-size children)))))
+
+(defn add-node
+  "Process the next element of data to add a node to the tree, 
+   recursively"
+  [[n-children n-metadata & remaining]]
+  (if (zero? n-children)
+    {:children [] :metadata (take n-metadata remaining)}
+    (loop [children [] shift 0]
+      (if (= n-children (count children))
+        {:children children
+         :metadata (take n-metadata (drop shift remaining))}
+        (let [next-child (add-node (drop shift remaining))
+              size (node-size next-child)]
+          (recur (conj children next-child)
+                 (+ shift size)))))))
+
+(defn metadata-sum
+  "Computes the sum of all the node's metadata and the metadata of
+   it children, recursively"
+  [{:keys [children metadata]}]
+  (if (empty? children)
+    (reduce + metadata)
+    (+ (reduce + metadata)
+       (reduce + (map metadata-sum children)))))
+
+;; Puzzle solutions
+(defn part1
+  "What is the sum of all metadata entries?"
+  [input]
+  (metadata-sum (add-node input)))
--- test/aoc_clj/2018/day08_test.clj
+++ test/aoc_clj/2018/day08_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2018.day08-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2018.day08 :as d08]))
 
 (def d08-s00-raw
@@ -11,3 +12,49 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d08-s00 (d08/parse d08-s00-raw)))))
+
+
+(deftest node-size-test
+  (testing "Determines the number of elements representing a node"
+    (is (= 2 (d08/node-size {:children [] :metadata []})))
+    (is (= 4 (d08/node-size {:children [] :metadata [1 2]})))
+
+    (is (= 9 (d08/node-size {:children
+                             [{:children {} :metadata []}
+                              {:children {} :metadata [1]}]
+                             :metadata [3 4]})))))
+
+(deftest add-node-test
+  (testing "Constructs the tree node by node"
+    (is (= {:children [] :metadata []} (d08/add-node [0 0])))
+    (is (= {:children [] :metadata [2]} (d08/add-node [0 1 2])))
+    (is (= {:children
+            [{:children [] :metadata []}]
+            :metadata []} (d08/add-node [1 0 0 0])))
+    (is (= {:children
+            [{:children
+              [{:children [] :metadata []}] :metadata []}]
+            :metadata []} (d08/add-node [1 0 1 0 0 0])))
+
+    (is (= {:children
+            [{:children [] :metadata [10 11 12]}
+             {:children
+              [{:children [] :metadata [99]}]
+              :metadata [2]}]
+            :metadata [1 1 2]}
+           (d08/add-node d08-s00)))))
+
+(deftest metadata-sum-test
+  (testing "Computes the sum of all the metadata in the tree"
+    (is (= 0 (d08/metadata-sum {:children [] :metadata []})))
+    (is (= 10 (d08/metadata-sum {:children [] :metadata [4 6]})))
+    (is (= 20 (d08/metadata-sum {:children
+                                 [{:children [] :metadata [10]}]
+                                 :metadata [4 6]})))
+    (is (= 138 (d08/metadata-sum (d08/add-node d08-s00))))))
+
+(def day08-input (u/parse-puzzle-input d08/parse 2018 8))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day08, part1"
+    (is (= 41028 (d08/part1 day08-input)))))
