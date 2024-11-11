f5fe5957fc66776c13088720b407262e111e7156
Ken Smith <ken@klsmith.me>
2024-11-11 20:57:11 +0000

    Part1 soln

--- src/aoc_clj/2018/day12.clj
+++ src/aoc_clj/2018/day12.clj
@@ -5,6 +5,8 @@
 
 ;; Constants
 (def charmap {\# 1 \. 0})
+(def part1-generations 20)
+(def part2-generations 50000000000)
 
 ;; Input parsing
 (defn parse-state
@@ -22,7 +24,48 @@
 
 (defn parse
   [input]
-  (let [[state rules] (u/split-at-blankline input)]
-    {:state (parse-state state)
+  (let [[pots rules] (u/split-at-blankline input)]
+    {:pots  (parse-state pots)
      :rules (parse-rules rules)
      :left  0}))
+
+;; Puzzle logic
+(defn step
+  "Evolves the pot state forward by one iteration"
+  [{:keys [pots rules] :as state}]
+  (let [slices (->> (concat [0 0 0] pots [0 0 0])
+                    (partition 5 1))]
+    (-> state
+        (assoc :pots (mapv #(get rules % 0) slices))
+        (update :left dec))))
+
+(defn pots-with-plants
+  "Returns the ids of the pots that contain a plant"
+  [{:keys [pots left]}]
+  (->> (map-indexed vector pots)
+       (filter (comp pos? second))
+       (map #(+ left (first %)))))
+
+(defn pot-sum-at-n
+  "Returns the sum of all the pot ids that contain a plant after n
+   generations"
+  [n state]
+  (->> state
+       (iterate step)
+       (drop n)
+       first
+       pots-with-plants
+       (reduce +)))
+
+;; Puzzle solutions
+(defn part1
+  "After 20 generations, what is the sum of the numbers of all pots
+   which contain a plant?"
+  [input]
+  (pot-sum-at-n part1-generations input))
+
+(defn part2
+  "After fifty billion (50000000000) generations, 
+   what is the sum of the numbers of all pots which contain a plant?"
+  [input]
+  (pot-sum-at-n part2-generations input))
--- test/aoc_clj/2018/day12_test.clj
+++ test/aoc_clj/2018/day12_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2018.day12-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2018.day12 :as d12]))
 
 (def d12-s00-raw
@@ -21,7 +22,7 @@
    "####. => #"])
 
 (def d12-s00
-  {:state [1 0 0 1 0 1 0 0 1 1 0 0 0 0 0 0 1 1 1 0 0 0 1 1 1]
+  {:pots  [1 0 0 1 0 1 0 0 1 1 0 0 0 0 0 0 1 1 1 0 0 0 1 1 1]
    :rules {[0 0 0 1 1] 1
            [0 0 1 0 0] 1
            [0 1 0 0 0] 1
@@ -41,3 +42,33 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d12-s00 (d12/parse d12-s00-raw)))))
+
+(deftest step-test
+  (testing "Evolves the pot state one iteration forward"
+    (is (= [0 1 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 0 1 0 0 1 0 0 1 0]
+           (:pots (d12/step d12-s00))))
+    (is (= [0 0 1 1 0 0 1 1 0 0 0 1 1 0 0 0 0 1 0 0 1 0 0 1 0 0 1 1 0]
+           (:pots (d12/step (d12/step d12-s00)))))
+    (is (= [0 0 1 0 1 0 0 0 1 0 0 1 0 1 0 0 0 0 1 0 0 1 0 0 1 0 0 0 1 0 0]
+           (:pots (d12/step (d12/step (d12/step d12-s00))))))))
+
+(deftest pots-with-plants-test
+  (testing "Returns the pot numbers that contain plants"
+    (is (= [0 3 5 8 9 16 17 18 22 23 24]
+           (d12/pots-with-plants d12-s00)))
+    (is (= [0 4 9 15 18 21 24]
+           (d12/pots-with-plants (d12/step d12-s00))))
+    (is (= [0 1 4 5 9 10 15 18 21 24 25]
+           (d12/pots-with-plants (d12/step (d12/step d12-s00)))))
+    (is (= [-1 1 5 8 10 15 18 21 25]
+           (d12/pots-with-plants (d12/step (d12/step (d12/step d12-s00))))))))
+
+(deftest pot-sum-at-n-test
+  (testing "Returns the sum of the pot ids with plants after n steps"
+    (is (= 325 (d12/pot-sum-at-n 20 d12-s00)))))
+
+(def day12-input (u/parse-puzzle-input d12/parse 2018 12))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day12, part1"
+    (is (= 3725 (d12/part1 day12-input)))))
