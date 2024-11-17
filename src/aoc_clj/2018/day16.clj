ee07aa71398ee08dd7f5fa918945428e77ca5745
Ken Smith <ken@klsmith.me>
2024-11-17 21:31:22 +0000

    Documentation

--- src/aoc_clj/2018/day16.clj
+++ src/aoc_clj/2018/day16.clj
@@ -18,26 +18,38 @@
 
 ;; Puzzle logic
 (defn dref
+  "Dereference the argument from regs.
+   If type is `:immediate`, return the arg as-is
+   If type is `:register`, return the register value at `arg`"
   [type regs arg]
   (case type
     :immediate arg
     :register  (get regs arg 0)))
 
-(defn mutate-op
+(defn- mutate-op
+  "Base implementation of mutating register c through an operation
+   on a and b, where b can either be an :immediate value or the
+   :register value, depending upon `type`"
   [op type regs [a b c]]
   (assoc regs c (op (dref :register regs a) (dref type regs b))))
 
-(defn set-op
+(defn- set-op
+  "Base implementation for setting register c based on either
+   the :immediate value of a, or the :register value, determined
+   by `type`"
   [type regs [a _ c]]
   (assoc regs c (dref type regs a)))
 
-(defn comparison-op
+(defn- comparison-op
+  "Base implementation for setting register c to 1 or 0 depending
+   upon whether the comparison operation between a and b is true,
+   with a and b each uniquely interpreted as :immediate or :register
+   based on `a-type` and `b-type`"
   [a-type b-type comp regs [a b c]]
-  (let [a-val (dref a-type regs a)
-        b-val (dref b-type regs b)]
-    (assoc regs c (if (comp a-val b-val) 1 0))))
+  (assoc regs c (if (comp (dref a-type regs a) (dref b-type regs b)) 1 0)))
 
 (def opcodes
+  "The sixteen possible opcodes"
   {:addr (partial mutate-op + :register)
    :addi (partial mutate-op + :immediate)
    :mulr (partial mutate-op * :register)
@@ -56,6 +68,8 @@
    :eqrr (partial comparison-op :register :register  =)})
 
 (defn compatible-opcodes
+  "For a given sample, returns the set of opcodes that could
+   possibly produce the same effect"
   [[before [_ & args] after]]
   (->> opcodes
        (filter #(= after ((val %) before args)))
@@ -63,6 +77,8 @@
        set))
 
 (defn three-or-more-opcode-count
+  "Returns the count of samples for which three or more opcodes are
+   possibly consistent with the sample."
   [samples]
   (->> samples
        (map compatible-opcodes)
@@ -70,41 +86,60 @@
        count))
 
 (defn opcode-map
+  "For a given sample, returns a map of the opcode-number and the 
+   possible opcode names it's compatible with"
   [[_ [opcode & _] _ :as sample]]
   {opcode (compatible-opcodes sample)})
 
 (defn possible-mapping
+  "Given the samples provided, returns a map between opcode numbers
+   and the sets of potentially compatible opcode names"
   [samples]
   (->> samples
        (map opcode-map)
        (apply merge-with set/intersection)))
 
-(defn mapping-solve
+(defn opcode-id-to-name-map
+  "Returns the unique opcode-number-to-name map for the provided samples."
   [samples]
+  ;; Starts with the possible mapping and then iteratively collapses
+  ;; the possibilities based on removing the mappings for which there's
+  ;; only one possible match.
   (loop [mapping (possible-mapping samples) solved {}]
     (if (= 16 (count solved))
       solved
+      ;; done is the map of all id-to-name pairs that only have one name
       (let [done (->> mapping
                       (filter #(= 1 (count (val %))))
                       (u/fmap first))
             left-to-map (u/without-keys mapping (keys done))]
+        ;; In the next recurrence, the possible mapping will strip out
+        ;; the done values
         (recur (u/fmap #(set/difference % (set (vals done))) left-to-map)
                (merge solved done))))))
 
 (defn op-step
+  "Returns the new register state after applying an opcode instruction
+   to the current `regs`, given the id-to-name mapping in `mapping`"
   [mapping regs [opcode & args]]
   ((opcodes (mapping opcode)) regs args))
 
 (defn execute
+  "Given the input of sample effects and the program to execute,
+   first determine the correct id-to-name mapping for opcodes and
+   then execute the sample program. Returns the final register state"
   [{:keys [samples program]}]
-  (let [opcode-mapping (mapping-solve samples)]
+  (let [opcode-mapping (opcode-id-to-name-map samples)]
     (reduce (partial op-step opcode-mapping) [0 0 0 0] program)))
 
 ;; Puzzle solutions
 (defn part1
+  "Ignoring the opcode numbers, how many samples in your puzzle
+   input behave like three or more opcodes?"
   [{:keys [samples]}]
   (three-or-more-opcode-count samples))
 
 (defn part2
+  "What value is contained in register 0 after executing the test program?"
   [input]
   (first (execute input)))
