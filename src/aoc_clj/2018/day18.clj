457ce9a39dc288bb44fede6bde486baf28f2dd32
Ken Smith <ken@klsmith.me>
2024-11-23 21:27:47 +0000

    Part1

--- src/aoc_clj/2018/day18.clj
+++ src/aoc_clj/2018/day18.clj
@@ -1,8 +1,12 @@
 (ns aoc-clj.2018.day18
   "Solution to https://adventofcode.com/2018/day/18"
-  (:require [aoc-clj.utils.grid.vecgrid :as vg]))
+  (:require [aoc-clj.utils.grid :as grid]
+            [aoc-clj.utils.grid.vecgrid :as vg]))
 
+;; Constants
+(def part1-time 10)
 
+;; Input parsing
 (def charmap
   {\. :o ;; open
    \# :l ;; lumberyard
@@ -12,3 +16,67 @@
 (defn parse
   [input]
   (vg/ascii->VecGrid2D charmap input :down true))
+
+;; Puzzle logic
+(defn cell-update
+  "Given the current scan state, and a position in the grid `pos`,
+   return the new value for that position using the logic provideds"
+  [scan pos]
+  (let [neighbor-cnts (->> (grid/neighbor-data scan pos :diagonals true)
+                           (map :val)
+                           frequencies)]
+    ;; (print (grid/value scan pos))
+    (case (grid/value scan pos)
+      ;; An open acre becomes trees if 3 or more adjacent acres contain trees
+      ;; Else, nothing happens
+      :o (if (>= (get neighbor-cnts :t 0) 3)
+           :t
+           :o)
+      ;; A tree acre becomes a lumberyard if 3 or more adjacent acres were lumberyards
+      ;; Else, nothing happens
+      :t (if (>= (get neighbor-cnts :l 0) 3)
+           :l
+           :t)
+      ;; A lumberyard remains a lumberyard if it was next to at least one
+      ;; lumberyard and at least one acre containing trees.
+      ;; Else, it becomes open.
+      :l (if (and (pos? (get neighbor-cnts :l 0))
+                  (pos? (get neighbor-cnts :t 0)))
+           :l
+           :o))))
+
+(defn step
+  "Returns the new scan state, updated by one increment in time"
+  [scan]
+  (let [width (grid/width scan)]
+    (->> (grid/pos-seq scan)
+         (map #(cell-update scan %))
+         (partition width)
+         (mapv vec)
+         vg/->VecGrid2D)))
+
+(defn state-at-t
+  "Returns the updated state of the scan at time `t`"
+  [scan t]
+  (->> scan
+       (iterate step)
+       (drop t)
+       first))
+
+(defn resource-value
+  "The resource value is the number of tree acres times the number of 
+   lumberyard acres"
+  [scan]
+  (let [freqs (->> scan
+                   grid/val-seq
+                   frequencies)]
+    (* (get freqs :t)
+       (get freqs :l))))
+
+;; Puzzle solutions
+(defn part1
+  "What will the total resource value of the lumber collection area be
+   after 10 minutes?"
+  [input]
+  (->> (state-at-t input part1-time)
+       resource-value))
