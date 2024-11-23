96d48c2f63374f4c7b73fd4b0f24b3744ce28e47
Ken Smith <ken@klsmith.me>
2024-11-23 20:02:38 +0000

    Clay map and all-clay cells

--- src/aoc_clj/2018/day17.clj
+++ src/aoc_clj/2018/day17.clj
@@ -1,5 +1,10 @@
 (ns aoc-clj.2018.day17
-  "Solution to https://adventofcode.com/2018/day/17")
+  "Solution to https://adventofcode.com/2018/day/17"
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.vectors :as vec]))
+
+;; Constants
+(def spring [500 0])
 
 ;; Input parsing
 (defn parse-line
@@ -16,14 +21,47 @@
 
 ;; Puzzle logic
 (defn clay-line
+  "Expands the definition of a line of clay into its full coordinates"
   [[dir fixed start end]]
   (for [changing (range start (inc end))]
     (case dir
       :v [fixed changing]
       :h [changing fixed])))
 
-(defn all-clay
+(defn clay-map
+  "Expands the clay vein definitions into a map with the vertical coordinate
+   as the key and the value is the horizontal coordinates of clay"
   [veins]
   (->> veins
        (mapcat clay-line)
-       set))
+       set
+       (group-by second)
+       (u/fmap #(sort (map first %)))))
+
+(defn all-clay
+  "Returns a set of all the coordinates of the clay cells from the vertical
+   clay map"
+  [clay-map]
+  (set (mapcat (fn [[y xs]] (map #(vector % y) xs)) clay-map)))
+
+(def lowest
+  "Returns the lowest (maximum y) value of all the clay"
+  (memoize
+   (fn [clay]
+     (apply max (keys clay)))))
+
+;; The defining characteristic of whether water *can* settle at a given
+;; position is that there's a clay cell to the left and a clay cell to
+;; the right on the current line, with every value on the next line
+;; down already occupied.
+
+(defn water-can-settle
+  [])
+
+(defn add-water
+  [occupied]
+  (let [y (->> spring
+               (iterate #(vec/vec-add [0 1] %))
+               (drop-while (complement occupied))
+               first)]
+    y))
--- test/aoc_clj/2018/day17_test.clj
+++ test/aoc_clj/2018/day17_test.clj
@@ -33,8 +33,23 @@
     (is (= [[495 7] [496 7] [497 7] [498 7] [499 7] [500 7] [501 7]]
            (d17/clay-line (nth d17-s00 1))))))
 
-(deftest all-clay-test
+(deftest clay-map-test
   (testing "Expands all the clay veins into the set of clay cells"
+    (is (= {1  [506]
+            2  [495 498 506]
+            3  [495 498 501]
+            4  [495 498 501]
+            5  [495 501]
+            6  [495 501]
+            7  [495 496 497 498 499 500 501]
+            10 [498 504]
+            11 [498 504]
+            12 [498 504]
+            13 [498 499 500 501 502 503 504]}
+           (d17/clay-map d17-s00)))))
+
+(deftest all-clay-test
+  (testing "Expands the clay map into the set of clay cells"
     (is (= #{[495 2] [495 3] [495 4] [495 5] [495 6] [495 7]
              [496 7] [497 7] [498 7] [499 7] [500 7] [501 7]
              [501 3] [501 4] [501 5] [501 6]
@@ -43,4 +58,8 @@
              [498 10] [498 11] [498 12] [498 13]
              [504 10] [504 11] [504 12] [504 13]
              [499 13] [500 13] [501 13] [502 13] [503 13]}
-           (d17/all-clay d17-s00)))))
+           (d17/all-clay (d17/clay-map d17-s00))))))
+
+(deftest lowest-test
+  (testing "Finds the lowest point in the scan"
+    (is (= 13 (d17/lowest (d17/clay-map d17-s00))))))
