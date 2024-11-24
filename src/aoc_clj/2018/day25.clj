c49fd497c9e509b88fb409fc0c06adb27d317cc7
Ken Smith <ken@klsmith.me>
2024-11-24 21:01:13 +0000

    Part1

--- src/aoc_clj/2018/day25.clj
+++ src/aoc_clj/2018/day25.clj
@@ -1,5 +1,9 @@
 (ns aoc-clj.2018.day25
-  "Solution to https://adventofcode.com/2018/day/25")
+  "Solution to https://adventofcode.com/2018/day/25"
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.set :as set]
+            [aoc-clj.utils.vectors :as vec]
+            [aoc-clj.utils.core :as u]))
 
 ;; Input parsing
 (defn parse-line
@@ -9,3 +13,65 @@
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn close-enough?
+  "Two points are close enough if their Manhattan distance is 3 or less"
+  [[a b]]
+  (<= (vec/manhattan a b) 3))
+
+(defn can-merge?
+  "Two constellations can be merged into one if *any* of their member points
+   are close enough to each other"
+  [const-a const-b]
+  (->> (combo/cartesian-product const-a const-b)
+       (some close-enough?)
+       boolean))
+
+(defn merge-pass
+  "A single merge pass takes a collection of constellations
+   (represented as sets of points), and attempts to merge the ones
+   that are within range of each other"
+  [constellations]
+  (loop [const  (first constellations)
+         others (rest constellations)
+         new-consts []]
+    (if (empty? others)
+      new-consts
+      (let [;; Mergable are the other constellations within range of const
+            mergable   (filter #(can-merge? const %) others)
+            ;; The new-others take out all the constellations we can
+            ;; merge with the one currently being examined
+            new-others (remove (set mergable) others)
+            ;; Combine all the mergeable points together
+            merged     (apply set/union const mergable)]
+        (recur (first new-others)
+               new-others
+               (conj new-consts merged))))))
+
+(defn coll-as-sets
+  "Convert each element of coll into a set of that one element"
+  [coll]
+  (map #(conj #{} %) coll))
+
+(defn constellations
+  "Given the collection of points, returns the constellations
+   (sets of points) that can be formed"
+  [points]
+  ;; Initially, treat each point as being in a constellation by itself
+  (->> (coll-as-sets points)
+       ;; Iteratively collapse constellations until no more changes 
+       (u/converge merge-pass)
+       ;; Return the final configuration of constellations
+       last))
+
+(defn constellation-count
+  "Returns the number of constellations that can be formed from the points"
+  [points]
+  (count (constellations points)))
+
+;; Puzzle solutions
+(defn part1
+  "How many constellations are formed by the fixed points in spacetime?"
+  [input]
+  (constellation-count input))
--- test/aoc_clj/2018/day25_test.clj
+++ test/aoc_clj/2018/day25_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2018.day25-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2018.day25 :as d25]))
 
 (def d25-s00-raw
@@ -61,3 +62,48 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d25-s00 (d25/parse d25-s00-raw)))))
+
+(deftest merge-pass-test
+  (testing "In a single pass, combines the constellations that are
+            within range of each other"
+    (is (= [#{[0 0 0 0]
+              [3 0 0 0]
+              [0 3 0 0]
+              [0 0 3 0]
+              [0 0 0 3]}
+            #{[0 0 0 6]}
+            #{[9 0 0 0]
+              [12 0 0 0]}]
+           (d25/merge-pass (d25/coll-as-sets d25-s00))))
+
+    (is (= [#{[0 0 0 0]
+              [3 0 0 0]
+              [0 3 0 0]
+              [0 0 3 0]
+              [0 0 0 3]
+              [0 0 0 6]}
+            #{[9 0 0 0]
+              [12 0 0 0]}]
+           (d25/merge-pass
+            [#{[0 0 0 0]
+               [3 0 0 0]
+               [0 3 0 0]
+               [0 0 3 0]
+               [0 0 0 3]}
+             #{[0 0 0 6]}
+             #{[9 0 0 0]
+               [12 0 0 0]}])))))
+
+
+(deftest constellation-count-test
+  (testing "Counts the number of constellations among the points"
+    (is (= 2 (d25/constellation-count d25-s00)))
+    (is (= 4 (d25/constellation-count d25-s01)))
+    (is (= 3 (d25/constellation-count d25-s02)))
+    (is (= 8 (d25/constellation-count d25-s03)))))
+
+(def day25-input (u/parse-puzzle-input d25/parse 2018 25))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day25, part1"
+    (is (= 386 (d25/part1 day25-input)))))
