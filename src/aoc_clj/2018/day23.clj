ef99d5bb1e8aa1a7cb5e001ca5e058f35a27534f
Ken Smith <ken@klsmith.me>
2024-11-24 19:54:04 +0000

    Part1

--- src/aoc_clj/2018/day23.clj
+++ src/aoc_clj/2018/day23.clj
@@ -1,12 +1,41 @@
 (ns aoc-clj.2018.day23
-  "Solution to https://adventofcode.com/2018/day/23")
+  "Solution to https://adventofcode.com/2018/day/23"
+  (:require
+   [aoc-clj.utils.vectors :as vec]))
 
 ;; Input parsing
 (defn parse-line
   [line]
-  (let [[x y z r] (map read-string (re-seq #"\d+" line))]
+  (let [[x y z r] (map read-string (re-seq #"-?\d+" line))]
     [[x y z] r]))
 
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn strongest-signal
+  "Returns the nanobot with the strongest signal"
+  [nanobots]
+  (apply max-key second nanobots))
+
+(defn in-range?
+  "Returns whether nanobot b is within range of nanobot a"
+  [[pos-a r] [pos-b _]]
+  (<= (vec/manhattan pos-a pos-b) r))
+
+(defn in-range-of-strongest-count
+  "Counts the number of nanobots that are within range of the one
+   with the strongest signal"
+  [nanobots]
+  (let [strongest (strongest-signal nanobots)]
+    (->> nanobots
+         (filter #(in-range? strongest %))
+         count)))
+
+;; Puzzle solutions
+(defn part1
+  "Find the nanobot with the largest signal radius. 
+   How many nanobots are in range of its signals?"
+  [input]
+  (in-range-of-strongest-count input))
--- test/aoc_clj/2018/day23_test.clj
+++ test/aoc_clj/2018/day23_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2018.day23-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2018.day23 :as d23]))
 
 (def d23-s00-raw
@@ -27,3 +28,29 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d23-s00 (d23/parse d23-s00-raw)))))
+
+(deftest strongest-signal-test
+  (testing "Finds the nanobot with the strongest signal"
+    (is (= [[0 0 0] 4] (d23/strongest-signal d23-s00)))))
+
+(deftest in-range?-test
+  (testing "Determines whether a nanobot is in range of the provided one"
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 0))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 1))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 2))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 3))))
+    (is (= false (d23/in-range? [[0 0 0] 4] (nth d23-s00 4))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 5))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 6))))
+    (is (= true  (d23/in-range? [[0 0 0] 4] (nth d23-s00 7))))
+    (is (= false (d23/in-range? [[0 0 0] 4] (nth d23-s00 8))))))
+
+(deftest in-range-of-strongest-count-test
+  (testing "Counts the number of nanobots in range of the strongest one"
+    (is (= 7 (d23/in-range-of-strongest-count d23-s00)))))
+
+(def day23-input (u/parse-puzzle-input d23/parse 2018 23))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day23, part1"
+    (is (= 393 (d23/part1 day23-input)))))
