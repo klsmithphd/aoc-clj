1e00abb51aa5066d095dc897d7fc4a19087017a1
Ken Smith <ken@klsmith.me>
2024-12-06 12:21:45 +0000

    Part1 WIP

--- src/aoc_clj/2024/day06.clj
+++ src/aoc_clj/2024/day06.clj
@@ -1,10 +1,35 @@
 (ns aoc-clj.2024.day06
   "Solution to https://adventofcode.com/2024/day/6"
-  (:require [aoc-clj.utils.grid.mapgrid :as mg]))
+  (:require [aoc-clj.utils.grid :as grid :refer [value]]
+            [aoc-clj.utils.grid.mapgrid :as mg]))
 
 ;; Input parsing
 (def charmap {\. nil \# :wall \^ :guard})
 
 (defn parse
   [input]
-  (mg/ascii->MapGrid2D charmap input :down true))
+  (mg/ascii->MapGrid2D charmap input))
+
+;; Puzzle logic
+(defn guard-start
+  [{:keys [grid]}]
+  {:heading :n
+   :pos (ffirst (filter #(= :guard (val %)) grid))})
+
+(defn next-move
+  [grid guard]
+  (let [nxt (grid/forward guard 1)]
+    (if (= :wall (value grid (:pos nxt)))
+      (next-move grid (grid/turn guard :right))
+      nxt)))
+
+(defn guard-path
+  [grid]
+  (->> (guard-start grid)
+       (iterate #(next-move grid %))
+       (take-while #(grid/in-grid? grid (:pos %)))))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (count (set (guard-path input))))
--- test/aoc_clj/2024/day06_test.clj
+++ test/aoc_clj/2024/day06_test.clj
@@ -1,5 +1,7 @@
 (ns aoc-clj.2024.day06-test
-  (:require [aoc-clj.2024.day06 :as d06]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2024.day06 :as d06]))
 
 (def d06-s00
   (d06/parse
@@ -14,3 +16,9 @@
     "#........."
     "......#..."]))
 
+(def day06-input (u/parse-puzzle-input d06/parse 2024 6))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day06, part1"
+    ;; Too high
+    (is (= 5141 (d06/part1 day06-input)))))
