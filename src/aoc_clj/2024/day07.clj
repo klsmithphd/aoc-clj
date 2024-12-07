25ae878277a14f0cb3fde25e41e91843f27ebd3e
Ken Smith <ken@klsmith.me>
2024-12-07 18:51:29 +0000

    Part1 soln

--- src/aoc_clj/2024/day07.clj
+++ src/aoc_clj/2024/day07.clj
@@ -9,3 +9,40 @@
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn possible-values
+  "Returns the possible result values that could be generated when
+   evaluating arguments left-to-right and inserting either * or +
+   between consecutive args"
+  [[a b & others]]
+  (if (empty? others)
+    [(* a b) (+ a b)]
+    (mapcat possible-values [(cons (* a b) others) (cons (+ a b) others)])))
+
+(defn possibly-true-eqn?
+  "Determines whether the equation could possibly be true when
+   inserting operators between arguments"
+  [[result & args]]
+  (some #(= result %) (possible-values args)))
+
+(defn possibly-true-eqns
+  "Returns a filtered coll of only the possibly true equations"
+  [eqns]
+  (filter possibly-true-eqn? eqns))
+
+(defn possibly-true-test-sums
+  "Returns the sum of the test values of all the equations that could
+   possibly be true"
+  [eqns]
+  (->> eqns
+       possibly-true-eqns
+       (map first)
+       (reduce +)))
+
+;; Puzzle solutions
+(defn part1
+  "Determine which equations could possibly be true. 
+   What is their total calibration result?"
+  [input]
+  (possibly-true-test-sums input))
--- test/aoc_clj/2024/day07_test.clj
+++ test/aoc_clj/2024/day07_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day07-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day07 :as d07]))
 
 (def d07-s00-raw
@@ -27,3 +28,28 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d07-s00 (d07/parse d07-s00-raw)))))
+
+(deftest possible-values-test
+  (testing "Determines the possible values that could result from inserting +
+            and * operators"
+    (is (= [190 29]              (d07/possible-values (next (nth d07-s00 0)))))
+    (is (= [87480 3267 3267 148] (d07/possible-values (next (nth d07-s00 1)))))))
+
+(deftest possibly-true-eqns-test
+  (testing "Filters to just the equations that could possibly be true with
+            the correct operators inserted"
+    (is (= [[190 10 19]
+            [3267 81 40 27]
+            [292 11 6 16 20]]
+           (d07/possibly-true-eqns d07-s00)))))
+
+(deftest possibly-true-test-sums-test
+  (testing "Computes the sum of the test values from eqns that could possibly
+            be true"
+    (is (= 3749 (d07/possibly-true-test-sums d07-s00)))))
+
+(def day07-input (u/parse-puzzle-input d07/parse 2024 7))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day07, part1"
+    (is (= 1289579105366 (d07/part1 day07-input)))))
