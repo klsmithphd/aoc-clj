6364fef09cffe7ff66c56a177784636eb73eb406
Ken Smith <ken@klsmith.me>
2024-12-07 23:01:04 +0000

    Part2 soln

--- src/aoc_clj/2024/day06.clj
+++ src/aoc_clj/2024/day06.clj
@@ -12,64 +12,79 @@
 
 ;; Puzzle logic
 (defn guard-start
+  "Returns the starting state of the guard given the grid"
   [{:keys [grid]}]
   {:heading :n
    :pos (ffirst (filter #(= :guard (val %)) grid))
-   :loop? false
-   :seen #{}})
+   :visited #{}})
+
+(defn guard-state
+  "Returns the current state of the guard, which is just the submap
+   of data including the guard's current position and heading"
+  [guard]
+  (select-keys guard [:pos :heading]))
 
 (defn next-move
-  [grid {:keys [seen] :as guard}]
-  (let [ahead (grid/forward guard 1)
+  "Returns an updated guard state after making one move."
+  [grid guard]
+  (let [ahead     (grid/forward guard 1)
         new-guard (if (= :wall (value grid (:pos ahead)))
                     (next-move grid (grid/turn guard :right))
-                    ahead)
-        loc   (select-keys new-guard [:pos :heading])]
-    (if (seen loc)
-      (do
-        (println "We've got a loop!")
-        (assoc new-guard :loop? true))
-      (update new-guard :seen conj loc))))
+                    ahead)]
+    (update new-guard :visited conj (guard-state guard))))
 
-;; (defn take-until
-;;   [pred coll]
-;;   (let [foo (partition-by pred coll)]
-;;     (into (vec (first foo)) (take 1 (second foo)))))
+(defn new-state?
+  "Returns true if the current location hasn't been previously encountered"
+  [{:keys [visited] :as guard}]
+  (not (visited (guard-state guard))))
 
 (defn guard-path
+  "Returns the path that the guard will follow"
   [grid]
   (->> (guard-start grid)
        (iterate #(next-move grid %))
-       (take-while (complement :loop?))
+       (take-while new-state?)
        (take-while #(grid/in-grid? grid (:pos %)))))
 
-(defn with-obstruction
-  [grid pos]
-  (assoc-in grid [:grid pos] :wall))
-
-(defn loop?
-  [path]
-  (= (:pos (first path))
-     (:pos (last path))))
+(defn will-loop-with-obstruction?
+  "Identifies whether adding an obstruction at `obs` will
+   cause the guard to enter a loop"
+  [grid obs]
+  (let [new-grid (assoc-in grid [:grid obs] :wall)
+        end      (last (guard-path new-grid))]
+    (not (new-state? (next-move new-grid end)))))
 
 (defn looping-guard-paths
+  "Returns the count of the number of obstructions that can be placed
+   in the guard's path in order to cause them to loop forever"
   [grid]
   (let [start #{(:pos (guard-start grid))}
         orig-path (->> (guard-path grid)
                        (map :pos)
-                       rest
+                       set
                        (remove start))]
     (->> orig-path
-         (map #(with-obstruction grid %))
-         (map guard-path)
-         (filter loop?)
+         (filter #(will-loop-with-obstruction? grid %))
          count)))
 
 (defn distinct-guard-positions
+  "Returns the count of the unique positions that the guard visited"
   [grid]
-  (count (set (map :pos (guard-path grid)))))
+  (->> (guard-path grid)
+       (map :pos)
+       set
+       count))
 
 ;; Puzzle solutions
 (defn part1
+  "How many distinct positions will the guard visit before leaving the
+   mapped area?"
   [input]
   (distinct-guard-positions input))
+
+(defn part2
+  "You need to get the guard stuck in a loop by adding a single new
+   obstruction. How many different positions could you choose for this
+   obstruction?"
+  [input]
+  (looping-guard-paths input))
--- test/aoc_clj/2024/day06_test.clj
+++ test/aoc_clj/2024/day06_test.clj
@@ -16,11 +16,19 @@
     "#........."
     "......#..."]))
 
+(deftest guard-start-test
+  (testing "Finds the initial state of the guard"
+    (is (= {:heading :n :pos [4 3] :visited #{}}
+           (d06/guard-start d06-s00)))))
+
 (deftest distinct-guard-positions-test
   (testing "Counts the number of unique positions visited by the guard"
     (is (= 41 (d06/distinct-guard-positions d06-s00)))))
 
-
+(deftest looping-guard-paths-test
+  (testing "Counts how many unique places an obstruction can be placed
+            to cause the guard to enter a loop"
+    (is (= 6 (d06/looping-guard-paths d06-s00)))))
 
 (def day06-input (u/parse-puzzle-input d06/parse 2024 6))
 
@@ -28,21 +36,6 @@
   (testing "Reproduces the answer for day06, part1"
     (is (= 4656 (d06/part1 day06-input)))))
 
-
-
-;; (->> (d06/guard-start d06-s00)
-;;      (d06/next-move d06-s00)
-;;      (d06/next-move d06-s00)
-;;      (d06/next-move d06-s00)
-;;      (d06/next-move d06-s00)
-;;      (d06/next-move d06-s00)
-;;      (d06/next-move d06-s00)
-;;     ;;  (d06/next-move d06-s00)
-;;      )
-
-;; (:loop? (last (d06/guard-path
-;;                     (assoc-in d06-s00 [:grid [3 3]] :wall))))
-
-;; (d06/looping-guard-paths d06-s00)
-
-;; (rest (:path (d06/guard-path d06-s00)))
+(deftest ^:slow part2-test
+  (testing "Reproduces the answer for day06, part2"
+    (is (= 1575 (d06/part2 day06-input)))))
