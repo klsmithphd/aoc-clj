7031ff478839993382d91a26ed8a2adaed3d98f9
Ken Smith <ken@klsmith.me>
2024-12-07 21:37:52 +0000

    Part2 WIP

--- src/aoc_clj/2024/day06.clj
+++ src/aoc_clj/2024/day06.clj
@@ -14,23 +14,62 @@
 (defn guard-start
   [{:keys [grid]}]
   {:heading :n
-   :pos (ffirst (filter #(= :guard (val %)) grid))})
+   :pos (ffirst (filter #(= :guard (val %)) grid))
+   :loop? false
+   :seen #{}})
 
 (defn next-move
-  [grid guard]
-  (let [nxt (grid/forward guard 1)]
-    (if (= :wall (value grid (:pos nxt)))
-      (grid/turn guard :right)
-      nxt)))
+  [grid {:keys [seen] :as guard}]
+  (let [ahead (grid/forward guard 1)
+        new-guard (if (= :wall (value grid (:pos ahead)))
+                    (next-move grid (grid/turn guard :right))
+                    ahead)
+        loc   (select-keys new-guard [:pos :heading])]
+    (if (seen loc)
+      (do
+        (println "We've got a loop!")
+        (assoc new-guard :loop? true))
+      (update new-guard :seen conj loc))))
+
+;; (defn take-until
+;;   [pred coll]
+;;   (let [foo (partition-by pred coll)]
+;;     (into (vec (first foo)) (take 1 (second foo)))))
 
 (defn guard-path
   [grid]
   (->> (guard-start grid)
        (iterate #(next-move grid %))
-       (take-while #(grid/in-grid? grid (:pos %)))
-       (map :pos)))
+       (take-while (complement :loop?))
+       (take-while #(grid/in-grid? grid (:pos %)))))
+
+(defn with-obstruction
+  [grid pos]
+  (assoc-in grid [:grid pos] :wall))
+
+(defn loop?
+  [path]
+  (= (:pos (first path))
+     (:pos (last path))))
+
+(defn looping-guard-paths
+  [grid]
+  (let [start #{(:pos (guard-start grid))}
+        orig-path (->> (guard-path grid)
+                       (map :pos)
+                       rest
+                       (remove start))]
+    (->> orig-path
+         (map #(with-obstruction grid %))
+         (map guard-path)
+         (filter loop?)
+         count)))
+
+(defn distinct-guard-positions
+  [grid]
+  (count (set (map :pos (guard-path grid)))))
 
 ;; Puzzle solutions
 (defn part1
   [input]
-  (count (set (guard-path input))))
+  (distinct-guard-positions input))
--- test/aoc_clj/2024/day06_test.clj
+++ test/aoc_clj/2024/day06_test.clj
@@ -16,8 +16,33 @@
     "#........."
     "......#..."]))
 
+(deftest distinct-guard-positions-test
+  (testing "Counts the number of unique positions visited by the guard"
+    (is (= 41 (d06/distinct-guard-positions d06-s00)))))
+
+
+
 (def day06-input (u/parse-puzzle-input d06/parse 2024 6))
 
 (deftest part1-test
   (testing "Reproduces the answer for day06, part1"
     (is (= 4656 (d06/part1 day06-input)))))
+
+
+
+;; (->> (d06/guard-start d06-s00)
+;;      (d06/next-move d06-s00)
+;;      (d06/next-move d06-s00)
+;;      (d06/next-move d06-s00)
+;;      (d06/next-move d06-s00)
+;;      (d06/next-move d06-s00)
+;;      (d06/next-move d06-s00)
+;;     ;;  (d06/next-move d06-s00)
+;;      )
+
+;; (:loop? (last (d06/guard-path
+;;                     (assoc-in d06-s00 [:grid [3 3]] :wall))))
+
+;; (d06/looping-guard-paths d06-s00)
+
+;; (rest (:path (d06/guard-path d06-s00)))
