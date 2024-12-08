8ceda4366fa154e19dc7baf0f6e5dc56588365f3
Ken Smith <ken@klsmith.me>
2024-12-08 14:39:34 +0000

    Found a better data representation

--- src/aoc_clj/2024/day08.clj
+++ src/aoc_clj/2024/day08.clj
@@ -1,8 +1,15 @@
 (ns aoc-clj.2024.day08
   "Solution to https://adventofcode.com/2024/day/8"
-  (:require [aoc-clj.utils.grid.mapgrid :as mg]))
+  (:require [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.mapgrid :as mg]))
 
 ;; Input parsing
 (defn parse
   [input]
-  (mg/ascii->MapGrid2D identity input))
+  (let [{:keys [width height grid]} (mg/ascii->MapGrid2D identity input)]
+    {:width width
+     :height height
+     :antennae (->> grid
+                    (remove #(= \. (val %)))
+                    (group-by val)
+                    (u/fmap #(into #{} (map first %))))}))
