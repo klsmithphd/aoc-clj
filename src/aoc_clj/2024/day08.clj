babb534c937bcfac7bda89eca74dd1274fe7540f
Ken Smith <ken@klsmith.me>
2024-12-08 15:17:00 +0000

    Part1 soln

--- src/aoc_clj/2024/day08.clj
+++ src/aoc_clj/2024/day08.clj
@@ -1,7 +1,9 @@
 (ns aoc-clj.2024.day08
   "Solution to https://adventofcode.com/2024/day/8"
-  (:require [aoc-clj.utils.core :as u]
-            [aoc-clj.utils.grid.mapgrid :as mg]))
+  (:require [clojure.math.combinatorics :as combo]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.utils.grid.mapgrid :as mg]
+            [aoc-clj.utils.vectors :as v]))
 
 ;; Input parsing
 (defn parse
@@ -13,3 +15,32 @@
                     (remove #(= \. (val %)))
                     (group-by val)
                     (u/fmap #(into #{} (map first %))))}))
+
+;; Puzzle logic
+(defn in-bounds?
+  [width height [x y]]
+  (and (< -1 x width) (< -1 y height)))
+
+(defn pair-antinodes
+  [[[x1 y1 :as a] [x2 y2 :as b]]]
+  (let [diff [(- x2 x1) (- y2 y1)]]
+    [(v/vec-add b diff)
+     (v/vec-add a (v/scalar-mult diff -1))]))
+
+(defn antenna-antinodes
+  [width height [_ locs]]
+  (->> (combo/combinations locs 2)
+       (mapcat pair-antinodes)
+       (filter #(in-bounds? width height %))
+       set))
+
+(defn all-antinodes
+  [{:keys [width height antennae]}]
+  (->> antennae
+       (mapcat #(antenna-antinodes width height %))
+       set))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (count (all-antinodes input)))
--- test/aoc_clj/2024/day08_test.clj
+++ test/aoc_clj/2024/day08_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day08-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day08 :as d08]))
 
 (def d08-s00-raw
@@ -26,3 +27,32 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d08-s00 (d08/parse d08-s00-raw)))))
+
+(deftest pair-antinodes-test
+  (testing "Returns the antinode locations for a pair of antennae"
+    (is (= #{[3 8] [6 2]}
+           (set (d08/pair-antinodes [[5 4] [4 6]]))))
+
+    (is (= #{[0 7] [12 4]}
+           (set (d08/pair-antinodes [[4 6] [8 5]]))))))
+
+(deftest antenna-antinodes-test
+  (testing "Returns the antinodes within the map bounds for an antenna type"
+    (is (= #{[0 4] [3 5] [1 6] [6 6] [9 7] [2 8] [10 9] [3 10] [6 11] [11 11]}
+           (d08/antenna-antinodes 12 12 [\0 #{[8 10] [5 9] [7 8] [4 7]}])))
+
+    (is (= #{[10 0] [10 1] [7 4] [4 9] [3 10]}
+           (d08/antenna-antinodes 12 12 [\A #{[9 2] [8 3] [6 6]}])))))
+
+(deftest all-antinodes-test
+  (testing "Returns the set of unique antinodes in the map bounds for all
+            antennae"
+    (is (= #{[0 4] [3 5] [1 6] [6 6] [9 7] [2 8] [10 9]
+             [3 10] [6 11] [11 11] [10 0] [10 1] [7 4] [4 9]}
+           (d08/all-antinodes d08-s00)))))
+
+(def day08-input (u/parse-puzzle-input d08/parse 2024 8))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day08, part1"
+    (is (= 276 (d08/part1 day08-input)))))
