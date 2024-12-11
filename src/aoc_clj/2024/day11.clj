2e9389f4fdc39b09abfc47dfddc08d05fed765f9
Ken Smith <ken@klsmith.me>
2024-12-11 11:57:39 +0000

    Part1 soln

--- src/aoc_clj/2024/day11.clj
+++ src/aoc_clj/2024/day11.clj
@@ -1,8 +1,37 @@
 (ns aoc-clj.2024.day11
   "Solution to https://adventofcode.com/2024/day/11"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math :as math]))
 
 ;; Input parsing
 (defn parse
   [input]
-  (str/split (first input) #" "))
+  (map read-string (str/split (first input) #" ")))
+
+;; Puzzle logic
+(defn stone-update
+  [stone]
+  (if (zero? stone)
+    [1]
+    (let [digits (count (str stone))
+          div    (long (math/pow 10 (/ digits 2)))]
+      (if (even? digits)
+        [(quot stone div) (mod stone div)]
+        [(* 2024 stone)]))))
+
+(defn blink
+  [stones]
+  (mapcat stone-update stones))
+
+(defn stones-after-n-blinks
+  [n stones]
+  (->> stones
+       (iterate blink)
+       (drop n)
+       first
+       count))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (stones-after-n-blinks 25 input))
--- test/aoc_clj/2024/day11_test.clj
+++ test/aoc_clj/2024/day11_test.clj
@@ -1,12 +1,36 @@
 (ns aoc-clj.2024.day11-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day11 :as d11]))
 
 (def d11-s00-raw ["0 1 10 99 999"])
 
-(def d11-s00 ["0" "1" "10" "99" "999"])
-(def d11-s01 ["125" "17"])
+(def d11-s00 [0 1 10 99 999])
+(def d11-s01 [125 17])
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d11-s00 (d11/parse d11-s00-raw)))))
+
+(deftest blink-test
+  (testing "Updates all the stones per the rules"
+    (is (= [1 2024 1 0 9 9 2021976]
+           (d11/blink d11-s00)))
+
+    (is (= [253000 1 7]
+           (d11/blink d11-s01)))
+
+    (is (= [253 0 2024 14168]
+           (d11/blink
+            [253000 1 7])))))
+
+(deftest stones-after-n-blinks-test
+  (testing "The number of stones after n blinks"
+    (is (= 22 (d11/stones-after-n-blinks 6 d11-s01)))
+    (is (= 55312 (d11/stones-after-n-blinks 25 d11-s01)))))
+
+(def day11-input (u/parse-puzzle-input d11/parse 2024 11))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day11, part1"
+    (is (= 186996 (d11/part1 day11-input)))))
