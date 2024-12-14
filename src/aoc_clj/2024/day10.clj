c977fb4d4cbf56fd15eac72d696e0331f4ccd2fb
Ken Smith <ken@klsmith.me>
2024-12-14 20:19:32 +0000

    Part1 soln

--- src/aoc_clj/2024/day10.clj
+++ src/aoc_clj/2024/day10.clj
@@ -1,7 +1,25 @@
 (ns aoc-clj.2024.day10
   "Solution to https://adventofcode.com/2024/day/10"
-  (:require [aoc-clj.utils.grid :as grid]
+  (:require [aoc-clj.utils.graph :as graph :refer [Graph]]
+            [aoc-clj.utils.grid :as grid]
             [aoc-clj.utils.grid.vecgrid :as vg]))
+
+;; Records
+(defrecord GridGraph [grid]
+  Graph
+  (vertices
+    [_]
+    (grid/pos-seq grid))
+
+  (edges
+    [_ v]
+    (->> (grid/adj-coords-2d v)
+         (filter #(number? (grid/value grid %)))
+         (filter #(= 1 (- (grid/value grid %) (grid/value grid v))))))
+
+  (distance
+    [_ _ _]
+    1))
 
 ;; Input parsing
 (defn parse
@@ -10,7 +28,35 @@
 
 ;; Puzzle logic
 (defn trailheads
+  "Returns a set of all the possible trailheads"
   [grid]
   (->> (grid/pos-seq grid)
        (filter #(= 0 (grid/value grid %)))
        set))
+
+(defn finish?
+  "Returns true when reaching the finish state (a height of 9)"
+  [grid pos]
+  (= 9 (grid/value grid pos)))
+
+(defn score
+  "A trailhead's score is the number of unique summits that can be reached
+   via a hiking trail "
+  [grid trailhead]
+  (->> (graph/all-paths-dfs (->GridGraph grid) trailhead (partial finish? grid))
+       (map last)
+       set
+       count))
+
+(defn trailhead-score-sum
+  "Returns the sum of the scores from all the trailheads contained in the grid"
+  [grid]
+  (->> (trailheads grid)
+       (map #(score grid %))
+       (reduce +)))
+
+;; Puzzle solutions
+(defn part1
+  "What is the sum of the scores of all trailheads on your topographic map?"
+  [input]
+  (trailhead-score-sum input))
--- test/aoc_clj/2024/day10_test.clj
+++ test/aoc_clj/2024/day10_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day10-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day10 :as d10]))
 
 (def d10-s00
@@ -58,3 +59,21 @@
     (is (= #{[1 0] [5 6]} (d10/trailheads d10-s03)))
     (is (= #{[2 0] [4 0] [4 2] [6 4] [2 5] [5 5] [0 6] [6 6] [1 7]}
            (d10/trailheads d10-s04)))))
+
+(deftest score-test
+  (testing "Returns the number of hiking paths from a given trailhead"
+    (is (= 1 (d10/score d10-s00 [0 0])))
+    (is (= 2 (d10/score d10-s01 [3 0])))
+    (is (= 4 (d10/score d10-s02 [3 0])))
+    (is (= 1 (d10/score d10-s03 [1 0])))
+    (is (= 2 (d10/score d10-s03 [5 6])))))
+
+(deftest trailhead-score-sum-test
+  (testing "Returns the sum of the scores of all the trailheads"
+    (is (= 36 (d10/trailhead-score-sum d10-s04)))))
+
+(def day10-input (u/parse-puzzle-input d10/parse 2024 10))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day10, part1"
+    (is (= 468 (d10/part1 day10-input)))))
