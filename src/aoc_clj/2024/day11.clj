7ba4a4212a57988f4b18d2145f56cdbad2ae27a4
Ken Smith <ken@klsmith.me>
2024-12-14 20:50:40 +0000

    Add docstrings and some notes

--- src/aoc_clj/2024/day11.clj
+++ src/aoc_clj/2024/day11.clj
@@ -14,6 +14,7 @@
 
 ;; Puzzle logic
 (defn stone-update
+  "Updates a single stone, returning a collection of either 1 or 2 stones"
   [stone]
   (if (zero? stone)
     [1]
@@ -24,10 +25,12 @@
         [(* 2024 stone)]))))
 
 (defn blink
+  "Returns the new collection of stones after updating all of them"
   [stones]
   (mapcat stone-update stones))
 
 (defn stones-after-n-blinks
+  "Returns the number of stones present after n blinks"
   [n stones]
   (->> stones
        (iterate blink)
@@ -37,9 +40,19 @@
 
 ;; Puzzle solutions
 (defn part1
+  "How many stones will you have after blinking 25 times?"
   [input]
   (stones-after-n-blinks part1-blinks input))
 
 (defn part2
+  "How many stones would you have after blinking a total of 75 times?"
   [input]
   (stones-after-n-blinks part2-blinks input))
+
+
+;; Some thoughts...
+;; Every value has a completely deterministic collection of values that will 
+;; be spawned from it. Many values will recur. So, if we start building up
+;; a collection of how many values will be spawned from a given number up to a certain
+;; depth, we should be able to build up dynamic-programming style the total count
+;; 0 -> 1 -> 2024 -> 20 24 -> 2 0 2 4 -> 4048 1 4048 9096 -> 40 48 2024 40 48 90 96 -> 4 0 4 8 20 24 4 0 4 8 9 0 9 6
