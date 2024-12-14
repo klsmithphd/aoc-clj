f76c3b0aee04866b8dcf7e7052a9b6947e769e69
Ken Smith <ken@klsmith.me>
2024-12-14 22:11:37 +0000

    Part1 WIP

---
+++ src/aoc_clj/2024/day12.clj
@@ -0,0 +1,85 @@
+(ns aoc-clj.2024.day12
+  "Solution to https://adventofcode.com/2024/day/12"
+  (:require [clojure.set :as set]
+            [aoc-clj.utils.grid :as grid]
+            [aoc-clj.utils.grid.mapgrid :as mg]
+            [aoc-clj.utils.core :as u]))
+
+;; Input parsing
+(defn adjacent
+  "Returns a set of the adjacent cells that are part of the same plot"
+  [cells cell]
+  (set/intersection cells (set (grid/adj-coords-2d cell))))
+
+(defn region
+  "Given the cells belonging to a plot and a specific cell, return a set
+   of all the contiguously connected cells"
+  [cells cell]
+  (loop [regn #{cell}
+         front (adjacent cells cell)]
+    (if-not (seq front)
+      regn
+      (recur (into regn front)
+             (remove regn (mapcat #(adjacent cells %) front))))))
+
+(defn regions
+  "Returns all the regions (mutually connected cells)"
+  [cells]
+  (loop [groups #{} yet-ungrouped cells]
+    (if (empty? yet-ungrouped)
+      groups
+      (let [new-group (region cells (first yet-ungrouped))]
+        (recur (conj groups new-group)
+               (remove new-group yet-ungrouped))))))
+
+(defn plots
+  "Returns a map of all the lettered plots, with the plot letter as the key
+   and the continguous coordinate location regions as the value"
+  [{:keys [grid]}]
+  (->> (group-by val grid)
+       (u/fmap #(regions (set (map key %))))))
+
+(defn parse
+  [input]
+  (plots (mg/ascii->MapGrid2D identity input :down true)))
+
+;; Puzzle logic
+(defn area
+  "Computes the area of a plot"
+  [cells]
+  (count cells))
+
+(defn cell-edges
+  "Counts how many perimeter edges a given cell has"
+  [cells cell]
+  (count (set/difference (set (grid/adj-coords-2d cell)) cells)))
+
+(defn perimeter
+  "Computes the perimeter of a plot (including both inner and outer
+   perimeters)"
+  [cells]
+  (->> cells
+       (map #(cell-edges cells %))
+       (reduce +)))
+
+(defn region-price
+  "Returns the price, which is the product of the area and the perimeter"
+  [cells]
+  (* (perimeter cells) (area cells)))
+
+(defn plot-price
+  "Returns the price for a given plot of a type of plant"
+  [[_ regions]]
+  (->> (map region-price regions)
+       (reduce +)))
+
+(defn total-price
+  "Computes the total price for a map of regions"
+  [region-map]
+  (->> (map plot-price region-map)
+       (reduce +)))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (total-price input))
---
+++ test/aoc_clj/2024/day12_test.clj
@@ -0,0 +1,90 @@
+(ns aoc-clj.2024.day12-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2024.day12 :as d12]))
+
+(def d12-s00-raw
+  ["AAAA"
+   "BBCD"
+   "BBCC"
+   "EEEC"])
+
+(def d12-s00
+  {\A #{#{[0 0] [1 0] [2 0] [3 0]}}
+   \B #{#{[0 1] [1 1] [0 2] [1 2]}}
+   \C #{#{[2 1] [2 2] [3 2] [3 3]}}
+   \D #{#{[3 1]}}
+   \E #{#{[0 3] [1 3] [2 3]}}})
+
+(def d12-s01-raw
+  ["OOOOO"
+   "OXOXO"
+   "OOOOO"
+   "OXOXO"
+   "OOOOO"])
+
+(def d12-s01
+  {\X #{#{[1 3]} #{[1 1]} #{[3 3]} #{[3 1]}}
+   \O #{#{[0 0] [1 0] [2 0] [3 0] [4 0]
+          [0 1]       [2 1]       [4 1]
+          [0 2] [1 2] [2 2] [3 2] [4 2]
+          [0 3]       [2 3]       [4 3]
+          [0 4] [1 4] [2 4] [3 4] [4 4]}}})
+
+(def d12-s02
+  (d12/parse
+   ["RRRRIICCFF"
+    "RRRRIICCCF"
+    "VVRRRCCFFF"
+    "VVRCCCJFFF"
+    "VVVVCJJCFE"
+    "VVIVCCJJEE"
+    "VVIIICJJEE"
+    "MIIIIIJJEE"
+    "MIIISIJEEE"
+    "MMMISSJEEE"]))
+
+(deftest parse-test
+  (testing "Returns the plot sets for a given field"
+    (is (= d12-s00 (d12/parse d12-s00-raw)))
+    (is (= d12-s01 (d12/parse d12-s01-raw)))))
+
+(deftest area-test
+  (testing "Computes the area of a given plot"
+    (is (= 4  (d12/area (first (get d12-s00 \A)))))
+    (is (= 21 (d12/area (first (get d12-s01 \O)))))
+    (is (= 1  (d12/area (first (get d12-s01 \X)))))))
+
+(deftest perimeter-test
+  (testing "Computes the perimeter of a given plot"
+    (is (= 10 (d12/perimeter (first (get d12-s00 \A)))))
+    (is (= 8  (d12/perimeter (first (get d12-s00 \B)))))
+    (is (= 10 (d12/perimeter (first (get d12-s00 \C)))))
+    (is (= 4  (d12/perimeter (first (get d12-s00 \D)))))
+    (is (= 8  (d12/perimeter (first (get d12-s00 \E)))))
+
+    (is (= 4  (d12/perimeter (first (get d12-s01 \X)))))
+    (is (= 36 (d12/perimeter (first (get d12-s01 \O)))))))
+
+(deftest region-price-test
+  (testing "Computes the price of a given plot"
+    (is (= 40 (d12/region-price (first (get d12-s00 \A)))))
+    (is (= 32 (d12/region-price (first (get d12-s00 \B)))))
+    (is (= 40 (d12/region-price (first (get d12-s00 \C)))))
+    (is (= 4  (d12/region-price (first (get d12-s00 \D)))))
+    (is (= 24 (d12/region-price (first (get d12-s00 \E)))))
+
+    (is (= 756 (d12/region-price (first (get d12-s01 \O)))))
+    (is (= 4   (d12/region-price (first (get d12-s01 \X)))))))
+
+(deftest total-price-test
+  (testing "Computes the total price for the entire field"
+    (is (= 140  (d12/total-price d12-s00)))
+    (is (= 772  (d12/total-price d12-s01)))
+    (is (= 1930 (d12/total-price d12-s02)))))
+
+;; (def day12-input (u/parse-puzzle-input d12/parse 2024 12))
+
+;; (deftest part1-test
+;;   (testing "Reproduces the answer for day12, part1"
+;;     (is (= 186996 (d12/part1 day12-input)))))
