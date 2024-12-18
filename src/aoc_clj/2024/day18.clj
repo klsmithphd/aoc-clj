5cc2ec9f40d57488347c9d01f6d36d9133fc8b7b
Ken Smith <ken@klsmith.me>
2024-12-18 12:22:15 +0000

    Part1 soln

--- src/aoc_clj/2024/day18.clj
+++ src/aoc_clj/2024/day18.clj
@@ -1,5 +1,35 @@
 (ns aoc-clj.2024.day18
-  "Solution to https://adventofcode.com/2024/day/18")
+  "Solution to https://adventofcode.com/2024/day/18"
+  (:require [aoc-clj.utils.graph :as graph :refer [Graph]]
+            [aoc-clj.utils.grid :as grid]))
+
+;; Constants
+(def grid-size 71)
+(def byte-limit 1024)
+
+;; Records
+(defrecord MoveGraph [size corrupted]
+  Graph
+  (vertices
+    [_]
+    (->> (for [y (range size)
+               x (range size)]
+           [x y])
+         (remove corrupted)))
+
+  (edges
+    [_ [x y :as v]]
+    (if (nil? v)
+      nil
+      (->> (grid/adj-coords-2d [x y])
+           (filter (fn [[a b]]
+                     (and (< -1 a size)
+                          (< -1 b size))))
+           (remove corrupted))))
+
+  (distance
+    [_ _ _]
+    1))
 
 ;; Input parsing
 (defn parse-line
@@ -9,3 +39,16 @@
 (defn parse
   [input]
   (map parse-line input))
+
+;; Puzzle logic
+(defn shortest-path
+  [size limit bytes]
+  (let [graph   (->MoveGraph size (set (take limit bytes)))
+        start   [0 0]
+        finish? #(= [(dec size) (dec size)] %)]
+    (dec (count (graph/dijkstra graph start finish?)))))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (shortest-path grid-size byte-limit input))
--- test/aoc_clj/2024/day18_test.clj
+++ test/aoc_clj/2024/day18_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day18-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day18 :as d18]))
 
 (def d18-s00-raw
@@ -59,3 +60,15 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d18-s00 (d18/parse d18-s00-raw)))))
+
+(deftest shortest-path-test
+  (testing "Finds the length of the shortest path"
+    (is (= 22 (d18/shortest-path 7 12 d18-s00)))
+    (is (= 24 (d18/shortest-path 7 20 d18-s00)))
+    (is (= -1 (d18/shortest-path 7 21 d18-s00)))))
+
+(def day18-input (u/parse-puzzle-input d18/parse 2024 18))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day18, part1"
+    (is (= 340 (d18/part1 day18-input)))))
