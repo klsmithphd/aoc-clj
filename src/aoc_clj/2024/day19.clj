87b059f84e9a36ed1cc6c4c11b7d2212ebd3bbb9
Ken Smith <ken@klsmith.me>
2024-12-21 21:17:20 +0000

    Part1 WIP. Too slow to use.

--- src/aoc_clj/2024/day19.clj
+++ src/aoc_clj/2024/day19.clj
@@ -9,3 +9,40 @@
   (let [[towels patterns] (u/split-at-blankline input)]
     {:towels (str/split (first towels) #", ")
      :patterns patterns}))
+
+;; Puzzle logic
+(defn possible-helper?
+  [towels pattern]
+  (if (empty? pattern)
+    [true]
+    (if-let [matches (filter #(str/starts-with? pattern %) towels)]
+      (->> matches
+           (map #(subs pattern (count %)))
+           (mapcat #(possible-helper? towels %)))
+      [false])))
+
+(defn possible?
+  [towels pattern]
+  (boolean (not-empty (possible-helper? towels pattern))))
+
+(defn possible-count
+  [{:keys [towels patterns]}]
+  (->> patterns
+       (filter #(possible? towels %))
+       count))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (possible-count input))
+
+
+;; r -> ["" b]
+;; w -> r
+;; b -> ["" r {w [u]}]
+;; g -> ["" b]
+
+
+;; gur, uub, wgwu, rggu, urguur
+
+;; (str/starts-with?)
--- test/aoc_clj/2024/day19_test.clj
+++ test/aoc_clj/2024/day19_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day19-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day19 :as d19]))
 
 (def d19-s00-raw
@@ -28,3 +29,24 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d19-s00 (d19/parse d19-s00-raw)))))
+
+(deftest possible?-test
+  (testing "Determines whether the pattern can be created from the available towels"
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 0))))
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 1))))
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 2))))
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 3))))
+    (is (= false (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 4))))
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 5))))
+    (is (= true  (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 6))))
+    (is (= false (d19/possible? (:towels d19-s00) (nth (:patterns d19-s00) 7))))))
+
+(deftest possible-count-test
+  (testing "Counts the number of possible towel patterns that can be made"
+    (is (= 6 (d19/possible-count d19-s00)))))
+
+(def day19-input (u/parse-puzzle-input d19/parse 2024 19))
+
+(deftest ^:slow part1-test
+  (testing "Reproduces the answer for day19, part1"
+    (is (= 340 (d19/part1 day19-input)))))
