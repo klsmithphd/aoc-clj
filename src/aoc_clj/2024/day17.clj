07fb9d6c5f9273115ce240168b8963e306018cef
Ken Smith <ken@klsmith.me>
2024-12-21 15:03:03 +0000

    Part1 soln

--- src/aoc_clj/2024/day17.clj
+++ src/aoc_clj/2024/day17.clj
@@ -1,6 +1,7 @@
 (ns aoc-clj.2024.day17
   "Solution to https://adventofcode.com/2024/day/17"
-  (:require [aoc-clj.utils.core :as u]))
+  (:require [clojure.string :as str]
+            [aoc-clj.utils.core :as u]))
 
 ;; Input parsing
 (defn get-nums
@@ -10,16 +11,119 @@
 (defn parse
   [input]
   (let [[reg-str prog-str] (u/split-at-blankline input)]
-    {:regs (mapcat get-nums reg-str)
+    {:regs (zipmap [:a :b :c] (mapcat get-nums reg-str))
      :prog (vec (get-nums (first prog-str)))}))
 
 ;; Puzzle logic
-(defn read-operand
+(def combo-map
+  {4 :a
+   5 :b
+   6 :c})
+
+(defn combo-operand
+  "Returns the combo operand value.
+   Combo operands 0-3 represent the literal values 0 through 3
+   Combo operands 4, 5, and 6 map to register A, B, and C, respectively."
   [regs operand]
   (if (<= operand 3)
     operand
-    (get regs (- operand 4))))
+    (regs (combo-map operand))))
 
-(defn adv
-  [regs operand]
-  ())
+(defn inc-ip
+  "Increases the instruction pointer by 2"
+  [state]
+  (update state :ip + 2))
+
+(defn division
+  "Computes a truncated division operation, with register A's value in the
+   numerator and saving the result in the register given by `store`.
+   
+   The divisor is 2 to the power of the combo operand"
+  [store {:keys [regs] :as state} operand]
+  (let [two-pow (bit-shift-left 1 (combo-operand regs operand))]
+    (-> state
+        (assoc-in [:regs store] (quot (regs :a) two-pow))
+        inc-ip)))
+
+(def adv (partial division :a))
+(def bdv (partial division :b))
+(def cdv (partial division :c))
+
+(defn bxl
+  "Bitwise XOR of register B and literal operand, stored in B"
+  [{:keys [regs] :as state} operand]
+  (-> state
+      (assoc-in [:regs :b] (bit-xor (regs :b) operand))
+      inc-ip))
+
+(defn bst
+  "Combo operand mod 8, stored in B"
+  [{:keys [regs] :as state} operand]
+  (-> state
+      (assoc-in [:regs :b] (mod (combo-operand regs operand) 8))
+      inc-ip))
+
+(defn jnz
+  "Do nothing if register A is zero, else set instruction pointer to
+   value of literal operand"
+  [{:keys [regs] :as state} operand]
+  (if (zero? (regs :a))
+    (inc-ip state)
+    (assoc state :ip operand)))
+
+(defn bxc
+  "Bitwise XOR of register B and register C, stored in B"
+  [{:keys [regs] :as state} _]
+  (-> state
+      (assoc-in [:regs :b] (bit-xor (regs :b) (regs :c)))
+      inc-ip))
+
+(defn out
+  "Combo operand mod 8, appended to output"
+  [{:keys [regs] :as state} operand]
+  (-> state
+      (update :out conj (mod (combo-operand regs operand) 8))
+      inc-ip))
+
+(def opcode-map
+  {0 adv
+   1 bxl
+   2 bst
+   3 jnz
+   4 bxc
+   5 out
+   6 bdv
+   7 cdv})
+
+(defn step
+  "Increments the program state by one step, based on the current
+   instruction pointer `ip`"
+  [{:keys [ip prog] :as state}]
+  (let [[opcode operand] (take 2 (drop ip prog))]
+    ((opcode-map opcode) state operand)))
+
+(defn running?
+  "Returns true while the instruction pointer is in bounds of the program"
+  [{:keys [ip prog]}]
+  (< -1 ip (count prog)))
+
+(defn init-state
+  "Returns the initial state of the program"
+  [input]
+  (into input {:ip 0 :out []}))
+
+(defn execute
+  "Executes the instructions in the program until the instruction pointer
+   is out of bounds of the instructions and returns the final state"
+  [input]
+  (->> (init-state input)
+       (iterate step)
+       (drop-while running?)
+       first))
+
+;; Puzzle solutions
+(defn part1
+  "Once it halts, what do you get if you use commas to join the values it
+   output into a single string?"
+  [input]
+  (str/join "," (:out (execute input))))
--- test/aoc_clj/2024/day17_test.clj
+++ test/aoc_clj/2024/day17_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day17-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day17 :as d17]))
 
 (def d17-s00-raw
@@ -10,9 +11,69 @@
    "Program: 0,1,5,4,3,0"])
 
 (def d17-s00
-  {:regs [729 0 0]
+  {:regs {:a 729 :b 0 :c 0}
    :prog [0 1 5 4 3 0]})
+
+(def d17-s01
+  {:regs {:a 0 :b 0 :c 9}
+   :prog [2 6]})
+
+(def d17-s02
+  {:regs {:a 10 :b 0 :c 0}
+   :prog [5 0 5 1 5 4]})
+
+(def d17-s03
+  {:regs {:a 2024 :b 0 :c 0}
+   :prog [0 1 5 4 3 0]})
+
+(def d17-s04
+  {:regs {:a 0 :b 29 :c 0}
+   :prog [1 7]})
+
+(def d17-s05
+  {:regs {:a 0 :b 2024 :c 43690}
+   :prog [4 0]})
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d17-s00 (d17/parse d17-s00-raw)))))
+
+(deftest execute-test
+  (testing "Returns the expected result for the program"
+    (is (= (-> (d17/init-state d17-s00)
+               (assoc-in [:regs :a] 0)
+               (assoc :out [4 6 3 5 6 3 5 2 1 0])
+               (assoc :ip 6))
+           (d17/execute d17-s00)))
+
+    (is (= (-> (d17/init-state d17-s01)
+               (assoc-in [:regs :b] 1)
+               (assoc :ip 2))
+           (d17/execute d17-s01)))
+
+    (is (= (-> (d17/init-state d17-s02)
+               (assoc :out [0 1 2])
+               (assoc :ip 6))
+           (d17/execute d17-s02)))
+
+    (is (= (-> (d17/init-state d17-s03)
+               (assoc-in [:regs :a] 0)
+               (assoc :out [4 2 5 6 7 7 7 7 3 1 0])
+               (assoc :ip 6))
+           (d17/execute d17-s03)))
+
+    (is (= (-> (d17/init-state d17-s04)
+               (assoc-in [:regs :b] 26)
+               (assoc :ip 2))
+           (d17/execute d17-s04)))
+
+    (is (= (-> (d17/init-state d17-s05)
+               (assoc-in [:regs :b] 44354)
+               (assoc :ip 2))
+           (d17/execute d17-s05)))))
+
+(def day17-input (u/parse-puzzle-input d17/parse 2024 17))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day17, part1"
+    (is (= "1,4,6,1,6,4,3,0,3" (d17/part1 day17-input)))))
