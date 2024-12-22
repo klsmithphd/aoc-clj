af92163702d229e1208510e9ae250bc64c7e418c
Ken Smith <ken@klsmith.me>
2024-12-22 15:50:10 +0000

    Part 1 soln

--- src/aoc_clj/2024/day22.clj
+++ src/aoc_clj/2024/day22.clj
@@ -1,7 +1,56 @@
 (ns aoc-clj.2024.day22
   "Solution to https://adventofcode.com/2024/day/22")
 
+;; Constants
+(def part1-n 2000)
+
 ;; Input parsing
 (defn parse
   [input]
   (map read-string input))
+
+;; Puzzle logic
+(defn mix
+  "Mixing is defined as taking this bitwise XOR of a value into the number"
+  [num mix-val]
+  (bit-xor num mix-val))
+
+(defn prune
+  "Pruning is defined as the value of the number modulo 16777216"
+  [num]
+  (mod num 16777216))
+
+(defn op-mix-and-prune
+  [op arg num]
+  (->> (op num arg)
+       (mix num)
+       prune))
+
+(defn next-secret
+  "Returns the next secret in the sequence from the current num"
+  [num]
+  (->> num
+       (op-mix-and-prune bit-shift-left 6)  ;; Multiply by 64
+       (op-mix-and-prune bit-shift-right 5) ;; Truncate-divide by 32
+       (op-mix-and-prune bit-shift-left 11) ;; Multiply by 2048
+       ))
+
+(defn secret-seq
+  "Returns a lazy seq of the next values of the secret"
+  [num]
+  (iterate next-secret num))
+
+(defn secret-at-n
+  [n num]
+  (first (drop n (secret-seq num))))
+
+(defn secrets-at-n-sum
+  [n nums]
+  (->> nums
+       (map #(secret-at-n n %))
+       (reduce +)))
+
+;; Puzzle solutions
+(defn part1
+  [input]
+  (secrets-at-n-sum part1-n input))
--- test/aoc_clj/2024/day22_test.clj
+++ test/aoc_clj/2024/day22_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day22-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day22 :as d22]))
 
 (def d22-s00-raw
@@ -14,3 +15,32 @@
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d22-s00 (d22/parse d22-s00-raw)))))
+
+(deftest secret-seq-test
+  (testing "Returns the next values in the secret sequence"
+    (is (= [123
+            15887950
+            16495136
+            527345
+            704524
+            1553684
+            12683156
+            11100544
+            12249484
+            7753432
+            5908254]
+           (take 11 (d22/secret-seq 123))))))
+
+(deftest secret-at-n-test
+  (testing "Returns the Nth new secret number"
+    (is (= 8685429 (d22/secret-at-n 2000 1)))))
+
+(deftest secret-at-n-sum
+  (testing "Computes the sum of all the Nth new secret numbers"
+    (is (= 37327623 (d22/secrets-at-n-sum 2000 d22-s00)))))
+
+(def day22-input (u/parse-puzzle-input d22/parse 2024 22))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day22, part1"
+    (is (= 18525593556 (d22/part1 day22-input)))))
