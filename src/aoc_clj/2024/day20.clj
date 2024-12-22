1972e41b07ffc0cc9507cdd099cb09bfcffe2383
Ken Smith <ken@klsmith.me>
2024-12-22 20:30:06 +0000

    Part1 soln

--- src/aoc_clj/2024/day20.clj
+++ src/aoc_clj/2024/day20.clj
@@ -3,6 +3,9 @@
   (:require [aoc-clj.utils.grid :as grid]
             [aoc-clj.utils.grid.mapgrid :as mg]))
 
+;; Constants
+(def part1-savings 100)
+
 ;; Input parsing
 (def charmap
   {\# :wall
@@ -13,3 +16,64 @@
 (defn parse
   [input]
   (mg/ascii->MapGrid2D charmap input))
+
+;; Puzzle logic
+(def start-node (comp first (partial grid/find-nodes :start)))
+
+(defn extend-path
+  "Examine the neighborhood around the last node on the path and add
+   the next open cell to the path."
+  [grid path]
+  (let [newnode (->> (grid/adj-coords-2d (peek path))
+                     (filter #(not= :wall (grid/value grid %)))
+                     (filter #(not= (peek (pop path)) %))
+                     first)]
+    (conj path newnode)))
+
+(defn full-path
+  "Returns a map of the full maze path, with the keys being the coordinate
+   location of each step and the value being its order from 0..N along
+   the path"
+  [grid]
+  (->> [(start-node grid)]
+       (iterate #(extend-path grid %))
+       (take-while #(some? (peek %)))
+       last
+       (map-indexed #(vector %2 %1))
+       (into {})))
+
+(defn cheat-shifts
+  [[x y]]
+  [[(+ x 2) y]
+   [(- x 2) y]
+   [x (+ y 2)]
+   [x (- y 2)]])
+
+(defn cheat-savings
+  "Computes the possible cheat savings options for a given node along the
+   full path"
+  [fullpath [node idx]]
+  (->> (cheat-shifts node)
+       (map #(- (get fullpath % -1) (+ idx 2)))
+       (filter pos?)))
+
+(defn cheat-freqs
+  "Computes a histogram map of how many cheats result in what savings"
+  [grid]
+  (let [fullpath (full-path grid)]
+    (->> (mapcat #(cheat-savings fullpath %) fullpath)
+         frequencies)))
+
+(defn cheats-more-than
+  "Computes the number of cheats that are more than a given number of picoseconds"
+  [n grid]
+  (let [fullpath (full-path grid)]
+    (->> (mapcat #(cheat-savings fullpath %) fullpath)
+         (filter #(>= % n))
+         count)))
+
+;; Puzzle solutions
+(defn part1
+  "How many cheats would save you at least 100 picoseconds?"
+  [input]
+  (cheats-more-than part1-savings input))
--- test/aoc_clj/2024/day20_test.clj
+++ test/aoc_clj/2024/day20_test.clj
@@ -1,5 +1,7 @@
 (ns aoc-clj.2024.day20-test
-  (:require [aoc-clj.2024.day20 :as d20]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
+            [aoc-clj.2024.day20 :as d20]))
 
 (def d20-s00
   (d20/parse
@@ -18,3 +20,36 @@
     "#.#.#.#.#.#.###"
     "#...#...#...###"
     "###############"]))
+
+(deftest full-path-test
+  (testing "Finds the full set of positions from start to finish"
+    (is (= 85 (count (d20/full-path d20-s00))))))
+
+(deftest cheat-savings-test
+  (testing "Finds how much time can be saved by various cheats"
+    (let [fullpath (d20/full-path d20-s00)]
+      (is (= [4] (d20/cheat-savings fullpath [[1 11] 0])))
+      (is (= [12] (d20/cheat-savings fullpath [[7 13] 12])))
+      (is (= [20 36] (d20/cheat-savings fullpath [[9 7] 20])))
+      (is (= [64 40] (d20/cheat-savings fullpath [[7 7] 18]))))))
+
+(deftest cheat-freqs-test
+  (testing "Computes a histogram of the possible cheat savings opportunities"
+    (is (= {2 14
+            4 14
+            6 2
+            8 4
+            10 2
+            12 3
+            20 1
+            36 1
+            38 1
+            40 1
+            64 1}
+           (d20/cheat-freqs d20-s00)))))
+
+(def day20-input (u/parse-puzzle-input d20/parse 2024 20))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day20, part1"
+    (is (= 1402 (d20/part1 day20-input)))))
