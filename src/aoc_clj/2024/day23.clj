0019aff1eb6c471230b2473877c09e8ff665b6a8
Ken Smith <ken@klsmith.me>
2024-12-23 16:22:32 +0000

    Part2 soln

--- src/aoc_clj/2024/day23.clj
+++ src/aoc_clj/2024/day23.clj
@@ -1,32 +1,24 @@
 (ns aoc-clj.2024.day23
   "Solution to https://adventofcode.com/2024/day/23"
-  (:require [clojure.set :as set]
-            [clojure.math.combinatorics :as combo]
-            [clojure.string :as str]))
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 ;; Input parsing
 (defn parse-line
   [line]
-  {(subs line 0 2) #{(subs line 3)}})
+  (let [n1 (subs line 0 2)
+        n2 (subs line 3)]
+    ;; Because the graph is undirected, we emit the edges
+    ;; in both directions
+    [{n1 #{n2}} {n2 #{n1}}]))
 
 (defn parse
   [input]
-  (->> (map parse-line input)
+  (->> (mapcat parse-line input)
        (apply merge-with into)))
 
 ;; Puzzle logic
-(defn symmetrize
-  "Update the graph such that all the nodes connected to node k
-   also have node k contained among their connections"
-  [graph [k nodes]]
-  (reduce #(update %1 %2 conj k) graph nodes))
-
-(defn symmetric
-  "Update the graph such that all node connections appear in
-   both directions"
-  [graph]
-  (reduce symmetrize graph graph))
-
 (defn node-triples
   "For two nodes and their respective sets of connected nodes,
    return all the possible mutually connected node triples"
@@ -54,13 +46,66 @@
    node starts with the letter `t`"
   [graph]
   (->> graph
-       symmetric
        three-node-networks
        (filter contains-t?)
        count))
+
+(defn maximal-intersection
+  "Returns the neighbor and the set of mutually adjacent neighbors
+   with the largest amount of overlap"
+  [graph neighbors]
+  (->> neighbors
+       ;; We look for the set intersection of the current set of
+       ;; neighbors, and the neighbors of each node within that set
+       (map #(vector % (set/intersection (disj neighbors %) (graph %))))
+       ;; Then pick the node-intersection combo that has the most elements
+       ;; That's the largest addition to our candidate clique we can find.
+       (apply max-key (comp count second))))
+
+(defn clique-for-node
+  "Finds the largest clique that includes the given node"
+  [graph node]
+  ;; We add the node itself to our candidate clique first,
+  ;; and then look at its neighbors
+  (loop [clique    #{node}
+         neighbors (graph node)]
+    (if (empty? neighbors)
+      clique
+      ;; We expand the clique by the neighbor with the greatest
+      ;; number of mutually reachable next-neighbors
+      ;; Those mutually reachable next-neighbors then become the
+      ;; next group to consider for further expansion
+      (let [[nxt others] (maximal-intersection graph neighbors)]
+        (recur (conj clique nxt) others)))))
+
+(defn cliques
+  "Returns a set of the possible cliques discovered within the graph"
+  [graph]
+  (loop [explore-set (set (keys graph))
+         cliques     #{}]
+    (if (empty? explore-set)
+      cliques
+      (let [clique (clique-for-node graph (first explore-set))]
+        (recur (set/difference explore-set clique)
+               (conj cliques clique))))))
+
+(defn largest-clique
+  "Returns the largest clique (mutually interconnected subgraph)"
+  [graph]
+  (apply max-key count (cliques graph)))
+
+(defn password
+  "Returns the password: nodes sorted alphabetically and joined by commas"
+  [nodes]
+  (str/join "," (sort nodes)))
 
 ;; Puzzle solutions
 (defn part1
   "How many contain at least one computer with a name that starts with t?"
   [input]
   (three-node-networks-with-t input))
+
+(defn part2
+  "What is the password to get into the LAN party?"
+  [input]
+  (password (largest-clique input)))
--- test/aoc_clj/2024/day23_test.clj
+++ test/aoc_clj/2024/day23_test.clj
@@ -38,46 +38,26 @@
    "td-yn"])
 
 (def d23-s00
-  {"aq" #{"cg"}
-   "cg" #{"tb"}
-   "co" #{"tc"}
-   "de" #{"cg" "co" "ta"}
-   "ka" #{"co" "de"}
-   "kh" #{"ta" "tc" "ub"}
-   "qp" #{"kh" "ub"}
-   "ta" #{"co" "ka"}
-   "tb" #{"ka" "vc" "wq"}
-   "tc" #{"td"}
-   "td" #{"qp" "yn"}
-   "ub" #{"vc"}
-   "vc" #{"aq"}
-   "wh" #{"qp" "tc" "td" "yn"}
-   "wq" #{"aq" "ub" "vc"}
-   "yn" #{"aq" "cg"}})
+  {"aq" #{"cg" "wq" "vc" "yn"}
+   "cg" #{"tb" "de" "aq" "yn"}
+   "co" #{"de" "tc" "ka" "ta"}
+   "de" #{"cg" "co" "ka" "ta"}
+   "ka" #{"tb" "co" "de" "ta"}
+   "kh" #{"tc" "qp" "ta" "ub"}
+   "qp" #{"kh" "td" "wh" "ub"}
+   "ta" #{"kh" "co" "de" "ka"}
+   "tb" #{"cg" "wq" "vc" "ka"}
+   "tc" #{"kh" "td" "co" "wh"}
+   "td" #{"yn" "tc" "wh" "qp"}
+   "ub" #{"wq" "kh" "vc" "qp"}
+   "vc" #{"wq" "tb" "aq" "ub"}
+   "wh" #{"td" "yn" "tc" "qp"}
+   "wq" #{"tb" "vc" "aq" "ub"}
+   "yn" #{"cg" "td" "aq" "wh"}})
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d23-s00 (d23/parse d23-s00-raw)))))
-
-(deftest symmetric-test
-  (testing "Returns a symmetric representation of the graph"
-    (is (= {"aq" #{"cg" "wq" "vc" "yn"}
-            "cg" #{"tb" "de" "aq" "yn"}
-            "co" #{"de" "tc" "ka" "ta"}
-            "de" #{"cg" "co" "ka" "ta"}
-            "ka" #{"tb" "co" "de" "ta"}
-            "kh" #{"tc" "qp" "ta" "ub"}
-            "qp" #{"kh" "td" "wh" "ub"}
-            "ta" #{"kh" "co" "de" "ka"}
-            "tb" #{"cg" "wq" "vc" "ka"}
-            "tc" #{"kh" "td" "co" "wh"}
-            "td" #{"yn" "tc" "wh" "qp"}
-            "ub" #{"wq" "kh" "vc" "qp"}
-            "vc" #{"wq" "tb" "aq" "ub"}
-            "wh" #{"td" "yn" "tc" "qp"}
-            "wq" #{"tb" "vc" "aq" "ub"}
-            "yn" #{"cg" "td" "aq" "wh"}}
-           (d23/symmetric d23-s00)))))
 
 (deftest three-node-networks
   (testing "Finds all of the three-node networks"
@@ -93,14 +73,24 @@
              #{"tc" "td" "wh"}
              #{"td" "wh" "yn"}
              #{"ub" "vc" "wq"}}
-           (d23/three-node-networks (d23/symmetric d23-s00))))))
+           (d23/three-node-networks d23-s00)))))
 
 (deftest three-node-networks-with-t-test
   (testing "Counts the three-node networks with nodes starting with t"
     (is (= 7 (d23/three-node-networks-with-t d23-s00)))))
+
+(deftest largest-clique-test
+  (testing "Finds the largest subnetwork of mutually interconnected nodes"
+    (is (= #{"co" "de" "ka" "ta"}
+           (d23/largest-clique d23-s00)))))
 
 (def day23-input (u/parse-puzzle-input d23/parse 2024 23))
 
 (deftest part1-test
   (testing "Reproduces the answer for day23, part1"
     (is (= 1075 (d23/part1 day23-input)))))
+
+(deftest part2-test
+  (testing "Reproduces the answer for day23, part2"
+    (is (= "az,cg,ei,hz,jc,km,kt,mv,sv,sx,wc,wq,xy"
+           (d23/part2 day23-input)))))
