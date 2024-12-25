512d2697e2c48c3dc1700fc5e505751d34e0913d
Ken Smith <ken@klsmith.me>
2024-12-25 12:17:46 +0000

    Part 1 soln

--- src/aoc_clj/2024/day25.clj
+++ src/aoc_clj/2024/day25.clj
@@ -1,6 +1,7 @@
 (ns aoc-clj.2024.day25
   "Solution to https://adventofcode.com/2024/day/25"
-  (:require [aoc-clj.utils.core :as u]
+  (:require [clojure.math.combinatorics :as combo]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.utils.grid.vecgrid :as vg]))
 
 ;; Input parsing
@@ -9,8 +10,8 @@
 (defn key-lock-parse
   [lines]
   [(case (ffirst lines)
-     0 :lock
-     1 :key)
+     0 :locks
+     1 :keys)
    (mapv #(reduce + -1 %) lines)])
 
 (defn parse
@@ -18,4 +19,31 @@
   (->> (u/split-at-blankline input)
        (map #(:v (vg/ascii->VecGrid2D charmap %)))
        (map u/transpose)
-       (map key-lock-parse)))
+       (map key-lock-parse)
+       (group-by first)
+       (u/fmap #(map second %))))
+
+;; Puzzle logic
+(defn fit?
+  "A lock and a key pair fit together if the number of pins
+   on the key and the lock don't overlap"
+  [[lock key]]
+  (every? #(<= % 5) (map + lock key)))
+
+(defn lock-key-fits
+  "Returns only lock-key pairs that fit together"
+  [{:keys [locks keys]}]
+  (->> (combo/cartesian-product locks keys)
+       (filter fit?)))
+
+(defn lock-key-fits-count
+  "Returns the count of lock/key pairs that fit together"
+  [input]
+  (count (lock-key-fits input)))
+
+;; Part 1 soln
+(defn part1
+  "How many unique lock/key pairs fit together without overlapping in any
+   column?"
+  [input]
+  (lock-key-fits-count input))
--- test/aoc_clj/2024/day25_test.clj
+++ test/aoc_clj/2024/day25_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2024.day25-test
   (:require [clojure.test :refer [deftest testing is]]
+            [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day25 :as d25]))
 
 (def d25-s00-raw
@@ -44,12 +45,29 @@
    "#####"])
 
 (def d25-s00
-  [[:lock [0 5 3 4 3]]
-   [:lock [1 2 0 5 3]]
-   [:key  [5 0 2 1 3]]
-   [:key  [4 3 4 0 2]]
-   [:key  [3 0 2 0 1]]])
+  {:locks [[0 5 3 4 3]
+           [1 2 0 5 3]]
+   :keys  [[5 0 2 1 3]
+           [4 3 4 0 2]
+           [3 0 2 0 1]]})
 
 (deftest parse-test
   (testing "Correctly parses the input"
     (is (= d25-s00 (d25/parse d25-s00-raw)))))
+
+(deftest lock-key-fits-test
+  (testing "Finds the lock-key combinations that fit"
+    (is (= [[[0 5 3 4 3] [3 0 2 0 1]]
+            [[1 2 0 5 3] [4 3 4 0 2]]
+            [[1 2 0 5 3] [3 0 2 0 1]]]
+           (d25/lock-key-fits d25-s00)))))
+
+(deftest lock-key-fits-count-test
+  (testing "Counts the number of lock-key pairs that fit"
+    (is (= 3 (d25/lock-key-fits-count d25-s00)))))
+
+(def day25-input (u/parse-puzzle-input d25/parse 2024 25))
+
+(deftest part1-test
+  (testing "Reproduces the answer for day25, part1"
+    (is (= 3223 (d25/part1 day25-input)))))
