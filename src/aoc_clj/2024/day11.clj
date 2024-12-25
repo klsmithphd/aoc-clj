bcfba99b4ae9fc3f30d6439f02e4d5f4a4e41a12
Ken Smith <ken@klsmith.me>
2024-12-25 14:23:37 +0000

    Part2 soln

--- src/aoc_clj/2024/day11.clj
+++ src/aoc_clj/2024/day11.clj
@@ -13,30 +13,48 @@
   (map read-string (str/split (first input) #" ")))
 
 ;; Puzzle logic
-(defn stone-update
-  "Updates a single stone, returning a collection of either 1 or 2 stones"
-  [stone]
+(defn even-divisor
+  "If `num` has an even number of digits, returns the divisor to split the
+   number into two equal-sized numbers. Otherwise, returns 0"
+  [num]
+  (let [digits (count (str num))
+        div    (long (math/pow 10 (/ digits 2)))]
+    (if (even? digits)
+      div
+      0)))
+
+;; The number of stones will grow exponentially, so it becomes infeasible
+;; to keep track of a collection of the stones directly. Fortunately, we only
+;; need to know the number of stones available at any given time.
+;; We update a map of the stones to their counts, which might grow large,
+;; but never prohibitively so.
+(defn stone-count-updater
+  "For a given `stone`, and the `cnt` of times seen so far, updates
+   the `new-counts` map after another blink."
+  [new-counts [stone cnt]]
   (if (zero? stone)
-    [1]
-    (let [digits (count (str stone))
-          div    (long (math/pow 10 (/ digits 2)))]
-      (if (even? digits)
-        [(quot stone div) (mod stone div)]
-        [(* 2024 stone)]))))
+    (update new-counts 1 (fnil + 0) cnt)
+    (let [div (even-divisor stone)]
+      (if (pos? div)
+        (-> new-counts
+            (update (quot stone div) (fnil + 0) cnt)
+            (update (mod stone div)  (fnil + 0) cnt))
+        (update new-counts (* 2024 stone) (fnil + 0) cnt)))))
 
 (defn blink
-  "Returns the new collection of stones after updating all of them"
-  [stones]
-  (mapcat stone-update stones))
+  "Updates the map of the counts of each stone after one blink"
+  [stone-counts]
+  (reduce stone-count-updater {} stone-counts))
 
 (defn stones-after-n-blinks
-  "Returns the number of stones present after n blinks"
+  "Returns the count of stones present after n iterations"
   [n stones]
-  (->> stones
+  (->> (frequencies stones)
        (iterate blink)
        (drop n)
        first
-       count))
+       vals
+       (reduce +)))
 
 ;; Puzzle solutions
 (defn part1
@@ -48,11 +66,3 @@
   "How many stones would you have after blinking a total of 75 times?"
   [input]
   (stones-after-n-blinks part2-blinks input))
-
-
-;; Some thoughts...
-;; Every value has a completely deterministic collection of values that will 
-;; be spawned from it. Many values will recur. So, if we start building up
-;; a collection of how many values will be spawned from a given number up to a certain
-;; depth, we should be able to build up dynamic-programming style the total count
-;; 0 -> 1 -> 2024 -> 20 24 -> 2 0 2 4 -> 4048 1 4048 9096 -> 40 48 2024 40 48 90 96 -> 4 0 4 8 20 24 4 0 4 8 9 0 9 6
--- test/aoc_clj/2024/day11_test.clj
+++ test/aoc_clj/2024/day11_test.clj
@@ -13,20 +13,19 @@
     (is (= d11-s00 (d11/parse d11-s00-raw)))))
 
 (deftest blink-test
-  (testing "Updates all the stones per the rules"
-    (is (= [1 2024 1 0 9 9 2021976]
-           (d11/blink d11-s00)))
+  (testing "Updates the count of stones after a blink"
+    (is (= {1 2 2024 1 0 1 9 2 2021976 1}
+           (d11/blink (frequencies d11-s00))))
 
-    (is (= [253000 1 7]
-           (d11/blink d11-s01)))
+    (is (= {253000 1 1 1 7 1}
+           (d11/blink (frequencies d11-s01))))
 
-    (is (= [253 0 2024 14168]
-           (d11/blink
-            [253000 1 7])))))
+    (is (= {253 1 0 1 2024 1 14168 1}
+           (d11/blink {253000 1 1 1 7 1})))))
 
 (deftest stones-after-n-blinks-test
   (testing "The number of stones after n blinks"
-    (is (= 22 (d11/stones-after-n-blinks 6 d11-s01)))
+    (is (= 22    (d11/stones-after-n-blinks 6 d11-s01)))
     (is (= 55312 (d11/stones-after-n-blinks 25 d11-s01)))))
 
 (def day11-input (u/parse-puzzle-input d11/parse 2024 11))
@@ -37,4 +36,4 @@
 
 (deftest part2-test
   (testing "Reproduces the answer for day11, part2"
-    (is (= 186996 (d11/part2 day11-input)))))
+    (is (= 221683913164898 (d11/part2 day11-input)))))
