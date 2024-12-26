bd1ce234c9fa75cb7ad411e389642c4568c3d5ef
Ken Smith <ken@klsmith.me>
2024-12-26 13:35:53 +0000

    Docstrings for what I haave at present

--- src/aoc_clj/2024/day21.clj
+++ src/aoc_clj/2024/day21.clj
@@ -9,6 +9,10 @@
 (def part2-layers 25)
 
 (def numeric-keypad
+  "map representation of a 11-button numerical keypad (0-9 plus an A button),
+   where the keys of the map represent the keypad buttons, and the values are
+   a sub-map representing the adjacent keys and the direction to move to
+   reach them"
   {\A {\0 \<
        \3 \^}
    \0 {\A \>
@@ -41,6 +45,10 @@
        \8 \<}})
 
 (def directional-keypad
+  "map representation of a 5-key directional keypad, where the keys
+   represent the left,right,up,down and Activate keypad buttons, and the
+   values are a sub-map representing the adjacent keys and the direction
+   to move to reach them"
   {\< {\v \>}
    \v {\< \<
        \^ \^
@@ -91,6 +99,9 @@
          (into {}))))
 
 (defn all-alternatives
+  "For a given sequence of `moves`, i.e. a collection where each element is
+   itself a collection that represents the possible moves to get from one button
+   to the next, return all viable button press sequences."
   [moves]
   (loop [options (first moves) rst (next moves)]
     (if (nil? rst)
@@ -99,6 +110,10 @@
              (next rst)))))
 
 (defn directions
+  "For a given move-map (keypad-specific mapping between pairs-of-buttons
+   and the moves required to there) and a collection of intended buttons
+   to press, returns all possible sequences of moves for the next-level
+   keypad."
   [move-map coll]
   (->> (concat [\A] coll)
        (partition 2 1)
@@ -108,44 +123,56 @@
 (def robot-dirs  (partial directions (keypad-paths numeric-keypad)))
 (def remote-dirs (partial directions (keypad-paths directional-keypad)))
 
-(defn cull
+(defn keep-shortest
+  "Keeps only the shortest sequences from among the current coll"
   [coll]
   (let [min-size (apply min (map count coll))]
     (remove #(> (count %) min-size) coll)))
 
 (defn remote-layer
+  "A single layer of the remote control"
   [input]
   (->> (mapcat remote-dirs input)
-       cull))
+       keep-shortest))
 
 (defn remote-layers
-  [layers input]
+  "Applies `n` instances of the remote control layer"
+  [n input]
   (->> input
        (iterate remote-layer)
-       (drop layers)
+       (drop n)
        first))
 
 (defn all-presses
+  "For `n` layers of the remote controls, returns the shortest seq of button
+   presses required for the deepest layer to ultimately press the `code`
+   at the first robot."
   [layers code]
   (->> code
        robot-dirs
-       cull
+       keep-shortest
        (remote-layers layers)
        (apply min-key count)))
 
 (defn seq-length
+  "Returns the length of the shortest button-press sequence"
   [layers code]
   (count (all-presses layers code)))
 
 (defn numeric-part
+  "Returns the numeric part of the keypad code"
   [code]
   (Integer/parseInt (subs code 0 3)))
 
 (defn complexity
+  "Returns the complexity, defined as the product of the number of
+   deepest layer remote control button presses and the numeric value of the
+   code"
   [layers code]
   (* (seq-length layers code) (numeric-part code)))
 
 (defn complexity-sum
+  "Returns the sum of the complexities across all codes"
   [layers codes]
   (->> codes
        (map #(complexity layers %))
@@ -153,9 +180,11 @@
 
 ;; Puzzle solutions
 (defn part1
+  "What is the sum of the complexities of the five codes on your list?"
   [input]
   (complexity-sum part1-layers input))
 
 (defn part2
+  "What is the sum of the complexities of the five codes on your list?"
   [input]
   (complexity-sum part2-layers input))
