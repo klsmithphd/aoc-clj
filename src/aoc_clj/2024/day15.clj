a4a174379b74c8324163071b6277e13a096d4381
Ken Smith <ken@klsmith.me>
2024-12-29 18:36:45 +0000

    Major representation refactor to facilitate part2

--- src/aoc_clj/2024/day15.clj
+++ src/aoc_clj/2024/day15.clj
@@ -26,59 +26,47 @@
 
 (defn parse
   [input]
-  (let [[grid-str moves-str] (u/split-at-blankline input)]
-    {:grid (mg/ascii->MapGrid2D grid-charmap grid-str)
+  (let [[grid-str moves-str] (u/split-at-blankline input)
+        grid (mg/ascii->MapGrid2D grid-charmap grid-str :down true)]
+    {:robot (first (grid/find-nodes :robot grid))
+     :walls (set (grid/find-nodes :wall grid))
+     :boxes (set (grid/find-nodes :box grid))
      :moves (parse-moves moves-str)}))
 
 ;; Puzzle logic
-(defn robot-pos
-  "Returns the location of the robot"
-  [grid]
-  (first (filter #(= :robot (grid/value grid %)) (grid/pos-seq grid))))
-
-(defn box-positions
-  "Returns the location of all the boxes"
-  [grid]
-  (filter #(= :box (grid/value grid %)) (grid/pos-seq grid)))
-
 (defn robot-move
-  "Updates the state by moving the robot from its current position
-   to the new position and marking the previous position as a space"
-  [state robot-pos to-pos]
-  (-> state
-      (assoc :robot-pos to-pos)
-      (assoc-in [:grid :grid to-pos] :robot)
-      (assoc-in [:grid :grid robot-pos] :space)))
+  "Updates the state by moving the robot to its new position"
+  [state to-pos]
+  (assoc state :robot to-pos))
 
-(defn aligned-boxes
+(defn boxes-to-be-moved
   "Returns a seq of all the boxes that are going to each be pushed if
    the robot attempts to move to `to-pos`"
-  [grid delta to-pos]
+  [part boxes delta to-pos]
   (->> (iterate #(v/vec-add delta %) to-pos)
-       (take-while #(= :box (grid/value grid %)))))
+       (take-while boxes)))
 
-(defn beyond-aligned-boxes
+(defn boxes-free-to-move?
   "Returns the state of the space just beyond the boxes to be moved"
-  [grid delta boxes]
-  (->> (last boxes)
-       (v/vec-add delta)
-       (grid/value grid)))
+  [part walls delta boxes-to-move]
+  (let [not-a-wall (complement walls)]
+    (->> (last boxes-to-move)
+         (v/vec-add delta)
+         not-a-wall)))
 
 (defn box-move
   "Updates the state by moving the robot and any boxes in front of it
    if they can be moved (meaning there's a free space at the end
    of the line of boxes). If the boxes can't be moved, just returns
    the state as-is"
-  [{:keys [grid robot-pos] :as state} delta to-pos]
-  (let [box-seq     (aligned-boxes grid delta to-pos)
-        after-boxes (beyond-aligned-boxes grid delta box-seq)]
-    (case after-boxes
-      :wall state
-      :space
-      (let [new-boxes (zipmap (map #(v/vec-add delta %) box-seq)
-                              (repeat :box))]
-        (-> (robot-move state robot-pos to-pos)
-            (update-in [:grid :grid] into new-boxes))))))
+  [part {:keys [boxes walls] :as state} delta to-pos]
+  (let [box-seq     (boxes-to-be-moved part boxes delta to-pos)]
+    (if (boxes-free-to-move? part walls delta box-seq)
+      (let [new-boxes  (map #(v/vec-add delta %) box-seq)]
+        (-> (robot-move state to-pos)
+            (update :boxes #(reduce disj % box-seq))
+            (update :boxes #(reduce conj % new-boxes))))
+      state)))
 
 (defn move
   "Attempts to perform the move of the robot.
@@ -86,41 +74,45 @@
    If the move is to a free space, the robot moves to that space.
    If the move encounters a box, then we check to see if what could be
    a line of 1-to-N boxes can be moved."
-  [{:keys [grid robot-pos] :as state} dir]
-  (let [delta  (grid/cardinal-offsets dir)
-        to-pos (v/vec-add robot-pos delta)]
-    (case (grid/value grid to-pos)
-      :wall state
-      :space (robot-move state robot-pos to-pos)
-      :box   (box-move state delta to-pos))))
+  [part {:keys [walls boxes robot] :as state} dir]
+  (let [delta  (case dir
+                 :n [0 -1]
+                 :s [0 1]
+                 :e [1 0]
+                 :w [-1 0])
+        to-pos (v/vec-add robot delta)]
+    (cond
+      (walls to-pos) state
+      (boxes to-pos) (box-move part state delta to-pos)
+      :else          (robot-move state to-pos))))
 
 (defn all-moves
   "Returns the state after all the moves in the move sequence have
    been attempted"
-  [{:keys [grid moves]}]
-  (let [start (robot-pos grid)]
-    (reduce move {:grid grid :robot-pos start} moves)))
+  [part input]
+  (reduce (partial move part) (u/without-keys input [:moves]) (:moves input)))
 
 (defn box-gps
   "Returns the GPS coordinate of a box, which is 100 times its distance
    from the top edge of the map plus its distance from the left edge"
-  [height [x y]]
-  (+ x (* (- height y 1) 100)))
+  [[x y]]
+  (+ x (* y 100)))
 
 (defn box-gps-sum
   "Returns the sum of the GPS coordinates of all the boxes"
-  [{:keys [grid]}]
-  (let [height (grid/height grid)]
-    (->> (box-positions grid)
-         (map #(box-gps height %))
-         (reduce +))))
+  [{:keys [boxes]}]
+  (->> boxes
+       (map box-gps)
+       (reduce +)))
 
 ;; Puzzle solutions
 (defn part1
   "After the robot is finished moving, what is the sum of all boxes'
    GPS coordinates?"
   [input]
-  (-> input all-moves box-gps-sum))
+  (->> input
+       (all-moves :part1)
+       box-gps-sum))
 
 ;; In part 2, the boxes are 2 units wide in the horizontal direction.
 ;; This means that it's possible that pushing on one box up or down can
@@ -133,3 +125,11 @@
 ;; * a new representation that knows that boxes are either 1 unit or 2 units wide
 ;; * a function to expand the input space by 2x in the horizontal dir
 ;; * New logic for identifying the chain of boxes (and any walls they touch)
+
+;; One possible representation -- leave the grid as is, but have the robot
+;; only move 1/2 a step in the :w :e directions. Then, when checking to 
+;; see if there are any touching boxes, I need to look at boxes whose
+;; coordinates are 1/2 a unit to the left or equal to the x pos of
+;; the previous box (or robot)
+;; 
+
--- test/aoc_clj/2024/day15_test.clj
+++ test/aoc_clj/2024/day15_test.clj
@@ -1,6 +1,5 @@
 (ns aoc-clj.2024.day15-test
   (:require [clojure.test :refer [deftest testing is]]
-            [aoc-clj.utils.grid.mapgrid :as mg]
             [aoc-clj.utils.core :as u]
             [aoc-clj.2024.day15 :as d15]))
 
@@ -17,32 +16,35 @@
    "<^^>>>vv<v>>v<<"])
 
 (def d15-s00
-  {:grid
-   (mg/->MapGrid2D
-    8 8
-    {[0 7] :wall [1 7] :wall [2 7] :wall [3 7] :wall [4 7] :wall [5 7] :wall [6 7] :wall [7 7] :wall
-     [0 6] :wall [1 6] :space [2 6] :space [3 6] :box [4 6] :space [5 6] :box [6 6] :space [7 6] :wall
-     [0 5] :wall [1 5] :wall [2 5] :robot [3 5] :space [4 5] :box [5 5] :space [6 5] :space [7 5] :wall
-     [0 4] :wall [1 4] :space [2 4] :space [3 4] :space [4 4] :box [5 4] :space [6 4] :space [7 4] :wall
-     [0 3] :wall [1 3] :space [2 3] :wall [3 3] :space [4 3] :box [5 3] :space [6 3] :space [7 3] :wall
-     [0 2] :wall [1 2] :space [2 2] :space [3 2] :space [4 2] :box [5 2] :space [6 2] :space [7 2] :wall
-     [0 1] :wall [1 1] :space [2 1] :space [3 1] :space [4 1] :space [5 1] :space [6 1] :space [7 1] :wall
-     [0 0] :wall [1 0] :wall [2 0] :wall [3 0] :wall [4 0] :wall [5 0] :wall [6 0] :wall [7 0] :wall})
-   :moves
-   [:w :n :n :e :e :e :s :s :w :s :e :e :s :w :w]})
+  {:robot [2 2]
+   :walls #{[0 0] [1 0] [2 0] [3 0] [4 0] [5 0] [6 0] [7 0]
+            [0 1] [7 1]
+            [0 2] [1 2] [7 2]
+            [0 3] [7 3]
+            [0 4] [2 4] [7 4]
+            [0 5] [7 5]
+            [0 6] [7 6]
+            [0 7] [1 7] [2 7] [3 7] [4 7] [5 7] [6 7] [7 7]}
+   :boxes #{[3 1] [5 1]
+            [4 2]
+            [4 3]
+            [4 4]
+            [4 5]}
+   :moves [:w :n :n :e :e :e :s :s :w :s :e :e :s :w :w]})
 
 (def d15-s00-finish
-  (d15/parse
-   ["########"
-    "#....OO#"
-    "##.....#"
-    "#.....O#"
-    "#.#O@..#"
-    "#...O..#"
-    "#...O..#"
-    "########"
-    ""
-    ""]))
+  (-> ["########"
+       "#....OO#"
+       "##.....#"
+       "#.....O#"
+       "#.#O@..#"
+       "#...O..#"
+       "#...O..#"
+       "########"
+       ""
+       ""]
+      d15/parse
+      (u/without-keys [:moves])))
 
 (def d15-s01
   (d15/parse
@@ -69,19 +71,20 @@
     "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"]))
 
 (def d15-s01-finish
-  (d15/parse
-   ["##########"
-    "#.O.O.OOO#"
-    "#........#"
-    "#OO......#"
-    "#OO@.....#"
-    "#O#.....O#"
-    "#O.....OO#"
-    "#O.....OO#"
-    "#OO....OO#"
-    "##########"
-    ""
-    ""]))
+  (-> ["##########"
+       "#.O.O.OOO#"
+       "#........#"
+       "#OO......#"
+       "#OO@.....#"
+       "#O#.....O#"
+       "#O.....OO#"
+       "#O.....OO#"
+       "#OO....OO#"
+       "##########"
+       ""
+       ""]
+      d15/parse
+      (u/without-keys [:moves])))
 
 (def d15-s02
   (d15/parse
@@ -102,90 +105,68 @@
 (deftest move-test
   (testing "Performs a move attempt by the robot and updates the state"
     ;; Can't move due to wall in the way
-    (is (= {:grid (:grid d15-s00) :robot-pos [2 5]}
-           (d15/move {:grid (:grid d15-s00) :robot-pos [2 5]} :w)))
+    (is (= d15-s00
+           (d15/move :part1 d15-s00 :w)))
 
     ;; Robot moves up one
-    (is (= {:grid (-> (:grid d15-s00)
-                      (assoc-in [:grid [2 5]] :space)
-                      (assoc-in [:grid [2 6]] :robot))
-            :robot-pos [2 6]}
-           (d15/move {:grid (:grid d15-s00) :robot-pos [2 5]} :n)))
+    (is (= (assoc d15-s00 :robot [2 1])
+           (d15/move :part1 d15-s00 :n)))
 
-    ;; Robot moves right one
-    (is (= {:grid (-> (:grid d15-s00)
-                      (assoc-in [:grid [2 5]] :space)
-                      (assoc-in [:grid [3 6]] :robot)
-                      (assoc-in [:grid [4 6]] :box))
-            :robot-pos [3 6]}
-           (d15/move
-            {:grid (-> (:grid d15-s00)
-                       (assoc-in [:grid [2 5]] :space)
-                       (assoc-in [:grid [2 6]] :robot))
-             :robot-pos [2 6]}
-            :e)))
+    ;; Robot moves right one, pushing a box
+    (is (= (-> d15-s00
+               (assoc :robot [3 1])
+               (update :boxes disj [3 1])
+               (update :boxes conj [4 1]))
+           (d15/move :part1 (assoc d15-s00 :robot [2 1]) :e)))
 
-    ;; Robot moves right one
-    (is (= {:grid (-> (:grid d15-s00)
-                      (assoc-in [:grid [2 5]] :space)
-                      (assoc-in [:grid [3 6]] :space)
-                      (assoc-in [:grid [4 6]] :robot)
-                      (assoc-in [:grid [6 6]] :box))
-            :robot-pos [4 6]}
-           (d15/move
-            {:grid (-> (:grid d15-s00)
-                       (assoc-in [:grid [2 5]] :space)
-                       (assoc-in [:grid [3 6]] :robot)
-                       (assoc-in [:grid [4 6]] :box))
-             :robot-pos [3 6]}
-            :e)))
+    ;; Robot moves right one, pushing two boxes
+    (is (= (-> d15-s00
+               (assoc :robot [4 1])
+               (update :boxes disj [3 1])
+               (update :boxes conj [6 1]))
+           (d15/move :part1
+                     (-> d15-s00
+                         (assoc :robot [3 1])
+                         (update :boxes disj [3 1])
+                         (update :boxes conj [4 1]))
+                     :e)))
 
     ;; Robot can't move further right
-    (is (= {:grid (-> (:grid d15-s00)
-                      (assoc-in [:grid [2 5]] :space)
-                      (assoc-in [:grid [3 6]] :space)
-                      (assoc-in [:grid [4 6]] :robot)
-                      (assoc-in [:grid [6 6]] :box))
-            :robot-pos [4 6]}
-           (d15/move
-            {:grid (-> (:grid d15-s00)
-                       (assoc-in [:grid [2 5]] :space)
-                       (assoc-in [:grid [3 6]] :space)
-                       (assoc-in [:grid [4 6]] :robot)
-                       (assoc-in [:grid [6 6]] :box))
-             :robot-pos [4 6]}
-            :e)))
+    (is (= (-> d15-s00
+               (assoc :robot [4 1])
+               (update :boxes disj [3 1])
+               (update :boxes conj [6 1]))
+           (d15/move :part1
+                     (-> d15-s00
+                         (assoc :robot [4 1])
+                         (update :boxes disj [3 1])
+                         (update :boxes conj [6 1]))
+                     :e)))
 
     ;; Robot moves line of boxes down one
-    (is (= {:grid (-> (:grid d15-s00)
-                      (assoc-in [:grid [2 5]] :space)
-                      (assoc-in [:grid [3 6]] :space)
-                      (assoc-in [:grid [4 6]] :space)
-                      (assoc-in [:grid [4 5]] :robot)
-                      (assoc-in [:grid [6 6]] :box)
-                      (assoc-in [:grid [4 1]] :box))
-            :robot-pos [4 5]}
-           (d15/move
-            {:grid (-> (:grid d15-s00)
-                       (assoc-in [:grid [2 5]] :space)
-                       (assoc-in [:grid [3 6]] :space)
-                       (assoc-in [:grid [4 6]] :robot)
-                       (assoc-in [:grid [6 6]] :box))
-             :robot-pos [4 6]}
-            :s)))))
+    (is (= (-> d15-s00
+               (assoc :robot [4 2])
+               (update :boxes disj [3 1])
+               (update :boxes disj [4 2])
+               (update :boxes conj [6 1])
+               (update :boxes conj [4 6]))
+           (d15/move :part1
+                     (-> d15-s00
+                         (assoc :robot [4 1])
+                         (update :boxes disj [3 1])
+                         (update :boxes conj [6 1]))
+                     :s)))))
 
 (deftest all-moves-test
   (testing "Performs all the moves correctly and returns the end state"
-    (is (= {:grid (:grid d15-s00-finish) :robot-pos [4 3]}
-           (d15/all-moves d15-s00)))
+    (is (= d15-s00-finish (d15/all-moves :part1 d15-s00)))
 
-    (is (= {:grid (:grid d15-s01-finish) :robot-pos [3 5]}
-           (d15/all-moves d15-s01)))))
+    (is (= d15-s01-finish (d15/all-moves :part1 d15-s01)))))
 
 (deftest box-gps-sum
   (testing "Computes the sum of the GPS coordinates of all the boxes"
-    (is (= 2028  (d15/box-gps-sum (d15/all-moves d15-s00))))
-    (is (= 10092 (d15/box-gps-sum (d15/all-moves d15-s01))))))
+    (is (= 2028  (d15/box-gps-sum (d15/all-moves :part1 d15-s00))))
+    (is (= 10092 (d15/box-gps-sum (d15/all-moves :part1 d15-s01))))))
 
 (def day15-input (u/parse-puzzle-input d15/parse 2024 15))
 
