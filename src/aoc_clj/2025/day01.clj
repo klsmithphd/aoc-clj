b33e667893cdbded5ab0953ec199c121907b7d62
Ken Smith <ken@spatiallogic.com>
2025-12-01 14:22:17 +0000

    Initial input parsing

--- src/aoc_clj/2025/day01.clj
+++ src/aoc_clj/2025/day01.clj
@@ -1,10 +1,22 @@
 (ns aoc-clj.2025.day01
-  "Solution to https://adventofcode.com/2025/day/1")
+  "Solution to https://adventofcode.com/2025/day/1" 
+  (:require
+    [clojure.string :as str]))
+
+;; Constants
+(def init-pos 50)
 
 ;; Input parsing
+(defn parse-line
+  [line]
+  (let [num (read-string (subs line 1))]
+    (if (str/starts-with? line "L")
+      (- num)
+      num)))
+
 (defn parse
   [input]
-  input)
+  (map parse-line input))
 
 ;; Puzzle logic
 
--- test/aoc_clj/2025/day01_test.clj
+++ test/aoc_clj/2025/day01_test.clj
@@ -3,6 +3,25 @@
             [aoc-clj.utils.core :as u]
             [aoc-clj.2025.day01 :as d01]))
 
+(def d01-s00-raw
+  ["L68"
+   "L30"
+   "R48"
+   "L5"
+   "R60"
+   "L55"
+   "L1"
+   "L99"
+   "R14"
+   "L82"])
+
+(def d01-s00
+  [-68 -30 48 -5 60 -55 -1 -99 14 -82])
+
+(deftest parse-test
+  (testing "Correctly parses the input"
+    (is (= d01-s00 (d01/parse d01-s00-raw)))))
+
 (def day01-input (u/parse-puzzle-input d01/parse 2025 1))
 
 (deftest part1-test
