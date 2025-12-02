420386ae8787e5bae7ccf5a30dd95b3a22d12dd4
Ken Smith <ken@spatiallogic.com>
2025-12-02 09:14:04 +0000

    Part 1 soln

--- src/aoc_clj/2025/day02.clj
+++ src/aoc_clj/2025/day02.clj
@@ -8,19 +8,40 @@
   (->> (str/split range #"-")
        (mapv read-string)))
 
-
 (defn parse
   [input]
   (->> (str/split (first input) #",")
        (map parse-range)))
 
 ;; Puzzle logic
+(defn candidates
+  "Returns all the values in a range (ends inclusive)"
+  [[s e]]
+  (range s (inc e)))
 
+(defn part1-invalid?
+  "Returns true if the number represents an invalid id, where invalid id consists
+   only of some sequence of digits repeated twice"
+  [num]
+  (some? (re-matches #"^(\d+)(\1)$" (str num))))
+
+(defn all-invalid-ids
+  "Returns all of the invalid IDs across the ranges given"
+  [invalid? ranges]
+  (->> (mapcat candidates ranges)
+       (filter invalid?)))
+
+(defn all-invalid-ids-sum
+  "Computes the sum of all the invalid IDs in the ranges given"
+  [invalid? ranges]
+  (->> (all-invalid-ids invalid? ranges)
+       (reduce +)))
 
 ;; Puzzle solutions
 (defn part1
-  [_]
-  :not-implemented)
+  "What do you get if you add up all of the invalid IDs?"
+  [input]
+  (all-invalid-ids-sum part1-invalid? input))
 
 (defn part2
   [_]
--- test/aoc_clj/2025/day02_test.clj
+++ test/aoc_clj/2025/day02_test.clj
@@ -23,12 +23,31 @@
   (testing "Correctly parses the input"
     (is (= d02-s00 (d02/parse d02-s00-raw)))))
 
+(deftest part1-invalid?-test
+  (testing "Correctly identifies IDs that are deemed invalid"
+    (is (= true (d02/part1-invalid? 11)))
+    (is (= true (d02/part1-invalid? 22)))
+    (is (= true (d02/part1-invalid? 1010)))
+    (is (= false (d02/part1-invalid? 9119)))
+    (is (= true (d02/part1-invalid? 1188511885)))
+    (is (= true (d02/part1-invalid? 222222)))
+    (is (= true (d02/part1-invalid? 446446)))
+    (is (= true (d02/part1-invalid? 38593859)))))
 
-(def day02-input (u/parse-puzzle-input d02/parse 2025 1))
+(deftest all-invalid-ids
+  (testing "For a collection of ranges, returns all the invalid IDs"
+    (is (= [11 22 99 1010 1188511885 222222 446446 38593859]
+           (d02/all-invalid-ids d02/part1-invalid? d02-s00)))))
+
+(deftest all-invalid-ids-sum
+  (testing "Adds up all the invalid IDs in the ranges given"
+    (is (= 1227775554 (d02/all-invalid-ids-sum d02/part1-invalid? d02-s00)))))
+
+(def day02-input (u/parse-puzzle-input d02/parse 2025 2))
 
 (deftest part1-test
   (testing "Reproduces the answer for day02, part1"
-    (is (= :not-implemented (d02/part1 day02-input)))))
+    (is (= 32976912643 (d02/part1 day02-input)))))
 
 (deftest part2-test
   (testing "Reproduces the answer for day02, part2"
